322493607b3e1e55c5b74e7e44b95abb02891bc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 17:03:20 +0000

    Add package coldplay-agent/0.0.5/coldplay_agent-0.0.5.tar.gz

---
+++ packages/coldplay-agent/coldplay_agent-0.0.5.tar.gz/coldplay_agent-0.0.5/app/service/task_service.py
@@ -0,0 +1,44 @@
+# app/service/task_service.py
+
+import json
+import shlex
+import subprocess
+from app.service.util_service import UtilService
+
+class TaskService:
+    
+    @staticmethod
+    def task_run(data):
+        """
+        处理任务运行逻辑
+        """
+        print(f"data: {data}")
+        # 执行脚本文件
+        # 使用 subprocess.Popen 让命令在后台执行
+        coldplay_config = UtilService.load_coldplay_config()
+        apiserver_host = coldplay_config['DEFAULT'].get('apiserver_host')
+        project_url = f"{apiserver_host}/{data['code_url'].strip('/')}"
+        script_run_url = data['script_run_url']
+        script_name = data['script_name']
+        script_run_url_new = f"{script_run_url.strip('/')}/{script_name}"
+        print(f"project_url:{project_url} script_run_url_new:{script_run_url_new}")
+        project_url = shlex.quote(project_url)
+        script_run_url_new = shlex.quote(script_run_url_new)
+
+        process = subprocess.Popen(["bash", "/home/yons/work/www/coldplay_agent/app/run_task.sh", project_url, script_run_url_new])
+        print(f"执行成功")
+
+        return True
+    
+    @staticmethod
+    def task_stop():
+        """
+        处理任务终止逻辑
+        """
+        print(f"stop run")
+        # 执行脚本文件
+        # 使用 subprocess.Popen 让命令在后台执行
+        subprocess.Popen(["bash", "/home/yons/work/www/coldplay_agent/app/stop_task.sh"])
+        print(f"执行成功")
+
+        return True
---
+++ packages/coldplay-agent/coldplay_agent-0.0.5.tar.gz/coldplay_agent-0.0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='coldplay_agent',  # 包名
+    version='0.0.5',    # 版本号
+    description='coldplay agent package',  # 简短描述
+    author='Joey',
+    author_email='joeym@limxdynamics.com',
+    url='https://github.com/zoie438730732/limx-coldplay-agent',  # 项目主页链接
+    packages=find_packages(),  # 自动发现包
+    install_requires=[  # 依赖包
+        'pika==1.3.2',
+    ],
+    entry_points={
+        'console_scripts': [
+            'coldplayagent = app.main:main',  # 指定入口函数
+            'coldplayagent-init = app.init:initialize',  # 创建初始化命令
+        ],
+    },
+    classifiers=[  # 分类标记
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',  # 支持的Python版本
+)
