ea62317d6123c7a807906cf513e9d8e5028899f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 14:03:09 +0000

    Add package scitrera-app-framework/0.0.2/scitrera_app_framework-0.0.2-py3-none-any.whl

---
+++ packages/scitrera-app-framework/scitrera_app_framework-0.0.2-py3-none-any.whl/scitrera_app_framework/base_plugins/__init__.py
@@ -0,0 +1,23 @@
+from ..api import Variables
+from ..core.plugins import get_extension
+from .bg_exec import EXT_BACKGROUND_EXEC, JobExecutorEngine
+from .progress_tracker import EXT_PROGRESS_TRACKER, ProgressTracker
+
+
+def register_package_plugins(package, v=None):
+    from ..api import Plugin
+    from ..util.imports import find_types_in_modules
+    from ..core.plugins import register_plugin
+
+    for plugin in find_types_in_modules(package, Plugin, recursive=False, exclude_base_type=True):
+        register_plugin(plugin, v)
+
+    return
+
+
+def get_background_exec(v: Variables = None) -> JobExecutorEngine:
+    return get_extension(EXT_BACKGROUND_EXEC, v)
+
+
+def get_progress_tracker(v: Variables = None) -> ProgressTracker:
+    return get_extension(EXT_PROGRESS_TRACKER, v)
