34745518a06b76e47c5fe965bc0da9babe18d9cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 15:04:54 +0000

    Add package rwrapr/0.1.3/rwrapr-0.1.3.tar.gz

---
+++ packages/rwrapr/rwrapr-0.1.3.tar.gz/rwrapr-0.1.3/src/rwrapr/rdataframe.py
@@ -0,0 +1,65 @@
+import warnings
+from typing import Any
+
+import pandas as pd
+import rpy2.robjects as ro
+import rpy2.robjects.vectors as vc
+from rpy2.robjects import pandas2ri
+
+from .rattributes import get_Rattributes
+
+
+class RDataFrame(pd.DataFrame):
+    def __init__(self, data_frame: vc.DataFrame | pd.DataFrame):
+        if isinstance(data_frame, vc.DataFrame):
+            df = toPandas(data_frame)
+            attrs = get_attributes_dataframe(data_frame)
+        else:
+            df = data_frame
+            attrs = None
+        super().__init__(df)
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            self._Rattributes = attrs
+
+    def toR(self) -> vc.DataFrame:
+        from .rattributes import attributes2r
+        from .rattributes import structure
+
+        with (ro.default_converter + pandas2ri.converter).context():
+            R_df = ro.conversion.get_conversion().py2rpy(self)
+
+        if self._Rattributes is None:
+            return R_df
+        else:
+            attributes = attributes2r(self._Rattributes)
+            if attributes is None:
+                return R_df
+            return structure(R_df, **attributes)
+
+    def toPy(self) -> pd.DataFrame:
+        return pd.DataFrame(self)
+
+
+def get_attributes_dataframe(df: vc.DataFrame) -> dict[str, Any] | None | Any:
+    return get_Rattributes(df, exclude=["names", "class", "row.names"])
+
+
+def toPandas(df: vc.DataFrame) -> pd.DataFrame:
+    with (ro.default_converter + pandas2ri.converter).context():
+        pd_df = ro.conversion.get_conversion().rpy2py(df)
+
+    return pd_df
+
+
+def pandas2r(data: pd.DataFrame) -> vc.DataFrame:
+    with (ro.default_converter + pandas2ri.converter).context():
+        return ro.conversion.get_conversion().py2rpy(data)
+
+
+def attempt_pandas_conversion(data: Any) -> RDataFrame | TypeError:
+    try:
+        return RDataFrame(pd.DataFrame(data))
+    except TypeError:
+        return TypeError(f"This data will not convert {data}")
---
+++ packages/rwrapr/rwrapr-0.1.3.tar.gz/rwrapr-0.1.3/src/rwrapr/rview.py
@@ -0,0 +1,67 @@
+from collections.abc import Callable
+from collections.abc import Iterator
+from typing import Any
+
+import rpy2.robjects as ro
+import scipy
+
+from .nputils import np_collapse
+from .rlist import RDict
+from .rlist import RList
+from .rutils import as_matrix
+from .rutils import get_rclass
+
+
+class RView:
+    def __init__(self, Robj: Any):
+        from .rarray import RArray
+        from .rdataframe import RDataFrame
+
+        if isinstance(Robj, RArray | RDataFrame | RList | RDict):
+            self.Robj = Robj.toR()
+        else:
+            self.Robj = Robj
+
+    def __str__(self) -> str | Any:
+        # return captureRprint(self.Robj)
+        return self.Robj.__str__()
+
+    def __repr__(self) -> str | Any:
+        # return self.Robj.__repr__()
+        return self.Robj.__str__()
+
+    def __getattr__(self, name: str) -> Any:
+        from .function_wrapper import rfunc
+
+        fun: Callable[..., Any] = rfunc(name)
+        return fun(self.Robj)
+
+    def __getitem__(self, *args: Any) -> Any:
+        return self.Robj.__getitem__(*args)
+
+    def __iter__(self) -> Iterator[Any] | Any:
+        return self.Robj.__iter__()
+
+    def toPy(self, ignoreS3: bool = False) -> Any:
+        from .convert_r2py import convert_r2py
+
+        return convert_r2py(self.Robj, ignoreS3=ignoreS3)
+
+    def toR(self) -> Any:
+        return self.Robj
+
+
+def convert_s4(x: ro.methods.RS4) -> Any:
+    from .rarray import convert_numpy
+
+    rclass = get_rclass(x)
+    if rclass is None:
+        return RView(x)
+
+    match np_collapse(rclass):
+        case "dgCMatrix":  # todo: put this in a separate function
+            dense = convert_numpy(as_matrix(x))
+            sparse = scipy.sparse.coo_matrix(dense)
+            return sparse
+        case _:
+            return RView(x)
