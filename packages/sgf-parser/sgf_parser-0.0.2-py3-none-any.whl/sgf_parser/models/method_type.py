e2ac4192d73119b0e9bbc02f29c42d0e6e9282ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 15:04:52 +0000

    Add package sgf-parser/0.0.2/sgf_parser-0.0.2-py3-none-any.whl

---
+++ packages/sgf-parser/sgf_parser-0.0.2-py3-none-any.whl/sgf_parser/models/method_type.py
@@ -0,0 +1,16 @@
+import enum
+
+
+class MethodType(enum.StrEnum):
+    """
+    Method type
+    """
+
+    CPT = "CPT"
+    DP = "DP"
+    DT = "DT"
+    RP = "RP"
+    SRS = "SRS"
+    SVT = "SVT"
+    TOT = "TOT"
+    WST = "WST"
