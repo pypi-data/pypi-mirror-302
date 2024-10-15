2690f12a3534f24b100ff48f87703d1d48df2bf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 15:04:43 +0000

    Add package rapid-api-client/0.4.0/rapid_api_client-0.4.0-py3-none-any.whl

---
+++ packages/rapid-api-client/rapid_api_client-0.4.0-py3-none-any.whl/rapid_api_client/utils.py
@@ -0,0 +1,28 @@
+"""
+Utility methods
+"""
+
+from inspect import Parameter
+from typing import Any, Dict, Type, TypeVar, get_args
+
+from .annotations import BaseAnnotation
+
+BA = TypeVar("BA", bound=BaseAnnotation)
+
+
+def filter_none_values(values: Dict[str, Any | None]) -> Dict[str, Any]:
+    """
+    Return a new map with only key/value if the value is set.
+    """
+    return {k: v for k, v in values.items() if v is not None}
+
+
+def find_annotation(param: Parameter, cls: Type[BA]) -> BA | None:
+    """
+    Check if the given parameter has an annotation which is or is a subclass of given type
+    """
+    if param.annotation:
+        for an in get_args(param.annotation):
+            if isinstance(an, cls):
+                return an
+    return None
