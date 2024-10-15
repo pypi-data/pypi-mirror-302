265dbecf7edaac24da7dcd6d5c011015914e5a6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 14:03:14 +0000

    Add package progressbar-fwx/1.5/progressbar_fwx-1.5.tar.gz

---
+++ packages/progressbar-fwx/progressbar_fwx-1.5.tar.gz/progressbar_fwx-1.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='progressbar-fwx',
+	version='1.5',
+	packages=find_packages(),
+	install_requires=["colorama"],
+	description = 'Best And Most LightWeigt Progressbar Displayer!',
+	authors = [{"name":"F1xGOD","email":"f1xgodim@gmail.com"}],
+	readme = "README.md"
+)
