3cf81f13799e1eeed9c2a5f3abb90ff27c5c55a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 15:03:41 +0000

    Add package mysql-connector-package/0.1.1/mysql_connector_package-0.1.1.tar.gz

---
+++ packages/mysql-connector-package/mysql_connector_package-0.1.1.tar.gz/mysql_connector_package-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mysql-connector-package",  # Package name
+    version="0.1.1",  # Version of the package
+    author="Harish Daga",
+    author_email="theharishdaga@gmail.com",
+    description="A simple MySQL connector using pymysql",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/Harishdaga/mysql-connector-package",  # GitHub URL
+    packages=find_packages(),  # Automatically finds packages in the folder
+    install_requires=['pymysql'],  # Required external packages
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
