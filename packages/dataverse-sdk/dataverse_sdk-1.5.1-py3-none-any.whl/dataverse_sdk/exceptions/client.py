1eaf89706c6992c2cc9ee0860c243648384ecb6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 15:03:27 +0000

    Add package dataverse-sdk/1.5.1/dataverse_sdk-1.5.1-py3-none-any.whl

---
+++ packages/dataverse-sdk/dataverse_sdk-1.5.1-py3-none-any.whl/dataverse_sdk/exceptions/client.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+
+class InvalidProcessError(Exception):
+    pass
+
+
+class ClientConnectionError(Exception):
+    pass
+
+
+class DataverseExceptionBase(Exception):
+    def __init__(
+        self,
+        status_code: Optional[int] = None,
+        type: Optional[str] = None,
+        detail: Optional[str] = None,
+        error: Optional[str | dict] = None,
+        **args,
+    ):
+        self.type = type
+        self.status_code = status_code
+        self.detail = detail
+        self.error = error
+
+
+class AsyncThirdPartyAPIException(Exception):
+    def __init__(
+        self, status_code: Optional[int] = None, detail: Optional[str] = "", **args
+    ):
+        self.status_code = status_code
+        self.detail = detail
