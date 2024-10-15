4d0629db84929a6417bc8b68e03821572506cb71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 15:04:57 +0000

    Add package funget/1.0.8/funget-1.0.8.tar.gz

---
+++ packages/funget/funget-1.0.8.tar.gz/funget-1.0.8/src/funget/download/__init__.py
@@ -0,0 +1,4 @@
+from .simple import download as simple_download
+from .work import Worker,WorkerFactory
+from .multi_thread_download import MultiThreadDownloader
+from .multi_thread_download import download as multi_thread_download
