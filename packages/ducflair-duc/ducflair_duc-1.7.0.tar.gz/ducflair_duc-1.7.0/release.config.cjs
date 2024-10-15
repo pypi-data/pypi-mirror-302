2e2bc86b07fccaed91d4cb7277686ee46c2813e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 14:03:14 +0000

    Add package ducflair-duc/1.7.0/ducflair_duc-1.7.0.tar.gz

---
+++ packages/ducflair-duc/ducflair_duc-1.7.0.tar.gz/ducflair_duc-1.7.0/release.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  branches: ["main", { name: "next", prerelease: true }],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/exec",
+      {
+        // Clean the dist directory and use double quotes for variable expansion
+        prepareCmd:
+          'pip install setuptools && rm -rf dist && sed -i "s/version=[\'\\\"][^\'\\\"]*[\'\\\"],/version=\'${nextRelease.version}\',/" setup.py && python3 setup.py sdist bdist_wheel',
+        publishCmd:
+          "python3 -m twine upload dist/* -u __token__ -p ${process.env.PYPI_TOKEN}"
+      }
+    ],
+    "@semantic-release/github",
+  ],
+  tagFormat: "@ducflair/duc-py@${version}"
+};
