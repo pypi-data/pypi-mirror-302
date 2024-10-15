3e6858d5d8749c7ebc88b7e314fa31b865d54bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 14:03:37 +0000

    Add package compas-wood/2.2.0/compas_wood-2.2.0.tar.gz

---
+++ packages/compas-wood/compas_wood-2.2.0.tar.gz/compas_wood-2.2.0/src/rhino/plugin/build/rh8/src/compas_wood/ProjectCommand_e1582709.cs
@@ -0,0 +1,32 @@
+using System;
+
+using Rhino;
+using Rhino.Commands;
+
+namespace RhinoCodePlatform.Rhino3D.Projects.Plugin
+{
+  [CommandStyle(Rhino.Commands.Style.ScriptRunner)]
+  public class ProjectCommand_e1582709 : Command
+  {
+    public Guid CommandId { get; } = new Guid("e1582709-4c30-4546-b87a-1dd26c08f329");
+
+    public ProjectCommand_e1582709() { Instance = this; }
+
+    public static ProjectCommand_e1582709 Instance { get; private set; }
+
+    public override string EnglishName => "w_dataset_beam_volume";
+
+    protected override Rhino.Commands.Result RunCommand(RhinoDoc doc, RunMode mode)
+    {
+      // NOTE:
+      // Initialize() attempts to loads the core rhinocode plugin
+      // and prepare the scripting platform. This call can not be in any static
+      // ctors of Command or Plugin classes since plugins can not be loaded while
+      // rhino is loading this plugin. The call has an initialized check and is
+      // very fast after the first run.
+      ProjectPlugin.Initialize();
+
+      return ProjectPlugin.RunCode(this, CommandId, doc, mode);
+    }
+  }
+}
---
+++ packages/compas-wood/compas_wood-2.2.0.tar.gz/compas_wood-2.2.0/src/rhino/plugin/commands/w_install.py
@@ -0,0 +1,7 @@
+#! python3
+# r: wood-nano==0.1.1, compas-wood==2.1.0, compas==2.1.1, wood-rui==0.1.0
+
+from wood_nano import test
+
+if __name__ == "__main__":
+    test()
