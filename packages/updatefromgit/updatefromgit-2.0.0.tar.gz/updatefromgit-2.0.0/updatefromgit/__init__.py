02537a60826aa9c3512927662476a607c73dfbae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 16:04:21 +0000

    Add package updatefromgit/2.0.0/updatefromgit-2.0.0.tar.gz

---
+++ packages/updatefromgit/updatefromgit-2.0.0.tar.gz/updatefromgit-2.0.0/updatefromgit/__init__.py
@@ -0,0 +1,12 @@
+"""
+     Initalize the dependencies
+"""
+
+from .gitfunctions import (
+    acquire_token_user_id_password_confidential,
+    acquire_token_user_id_password_public,
+    commit_all_items_to_git,
+    get_git_status,
+    update_workspace_from_git,
+)
+from .update_linked_services import update_linked_services
