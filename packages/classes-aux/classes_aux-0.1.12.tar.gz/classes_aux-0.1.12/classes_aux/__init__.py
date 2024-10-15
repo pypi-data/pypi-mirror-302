b07f1514a5242dd16fb0050ec91e043a98737180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 16:04:32 +0000

    Add package classes-aux/0.1.12/classes_aux-0.1.12.tar.gz

---
+++ packages/classes-aux/classes_aux-0.1.12.tar.gz/classes_aux-0.1.12/classes_aux/__init__.py
@@ -0,0 +1,81 @@
+# =====================================================================================================================
+# VERSION = (0, 0, 1)   # use import EXACT_OBJECTS! not *
+#   from .main import *                 # INcorrect
+#   from .main import EXACT_OBJECTS     # CORRECT
+
+
+# =====================================================================================================================
+# TEMPLATE
+# from .main import (
+#     # BASE
+#     EXACT_OBJECTS,
+#
+#     # AUX
+#
+#     # TYPES
+#
+#     # EXX
+# )
+# ---------------------------------------------------------------------------------------------------------------------
+from .cmp import (
+    # BASE
+    CmpInst,
+    # AUX
+    # TYPES
+    # EXX
+)
+from .number import (
+    # BASE
+    NumberArithmTranslateToAttr,
+    # AUX
+    # TYPES
+    TYPE__NUMBER,
+    # EXX
+    Exx__NumberArithm_NoName,
+)
+# ---------------------------------------------------------------------------------------------------------------------
+from .getattr_0_echo import (
+    # BASE
+    GetattrEcho,
+    GetattrEchoSpace,
+    # AUX
+    # TYPES
+    # EXX
+)
+from .getattr_1_aux import (
+    # BASE
+    GetattrAux,
+    # AUX
+    # TYPES
+    # EXX
+)
+from .getattr_2_anycase import (
+    # BASE
+    GetattrAnycase,
+    # AUX
+    # TYPES
+    # EXX
+)
+from .getattr_3_prefix_1_inst import (
+    # BASE
+    GetattrPrefixInst,
+    GetattrPrefixInst_RaiseIf,
+    # AUX
+    # TYPES
+    # EXX
+    Exx__GetattrPrefix,
+    Exx__GetattrPrefix_RaiseIf,
+)
+from .getattr_3_prefix_2_cls import GetattrPrefixCls_MetaTemplate
+
+# ---------------------------------------------------------------------------------------------------------------------
+from .middle_group import (
+    # BASE
+    ClsMiddleGroup,
+    # AUX
+    # TYPES
+    # EXX
+)
+
+
+# =====================================================================================================================
