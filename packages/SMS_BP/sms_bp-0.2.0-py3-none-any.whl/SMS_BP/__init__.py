8af3414699ffdcaebeb688ad37583ea53b5ee8df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 14:03:25 +0000

    Add package SMS_BP/0.2.0/sms_bp-0.2.0-py3-none-any.whl

---
+++ packages/SMS_BP/sms_bp-0.2.0-py3-none-any.whl/SMS_BP/__init__.py
@@ -0,0 +1,13 @@
+"""
+SMS_BP is a Python package for simulating single molecule localization microscopy experiments.
+See the README.md file for more information.
+GitHub: https://github.com/joemans3/SMS_BP
+
+Features:
+- fPALM / PALM
+- sptPALM
+- ...
+
+"""
+
+__version__ = "0.2.0"
