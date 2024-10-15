61bf925a10e29bf0093d9074dbfec69c0beb5dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 16:04:31 +0000

    Add package netbox-metatype-importer/0.3.3/netbox_metatype_importer-0.3.3.tar.gz

---
+++ packages/netbox-metatype-importer/netbox_metatype_importer-0.3.3.tar.gz/netbox_metatype_importer-0.3.3/netbox_metatype_importer/__init__.py
@@ -0,0 +1,27 @@
+from importlib.metadata import metadata
+
+from extras.plugins import PluginConfig
+
+metadata = metadata('netbox_metatype_importer')
+
+
+class NetBoxMetatypeImporterConfig(PluginConfig):
+    name = metadata.get('Name').replace('-', '_')
+    verbose_name = metadata.get('Summary')
+    description = metadata.get('Long-Description')
+    version = metadata.get('Version')
+    author = metadata.get('Author')
+    author_email = metadata.get('Author-email')
+    base_url = "meta-types"
+    min_version = '3.7.0'
+    max_version = '3.7.99'
+    default_settings = {
+        'repo_owner': 'netbox-community',
+        'repo': 'devicetype-library',
+        'branch': 'master',
+        'github_token': '',
+    }
+    required_settings = ['github_token']
+
+
+config = NetBoxMetatypeImporterConfig
---
+++ packages/netbox-metatype-importer/netbox_metatype_importer-0.3.3.tar.gz/netbox_metatype_importer-0.3.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='netbox-metatype-importer',
+    version='0.3.3',
+    description='Easily import Device and Module types from GitHub repo',
+    long_description='Import MetaTypes into NetBox',
+    long_description_content_type="text/markdown",
+    url='https://github.com/Onemind-Services-LLC/netbox-metatype-importer',
+    author='Abhimanyu Saharan',
+    author_email='asaharan@onemindservices.com',
+    maintainer='Prince Kumar',
+    maintainer_email='pkumar@onemindservices.com',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+    ],
+)
