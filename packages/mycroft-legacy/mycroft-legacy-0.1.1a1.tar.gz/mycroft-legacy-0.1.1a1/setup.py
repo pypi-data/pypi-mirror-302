5ccb54569db5b308b0c482c92f1b833ac4fb4c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 14:03:10 +0000

    Add package mycroft-legacy/0.1.1a1/mycroft-legacy-0.1.1a1.tar.gz

---
+++ packages/mycroft-legacy/mycroft-legacy-0.1.1a1.tar.gz/mycroft-legacy-0.1.1a1/setup.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+import os
+
+from setuptools import setup
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'mycroft', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
+
+
+def package_files(directory):
+    paths = []
+    for (path, _, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=').replace('~=', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
+
+with open(os.path.join(BASEDIR, "README.md"), "r") as f:
+    long_description = f.read()
+
+
+setup(
+    name='mycroft-legacy',
+    version=get_version(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='jarbasai',
+    author_email='jarbasai@mailfence.com',
+    url='https://github.com/OpenVoiceOS/mycroft-legacy',
+    license='apache-2.0',
+    packages=['mycroft', 'mycroft.skills'],
+    include_package_data=True,
+    install_requires=required("requirements.txt"),
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Text Processing :: Linguistic',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
