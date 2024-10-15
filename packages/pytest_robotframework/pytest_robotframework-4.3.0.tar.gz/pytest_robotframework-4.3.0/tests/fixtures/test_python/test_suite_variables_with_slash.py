88b7ac1bea4e9e41e342f291f172a817d8ebb98e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 15:03:41 +0000

    Add package pytest_robotframework/4.3.0/pytest_robotframework-4.3.0.tar.gz

---
+++ packages/pytest_robotframework/pytest_robotframework-4.3.0.tar.gz/pytest_robotframework-4.3.0/tests/fixtures/test_python/test_suite_variables_with_slash.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from robot.libraries.BuiltIn import BuiltIn
+
+from pytest_robotframework import set_variables
+
+set_variables({"foo": "bar\\baz"})
+
+
+def test_asdf():
+    assert (
+        BuiltIn().get_variable_value("$foo")  # pyright:ignore[reportUnknownMemberType]
+        == "bar\\baz"
+    )
