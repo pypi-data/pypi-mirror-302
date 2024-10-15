06efcd6990e17ce9c2a98add96dd2003411355cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 16:04:01 +0000

    Add package mtmlib/0.3.1202/mtmlib-0.3.1202-py3-none-any.whl

---
+++ packages/mtmlib/mtmlib-0.3.1202-py3-none-any.whl/mtmlib/decorators/mtform/mtform.py
@@ -0,0 +1,46 @@
+from camelCasing import camelCasing
+from pydantic import BaseModel, Field
+
+registered_forms: dict[str, type[BaseModel]] = {}
+
+
+def get_form_by_name(name: str) -> type[BaseModel]:
+    return registered_forms.get(name)
+
+
+def mtform(name: str | None = None):
+    """
+    表单装饰器，用于注册表单并设置表单属性
+    """
+
+    def decorator(cls: type[BaseModel]):
+        nonlocal name
+        if name is None:
+            name = camelCasing.toCamelCase(cls.__name__)
+        if name in registered_forms:
+            msg = f"Form with name '{name}' already exists"
+            raise ValueError(msg)
+        registered_forms[name] = cls
+        return cls
+
+    return decorator
+
+
+class FormFieldSchema(BaseModel):
+    name: str
+    placeholder: str | None = None
+    valueType: str | None = None
+    default: str | None = None
+    description: str | None = None
+    label: str
+    type: str
+    # examples: list[str] | None = None
+
+
+class MtForm(BaseModel):
+    properties: dict[str, FormFieldSchema]
+    title: str
+    type: str = Field(default="object")
+
+    class Config:  # noqa: D106
+        arbitrary_types_allowed = True
