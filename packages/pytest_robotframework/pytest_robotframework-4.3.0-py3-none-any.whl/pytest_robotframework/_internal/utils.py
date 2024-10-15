f37573acb08f02b98664423e9d6559c3d53503da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 15:03:41 +0000

    Add package pytest_robotframework/4.3.0/pytest_robotframework-4.3.0-py3-none-any.whl

---
+++ packages/pytest_robotframework/pytest_robotframework-4.3.0-py3-none-any.whl/pytest_robotframework/_internal/utils.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+from abc import abstractmethod
+from contextlib import AbstractContextManager
+from functools import wraps
+from typing import TYPE_CHECKING, Callable, Protocol, cast
+
+from basedtyping import P, T, out_T
+from typing_extensions import override
+
+if TYPE_CHECKING:
+    from types import TracebackType
+
+    from typing_extensions import Concatenate
+
+
+def patch_method(
+    cls: type[object], method_name: str | None = None
+) -> Callable[[Callable[Concatenate[Callable[P, T], P], T]], Callable[P, T]]:
+    """replaces a method of a class with the decorated one
+
+    example:
+    -------
+    >>> class Foo:
+    ...     def foo(self) -> int:
+    ...         return 1
+    ...
+    ... @patch_method(Foo)
+    ... def foo(old_method: Callable[[Foo], int], self: Foo) -> int:
+    ...     return old_method(self) + 1
+    ...
+    ... print(Foo().foo()) # 2
+
+    :param method_name: defaults to the name of the function being decorated
+    """
+
+    def decorator(fn: Callable[Concatenate[Callable[P, T], P], T]) -> Callable[P, T]:
+        nonlocal method_name
+        if method_name is None:
+            method_name = fn.__name__
+        old_method = cast(Callable[P, T], getattr(cls, method_name))
+
+        @wraps(fn)
+        def new_fn(*args: P.args, **kwargs: P.kwargs) -> T:
+            return fn(old_method, *args, **kwargs)
+
+        setattr(cls, method_name, new_fn)
+        return new_fn
+
+    return decorator
+
+
+class SuppressableContextManager(AbstractContextManager[out_T], Protocol[out_T]):
+    """removes `None` from the return type of `AbstractContextManager.__exit__` to prevent code
+    from being incorrectly marked as unreachable by pyright. see https://github.com/microsoft/pyright/issues/6034
+    """
+
+    @abstractmethod
+    @override
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_value: BaseException | None,
+        traceback: TracebackType | None,
+        /,
+    ) -> bool: ...
+
+
+main_package_name = __name__.split(".")[0]
+"""the name of the top level package (should be `pytest_robotframework`)"""
