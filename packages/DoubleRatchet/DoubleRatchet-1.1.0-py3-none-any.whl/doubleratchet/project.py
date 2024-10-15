30c5fb200ac696a560994f390900712aa8c2f4a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 15:04:54 +0000

    Add package DoubleRatchet/1.1.0/DoubleRatchet-1.1.0-py3-none-any.whl

---
+++ packages/DoubleRatchet/DoubleRatchet-1.1.0-py3-none-any.whl/doubleratchet/project.py
@@ -0,0 +1,13 @@
+__all__ = [ "project" ]
+
+project = {
+    "name"         : "DoubleRatchet",
+    "description"  : "A Python implementation of the Double Ratchet algorithm.",
+    "url"          : "https://github.com/Syndace/python-doubleratchet",
+    "year"         : "2024",
+    "author"       : "Tim Henkes (Syndace)",
+    "author_email" : "me@syndace.dev",
+    "categories"   : [
+        "Topic :: Security :: Cryptography"
+    ]
+}
