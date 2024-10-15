053ab2c2f3c5c8be24e87a42d5a629c6efdbdb89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 16:04:03 +0000

    Add package fastCodePrompts/0.1.1/fastcodeprompts-0.1.1.tar.gz

---
+++ packages/fastCodePrompts/fastcodeprompts-0.1.1.tar.gz/fastcodeprompts-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="fastCodePrompts",
+    version="0.1.1",  # Incrementar a versão
+    author="Hilton Queiroz Rebello",
+    author_email="rebello.hiltonqueiroz@gmail.com",
+    description="Pegar códigos python e inserir no contexto de um prompt",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hqr90/fastCodePrompts",
+    packages=["fastCodePrompts"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license="MIT",
+    include_package_data=True,
+    python_requires='>=3.8',
+    install_requires=[
+        "requests"
+    ],
+)
