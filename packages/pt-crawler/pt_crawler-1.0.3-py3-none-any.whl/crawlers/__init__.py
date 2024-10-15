9ce392d02a19aade7b5f896d467abc0e51f47fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 15:03:08 +0000

    Add package pt-crawler/1.0.3/pt_crawler-1.0.3-py3-none-any.whl

---
+++ packages/pt-crawler/pt_crawler-1.0.3-py3-none-any.whl/crawlers/__init__.py
@@ -0,0 +1,10 @@
+from .chdbits import CHDBits
+from .mteam import MTeam
+from .fsm import FSM
+from .ttg import TTG
+from .pterclub import PTerClub
+from .redleaves import RedLeaves
+
+VERSION = '1.0.3'
+
+__all__ = ['VERSION']
