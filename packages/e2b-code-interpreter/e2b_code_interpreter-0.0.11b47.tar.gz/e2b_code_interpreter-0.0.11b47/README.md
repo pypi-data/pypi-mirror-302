cc01ea80507d61337c1c1e5a536f07d040dcb400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 15:04:23 +0000

    Add package e2b-code-interpreter/0.0.11b47/e2b_code_interpreter-0.0.11b47.tar.gz

---
+++ packages/e2b-code-interpreter/e2b_code_interpreter-0.0.11b47.tar.gz/e2b_code_interpreter-0.0.11b47/README.md
@@ -0,0 +1,90 @@
+# Code interpreter extension for Python
+
+The repository contains a template and modules for the code interpreter sandbox. It is based on the Jupyter server and implements the Jupyter Kernel messaging protocol. This allows for sharing context between code executions and improves support for plotting charts and other display-able data.
+
+## Key Features
+
+- **Stateful Execution**: Unlike traditional sandboxes that treat each code execution independently, this package maintains context across executions.
+- **Displaying Graph & Data**: Implements parts of the [Jupyter Kernel messaging protocol](https://jupyter-client.readthedocs.io/en/latest/messaging.html), which support for interactive features like plotting charts, rendering DataFrames, etc.
+
+## Installation
+
+```sh
+pip install e2b-code-interpreter
+```
+
+## Examples
+
+### Minimal example with the sharing context
+
+```python
+from e2b_code_interpreter import Sandbox
+
+with Sandbox() as sandbox:
+    sandbox.run_code("x = 1")
+
+    execution = sandbox.run_code("x+=1; x")
+    print(execution.text)  # outputs 2
+
+```
+
+### Get charts and any display-able data
+
+```python
+import base64
+import io
+
+from matplotlib import image as mpimg, pyplot as plt
+
+from e2b_code_interpreter import Sandbox
+
+code = """
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 20, 100)
+y = np.sin(x)
+
+plt.plot(x, y)
+plt.show()
+"""
+
+with Sandbox() as sandbox:
+    # you can install dependencies in "jupyter notebook style"
+    sandbox.run_code("!pip install matplotlib")
+
+    # plot random graph
+    execution = sandbox.run_code(code)
+
+# there's your image
+image = execution.results[0].png
+
+# example how to show the image / prove it works
+i = base64.b64decode(image)
+i = io.BytesIO(i)
+i = mpimg.imread(i, format='PNG')
+
+plt.imshow(i, interpolation='nearest')
+plt.show()
+```
+
+### Streaming code output
+
+```python
+from e2b_code_interpreter import Sandbox
+
+code = """
+import time
+import pandas as pd
+
+print("hello")
+time.sleep(3)
+data = pd.DataFrame(data=[[1, 2], [3, 4]], columns=["A", "B"])
+display(data.head(10))
+time.sleep(3)
+print("world")
+"""
+
+with Sandbox() as sandbox:
+    sandbox.run_code(code, on_stdout=print, on_stderr=print, on_result=(lambda result: print(result.text)))
+```
---
+++ packages/e2b-code-interpreter/e2b_code_interpreter-0.0.11b47.tar.gz/e2b_code_interpreter-0.0.11b47/e2b_code_interpreter/code_interpreter_sync.py
@@ -0,0 +1,179 @@
+import logging
+import httpx
+
+from typing import Optional, Dict, overload
+from httpx import Client
+from e2b import Sandbox as BaseSandbox, InvalidArgumentException
+
+from e2b_code_interpreter.constants import (
+    DEFAULT_TEMPLATE,
+    JUPYTER_PORT,
+    DEFAULT_TIMEOUT,
+)
+from e2b_code_interpreter.models import (
+    Execution,
+    Context,
+    Result,
+    extract_exception,
+    parse_output,
+    OutputHandler,
+    OutputMessage,
+)
+from e2b_code_interpreter.exceptions import (
+    format_execution_timeout_error,
+    format_request_timeout_error,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class Sandbox(BaseSandbox):
+    default_template = DEFAULT_TEMPLATE
+
+    @property
+    def _jupyter_url(self) -> str:
+        return f"{'http' if self.connection_config.debug else 'https'}://{self.get_host(JUPYTER_PORT)}"
+
+    @property
+    def _client(self) -> Client:
+        return Client(transport=self._transport)
+
+    @overload
+    def run_code(
+        self,
+        code: str,
+        language: Optional[str] = None,
+        on_stdout: Optional[OutputHandler[OutputMessage]] = None,
+        on_stderr: Optional[OutputHandler[OutputMessage]] = None,
+        on_result: Optional[OutputHandler[Result]] = None,
+        envs: Optional[Dict[str, str]] = None,
+        timeout: Optional[float] = None,
+        request_timeout: Optional[float] = None,
+    ) -> Execution: ...
+
+    @overload
+    def run_code(
+        self,
+        code: str,
+        context: Optional[Context] = None,
+        on_stdout: Optional[OutputHandler[OutputMessage]] = None,
+        on_stderr: Optional[OutputHandler[OutputMessage]] = None,
+        on_result: Optional[OutputHandler[Result]] = None,
+        envs: Optional[Dict[str, str]] = None,
+        timeout: Optional[float] = None,
+        request_timeout: Optional[float] = None,
+    ) -> Execution: ...
+
+    def run_code(
+        self,
+        code: str,
+        language: Optional[str] = None,
+        context: Optional[Context] = None,
+        on_stdout: Optional[OutputHandler[OutputMessage]] = None,
+        on_stderr: Optional[OutputHandler[OutputMessage]] = None,
+        on_result: Optional[OutputHandler[Result]] = None,
+        envs: Optional[Dict[str, str]] = None,
+        timeout: Optional[float] = None,
+        request_timeout: Optional[float] = None,
+    ) -> Execution:
+        """
+        Runs the code in the specified language/context, if not specified, the default context is used.
+        You can reference previously defined variables, imports, and functions in the code.
+
+        :param code: The code to execute
+        :param language Based on the value, a default context for the language is used. If not defined and no context is provided, the default Python context is used.
+        :param context Concrete context to run the code in. If not specified, the default context for the language is used. It's mutually exclusive with the language.
+        :param on_stdout: Callback for stdout messages
+        :param on_stderr: Callback for stderr messages
+        :param on_result: Callback for the `Result` object
+        :param envs: Environment variables
+        :param timeout: Max time to wait for the execution to finish
+        :param request_timeout: Max time to wait for the request to finish
+        :return: Execution object
+        """
+
+        logger.debug(f"Executing code {code}")
+
+        if language and context:
+            raise InvalidArgumentException(
+                "You can provide context or language, but not both at the same time."
+            )
+
+        timeout = None if timeout == 0 else (timeout or DEFAULT_TIMEOUT)
+        request_timeout = request_timeout or self._connection_config.request_timeout
+        context_id = context.id if context else None
+
+        try:
+            with self._client.stream(
+                "POST",
+                f"{self._jupyter_url}/execute",
+                json={
+                    "code": code,
+                    "context_id": context_id,
+                    "language": language,
+                    "env_vars": envs,
+                },
+                timeout=(request_timeout, timeout, request_timeout, request_timeout),
+            ) as response:
+                err = extract_exception(response)
+                if err:
+                    raise err
+
+                execution = Execution()
+
+                for line in response.iter_lines():
+                    parse_output(
+                        execution,
+                        line,
+                        on_stdout=on_stdout,
+                        on_stderr=on_stderr,
+                        on_result=on_result,
+                    )
+
+                return execution
+        except httpx.ReadTimeout:
+            raise format_execution_timeout_error()
+        except httpx.TimeoutException:
+            raise format_request_timeout_error()
+
+    def create_code_context(
+        self,
+        cwd: Optional[str] = None,
+        language: Optional[str] = None,
+        envs: Optional[Dict[str, str]] = None,
+        request_timeout: Optional[float] = None,
+    ) -> Context:
+        """
+        Creates a new context to run code in.
+
+        :param cwd: Set the current working directory for the context
+        :param language: Language of the context. If not specified, the default Python context is used.
+        :param envs: Environment variables
+        :param request_timeout: Max time to wait for the request to finish
+        :return: Context id
+        """
+        logger.debug(f"Creating new {language} context")
+
+        data = {}
+        if language:
+            data["language"] = language
+        if cwd:
+            data["cwd"] = cwd
+        if envs:
+            data["env_vars"] = envs
+
+        try:
+            response = self._client.post(
+                f"{self._jupyter_url}/contexts",
+                json=data,
+                timeout=request_timeout or self._connection_config.request_timeout,
+            )
+
+            err = extract_exception(response)
+            if err:
+                raise err
+
+            data = response.json()
+            return Context.from_json(data)
+        except httpx.TimeoutException:
+            raise format_request_timeout_error()
