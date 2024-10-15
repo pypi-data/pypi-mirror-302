eb75291d4e72b8c5e6eb1a418ae7551270f3f38e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 14:03:36 +0000

    Add package policyengine-us/1.122.0/policyengine_us-1.122.0-py3-none-any.whl

---
+++ packages/policyengine-us/policyengine_us-1.122.0-py3-none-any.whl/policyengine_us/variables/gov/states/ks/tax/income/exemptions/ks_exemptions.py
@@ -0,0 +1,26 @@
+from policyengine_us.model_api import *
+
+
+class ks_exemptions(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Kansas exemptions amount"
+    unit = USD
+    definition_period = YEAR
+    reference = "https://law.justia.com/codes/kansas/chapter-79/article-32/section-79-32-121/"
+    defined_for = StateCode.KS
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.states.ks.tax.income.exemptions
+        exemptions_count = tax_unit("ks_count_exemptions", period)
+        base_exemptions = exemptions_count * p.base
+        veteran_exemptions_count = add(
+            tax_unit,
+            period,
+            ["ks_disabled_veteran_exemptions_eligible_person"],
+        )
+        additional_exemptions = (
+            veteran_exemptions_count * p.disabled_veteran.base
+        )
+
+        return base_exemptions + additional_exemptions
