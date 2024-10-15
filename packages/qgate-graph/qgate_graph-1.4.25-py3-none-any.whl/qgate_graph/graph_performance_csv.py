f2f63be313d998188dbfbbabc7d8b921e9412982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 16:03:41 +0000

    Add package qgate-graph/1.4.25/qgate_graph-1.4.25-py3-none-any.whl

---
+++ packages/qgate-graph/qgate_graph-1.4.25-py3-none-any.whl/qgate_graph/graph_performance_csv.py
@@ -0,0 +1,17 @@
+from qgate_graph.graph_performance import GraphPerformance
+from qgate_graph.percentile_item import PercentileItem
+import os.path, os
+import logging
+
+
+class GraphPerformanceCsv(GraphPerformance):
+
+    def __init__(self, min_precision=-1, max_precision=-1, raw_format=False):
+        super().__init__(0, min_precision, max_precision, raw_format)
+
+    def _create_output(self, percentiles: {PercentileItem}, title, file_name, output_dir) -> str:
+        output_file = os.path.join(output_dir, f"CSV{file_name}.csv")
+        with open(output_file, 'w', newline='') as file:
+            file.write(super()._create_table(percentiles).get_csv_string(delimiter=','))
+            logging.info(f"  ... {output_file}")
+        return output_file
