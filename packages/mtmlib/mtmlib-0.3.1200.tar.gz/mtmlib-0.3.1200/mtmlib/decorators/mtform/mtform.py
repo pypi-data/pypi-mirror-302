2ed6c0601584327579da70087d7943cb7a90cd9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 16:04:05 +0000

    Add package mtmlib/0.3.1200/mtmlib-0.3.1200.tar.gz

---
+++ packages/mtmlib/mtmlib-0.3.1200.tar.gz/mtmlib-0.3.1200/mtmlib/decorators/mtform/mtform.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel, Field
+
+# 存储注册的表单
+registered_forms: dict[str, type[BaseModel]] = {}
+
+
+def get_form_by_name(name: str) -> type[BaseModel]:
+    return registered_forms.get(name)
+
+
+def mtform(name: str | None = None):
+    """
+    表单装饰器，用于注册表单并设置表单属性
+    """
+
+    def decorator(cls: type[BaseModel]):
+        nonlocal name
+        if name is None:
+            name = cls.__name__.replace("_", "").lower()
+            name = name[0].lower() + name[1:]
+        if name in registered_forms:
+            msg = f"Form with name '{name}' already exists"
+            raise ValueError(msg)
+        registered_forms[name] = cls
+        return cls
+
+    return decorator
+
+
+class FormFieldSchema(BaseModel):
+    default: str | None = None
+    description: str | None = None
+    title: str
+    type: str
+    examples: list[str] | None = None
+
+
+class MtForm(BaseModel):
+    properties: dict[str, FormFieldSchema]
+    title: str
+    type: str = Field(default="object")
+
+    class Config:  # noqa: D106
+        arbitrary_types_allowed = True
