b71b547a610571845a5667cee8c80e9fd80761ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 16:04:30 +0000

    Add package neutrino-client/0.0.4/neutrino_client-0.0.4.tar.gz

---
+++ packages/neutrino-client/neutrino_client-0.0.4.tar.gz/neutrino_client-0.0.4/neutrino_client/neutrino_task.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class NeutrinoTask(BaseModel):
+    task_name: str
+    task_id: str
+    status: str
+    threshold: Optional[int] = 1
+    threshold_unit: Optional[str] = 'minutes'
+    additional_data: Optional[dict[str, str]] = {}
+    timestamp: Optional[str] = ''
+    type: Optional[str] = ''
