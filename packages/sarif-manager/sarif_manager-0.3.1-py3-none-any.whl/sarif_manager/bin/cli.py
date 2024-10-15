e9357689c8a3eea28aa3926e2796df6324d0f07e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 14:03:35 +0000

    Add package sarif-manager/0.3.1/sarif_manager-0.3.1-py3-none-any.whl

---
+++ packages/sarif-manager/sarif_manager-0.3.1-py3-none-any.whl/sarif_manager/bin/cli.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+import click
+from sarif_manager.bin.logger import init_logger
+
+from sarif_manager.version import __version__
+from sarif_manager.command.azure import azure
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+@click.group
+@click.version_option(version=__version__)
+def sarif_manager():
+    """Parse and use SARIF files for security analysis."""
+    init_logger()
+
+
+sarif_manager.add_command(azure)
+
+
+def main():
+    """Parse and use SARIF files for security analysis."""
+    sarif_manager()
+
+
+if __name__ == '__main__':
+    main()
