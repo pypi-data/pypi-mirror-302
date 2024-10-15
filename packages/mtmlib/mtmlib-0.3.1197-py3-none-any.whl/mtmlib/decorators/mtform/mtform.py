21947684b3d00b4e726a9a59fee4fc8d744f511b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 15:05:00 +0000

    Add package mtmlib/0.3.1197/mtmlib-0.3.1197-py3-none-any.whl

---
+++ packages/mtmlib/mtmlib-0.3.1197-py3-none-any.whl/mtmlib/decorators/mtform/mtform.py
@@ -0,0 +1,52 @@
+from pydantic import BaseModel, Field
+
+# 存储注册的表单
+registered_forms: dict[str, type[BaseModel]] = {}
+
+
+def get_form_by_name(name: str) -> type[BaseModel]:
+    return registered_forms.get(name)
+
+
+class FormAttributes(BaseModel):
+    name: str
+    title: str | None = None
+    method: str = "POST"
+    action: str | None = None
+    enctype: str | None = None
+    target: str | None = None
+
+
+def mtform(**kwargs):
+    """
+    表单装饰器，用于注册表单并设置表单属性
+    """
+    form_attrs = FormAttributes(**kwargs)
+
+    def decorator(cls: type[BaseModel]):
+        if form_attrs.name in registered_forms:
+            msg = f"Form with name '{form_attrs.name}' already exists"
+            raise ValueError(msg)
+        registered_forms[form_attrs.name] = cls
+        cls.model_fields["form_attributes"] = Field(default=form_attrs)
+        return cls
+
+    return decorator
+
+
+class FormFieldSchema(BaseModel):
+    default: str | None = None
+    description: str | None = None
+    title: str
+    type: str
+    examples: list[str] | None = None
+
+
+class MyForm(BaseModel):
+    properties: dict[str, FormFieldSchema]
+    title: str
+    type: str = Field(default="object")
+    form_attributes: FormAttributes | None = None
+
+    class Config:  # noqa: D106
+        arbitrary_types_allowed = True
