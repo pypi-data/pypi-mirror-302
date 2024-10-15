382d0948ef677d7ac3db57783a8779403c6dbc5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 14:03:42 +0000

    Add package keras-hub-nightly/0.16.1.dev202410150342/keras_hub_nightly-0.16.1.dev202410150342.tar.gz

---
+++ packages/keras-hub-nightly/keras_hub_nightly-0.16.1.dev202410150342.tar.gz/keras_hub_nightly-0.16.1.dev202410150342/keras_hub/src/models/resnet/resnet_presets.py
@@ -0,0 +1,82 @@
+"""ResNet preset configurations."""
+
+backbone_presets = {
+    "resnet_18_imagenet": {
+        "metadata": {
+            "description": (
+                "18-layer ResNet model pre-trained on the ImageNet 1k dataset "
+                "at a 224x224 resolution."
+            ),
+            "params": 11186112,
+            "official_name": "ResNet",
+            "path": "resnet",
+            "model_card": "https://arxiv.org/abs/2110.00476",
+        },
+        "kaggle_handle": "kaggle://keras/resnetv1/keras/resnet_18_imagenet/1",
+    },
+    "resnet_50_imagenet": {
+        "metadata": {
+            "description": (
+                "50-layer ResNet model pre-trained on the ImageNet 1k dataset "
+                "at a 224x224 resolution."
+            ),
+            "params": 23561152,
+            "official_name": "ResNet",
+            "path": "resnet",
+            "model_card": "https://arxiv.org/abs/2110.00476",
+        },
+        "kaggle_handle": "kaggle://keras/resnetv1/keras/resnet_50_imagenet/1",
+    },
+    "resnet_101_imagenet": {
+        "metadata": {
+            "description": (
+                "101-layer ResNet model pre-trained on the ImageNet 1k dataset "
+                "at a 224x224 resolution."
+            ),
+            "params": 42605504,
+            "official_name": "ResNet",
+            "path": "resnet",
+            "model_card": "https://arxiv.org/abs/2110.00476",
+        },
+        "kaggle_handle": "kaggle://keras/resnetv1/keras/resnet_101_imagenet/1",
+    },
+    "resnet_152_imagenet": {
+        "metadata": {
+            "description": (
+                "152-layer ResNet model pre-trained on the ImageNet 1k dataset "
+                "at a 224x224 resolution."
+            ),
+            "params": 58295232,
+            "official_name": "ResNet",
+            "path": "resnet",
+            "model_card": "https://arxiv.org/abs/2110.00476",
+        },
+        "kaggle_handle": "kaggle://keras/resnetv1/keras/resnet_152_imagenet/1",
+    },
+    "resnet_v2_50_imagenet": {
+        "metadata": {
+            "description": (
+                "50-layer ResNetV2 model pre-trained on the ImageNet 1k "
+                "dataset at a 224x224 resolution."
+            ),
+            "params": 23561152,
+            "official_name": "ResNet",
+            "path": "resnet",
+            "model_card": "https://arxiv.org/abs/2110.00476",
+        },
+        "kaggle_handle": "kaggle://keras/resnetv2/keras/resnet_v2_50_imagenet/1",
+    },
+    "resnet_v2_101_imagenet": {
+        "metadata": {
+            "description": (
+                "101-layer ResNetV2 model pre-trained on the ImageNet 1k "
+                "dataset at a 224x224 resolution."
+            ),
+            "params": 42605504,
+            "official_name": "ResNet",
+            "path": "resnet",
+            "model_card": "https://arxiv.org/abs/2110.00476",
+        },
+        "kaggle_handle": "kaggle://keras/resnetv2/keras/resnet_v2_101_imagenet/1",
+    },
+}
