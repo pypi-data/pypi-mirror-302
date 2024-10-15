e4ec5a4990040f301f6083daeba5cf4a600f98d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 17:03:21 +0000

    Add package tapenade/0.0.4/tapenade-0.0.4.tar.gz

---
+++ packages/tapenade/tapenade-0.0.4.tar.gz/tapenade-0.0.4/src/tapenade/preprocessing/__init__.py
@@ -0,0 +1,35 @@
+from ._preprocessing import (
+    isotropize_and_normalize,
+    change_array_pixelsize,
+    compute_mask,
+    global_image_equalization,
+    local_image_equalization,
+    normalize_intensity,
+    segment_stardist,
+    segment_stardist_from_files,
+    align_array_major_axis,
+    align_array_major_axis_from_files,
+    crop_array_using_mask,
+    crop_array_using_mask_from_files,
+    masked_gaussian_smoothing,
+    masked_gaussian_smooth_dense_two_arrays_gpu,
+    masked_gaussian_smooth_sparse
+)
+
+__all__ = [
+    "isotropize_and_normalize",
+    "change_array_pixelsize",
+    "compute_mask",
+    "global_image_equalization",
+    "local_image_equalization",
+    "normalize_intensity",
+    "segment_stardist",
+    "segment_stardist_from_files",
+    "align_array_major_axis",
+    "align_array_major_axis_from_files",
+    "crop_array_using_mask",
+    "crop_array_using_mask_from_files",
+    "masked_gaussian_smoothing",
+    "masked_gaussian_smooth_dense_two_arrays_gpu",
+    "masked_gaussian_smooth_sparse",
+]
