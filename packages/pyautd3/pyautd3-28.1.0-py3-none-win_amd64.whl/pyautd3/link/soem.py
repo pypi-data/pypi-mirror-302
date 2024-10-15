5ebcb44fb08498e7aa96de5eb540546ab1d15fe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 15:03:38 +0000

    Add package pyautd3/28.1.0/pyautd3-28.1.0-py3-none-win_amd64.whl

---
+++ packages/pyautd3/pyautd3-28.1.0-py3-none-win_amd64.whl/pyautd3/link/soem.py
@@ -0,0 +1,169 @@
+import ctypes
+from collections.abc import Callable
+from datetime import timedelta
+
+from pyautd3.driver.link import Link, LinkBuilder
+from pyautd3.native_methods.autd3capi import ControllerPtr
+from pyautd3.native_methods.autd3capi import (
+    NativeMethods as Base,
+)
+from pyautd3.native_methods.autd3capi_driver import HandlePtr, LinkBuilderPtr, LinkPtr, SyncMode
+from pyautd3.native_methods.autd3capi_link_soem import (
+    LinkRemoteSOEMBuilderPtr,
+    LinkSOEMBuilderPtr,
+    ProcessPriority,
+    Status,
+    ThreadPriorityPtr,
+    TimerStrategy,
+)
+from pyautd3.native_methods.autd3capi_link_soem import NativeMethods as LinkSOEM
+from pyautd3.native_methods.utils import ConstantADT, _validate_ptr
+
+ErrHandlerFunc = ctypes.CFUNCTYPE(None, ctypes.c_void_p, ctypes.c_uint32, ctypes.c_uint8)  # type: ignore[arg-type]
+
+
+class EtherCATAdapter:
+    desc: str
+    name: str
+
+    def __init__(self: "EtherCATAdapter", name: str, desc: str) -> None:
+        self.desc = desc
+        self.name = name
+
+    def __repr__(self: "EtherCATAdapter") -> str:
+        return f"{self.desc}, {self.name}"
+
+
+class ThreadPriority(metaclass=ConstantADT):
+    Min: ThreadPriorityPtr = LinkSOEM().link_soem_thread_priority_min()
+    Max: ThreadPriorityPtr = LinkSOEM().link_soem_thread_priority_max()
+
+    @staticmethod
+    def Crossplatform(value: int) -> ThreadPriorityPtr:  # noqa: N802
+        if not (0 <= value <= 99):  # noqa: PLR2004
+            msg = "value must be between 0 and 99"
+            raise ValueError(msg)
+        return LinkSOEM().link_soem_thread_priority_crossplatform(value)
+
+
+class SOEM(Link):
+    class _Builder(LinkBuilder["SOEM"]):
+        _builder: LinkSOEMBuilderPtr
+
+        def __init__(self: "SOEM._Builder") -> None:
+            self._builder = LinkSOEM().link_soem()
+
+        def with_ifname(self: "SOEM._Builder", ifname: str) -> "SOEM._Builder":
+            self._builder = LinkSOEM().link_soem_with_ifname(self._builder, ifname.encode("utf-8"))
+            return self
+
+        def with_buf_size(self: "SOEM._Builder", size: int) -> "SOEM._Builder":
+            self._builder = LinkSOEM().link_soem_with_buf_size(self._builder, size)
+            return self
+
+        def with_send_cycle(self: "SOEM._Builder", cycle: int) -> "SOEM._Builder":
+            self._builder = LinkSOEM().link_soem_with_send_cycle(self._builder, cycle)
+            return self
+
+        def with_sync0_cycle(self: "SOEM._Builder", cycle: int) -> "SOEM._Builder":
+            self._builder = LinkSOEM().link_soem_with_sync_0_cycle(self._builder, cycle)
+            return self
+
+        def with_err_handler(self: "SOEM._Builder", handler: Callable[[int, Status, str], None]) -> "SOEM._Builder":
+            def callback_native(_context: ctypes.c_void_p, slave: ctypes.c_uint32, status: ctypes.c_uint8) -> None:
+                err = ctypes.create_string_buffer(128)  # pragma: no cover
+                status_ = Status(int(status))  # pragma: no cover
+                LinkSOEM().link_soem_status_get_msg(status_, err)  # pragma: no cover
+                handler(int(slave), status_, err.value.decode("utf-8"))  # pragma: no cover
+
+            self._err_handler = ErrHandlerFunc(callback_native)
+            self._builder = LinkSOEM().link_soem_with_err_handler(self._builder, self._err_handler, None)  # type: ignore[arg-type]
+            return self
+
+        def with_timer_strategy(self: "SOEM._Builder", strategy: TimerStrategy) -> "SOEM._Builder":
+            self._builder = LinkSOEM().link_soem_with_timer_strategy(self._builder, strategy)
+            return self
+
+        def with_sync_mode(self: "SOEM._Builder", mode: SyncMode) -> "SOEM._Builder":
+            self._builder = LinkSOEM().link_soem_with_sync_mode(self._builder, mode)
+            return self
+
+        def with_sync_tolerance(self: "SOEM._Builder", tolerance: timedelta) -> "SOEM._Builder":
+            self._builder = LinkSOEM().link_soem_with_sync_tolerance(self._builder, int(tolerance.total_seconds() * 1000 * 1000 * 1000))
+            return self
+
+        def with_sync_timeout(self: "SOEM._Builder", timeout: timedelta) -> "SOEM._Builder":
+            self._builder = LinkSOEM().link_soem_with_sync_timeout(self._builder, int(timeout.total_seconds() * 1000 * 1000 * 1000))
+            return self
+
+        def with_state_check_interval(self: "SOEM._Builder", interval: timedelta) -> "SOEM._Builder":
+            self._builder = LinkSOEM().link_soem_with_state_check_interval(self._builder, int(interval.total_seconds() / 1000))
+            return self
+
+        def with_timeout(self: "SOEM._Builder", timeout: timedelta) -> "SOEM._Builder":
+            self._builder = LinkSOEM().link_soem_with_timeout(self._builder, int(timeout.total_seconds() * 1000 * 1000 * 1000))
+            return self
+
+        def with_process_priority(self: "SOEM._Builder", priority: ProcessPriority) -> "SOEM._Builder":
+            self._builder = LinkSOEM().link_soem_with_process_priority(self._builder, priority)
+            return self
+
+        def with_thread_priority(self: "SOEM._Builder", priority: ThreadPriorityPtr) -> "SOEM._Builder":
+            self._builder = LinkSOEM().link_soem_with_thread_priority(self._builder, priority)
+            return self
+
+        def _link_builder_ptr(self: "SOEM._Builder") -> LinkBuilderPtr:
+            return LinkSOEM().link_soem_into_builder(self._builder)
+
+        def _resolve_link(self: "SOEM._Builder", handle: HandlePtr, ptr: ControllerPtr) -> "SOEM":
+            return SOEM(handle, Base().link_get(ptr), self._err_handler)  # pragma: no cover
+
+    @staticmethod
+    def enumerate_adapters() -> list[EtherCATAdapter]:
+        handle = LinkSOEM().adapter_pointer()
+        size = LinkSOEM().adapter_get_size(handle)
+
+        def get_adapter(i: int) -> EtherCATAdapter:
+            sb_desc = ctypes.create_string_buffer(128)
+            sb_name = ctypes.create_string_buffer(128)
+            LinkSOEM().adapter_get_adapter(handle, i, sb_desc, sb_name)
+            return EtherCATAdapter(sb_name.value.decode("utf-8"), sb_desc.value.decode("utf-8"))
+
+        res = list(map(get_adapter, range(int(size))))
+
+        LinkSOEM().adapter_pointer_delete(handle)
+
+        return res
+
+    def __init__(self: "SOEM", handle: HandlePtr, ptr: LinkPtr, err_handler) -> None:  # noqa: ANN001
+        super().__init__(handle, ptr)  # pragma: no cover
+        self._err_handler = err_handler  # pragma: no cover
+
+    @staticmethod
+    def builder() -> _Builder:
+        return SOEM._Builder()
+
+
+class RemoteSOEM(Link):
+    class _Builder(LinkBuilder["RemoteSOEM"]):
+        _builder: LinkRemoteSOEMBuilderPtr
+
+        def __init__(self: "RemoteSOEM._Builder", addr: str) -> None:
+            self._builder = _validate_ptr(LinkSOEM().link_remote_soem(addr.encode("utf-8")))
+
+        def with_timeout(self: "RemoteSOEM._Builder", timeout: timedelta) -> "RemoteSOEM._Builder":
+            self._builder = LinkSOEM().link_remote_soem_with_timeout(self._builder, int(timeout.total_seconds() * 1000 * 1000 * 1000))
+            return self
+
+        def _link_builder_ptr(self: "RemoteSOEM._Builder") -> LinkBuilderPtr:
+            return LinkSOEM().link_remote_soem_into_builder(self._builder)  # pragma: no cover
+
+        def _resolve_link(self: "RemoteSOEM._Builder", handle: HandlePtr, ptr: ControllerPtr) -> "RemoteSOEM":
+            return RemoteSOEM(handle, Base().link_get(ptr))  # pragma: no cover
+
+    def __init__(self: "RemoteSOEM", handle: HandlePtr, ptr: LinkPtr) -> None:
+        super().__init__(handle, ptr)  # pragma: no cover
+
+    @staticmethod
+    def builder(addr: str) -> _Builder:
+        return RemoteSOEM._Builder(addr)
---
+++ packages/pyautd3/pyautd3-28.1.0-py3-none-win_amd64.whl/pyautd3/modulation/sampling_mode.py
@@ -0,0 +1,278 @@
+import ctypes
+from abc import ABCMeta, abstractmethod
+
+import numpy as np
+
+from pyautd3.driver.defined.angle import Angle
+from pyautd3.driver.defined.freq import Freq
+from pyautd3.native_methods.autd3capi import NativeMethods as Base
+from pyautd3.native_methods.autd3capi_driver import LoopBehavior, ModulationPtr
+from pyautd3.native_methods.structs import SamplingConfig
+from pyautd3.native_methods.utils import _validate_ptr
+
+
+class ISamplingMode(metaclass=ABCMeta):
+    @abstractmethod
+    def sine_ptr(
+        self: "ISamplingMode",
+        config: SamplingConfig,
+        intensity: int,
+        offset: int,
+        phase: Angle,
+        clamp: bool,  # noqa: FBT001
+        loop_behavior: LoopBehavior,
+    ) -> ModulationPtr:
+        pass
+
+    @abstractmethod
+    def sine_freq(self: "ISamplingMode", ptr: ModulationPtr) -> int | float:
+        pass
+
+    @abstractmethod
+    def fourier_ptr(
+        self: "ISamplingMode",
+        components: np.ndarray,
+        size: int,
+        clamp: bool,  # noqa: FBT001
+        scale_factor: float,
+        offset: int,
+        loop_behavior: LoopBehavior,
+    ) -> ModulationPtr:
+        pass
+
+    @abstractmethod
+    def square_ptr(
+        self: "ISamplingMode",
+        config: SamplingConfig,
+        low: int,
+        high: int,
+        duty: float,
+        loop_behavior: LoopBehavior,
+    ) -> ModulationPtr:
+        pass
+
+    @abstractmethod
+    def square_freq(self: "ISamplingMode", ptr: ModulationPtr) -> int | float:
+        pass
+
+
+class SamplingModeExact(ISamplingMode):
+    _freq: Freq[int]
+
+    def __init__(self: "SamplingModeExact", freq: Freq[int]) -> None:
+        self._freq = freq
+
+    def sine_ptr(
+        self: "SamplingModeExact",
+        config: SamplingConfig,
+        intensity: int,
+        offset: int,
+        phase: Angle,
+        clamp: bool,  # noqa: FBT001
+        loop_behavior: LoopBehavior,
+    ) -> ModulationPtr:
+        return _validate_ptr(
+            Base().modulation_sine_exact(
+                self._freq.hz,
+                config,
+                intensity,
+                offset,
+                phase.radian,
+                clamp,
+                loop_behavior,
+            ),
+        )
+
+    def sine_freq(self: "SamplingModeExact", ptr: ModulationPtr) -> int | float:
+        return int(Base().modulation_sine_exact_freq(ptr))
+
+    def fourier_ptr(
+        self: "SamplingModeExact",
+        components: np.ndarray,
+        size: int,
+        clamp: bool,  # noqa: FBT001
+        scale_factor: float,
+        offset: int,
+        loop_behavior: LoopBehavior,
+    ) -> ModulationPtr:
+        return _validate_ptr(
+            Base().modulation_fourier_exact(
+                components.ctypes.data_as(ctypes.POINTER(ModulationPtr)),  # type: ignore[arg-type]
+                size,
+                clamp,
+                scale_factor,
+                offset,
+                loop_behavior,
+            ),
+        )
+
+    def square_ptr(
+        self: "SamplingModeExact",
+        config: SamplingConfig,
+        low: int,
+        high: int,
+        duty: float,
+        loop_behavior: LoopBehavior,
+    ) -> ModulationPtr:
+        return _validate_ptr(
+            Base().modulation_square_exact(
+                self._freq.hz,
+                config,
+                low,
+                high,
+                duty,
+                loop_behavior,
+            ),
+        )
+
+    def square_freq(self: "SamplingModeExact", ptr: ModulationPtr) -> int | float:
+        return int(Base().modulation_square_exact_freq(ptr))
+
+
+class SamplingModeExactFloat(ISamplingMode):
+    _freq: Freq[float]
+
+    def __init__(self: "SamplingModeExactFloat", freq: Freq[float]) -> None:
+        self._freq = freq
+
+    def sine_ptr(
+        self: "SamplingModeExactFloat",
+        config: SamplingConfig,
+        intensity: int,
+        offset: int,
+        phase: Angle,
+        clamp: bool,  # noqa: FBT001
+        loop_behavior: LoopBehavior,
+    ) -> ModulationPtr:
+        return _validate_ptr(
+            Base().modulation_sine_exact_float(
+                self._freq.hz,
+                config,
+                intensity,
+                offset,
+                phase.radian,
+                clamp,
+                loop_behavior,
+            ),
+        )
+
+    def sine_freq(
+        self: "SamplingModeExactFloat",
+        ptr: ModulationPtr,
+    ) -> int | float:
+        return float(Base().modulation_sine_exact_float_freq(ptr))
+
+    def fourier_ptr(
+        self: "SamplingModeExactFloat",
+        components: np.ndarray,
+        size: int,
+        clamp: bool,  # noqa: FBT001
+        scale_factor: float,
+        offset: int,
+        loop_behavior: LoopBehavior,
+    ) -> ModulationPtr:
+        return _validate_ptr(
+            Base().modulation_fourier_exact_float(
+                components.ctypes.data_as(ctypes.POINTER(ModulationPtr)),  # type: ignore[arg-type]
+                size,
+                clamp,
+                scale_factor,
+                offset,
+                loop_behavior,
+            ),
+        )
+
+    def square_ptr(
+        self: "SamplingModeExactFloat",
+        config: SamplingConfig,
+        low: int,
+        high: int,
+        duty: float,
+        loop_behavior: LoopBehavior,
+    ) -> ModulationPtr:
+        return _validate_ptr(
+            Base().modulation_square_exact_float(
+                self._freq.hz,
+                config,
+                low,
+                high,
+                duty,
+                loop_behavior,
+            ),
+        )
+
+    def square_freq(self: "SamplingModeExactFloat", ptr: ModulationPtr) -> int | float:
+        return int(Base().modulation_square_exact_float_freq(ptr))
+
+
+class SamplingModeNearest(ISamplingMode):
+    _freq: Freq[float]
+
+    def __init__(self: "SamplingModeNearest", freq: Freq[float]) -> None:
+        self._freq = freq
+
+    def sine_ptr(
+        self: "SamplingModeNearest",
+        config: SamplingConfig,
+        intensity: int,
+        offset: int,
+        phase: Angle,
+        clamp: bool,  # noqa: FBT001
+        loop_behavior: LoopBehavior,
+    ) -> ModulationPtr:
+        return _validate_ptr(
+            Base().modulation_sine_nearest(
+                self._freq.hz,
+                config,
+                intensity,
+                offset,
+                phase.radian,
+                clamp,
+                loop_behavior,
+            ),
+        )
+
+    def sine_freq(self: "SamplingModeNearest", ptr: ModulationPtr) -> int | float:
+        return float(Base().modulation_sine_nearest_freq(ptr))
+
+    def fourier_ptr(
+        self: "SamplingModeNearest",
+        components: np.ndarray,
+        size: int,
+        clamp: bool,  # noqa: FBT001
+        scale_factor: float,
+        offset: int,
+        loop_behavior: LoopBehavior,
+    ) -> ModulationPtr:
+        return _validate_ptr(
+            Base().modulation_fourier_nearest(
+                components.ctypes.data_as(ctypes.POINTER(ModulationPtr)),  # type: ignore[arg-type]
+                size,
+                clamp,
+                scale_factor,
+                offset,
+                loop_behavior,
+            ),
+        )
+
+    def square_ptr(
+        self: "SamplingModeNearest",
+        config: SamplingConfig,
+        low: int,
+        high: int,
+        duty: float,
+        loop_behavior: LoopBehavior,
+    ) -> ModulationPtr:
+        return _validate_ptr(
+            Base().modulation_square_nearest(
+                self._freq.hz,
+                config,
+                low,
+                high,
+                duty,
+                loop_behavior,
+            ),
+        )
+
+    def square_freq(self: "SamplingModeNearest", ptr: ModulationPtr) -> int | float:
+        return int(Base().modulation_square_nearest_freq(ptr))
