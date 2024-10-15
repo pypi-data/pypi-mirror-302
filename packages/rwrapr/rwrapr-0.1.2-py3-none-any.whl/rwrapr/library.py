7d5eb7fd8feb885311e37925c2ae25cd469ff498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 15:03:40 +0000

    Add package rwrapr/0.1.2/rwrapr-0.1.2-py3-none-any.whl

---
+++ packages/rwrapr/rwrapr-0.1.2-py3-none-any.whl/rwrapr/library.py
@@ -0,0 +1,18 @@
+import rpy2.robjects.packages as rpkg
+
+from .renv import Renv
+
+
+def library(env_name: str, interactive: bool = True) -> Renv:
+    """Load an R environment (package) into the current session."""
+    try:
+        return Renv(env_name, interactive=interactive)
+    except rpkg.PackageNotInstalledError:
+        if interactive:
+            raise
+        return Renv(None)
+
+
+def importr(env_name: str, interactive: bool = True) -> Renv:
+    """Load an R environment (package) into the current session."""
+    return library(env_name, interactive=interactive)
