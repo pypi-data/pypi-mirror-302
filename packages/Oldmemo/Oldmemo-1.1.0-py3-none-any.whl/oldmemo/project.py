c5a6da35bda7ae960e85aefc7e981aa2ee60d44d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 16:04:00 +0000

    Add package Oldmemo/1.1.0/Oldmemo-1.1.0-py3-none-any.whl

---
+++ packages/Oldmemo/Oldmemo-1.1.0-py3-none-any.whl/oldmemo/project.py
@@ -0,0 +1,15 @@
+__all__ = [ "project" ]
+
+project = {
+    "name"         : "Oldmemo",
+    "description"  : "Backend implementation of the namespace `eu.siacs.conversations.axolotl` for"
+                     " python-omemo.",
+    "url"          : "https://github.com/Syndace/python-oldmemo",
+    "year"         : "2024",
+    "author"       : "Tim Henkes (Syndace)",
+    "author_email" : "me@syndace.dev",
+    "categories"   : [
+        "Topic :: Communications :: Chat",
+        "Topic :: Security :: Cryptography"
+    ]
+}
