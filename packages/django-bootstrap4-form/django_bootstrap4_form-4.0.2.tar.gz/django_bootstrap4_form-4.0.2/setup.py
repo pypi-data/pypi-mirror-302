8a83ddbf8b6dc135357225054510d69ed42ef27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 14:03:02 +0000

    Add package django-bootstrap4-form/4.0.2/django_bootstrap4_form-4.0.2.tar.gz

---
+++ packages/django-bootstrap4-form/django_bootstrap4_form-4.0.2.tar.gz/django_bootstrap4_form-4.0.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from bootstrap4form.meta import VERSION
+
+setup(
+    name='django-bootstrap4-form',
+    version=str(VERSION),
+    description="django-bootstrap4-form",
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Environment :: Web Environment",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    keywords='bootstrap4,django',
+    author='gwasser',
+    author_email='garret@wassiverse.com',
+    url='http://github.com/gwasser/django-bootstrap4-form',
+    license='MIT',
+    test_suite='runtests.runtests',
+    install_requires = [
+        "django>=2.2",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
