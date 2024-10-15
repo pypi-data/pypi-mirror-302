5c4568e221bffa03de4619ceaa994fc3040664a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 15:03:09 +0000

    Add package fps-auth-fief/0.6.4/fps_auth_fief-0.6.4.tar.gz

---
+++ packages/fps-auth-fief/fps_auth_fief-0.6.4.tar.gz/fps_auth_fief-0.6.4/fps_auth_fief/main.py
@@ -0,0 +1,23 @@
+from asphalt.core import Component, Context
+
+from jupyverse_api.app import App
+from jupyverse_api.auth import Auth, AuthConfig
+
+from .config import _AuthFiefConfig
+from .routes import auth_factory
+
+
+class AuthFiefComponent(Component):
+    def __init__(self, **kwargs):
+        self.auth_fief_config = _AuthFiefConfig(**kwargs)
+
+    async def start(
+        self,
+        ctx: Context,
+    ) -> None:
+        ctx.add_resource(self.auth_fief_config, types=AuthConfig)
+
+        app = await ctx.request_resource(App)
+
+        auth_fief = auth_factory(app, self.auth_fief_config)
+        ctx.add_resource(auth_fief, types=Auth)
