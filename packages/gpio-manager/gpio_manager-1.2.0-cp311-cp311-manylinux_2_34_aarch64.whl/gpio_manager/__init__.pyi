7c3108f3cd72ba5050ea542d24ed01b9182dd0db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 14:03:38 +0000

    Add package gpio-manager/1.2.0/gpio_manager-1.2.0-cp311-cp311-manylinux_2_34_aarch64.whl

---
+++ packages/gpio-manager/gpio_manager-1.2.0-cp311-cp311-manylinux_2_34_aarch64.whl/gpio_manager/__init__.pyi
@@ -0,0 +1,148 @@
+class GPIOManager:
+    """GPIOManager provides methods to manage GPIO pins and register callbacks."""
+
+    def __init__(self) -> None:
+        """Initializes a new GPIOManager instance."""
+        ...
+
+    def add_input_pin(self, pin_num: int, pin_state: IPinState = IPinState.NONE) -> None:
+        """
+        Sets up an input pin but does not assign a callback yet.
+
+        :param pin_num: The GPIO pin to configure as input.
+        :param pin_state: The pin state (set it by using gpio_manager.IPinState.[PULLUP, PULLDOWN, or NONE]).
+        """
+        ...
+
+    def assign_callback(self, pin_num: int, trigger_edge: TriggerEdge, callback: Callable, args: Optional[Tuple] = None, debounce_time_ms: int = 2) -> None:
+        """
+        Assigns a callback to an input pin.
+
+        :param pin_num: The GPIO pin.
+        :param trigger_edge: The edge trigger type (set using gpio_manager.TriggerEdge.[RISING, FALLING, BOTH]).
+        :param callback: The callback function to be invoked on pin change.
+        :param args: The arguments to pass to the callback function.
+        :param debounce_time_ms: The debounce time in milliseconds.
+        """
+        ...
+
+    def add_output_pin(self, pin_num: int, pin_state: OPinState = OPinState.LOW, logic_level: LogicLevel = LogicLevel.HIGH) -> None:
+        """
+        Sets up an output pin.
+
+        :param pin_num: The GPIO pin to configure as output.
+        :param pin_state: The initial state of the pin (set it by using gpio_manager.OPinState.[HIGH or LOW]).
+        :param logic_level: The logic level of the pin (set it by using gpio_manager.LogicLevel.[HIGH or LOW]).
+        """
+        ...
+
+    def set_output_pin(self, pin_num: int, pin_state: OPinState) -> None:
+        """
+        Sets the state of an output pin.
+
+        :param pin_num: The GPIO pin.
+        :param pin_state: The desired state (set it by using gpio_manager.OPinState.[HIGH or LOW]).
+        """
+        ...
+
+    def get_pin(self, pin_num: int) -> OPinState:
+        """
+        Polls the current state of an input pin.
+
+        :param pin_num: The GPIO pin to get.
+        :return: The current state of the pin (check it by using gpio_manager.OPinState.[HIGH or LOW]).
+        """
+        ...
+
+    def unassign_callback(self, pin_num: int) -> None:
+        """
+        Unassigns a callback from an input pin.
+
+        :param pin_num: The GPIO pin whose callback is to be reset.
+        """
+        ...
+
+    def wait_for_edge(self, pin_num: int, trigger_edge: TriggerEdge = TriggerEdge.BOTH, timeout_ms: int = -1) -> None:
+        """
+        Waits for an edge on the assigned pin. This function block for the given timeout, or waits forever if it is set to a negative number.
+
+        :param pin_num: The GPIO pin.
+        :param trigger_edge: The trigger type (set using gpio_manager.TriggerEdge.[RISING, FALLING, BOTH]).
+        :param timeout_ms: Timeout in milliseconds.
+        """
+        ...
+
+    def setup_pwm(self, pin_num, frequency_hz = 60, duty_cycle = 0) -> None:
+        """
+        Sets up a PWM signal on the given pin. The pin must be set up as an output pin before calling this function or
+        the default values will be used when creating it (output low and logic high).
+        :param pin_num: The GPIO pin.
+        :param frequency_hz: The period of the pwm signal in hertz.
+        :param duty_cycle: The pulse width of the pwm signal as a percentage of the frequency.
+        """
+        ...
+
+    def set_pwm_duty_cycle(self, pin_num, duty_cycle) -> None:
+        """
+        Sets the PWM signal's duty cycle.
+        :param pin_num: The GPIO pin.
+        :param duty_cycle: The pulse width of the pwm signal as a percentage of the frequency.
+        """
+        ...
+
+    def set_pwm_frequency(self, pin_num, frequency_hz) -> None:
+        """
+        Sets the PWM signal's frequency.
+        :param pin_num: The GPIO pin.
+        :param frequency_hz: The period of the pwm signal in hertz.
+        """
+        ...
+
+    def start_pwm(self, pin_num) -> None:
+        """
+        Starts the PWM signal.
+        :param pin_num: The GPIO pin.
+        """
+        ...
+
+    def stop_pwm(self, pin_num) -> None:
+        """
+        Stops the PWM signal.
+        :param pin_num: The GPIO pin.
+        """
+        ...
+
+    def reset_pin(self, pin_num: int) -> None:
+        """
+        Resets the given pin so it can set to either input or output.
+        :param pin_num: The GPIO pin.
+        """
+    ...
+
+    def cleanup(self) -> None:
+        """
+        Cleans up the GPIO pins by setting all output pins to low and clearing all interrupts.
+        """
+        ...
+
+class IPinState:
+    """Enum representing the GPIO pin state types for input pins."""
+    PULLUP: 'IPinState'
+    PULLDOWN: 'IPinState'
+    NONE: 'IPinState'
+
+class OPinState:
+    """Enum representing the GPIO pin state types for output pins."""
+    HIGH: 'OPinState'
+    LOW: 'OPinState'
+
+class LogicLevel:
+    """Enum representing the logic levels of output pins."""
+    HIGH: 'LogicLevel'
+    LOW: 'LogicLevel'
+
+class TriggerEdge:
+    """Enum representing the trigger edge types."""
+    RISING: 'TriggerEdge'
+    FALLING: 'TriggerEdge'
+    BOTH: 'TriggerEdge'
