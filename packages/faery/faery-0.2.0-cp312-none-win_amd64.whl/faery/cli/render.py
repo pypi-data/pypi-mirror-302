cb97e0d259c718182f6e3b41280571ceaf7a4909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 03:08:47 +0000

    Add package faery/0.2.0/faery-0.2.0-cp312-none-win_amd64.whl

---
+++ packages/faery/faery-0.2.0-cp312-none-win_amd64.whl/faery/cli/render.py
@@ -0,0 +1,11 @@
+import argparse
+
+
+def add_to_subparsers(subparsers: argparse._SubParsersAction):
+    parser: argparse.ArgumentParser = subparsers.add_parser(
+        "render", help="generate a video or frames from an event file"
+    )
+
+
+def run(args: argparse.Namespace):
+    pass
