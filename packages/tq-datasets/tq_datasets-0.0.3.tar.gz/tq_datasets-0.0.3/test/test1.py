521763409a7ef8316f0946c9fe1468d7a82a64c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 16:04:02 +0000

    Add package tq-datasets/0.0.3/tq_datasets-0.0.3.tar.gz

---
+++ packages/tq-datasets/tq_datasets-0.0.3.tar.gz/tq_datasets-0.0.3/test/test1.py
@@ -0,0 +1,49 @@
+from typing import Optional, Any
+
+from tq_datasets.dataset_op_interface import TraverseDatasetItems
+from tq_utils import my_json, time_util
+
+
+def test_default_method():
+    print()
+    t = TraverseDatasetItems()
+    t.traverse_dataset(r'E:\Workspace\Pycharm\Datasets\data\Dataset-DOWL2\DOWL2\eng\al-monitor',
+                       'json')
+
+
+class TraverseDOWLItems(TraverseDatasetItems):
+    @staticmethod
+    def item_reader(file_abs_path: str) -> str:
+        print('oaushoiasdoiajsodiasoidowifoqn')
+        return my_json.load_json_safe(file_abs_path)
+
+    @staticmethod
+    def item_processor(**kwargs) -> Optional[Any]:
+        print('asdasda')
+        return kwargs['raw_item_data']
+
+
+def test_override_methods():
+    print()
+    t = TraverseDOWLItems()
+    with time_util.Timer('aa'):
+        r = t.traverse_dataset(r'E:\Workspace\Pycharm\Datasets\data\Dataset-DOWL2\DOWL2\eng',
+                               'json', external_raw_data={'s': 1})
+        print(len(r))
+    """
+    3174
+    Elapsed time: 2.8971 seconds
+    """
+
+
+def test_override_methods_multithread():
+    print()
+    t = TraverseDOWLItems()
+    with time_util.Timer('aa'):
+        r = t.traverse_dataset(r'E:\Workspace\Pycharm\Datasets\data\Dataset-DOWL2\DOWL2\eng',
+                               'json', external_raw_data={'s': 1}, multi_process=True)
+        print(len(r))
+    """
+    3174
+    Elapsed time: 0.9121 seconds
+    """
