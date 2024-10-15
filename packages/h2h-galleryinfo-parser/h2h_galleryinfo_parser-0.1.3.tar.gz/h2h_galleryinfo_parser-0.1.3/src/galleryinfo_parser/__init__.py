0b1b27dbc9d9aaa1f3bf808cf7c874c5a4516035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 15:03:20 +0000

    Add package h2h-galleryinfo-parser/0.1.3/h2h_galleryinfo_parser-0.1.3.tar.gz

---
+++ packages/h2h-galleryinfo-parser/h2h_galleryinfo_parser-0.1.3.tar.gz/h2h_galleryinfo_parser-0.1.3/src/galleryinfo_parser/__init__.py
@@ -0,0 +1,10 @@
+__all__ = [
+    "__version__",
+    "__version_info__",
+    "parse_gid",
+    "parse_galleryinfo",
+    "GalleryInfoParser",
+]
+
+
+from .galleryinfo_parser import parse_gid, parse_galleryinfo, GalleryInfoParser
