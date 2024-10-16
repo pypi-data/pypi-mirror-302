dfffc978d915e73a0074781e1b2d28c77fb2fd63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 04:04:17 +0000

    Add package ask-weather/0.1.5/ask_weather-0.1.5.tar.gz

---
+++ packages/ask-weather/ask_weather-0.1.5.tar.gz/ask_weather-0.1.5/ask_weather/utils/logger.py
@@ -0,0 +1,9 @@
+import logging
+
+
+def get_logger(name=__name__):
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
+    return logging.getLogger(name)
