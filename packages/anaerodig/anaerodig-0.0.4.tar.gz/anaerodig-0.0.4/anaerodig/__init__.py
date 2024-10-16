0cd3f26d1cc0a50b02c82dd87aef5adeccf61750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 03:10:22 +0000

    Add package anaerodig/0.0.4/anaerodig-0.0.4.tar.gz

---
+++ packages/anaerodig/anaerodig-0.0.4.tar.gz/anaerodig-0.0.4/anaerodig/__init__.py
@@ -0,0 +1,17 @@
+"""Anaerobic Digestion Models in Python
+
+The module contains submodules 'pyadm1', 'pyam2'.
+
+
+---------- Rules ----------
+
+--- I/O Rule ---
+
+- Custom classes which are not intermediary should have a "save" method and a "load" classmethod.
+The save function should output the name of the file (or folder) on which the data is
+saved.
+
+- As much as possible, the files should be human readable (e.g. use csv, json, yml files).
+For objects which can not be saved in human readable format (e.g. functions), use package
+dill as much as possible with extension '.dl'
+"""
---
+++ packages/anaerodig/anaerodig-0.0.4.tar.gz/anaerodig-0.0.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    # Make sure we strip all comments and options (e.g "--extra-index-url")
+    # that arise from a modified pip.conf file that configure global options
+    requires = []
+    for line in f:
+        req = line.split("#", 1)[0].strip()
+        if req and not req.startswith("--"):
+            requires.append(req)
+
+setup(
+    name="anaerodig",
+    version="0.0.4",
+    author="Antoine Picard-Weibel",
+    author_email="apicard.w@gmail.com",
+    description="Anaerobic Digestion models, in python",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "scipy>=1.7.0",
+        "numpy<=1.26",
+        "multiprocess>=0.70",
+        "matplotlib>=3.7.0",
+    ],
+    python_requires=">=3.9",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
