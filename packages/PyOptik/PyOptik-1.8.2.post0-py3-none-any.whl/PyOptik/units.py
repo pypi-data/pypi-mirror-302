4784ed7ca7b8116e65ce250bfbfa59755acbb7c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 14:04:36 +0000

    Add package PyOptik/1.8.2.post0/PyOptik-1.8.2.post0-py3-none-any.whl

---
+++ packages/PyOptik/PyOptik-1.8.2.post0-py3-none-any.whl/PyOptik/units.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pint import UnitRegistry
+ureg = UnitRegistry()
+Quantity = ureg.Quantity
+micrometer = ureg.micrometer
+meter = ureg.meter
+nanometer = ureg.nanometer
