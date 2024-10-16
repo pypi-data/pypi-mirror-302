bec1b9c6c9bd422b1cef745479ace1b80ddd148a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 05:04:45 +0000

    Add package automators-agent/0.1.0/automators_agent-0.1.0.tar.gz

---
+++ packages/automators-agent/automators_agent-0.1.0.tar.gz/automators_agent-0.1.0/README.md
@@ -0,0 +1,42 @@
+# Automators Agent
+
+
+## Installation
+
+Clone the repository:
+
+```bash
+git clone https://github.com/automators-com/agent.git
+cd agent
+```
+
+Install the dependencies:
+
+```bash
+uv sync --all-extras --dev
+```
+
+Install playwright:
+    
+```bash
+playwright install chromium
+```
+
+## Environment Variables
+
+Create a `.env` file in the root of the project with the following content:
+
+```bash
+OPENAI_API_KEY=""
+OPENAI_MODEL="gpt-4o"
+HEADLESS=true
+LOG_LEVEL="INFO"
+```
+
+## Usage
+
+Run the agent:
+
+```bash
+uv run agent
+```
---
+++ packages/automators-agent/automators_agent-0.1.0.tar.gz/automators_agent-0.1.0/src/agent/config.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+from agent.logging import logger
+
+# create a folder to store the generated code
+TEST_DIR = Path("tests")
+
+
+def setup():
+    if not TEST_DIR.exists():
+        TEST_DIR.mkdir()
+        # initialise the folder as a git repo
+        os.system(f"cd {TEST_DIR} && git init")
+
+    # install playwright browsers
+    os.popen("playwright install")
+
+
+def clean_dir():
+    """Clean the tests directory"""
+    logger.info(f"Deleting files in the {TEST_DIR} directory")
+    os.popen(f"rm -rf {TEST_DIR}/*").read()
