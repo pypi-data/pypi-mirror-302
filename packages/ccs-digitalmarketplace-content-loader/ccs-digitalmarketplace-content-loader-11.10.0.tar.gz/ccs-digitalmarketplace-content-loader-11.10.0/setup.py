6e78d6ecf773900a1566b40153598f07f5506aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 17:04:16 +0000

    Add package ccs-digitalmarketplace-content-loader/11.10.0/ccs-digitalmarketplace-content-loader-11.10.0.tar.gz

---
+++ packages/ccs-digitalmarketplace-content-loader/ccs-digitalmarketplace-content-loader-11.10.0.tar.gz/ccs-digitalmarketplace-content-loader-11.10.0/setup.py
@@ -0,0 +1,31 @@
+import re
+import ast
+from setuptools import setup, find_packages
+
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('dmcontent/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+setup(
+    name='ccs-digitalmarketplace-content-loader',
+    version=version,
+    url='https://github.com/Brickendon-DMp1-5/digitalmarketplace-content-loader.git',
+    license='MIT',
+    author='GDS Developers',
+    description='Digital Marketplace Content Loader',
+    long_description=__doc__,
+    packages=find_packages(),
+    package_data={'dmcontent': ['py.typed']},
+    include_package_data=True,
+    install_requires=[
+        'Flask>=3.0,<3.1',
+        'Markdown<4.0.0,>=3.0.0',
+        'PyYAML>=5.1.2,<7.0',
+        'inflection<1.0.0,>=0.3.1',
+        'ccs-digitalmarketplace-utils>=67.0.0'
+    ],
+    python_requires=">=3.10,<3.13",
+)
