c91bcf8026f413122994b4f1a8b46db7791008a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 14:04:52 +0000

    Add package eplremade2/1.0.0/eplremade2-1.0.0.tar.gz

---
+++ packages/eplremade2/eplremade2-1.0.0.tar.gz/eplremade2-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eplremade2',
+    version='1.0.0',
+    author='Silicon Yang',
+    author_email='yangsilicon@gmail.com',
+    description='Every Python Library Remade, a project to improve base Python modules and libraries.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Silicon27/EPLRemade',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
