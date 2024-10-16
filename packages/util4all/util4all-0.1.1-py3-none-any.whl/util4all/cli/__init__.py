f6aa199d1310440f4b40b6dfa6c59082366d0d12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 06:04:26 +0000

    Add package util4all/0.1.1/util4all-0.1.1-py3-none-any.whl

---
+++ packages/util4all/util4all-0.1.1-py3-none-any.whl/util4all/cli/__init__.py
@@ -0,0 +1,6 @@
+from .log_utils import log_info, log_success, log_critical, log_debug, log_error, log_warning, setup_logging
+
+
+__all__ = [
+    "log_info", "log_success", "log_critical", "log_debug", "log_error", "log_warning", "setup_logging"
+]
