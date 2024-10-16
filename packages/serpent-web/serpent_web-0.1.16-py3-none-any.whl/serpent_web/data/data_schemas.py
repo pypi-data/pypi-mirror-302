f0d4df93c3ab28c661a6315ba8333b8ae8b24b6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 05:06:02 +0000

    Add package serpent-web/0.1.16/serpent_web-0.1.16-py3-none-any.whl

---
+++ packages/serpent-web/serpent_web-0.1.16-py3-none-any.whl/serpent_web/data/data_schemas.py
@@ -0,0 +1,14 @@
+from typing import TypeVar, Generic, Optional
+
+from pydantic import BaseModel
+
+TModel = TypeVar('TModel')
+
+
+class PaginatedList(BaseModel, Generic[TModel]):
+    total: int
+    skip: int
+    limit: int
+    data: list[TModel]
+    next: Optional[int]
+    previous: Optional[int]
