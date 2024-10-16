e4f73b5df69e06b540c9a110cc368ee04c47ebb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 05:04:48 +0000

    Add package AgentMix/1.0/agentmix-1.0.tar.gz

---
+++ packages/AgentMix/agentmix-1.0.tar.gz/agentmix-1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AgentMix',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'httpx',
+    'asyncio',
+    'colorama',
+    'beautifulsoup4',# List any dependencies here, e.g., 'numpy', 'pandas'
+    ],
+    entry_points={
+        'console_scripts': [
+            'agentmix=AgentMix.main:main',  # Adjust 'main' if your entry function is different
+        ],
+    },
+)
