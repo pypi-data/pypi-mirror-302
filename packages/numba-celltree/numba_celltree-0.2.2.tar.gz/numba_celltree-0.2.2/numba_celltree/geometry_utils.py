87bb67bb3cb066701702a88cb205859ea8e6f14a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 04:04:08 +0000

    Add package numba-celltree/0.2.2/numba_celltree-0.2.2.tar.gz

---
+++ packages/numba-celltree/numba_celltree-0.2.2.tar.gz/numba_celltree-0.2.2/numba_celltree/geometry_utils.py
@@ -0,0 +1,332 @@
+from typing import Sequence, Tuple
+
+import numba as nb
+import numpy as np
+
+from numba_celltree.constants import (
+    FILL_VALUE,
+    NDIM,
+    PARALLEL,
+    TOLERANCE_ON_EDGE,
+    Box,
+    FloatArray,
+    FloatDType,
+    IntArray,
+    Point,
+    Triangle,
+    Vector,
+)
+from numba_celltree.utils import allocate_box_polygon, allocate_polygon
+
+
+@nb.njit(inline="always")
+def to_vector(a: Point, b: Point) -> Vector:
+    return Vector(b.x - a.x, b.y - a.y)
+
+
+@nb.njit(inline="always")
+def as_point(a: FloatArray) -> Point:
+    return Point(a[0], a[1])
+
+
+@nb.njit(inline="always")
+def as_box(arr: FloatArray) -> Box:
+    return Box(
+        arr[0],
+        arr[1],
+        arr[2],
+        arr[3],
+    )
+
+
+@nb.njit(inline="always")
+def as_triangle(vertices: FloatArray, face: IntArray) -> Triangle:
+    return Triangle(
+        as_point(vertices[face[0]]),
+        as_point(vertices[face[1]]),
+        as_point(vertices[face[2]]),
+    )
+
+
+@nb.njit(inline="always")
+def to_point(t: float, a: Point, V: Vector) -> Point:
+    return Point(a.x + t * V.x, a.y + t * V.y)
+
+
+@nb.njit(inline="always")
+def cross_product(u: Vector, v: Vector) -> float:
+    return u.x * v.y - u.y * v.x
+
+
+@nb.njit(inline="always")
+def dot_product(u: Vector, v: Vector) -> float:
+    return u.x * v.x + u.y * v.y
+
+
+@nb.njit(inline="always")
+def polygon_length(face: IntArray) -> int:
+    # A minimal polygon is a triangle
+    n = len(face)
+    for i in range(3, n):
+        if face[i] == FILL_VALUE:
+            return i
+    return n
+
+
+@nb.njit(inline="always")
+def polygon_area(polygon: Sequence) -> float:
+    length = len(polygon)
+    area = 0.0
+    a = Point(polygon[0][0], polygon[0][1])
+    b = Point(polygon[1][0], polygon[1][1])
+    U = to_vector(a, b)
+    for i in range(2, length):
+        c = Point(polygon[i][0], polygon[i][1])
+        V = to_vector(c, a)
+        area += abs(cross_product(U, V))
+        b = c
+        U = V
+    return 0.5 * area
+
+
+@nb.njit(inline="always")
+def point_in_polygon(p: Point, poly: Sequence) -> bool:
+    # Refer to: https://wrf.ecse.rpi.edu/Research/Short_Notes/pnpoly.html
+    # Copyright (c) 1970-2003, Wm. Randolph Franklin
+    # MIT license.
+    #
+    # Quote:
+    # > I run a semi-infinite ray horizontally (increasing x, fixed y) out from
+    # > the test point, and count how many edges it crosses. At each crossing,
+    # > the ray switches between inside and outside. This is called the Jordan
+    # > curve theorem.
+    # >
+    # > The case of the ray going thru a vertex is handled correctly via a
+    # > careful selection of inequalities. Don't mess with this code unless
+    # > you're familiar with the idea of Simulation of Simplicity. This pretends
+    # > to shift the ray infinitesimally down so that it either clearly
+    # > intersects, or clearly doesn't touch. Since this is merely a conceptual,
+    # > infinitesimal, shift, it never creates an intersection that didn't exist
+    # > before, and never destroys an intersection that clearly existed before.
+    # >
+    # > The ray is tested against each edge thus:
+    # > 1. Is the point in the half-plane to the left of the extended edge? and
+    # > 2. Is the point's Y coordinate within the edge's Y-range?
+    # >
+    # > Handling endpoints here is tricky.
+    #
+    # For the Simulation of Simplicity concept, see:
+    # Edelsbrunner, H., & Mücke, E. P. (1990). Simulation of simplicity: a
+    # technique to cope with degenerate cases in geometric algorithms. ACM
+    # Transactions on Graphics (tog), 9(1), 66-104.
+    #
+    # In this case, this guarantees there will be no "on-edge" answers, which
+    # are degenerative. For another application of simulation of simplicity,
+    # see:
+    # Rappoport, A. (1991). An efficient algorithm for line and polygon
+    # clipping. The Visual Computer, 7(1), 19-28.
+    length = len(poly)
+    v0 = as_point(poly[-1])
+    c = False
+    for i in range(length):
+        v1 = as_point(poly[i])
+        # Do not split this in two conditionals: if the first conditional fails,
+        # the second will not be executed in Python's (and C's) execution model.
+        # This matters because the second can result in division by zero.
+        if (v0.y > p.y) != (v1.y > p.y) and p.x < (
+            (v1.x - v0.x) * (p.y - v0.y) / (v1.y - v0.y) + v0.x
+        ):
+            c = not c
+        v0 = v1
+    return c
+
+
+@nb.njit(inline="always")
+def in_bounds(p: Point, a: Point, b: Point) -> bool:
+    """
+    Check whether point p falls within the bounding box created by a and b
+    (after we've checked the size of the cross product).
+
+    However, we must take into account that a line may be either vertical
+    (dx=0) or horizontal (dy=0) and only evaluate the non-zero value.
+
+    If the area created by p, a, b is tiny AND p is within the bounds of a and
+    b, the point lies very close to the edge.
+    """
+    dx = b.x - a.x
+    dy = b.y - a.y
+    if abs(dx) >= abs(dy):
+        if dx > 0:
+            return a.x <= p.x and p.x <= b.x
+        return b.x <= p.x and p.x <= a.x
+    else:
+        if dy > 0:
+            return a.y <= p.y and p.y <= b.y
+        return b.y <= p.y and p.y <= a.y
+
+
+@nb.njit(inline="always")
+def point_in_polygon_or_on_edge(p: Point, poly: FloatArray) -> bool:
+    length = len(poly)
+    v0 = as_point(poly[-1])
+    U = to_vector(p, v0)
+    c = False
+    for i in range(length):
+        v1 = as_point(poly[i])
+        if v1 == v0:
+            continue
+        V = to_vector(p, v1)
+        # Compute the (twofold) area of formed by the point (p) and two
+        # vertices of the polygon (v0, v1). If this area is extremely small,
+        # the point is (nearly) on the edge, or it is collinear. We can test if
+        # if's collinear by checking whether it falls in the bounding box of
+        # points v0 and v1.
+        if (abs(cross_product(U, V)) < TOLERANCE_ON_EDGE) and in_bounds(p, v0, v1):
+            return True
+
+        if (v0.y > p.y) != (v1.y > p.y) and p.x < (
+            (v1.x - v0.x) * (p.y - v0.y) / (v1.y - v0.y) + v0.x
+        ):
+            c = not c
+
+        v0 = v1
+        U = V
+    return c
+
+
+@nb.njit(inline="always")
+def boxes_intersect(a: Box, b: Box) -> bool:
+    """
+    Parameters
+    ----------
+    a: (xmin, xmax, ymin, ymax)
+    b: (xmin, xmax, ymin, ymax)
+    """
+    return a.xmin < b.xmax and b.xmin < a.xmax and a.ymin < b.ymax and b.ymin < a.ymax
+
+
+@nb.njit(inline="always")
+def box_contained(a: Box, b: Box) -> bool:
+    """
+    Whether a is contained by b.
+
+    Parameters
+    ----------
+    a: (xmin, xmax, ymin, ymax)
+    b: (xmin, xmax, ymin, ymax)
+    """
+    return (
+        a.xmin >= b.xmin and a.xmax <= b.xmax and a.ymin >= b.ymin and a.ymax <= b.ymax
+    )
+
+
+@nb.njit(inline="always")
+def bounding_box(
+    polygon: IntArray, vertices: FloatArray
+) -> Tuple[float, float, float, float]:
+    max_n_verts = len(polygon)
+    first_vertex = vertices[polygon[0]]
+    xmin = xmax = first_vertex[0]
+    ymin = ymax = first_vertex[1]
+    for i in range(1, max_n_verts):
+        index = polygon[i]
+        if index == FILL_VALUE:
+            break
+        vertex = vertices[index]
+        x = vertex[0]
+        y = vertex[1]
+        xmin = min(xmin, x)
+        xmax = max(xmax, x)
+        ymin = min(ymin, y)
+        ymax = max(ymax, y)
+    return (xmin, xmax, ymin, ymax)
+
+
+@nb.njit(cache=True)
+def build_bboxes(
+    faces: IntArray,
+    vertices: FloatArray,
+) -> FloatArray:
+    # Make room for the bounding box of every polygon.
+    n_polys = len(faces)
+    bbox_coords = np.empty((n_polys, NDIM * 2), FloatDType)
+
+    for i in nb.prange(n_polys):  # pylint: disable=not-an-iterable
+        polygon = faces[i]
+        bbox_coords[i] = bounding_box(polygon, vertices)
+
+    return bbox_coords
+
+
+@nb.njit(inline="always")
+def copy_vertices(vertices: FloatArray, face: IntArray) -> FloatArray:
+    length = polygon_length(face)
+    out = allocate_polygon()
+    for i in range(length):
+        v = vertices[face[i]]
+        out[i, 0] = v[0]
+        out[i, 1] = v[1]
+    return out[:length]
+
+
+@nb.njit(inline="always")
+def copy_vertices_into(
+    vertices: FloatArray, face: IntArray, out: FloatArray
+) -> FloatArray:
+    length = polygon_length(face)
+    for i in range(length):
+        v = vertices[face[i]]
+        out[i, 0] = v[0]
+        out[i, 1] = v[1]
+    return out[:length]
+
+
+@nb.njit(inline="always")
+def copy_box_vertices(box: Box) -> FloatArray:
+    a = allocate_box_polygon()
+    a[0, 0] = box.xmin
+    a[0, 1] = box.ymin
+    a[1, 0] = box.xmax
+    a[1, 1] = box.ymin
+    a[2, 0] = box.xmax
+    a[2, 1] = box.ymax
+    a[3, 0] = box.xmin
+    a[3, 1] = box.ymax
+    return a
+
+
+@nb.njit(inline="always")
+def point_inside_box(a: Point, box: Box):
+    return box.xmin < a.x and a.x < box.xmax and box.ymin < a.y and a.y < box.ymax
+
+
+@nb.njit(inline="always")
+def flip(face: IntArray, length: int) -> None:
+    end = length - 1
+    for i in range(int(length / 2)):
+        j = end - i
+        face[i], face[j] = face[j], face[i]
+    return
+
+
+@nb.njit(parallel=PARALLEL, cache=True)
+def counter_clockwise(vertices: FloatArray, faces: IntArray) -> None:
+    n_face = len(faces)
+    for i_face in nb.prange(n_face):
+        face = faces[i_face]
+        length = polygon_length(face)
+        a = as_point(vertices[face[length - 2]])
+        b = as_point(vertices[face[length - 1]])
+        for i in range(length):
+            c = as_point(vertices[face[i]])
+            u = to_vector(a, b)
+            v = to_vector(a, c)
+            product = cross_product(u, v)
+            if product == 0:
+                a = b
+                b = c
+            elif product < 0:
+                flip(face, length)
+            else:
+                break
+    return
---
+++ packages/numba-celltree/numba_celltree-0.2.2.tar.gz/numba_celltree-0.2.2/tests/test_geometry_utils.py
@@ -0,0 +1,329 @@
+import os
+
+import numba as nb
+import numpy as np
+
+from numba_celltree import geometry_utils as gu
+from numba_celltree.constants import Box, Point, Triangle, Vector
+
+
+def test_to_vector():
+    a = Point(0.0, 0.0)
+    b = Point(1.0, 2.0)
+    actual = gu.to_vector(a, b)
+    assert isinstance(actual, Vector)
+    assert actual.x == 1.0
+    assert actual.y == 2.0
+
+
+def test_as_point():
+    a = np.array([0.0, 1.0])
+    actual = gu.as_point(a)
+    assert isinstance(actual, Point)
+    assert actual.x == 0.0
+    assert actual.y == 1.0
+
+
+def test_as_box():
+    a = np.array([0.0, 1.0, 2.0, 3.0])
+    actual = gu.as_box(a)
+    assert isinstance(actual, Box)
+    assert actual.xmin == 0.0
+    assert actual.xmax == 1.0
+    assert actual.ymin == 2.0
+    assert actual.ymax == 3.0
+
+
+def test_as_triangle():
+    vertices = np.array(
+        [
+            [0.0, 0.0],
+            [1.0, 0.0],
+            [1.0, 1.0],
+        ]
+    )
+    face = np.array([2, 0, 1])
+    actual = gu.as_triangle(vertices, face)
+    assert isinstance(actual, Triangle)
+    assert actual.a == Point(1.0, 1.0)
+    assert actual.b == Point(0.0, 0.0)
+    assert actual.c == Point(1.0, 0.0)
+
+
+def test_to_point():
+    a = Point(0.0, 0.0)
+    b = Point(1.0, 2.0)
+    V = gu.to_vector(a, b)
+    t = 0.0
+    actual = gu.to_point(t, a, V)
+    assert np.allclose(actual, a)
+
+    t = 1.0
+    actual = gu.to_point(t, a, V)
+    assert np.allclose(actual, b)
+
+    t = 0.5
+    actual = gu.to_point(t, a, V)
+    assert np.allclose(actual, Point(0.5, 1.0))
+
+
+def test_cross_product():
+    u = Vector(1.0, 2.0)
+    v = Vector(3.0, 4.0)
+    assert np.allclose(gu.cross_product(u, v), u.x * v.y - u.y * v.x)
+
+
+def test_dot_product():
+    u = Vector(1.0, 2.0)
+    v = Vector(3.0, 4.0)
+    assert np.allclose(gu.dot_product(u, v), np.dot(u, v))
+
+
+def test_polygon_length():
+    face = np.array([0, 1, 2])
+    assert gu.polygon_length(face) == 3
+    assert gu.polygon_length(face) == 3
+    face = np.array([0, 1, 2, -1, -1])
+    assert gu.polygon_length(face) == 3
+    face = np.array([0, 1, 2, 3, -1])
+    assert gu.polygon_length(face) == 4
+
+
+def test_polygon_area():
+    # square
+    p = np.array(
+        [
+            [0.0, 0.0],
+            [1.0, 0.0],
+            [1.0, 1.0],
+            [0.0, 1.0],
+        ]
+    )
+    assert np.allclose(gu.polygon_area(p), 1.0)
+    # triangle
+    p = np.array(
+        [
+            [0.0, 0.0],
+            [1.0, 0.0],
+            [1.0, 1.0],
+        ]
+    )
+    assert np.allclose(gu.polygon_area(p), 0.5)
+    # pentagon, counter-clockwise
+    p = np.array(
+        [
+            [0.0, 0.0],
+            [1.0, 0.0],
+            [1.0, 1.0],
+            [0.5, 2.0],
+            [0.0, 1.0],
+        ]
+    )
+    assert np.allclose(gu.polygon_area(p), 1.5)
+    # clockwise
+    assert np.allclose(gu.polygon_area(p[::-1]), 1.5)
+
+
+def test_point_in_polygon():
+    poly = np.array(
+        [
+            [0.0, 0.0],
+            [1.0, 0.0],
+            [1.0, 1.0],
+        ]
+    )
+    assert gu.point_in_polygon(Point(0.5, 0.25), poly)
+    assert not gu.point_in_polygon(Point(1.5, 0.25), poly)
+
+    assert gu.point_in_polygon(Point(0.0, 0.0), poly)
+    assert gu.point_in_polygon(Point(0.0, 0.0), poly[::-1])
+    assert gu.point_in_polygon(Point(0.5, 0.5), poly)
+    assert gu.point_in_polygon(Point(0.5, 0.5), poly[::-1])
+    assert not gu.point_in_polygon(Point(1.0, 1.0), poly)
+    assert not gu.point_in_polygon(Point(1.0, 1.0), poly[::-1])
+
+
+def test_boxes_intersect():
+    # Identity
+    a = Box(0.0, 1.0, 0.0, 1.0)
+    b = a
+    assert gu.boxes_intersect(a, b)
+    assert gu.boxes_intersect(b, a)
+    # Overlap
+    b = Box(0.5, 1.5, 0.0, 1.0)
+    assert gu.boxes_intersect(a, b)
+    assert gu.boxes_intersect(b, a)
+    # No overlap
+    b = Box(1.5, 2.5, 0.5, 1.0)
+    assert not gu.boxes_intersect(a, b)
+    assert not gu.boxes_intersect(b, a)
+    # Different identity
+    b = a
+    assert gu.boxes_intersect(a, b)
+    assert gu.boxes_intersect(b, a)
+    # Inside
+    a = Box(0.0, 1.0, 0.0, 1.0)
+    b = Box(0.25, 0.75, 0.25, 0.75)
+    assert gu.boxes_intersect(a, b)
+    assert gu.boxes_intersect(b, a)
+
+
+def test_box_contained():
+    a = Box(0.0, 1.0, 0.0, 1.0)
+    b = Box(0.25, 0.75, 0.25, 0.75)
+    assert gu.box_contained(a, a)
+    assert gu.box_contained(b, a)
+    assert not gu.box_contained(a, b)
+
+
+def test_bounding_box():
+    face = np.array([0, 1, 2])
+    vertices = np.array(
+        [
+            [0.0, 1.0],
+            [1.0, 0.0],
+            [1.0, 1.0],
+        ]
+    )
+    assert gu.bounding_box(face, vertices) == (0.0, 1.0, 0.0, 1.0)
+    face = np.array([0, 1, 2, -1, -1])
+    assert gu.bounding_box(face, vertices) == (0.0, 1.0, 0.0, 1.0)
+
+
+def test_build_bboxes():
+    faces = np.array(
+        [
+            [0, 1, 2, -1],
+            [0, 1, 2, 3],
+        ]
+    )
+    vertices = np.array(
+        [
+            [0.0, 5.0],
+            [5.0, 0.0],
+            [5.0, 5.0],
+            [0.0, 5.0],
+        ]
+    )
+    expected = np.array(
+        [
+            [0.0, 5.0, 0.0, 5.0],
+            [0.0, 5.0, 0.0, 5.0],
+        ]
+    )
+    actual = gu.build_bboxes(faces, vertices)
+    assert np.array_equal(actual, expected)
+
+
+def test_copy_vertices():
+    """
+    This has to be tested inside of numba jitted function, because the vertices
+    are copied to a stack allocated array. This array is not returned properly
+    to dynamic python. This is OK: these arrays are exclusively for internal
+    use to temporarily store values.
+    """
+    if os.environ.get("NUMBA_DISABLE_JIT", "0") == "0":
+
+        @nb.njit()
+        def test():
+            face = np.array([0, 1, 2, -1, -1])
+            vertices = np.array(
+                [
+                    [0.0, 1.0],
+                    [1.0, 0.0],
+                    [1.0, 1.0],
+                ]
+            )
+            expected = vertices.copy()
+            actual = gu.copy_vertices(vertices, face)
+            result = True
+            for i in range(3):
+                result = result and actual[i, 0] == expected[i, 0]
+                result = result and actual[i, 1] == expected[i, 1]
+            return result
+
+        assert test()
+
+    else:
+        face = np.array([0, 1, 2, -1, -1])
+        vertices = np.array(
+            [
+                [0.0, 1.0],
+                [1.0, 0.0],
+                [1.0, 1.0],
+            ]
+        )
+        expected = vertices.copy()
+        actual = gu.copy_vertices(vertices, face)
+        assert np.array_equal(actual, expected)
+        assert len(actual) == 3
+
+
+def test_copy_vertices_into():
+    out = np.empty((10, 2))
+    face = np.array([0, 1, 2, -1, -1])
+    vertices = np.array(
+        [
+            [0.0, 1.0],
+            [1.0, 0.0],
+            [1.0, 1.0],
+        ]
+    )
+    expected = vertices.copy()
+    actual = gu.copy_vertices_into(vertices, face, out)
+    assert np.array_equal(actual, expected)
+    assert len(actual) == 3
+
+
+def test_point_inside_box():
+    box = Box(0.0, 1.0, 0.0, 1.0)
+    a = Point(0.5, 0.5)
+    assert gu.point_inside_box(a, box)
+    a = Point(-0.5, 0.5)
+    assert not gu.point_inside_box(a, box)
+    a = Point(0.5, -0.5)
+    assert not gu.point_inside_box(a, box)
+
+
+def test_flip():
+    face0 = np.array([0, 1, 2, -1, -1])
+    face1 = np.array([0, 1, 2, 3, -1])
+    face2 = np.array([0, 1, 2, 3, 4])
+    gu.flip(face0, 3)
+    gu.flip(face1, 4)
+    gu.flip(face2, 5)
+    assert np.array_equal(face0, [2, 1, 0, -1, -1])
+    assert np.array_equal(face1, [3, 2, 1, 0, -1])
+    assert np.array_equal(face2, [4, 3, 2, 1, 0])
+
+
+def test_counter_clockwise():
+    vertices = np.array(
+        [
+            [0.0, 0.0],
+            [0.5, 0.0],  # hanging node
+            [1.0, 0.0],
+            [1.0, 0.5],  # hanging node
+            [1.0, 1.0],
+            [0.0, 1.0],
+        ]
+    )
+    ccw_faces = np.array(
+        [
+            [0, 2, 4, 5, -1, -1],
+            [0, 1, 2, 3, 4, 5],
+        ]
+    )
+    cw_faces = np.array(
+        [
+            [5, 4, 2, 0, -1, -1],
+            [5, 4, 3, 2, 1, 0],
+        ]
+    )
+    expected = ccw_faces.copy()
+    # already counter clockwise should not be mutated
+    gu.counter_clockwise(vertices, ccw_faces)
+    assert np.array_equal(expected, ccw_faces)
+    # clockwise should be mutated
+    gu.counter_clockwise(vertices, cw_faces)
+    assert np.array_equal(expected, cw_faces)
