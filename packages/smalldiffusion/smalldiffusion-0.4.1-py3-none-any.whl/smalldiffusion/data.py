359eb64461e2d94969ad84298ae1ddf011c60daa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 14:05:06 +0000

    Add package smalldiffusion/0.4.1/smalldiffusion-0.4.1-py3-none-any.whl

---
+++ packages/smalldiffusion/smalldiffusion-0.4.1-py3-none-any.whl/smalldiffusion/data.py
@@ -0,0 +1,40 @@
+import torch
+import csv
+from torch.utils.data import Dataset
+
+class Swissroll(Dataset):
+    def __init__(self, tmin, tmax, N):
+        t = tmin + torch.linspace(0, 1, N) * tmax
+        self.vals = torch.stack([t*torch.cos(t)/tmax, t*torch.sin(t)/tmax]).T
+
+    def __len__(self):
+        return len(self.vals)
+
+    def __getitem__(self, i):
+        return self.vals[i]
+
+class DatasaurusDozen(Dataset):
+    def __init__(self, csv_file, dataset, enlarge_factor=15, delimiter='\t', scale=50, offset=50):
+        self.enlarge_factor = enlarge_factor
+        self.points = []
+        with open(csv_file, newline='') as f:
+            for name, *rest in csv.reader(f, delimiter=delimiter):
+                if name == dataset:
+                    point = torch.tensor(list(map(float, rest)))
+                    self.points.append((point - offset) / scale)
+
+    def __len__(self):
+        return len(self.points) * self.enlarge_factor
+
+    def __getitem__(self, i):
+        return self.points[i % len(self.points)]
+
+# Mainly used to discard labels and only output data
+class MappedDataset(Dataset):
+    def __init__(self, dataset, fn):
+        self.dataset = dataset
+        self.fn = fn
+    def __len__(self):
+        return len(self.dataset)
+    def __getitem__(self, i):
+        return self.fn(self.dataset[i])
