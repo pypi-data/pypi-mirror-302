34c16c2fe39ff4212728087eb726ce2964537176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 15:04:14 +0000

    Add package frametree/0.13.5/frametree-0.13.5.tar.gz

---
+++ packages/frametree/frametree-0.13.5.tar.gz/frametree-0.13.5/frametree/core/tests/test_dataset.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+import cloudpickle as cp
+from pydra import mark, Workflow
+from pydra.utils.hash import hash_object
+from frametree.core.frameset.base import FrameSet
+from frametree.core.serialize import asdict, fromdict
+
+
+def test_dataset_asdict_roundtrip(dataset):
+
+    dct = asdict(dataset, omit=["store", "id"])
+    undct = fromdict(dct, store=dataset.store, id=dataset.id)
+    assert isinstance(dct, dict)
+    assert "store" not in dct
+    del dataset.__annotations__["blueprint"]
+    assert dataset == undct
+
+
+def test_dataset_pickle(dataset: FrameSet, tmp_dir: Path):
+    fpath = tmp_dir / "dataset.pkl"
+    with fpath.open("wb") as fp:
+        cp.dump(dataset, fp)
+    with fpath.open("rb") as fp:
+        reloaded = cp.load(fp)
+    assert dataset == reloaded
+
+
+def test_dataset_in_workflow_pickle(dataset: FrameSet, tmp_dir: Path):
+
+    # Create the outer workflow to link the analysis workflow with the
+    # data row iteration and store connection rows
+    wf = Workflow(name="test", input_spec=["a"])
+
+    wf.add(func(a=wf.lzin.a, b=2, dataset=dataset, name="test_func"))
+
+    wf.set_output(("c", wf.test_func.lzout.c))
+
+    wf.pickle_task()
+
+
+@mark.task
+@mark.annotate({"a": int, "b": int, "dataset": FrameSet, "return": {"c": int}})
+def func(a, b, dataset):
+    return a + b
+
+
+def test_dataset_bytes_hash(dataset):
+
+    hsh = hash_object(dataset)
+    # Check hashing is stable
+    assert hash_object(dataset) == hsh
