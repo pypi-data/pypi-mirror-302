426f04a55c8d5b9648e74f247ad34d001161196e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 05:04:00 +0000

    Add package aliceio/0.1.1/aliceio-0.1.1-py3-none-any.whl

---
+++ packages/aliceio/aliceio-0.1.1-py3-none-any.whl/aliceio/dispatcher/event/bases.py
@@ -0,0 +1,31 @@
+from typing import Any, Awaitable, Callable, Dict, NoReturn, Optional, TypeVar, Union
+from unittest.mock import sentinel
+
+from aliceio.dispatcher.middlewares.base import BaseMiddleware
+from aliceio.types.base import AliceObject
+
+MiddlewareEventType = TypeVar("MiddlewareEventType", bound=AliceObject)
+NextMiddlewareType = Callable[[MiddlewareEventType, Dict[str, Any]], Awaitable[Any]]
+MiddlewareType = Union[
+    BaseMiddleware[Any],
+    Callable[
+        [NextMiddlewareType[MiddlewareEventType], MiddlewareEventType, Dict[str, Any]],
+        Awaitable[Any],
+    ],
+]
+
+UNHANDLED = sentinel.UNHANDLED
+REJECTED = sentinel.REJECTED
+
+
+class SkipHandler(Exception):
+    pass
+
+
+class CancelHandler(Exception):
+    pass
+
+
+def skip(message: Optional[str] = None) -> NoReturn:
+    """Вызов исключения :class:`SkipHandler`."""
+    raise SkipHandler(message or "Event skipped")
