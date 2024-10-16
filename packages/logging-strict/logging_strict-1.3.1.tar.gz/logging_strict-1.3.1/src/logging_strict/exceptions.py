238d1e4d7719e38cfb288238271647a0e8a07d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 03:10:15 +0000

    Add package logging-strict/1.3.1/logging_strict-1.3.1.tar.gz

---
+++ packages/logging-strict/logging_strict-1.3.1.tar.gz/logging_strict-1.3.1/src/logging_strict/exceptions.py
@@ -0,0 +1,95 @@
+"""
+.. moduleauthor:: Dave Faulkmore <faulkmore telegram>
+
+Custom exceptions
+
+Usage
+
+.. code-block:: python
+
+    from logging_strict import (
+        LoggingStrictError,
+        LoggingStrictPackageNameRequired,
+        LoggingStrictPackageStartFolderNameRequired,
+        LoggingStrictProcessCategoryRequired,
+        LoggingStrictGenreRequired,
+    )
+
+Module private variables
+-------------------------
+
+.. py:data:: __all__
+   :type: tuple[str, str, str, str, str]
+   :value: ("LoggingStrictError", "LoggingStrictPackageNameRequired", \
+   "LoggingStrictPackageStartFolderNameRequired", \
+   "LoggingStrictProcessCategoryRequired", "LoggingStrictGenreRequired")
+
+   Module exports
+
+Module objects
+---------------
+
+"""
+
+__all__ = (
+    "LoggingStrictError",
+    "LoggingStrictPackageNameRequired",
+    "LoggingStrictPackageStartFolderNameRequired",
+    "LoggingStrictProcessCategoryRequired",
+    "LoggingStrictGenreRequired",
+)
+
+
+class LoggingStrictError(ValueError):
+    """Catchall back exception
+
+    :ivar msg: The error message
+    :vartype msg: str
+    """
+
+    def __init__(self, msg: str) -> None:
+        super().__init__(msg)
+
+
+class LoggingStrictPackageNameRequired(LoggingStrictError):
+    """In entrypoint, package name is required
+
+    :ivar msg: The error message
+    :vartype msg: str
+    """
+
+    def __init__(self, msg: str) -> None:
+        super().__init__(msg)
+
+
+class LoggingStrictPackageStartFolderNameRequired(LoggingStrictError):
+    """In entrypoint, package start data folder name is required
+
+    :ivar msg: The error message
+    :vartype msg: str
+    """
+
+    def __init__(self, msg: str) -> None:
+        super().__init__(msg)
+
+
+class LoggingStrictProcessCategoryRequired(LoggingStrictError):
+    """Category is required
+
+    :ivar msg: The error message
+    :vartype msg: str
+    """
+
+    def __init__(self, msg: str) -> None:
+        super().__init__(msg)
+
+
+class LoggingStrictGenreRequired(LoggingStrictError):
+    """Genre is required
+
+    :ivar msg: The error message
+    :vartype msg: str
+    """
+
+    def __init__(self, msg: str) -> None:
+        super().__init__(msg)
