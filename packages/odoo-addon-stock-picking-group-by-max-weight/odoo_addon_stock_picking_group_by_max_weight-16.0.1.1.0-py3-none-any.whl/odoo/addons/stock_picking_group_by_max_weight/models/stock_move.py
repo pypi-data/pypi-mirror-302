dc9c58ec39b6823462ae9decb81bc222bd6d25df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 03:11:52 +0000

    Add package odoo-addon-stock-picking-group-by-max-weight/16.0.1.1.0/odoo_addon_stock_picking_group_by_max_weight-16.0.1.1.0-py3-none-any.whl

---
+++ packages/odoo-addon-stock-picking-group-by-max-weight/odoo_addon_stock_picking_group_by_max_weight-16.0.1.1.0-py3-none-any.whl/odoo/addons/stock_picking_group_by_max_weight/models/stock_move.py
@@ -0,0 +1,28 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+from odoo.osv.expression import AND
+
+
+class StockMove(models.Model):
+
+    _inherit = "stock.move"
+
+    def _search_picking_for_assignation_domain(self):
+        """
+        Group pickings per customer
+        """
+        domain = super()._search_picking_for_assignation_domain()
+        if self.picking_type_id.group_pickings_maxweight:
+            domain = AND(
+                [
+                    domain,
+                    [("assignation_max_weight", ">=", self.weight)],
+                ]
+            )
+        return domain
+
+    def _assign_picking_post_process(self, new=False):
+        self.picking_id._split_for_max_weight()
+        return super()._assign_picking_post_process(new=new)
