7e5792355be57a1f6068359ee1dd94b5369a5a0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 07:04:42 +0000

    Add package brand-universe/0.1/brand_universe-0.1.tar.gz

---
+++ packages/brand-universe/brand_universe-0.1.tar.gz/brand_universe-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='brand_universe',
+    version='0.1',
+    packages=find_packages(),
+    description='A package providing brand lists for the team',
+    author='Your Name',
+    author_email='your.email@example.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
