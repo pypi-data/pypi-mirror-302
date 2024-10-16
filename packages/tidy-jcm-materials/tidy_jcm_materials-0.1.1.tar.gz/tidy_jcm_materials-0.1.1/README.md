0cdf9483e3e7720eb1e4bb859da06104da934d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 03:12:10 +0000

    Add package tidy-jcm-materials/0.1.1/tidy_jcm_materials-0.1.1.tar.gz

---
+++ packages/tidy-jcm-materials/tidy_jcm_materials-0.1.1.tar.gz/tidy_jcm_materials-0.1.1/README.md
@@ -0,0 +1,9 @@
+# tidy_jcm_materials 0.1.1
+
+Converts materials from tidy3d database to JCMwave compatible pole residue format
+
+## Usage
+
+## Installation
+
+`make install`
---
+++ packages/tidy-jcm-materials/tidy_jcm_materials-0.1.1.tar.gz/tidy_jcm_materials-0.1.1/tidy_jcm_materials/__init__.py
@@ -0,0 +1,17 @@
+"""
+Tidy JCM materials
+=====
+
+Provides
+  A brief overview how to easily create python packages, that are:
+
+    + installable
+    + testable
+    + documented
+    -------------
+    = reproducible
+"""
+
+__version__ = "0.1.1"
+
+__all__ = []
