907b77a3f846366ea05eacc0be8af166e575a8f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 17:04:50 +0000

    Add package dsmc-tool/0.1.3/dsmc_tool-0.1.3.tar.gz

---
+++ packages/dsmc-tool/dsmc_tool-0.1.3.tar.gz/dsmc_tool-0.1.3/dsmc_tool/example.py
@@ -0,0 +1,17 @@
+import gymnasium as gym
+from gymnasium.wrappers import FlattenObservation
+import pgtg
+from stable_baselines3 import DQN
+from dsmc_tool.evaluator import Evaluator
+import dsmc_tool.property as prop
+
+env = gym.make("pgtg-v3")
+env = FlattenObservation(env)
+
+model = DQN("MlpPolicy", env,verbose=1)
+model.learn(total_timesteps=1000, log_interval=100)
+
+evaluator = Evaluator(env=env, initial_episodes=100, evaluation_episodes=50)
+property = prop.ReturnProperty()
+evaluator.register_property(property)
+results = evaluator.eval(model, epsilon=0.1, kappa=0.05, save_interim_results=True, relative_epsilon=True)
