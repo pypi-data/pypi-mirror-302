da24b0042dd4bef07ed9d774364669996783f235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 15:04:32 +0000

    Add package kapil-operands/0.1/kapil_operands-0.1-py3-none-any.whl

---
+++ packages/kapil-operands/kapil_operands-0.1-py3-none-any.whl/kapil_operands/__init__.py
@@ -0,0 +1,8 @@
+def operands(operand1,operand2):
+    if operand1 == operand2:
+        print("{} and {} operands are same".format(operand1,operand2))
+
+    else:
+        print("{} and {} operands are not same".format(operand1,operand2))
+    
+
