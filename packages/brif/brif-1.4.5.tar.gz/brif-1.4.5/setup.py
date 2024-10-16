a411381f7e65e4b6b805b7d971d9783ddb61c66a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 06:04:26 +0000

    Add package brif/1.4.5/brif-1.4.5.tar.gz

---
+++ packages/brif/brif-1.4.5.tar.gz/brif-1.4.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+import platform
+
+if platform.system() == 'Windows':
+    extra_flags = ['/openmp']
+    extra_link = []
+elif platform.system() != 'Darwin':
+    extra_flags = ['-fopenmp']
+    extra_link = ['-lgomp']
+else:
+    extra_flags = []
+    extra_link = []
+
+setup(name='brif',
+      version="1.4.5",
+      description = "Build decision trees and random forests for classification and regression.",
+      author = 'Yanchao Liu',
+      author_email = 'yanchaoliu@wayne.edu',
+      url = 'https://pypi.org/project/brif/',
+      packages = ['brif'],
+      ext_modules=[
+          Extension('brifc',
+                    ['pybrif.c','brif.c'],
+                    extra_compile_args = extra_flags,
+                    extra_link_args = extra_link
+                    )
+          ]
+      )
