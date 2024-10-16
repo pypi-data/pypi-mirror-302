572b557de850eefdd287261ee6370b0c4c0b63d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 07:05:32 +0000

    Add package crappy/2.0.6/crappy-2.0.6.tar.gz

---
+++ packages/crappy/crappy-2.0.6.tar.gz/crappy-2.0.6/src/crappy/camera/__init__.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+from typing import Dict, Type
+
+from .fake_camera import FakeCamera
+from .file_reader import FileReader
+from .opencv_camera_webcam import Webcam
+from .raspberry_pi_camera import RaspberryPiCamera
+from .seek_thermal_pro import SeekThermalPro
+from .ximea_xiapi import XiAPI
+
+from .cameralink import BaslerIronmanCameraLink
+from .cameralink import JaiGO5000CPMCL, JaiGO5000CPMCL8Bits
+
+from .meta_camera import Camera, MetaCamera, camera_setting
+
+from platform import system
+from subprocess import run
+if system() == 'Linux':
+  try:
+    run(['v4l2-ctl'], capture_output=True)
+  except FileNotFoundError:
+    from .gstreamer_camera_basic import CameraGstreamer
+    from .opencv_camera_basic import CameraOpencv
+  else:
+    from .gstreamer_camera_v4l2 import CameraGstreamer
+    from .opencv_camera_v4l2 import CameraOpencv
+else:
+  from .gstreamer_camera_basic import CameraGstreamer
+  from .opencv_camera_basic import CameraOpencv
+
+from ._deprecated import deprecated_cameras
+camera_dict: Dict[str, Type[Camera]] = MetaCamera.classes
