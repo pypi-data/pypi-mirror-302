b3720279e680dccf40da57d70fa64f904b8b799c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 16:04:17 +0000

    Add package myskoda/0.8.0/myskoda-0.8.0.tar.gz

---
+++ packages/myskoda/myskoda-0.8.0.tar.gz/myskoda-0.8.0/myskoda/__init__.py
@@ -0,0 +1,41 @@
+"""A library for interacting with the MySkoda APIs."""
+
+from .auth.authorization import Authorization, AuthorizationError, IDKAuthorizationCode, IDKSession
+from .models import (
+    air_conditioning,
+    charging,
+    common,
+    health,
+    info,
+    operation_request,
+    position,
+    service_event,
+    status,
+    user,
+)
+from .mqtt import Mqtt
+from .myskoda import TRACE_CONFIG, MySkoda
+from .rest_api import RestApi
+from .vehicle import Vehicle
+
+__all__ = [
+    "Authorization",
+    "AuthorizationError",
+    "IDKAuthorizationCode",
+    "IDKSession",
+    "air_conditioning",
+    "charging",
+    "common",
+    "health",
+    "info",
+    "position",
+    "status",
+    "RestApi",
+    "MySkoda",
+    "operation_request",
+    "service_event",
+    "Mqtt",
+    "Vehicle",
+    "user",
+    "TRACE_CONFIG",
+]
