a0194bc39687938c5f04b4c15cedf22b7b23b5e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 14:04:42 +0000

    Add package inputrules/0.0.3/inputrules-0.0.3.tar.gz

---
+++ packages/inputrules/inputrules-0.0.3.tar.gz/inputrules-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="inputrules",
+    version="0.0.3",
+    packages=find_packages(),
+    install_requires=[
+        # Dependencias aquí
+    ],
+    description="Predefined rules for processing data input (Forms/JSON/HTTP Requests)",
+    author="Alvaro De Leon",
+    author_email="info@alvarodeleon.com",
+    url="https://github.com/alvarodeleon/inputrules",
+) 
+
+
