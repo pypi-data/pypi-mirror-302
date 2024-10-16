63ace62b187756be3aea8ba5d3ecf410efa93506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 08:04:03 +0000

    Add package inputrules/0.0.2/inputrules-0.0.2.tar.gz

---
+++ packages/inputrules/inputrules-0.0.2.tar.gz/inputrules-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="inputrules",
+    version="0.0.2",
+    packages=find_packages(),
+    install_requires=[
+        # Dependencias aquí
+    ],
+    description="Descripción breve de tu librería",
+    author="Alvaro De Leon",
+    author_email="info@alvarodeleon.com",
+    url="https://github.com/alvarodeleon/inputrules",
+) 
+
+
