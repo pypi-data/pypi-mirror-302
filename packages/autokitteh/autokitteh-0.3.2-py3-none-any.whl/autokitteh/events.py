c53190214cba87b92c40926db501c1045835b48a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 05:05:34 +0000

    Add package autokitteh/0.3.2/autokitteh-0.3.2-py3-none-any.whl

---
+++ packages/autokitteh/autokitteh-0.3.2-py3-none-any.whl/autokitteh/events.py
@@ -0,0 +1,24 @@
+"""Un/subscribe and consume AutoKitteh connection events."""
+
+from datetime import timedelta
+from uuid import uuid4
+
+from .attr_dict import AttrDict
+
+
+def subscribe(connection_name: str, filter: str) -> str:
+    """Subscribe to events on connection. Optional filter is a CEL expression."""
+    # Dummy implementation for local development.
+    return uuid4().hex
+
+
+def unsubscribe(subscription_id: str) -> None:
+    """Unsubscribe from events."""
+    # Dummy implementation for local development.
+    pass
+
+
+def next_event(subscription_id: str, *, timeout: timedelta = None) -> AttrDict:
+    """Get the next event from the subscription."""
+    # Dummy implementation for local development.
+    return AttrDict()
