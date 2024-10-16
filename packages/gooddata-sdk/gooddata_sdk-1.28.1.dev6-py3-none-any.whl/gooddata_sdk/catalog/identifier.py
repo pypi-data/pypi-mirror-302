49d6fdb2dcb131636b543c9b368ca25075465e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 03:10:32 +0000

    Add package gooddata-sdk/1.28.1.dev6/gooddata_sdk-1.28.1.dev6-py3-none-any.whl

---
+++ packages/gooddata-sdk/gooddata_sdk-1.28.1.dev6-py3-none-any.whl/gooddata_sdk/catalog/identifier.py
@@ -0,0 +1,93 @@
+# (C) 2022 GoodData Corporation
+from __future__ import annotations
+
+import builtins
+
+import attr
+from gooddata_api_client.model.assignee_identifier import AssigneeIdentifier
+from gooddata_api_client.model.dataset_workspace_data_filter_identifier import DatasetWorkspaceDataFilterIdentifier
+from gooddata_api_client.model.declarative_user_group_identifier import DeclarativeUserGroupIdentifier
+from gooddata_api_client.model.declarative_user_identifier import DeclarativeUserIdentifier
+from gooddata_api_client.model.grain_identifier import GrainIdentifier
+from gooddata_api_client.model.label_identifier import LabelIdentifier
+from gooddata_api_client.model.reference_identifier import ReferenceIdentifier
+from gooddata_api_client.model.workspace_identifier import WorkspaceIdentifier
+
+from gooddata_sdk.catalog.base import Base, value_in_allowed
+
+
+@attr.s(auto_attribs=True, kw_only=True)
+class CatalogWorkspaceIdentifier(Base):
+    id: str
+
+    @staticmethod
+    def client_class() -> builtins.type[WorkspaceIdentifier]:
+        return WorkspaceIdentifier
+
+
+@attr.s(auto_attribs=True, kw_only=True)
+class CatalogReferenceIdentifier(Base):
+    id: str
+
+    @staticmethod
+    def client_class() -> builtins.type[ReferenceIdentifier]:
+        return ReferenceIdentifier
+
+
+@attr.s(auto_attribs=True, kw_only=True)
+class CatalogGrainIdentifier(Base):
+    id: str
+    type: str = attr.field(validator=value_in_allowed)
+
+    @staticmethod
+    def client_class() -> builtins.type[GrainIdentifier]:
+        return GrainIdentifier
+
+
+@attr.s(auto_attribs=True, kw_only=True)
+class CatalogAssigneeIdentifier(Base):
+    id: str
+    type: str = attr.field(validator=value_in_allowed)
+
+    @staticmethod
+    def client_class() -> builtins.type[AssigneeIdentifier]:
+        return AssigneeIdentifier
+
+
+@attr.s(auto_attribs=True, kw_only=True)
+class CatalogDeclarativeUserGroupIdentifier(Base):
+    id: str
+    type: str = attr.field(validator=value_in_allowed)
+
+    @staticmethod
+    def client_class() -> builtins.type[DeclarativeUserGroupIdentifier]:
+        return DeclarativeUserGroupIdentifier
+
+
+@attr.s(auto_attribs=True, kw_only=True)
+class CatalogUserIdentifier(Base):
+    id: str
+    type: str = attr.field(validator=value_in_allowed)
+
+    @staticmethod
+    def client_class() -> builtins.type[DeclarativeUserIdentifier]:
+        return DeclarativeUserIdentifier
+
+
+@attr.s(auto_attribs=True, kw_only=True)
+class CatalogLabelIdentifier(Base):
+    id: str
+    type: str = attr.field(validator=value_in_allowed)
+
+    @staticmethod
+    def client_class() -> builtins.type[LabelIdentifier]:
+        return LabelIdentifier
+
+
+@attr.s(auto_attribs=True, kw_only=True)
+class CatalogDatasetWorkspaceDataFilterIdentifier(Base):
+    id: str
+
+    @staticmethod
+    def client_class() -> builtins.type[DatasetWorkspaceDataFilterIdentifier]:
+        return DatasetWorkspaceDataFilterIdentifier
