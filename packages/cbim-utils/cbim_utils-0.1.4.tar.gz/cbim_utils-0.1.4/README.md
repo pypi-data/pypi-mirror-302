de36a0361b7cdca7e9e68e36aaf6fa541ceef857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 03:10:54 +0000

    Add package cbim-utils/0.1.4/cbim_utils-0.1.4.tar.gz

---
+++ packages/cbim-utils/cbim_utils-0.1.4.tar.gz/cbim_utils-0.1.4/README.md
@@ -0,0 +1,4 @@
+# Documentazione Moduli
+
+## db_connection.py: 
+Get a connection to a MySQL database from a url style connection string. (Param url: MySQL connection string, e.g.: mysql:/user:password@hostname:port/database)
---
+++ packages/cbim-utils/cbim_utils-0.1.4.tar.gz/cbim_utils-0.1.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cbim_utils",
+    version="0.1.4",
+    author_email="m.martinelli@cbim.it",
+    description="Various utilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    url = 'https://github.com/mmartinelli00/package_cbim.git',
+    install_requires=[
+        'PyMySQL==1.1.1',
+        ],
+    extras_require = {
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    }
+)
