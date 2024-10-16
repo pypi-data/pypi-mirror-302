9d12257437cb968838de21998cb5bb7e1fccb21e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 17:04:37 +0000

    Add package XICRA/1.5.2/XICRA-1.5.2-py3-none-any.whl

---
+++ packages/XICRA/XICRA-1.5.2-py3-none-any.whl/XICRA/modules/__init__.py
@@ -0,0 +1,20 @@
+__all__ = [
+	'biotype',
+	'config',
+	'citation',
+	'database',
+	'help_XICRA',
+	'join',
+	'miRNA',
+	'prep',
+	'qc',
+	'trim',
+	'tRNA',
+	'piRNA',
+	'map',
+	'test'
+	
+]
+
+from XICRA.modules import *
+
