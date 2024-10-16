2b91d8ed62224b05e0275e384aea0628568f1720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 07:05:23 +0000

    Add package drf-complex-filter/2.0.0/drf_complex_filter-2.0.0.tar.gz

---
+++ packages/drf-complex-filter/drf_complex_filter-2.0.0.tar.gz/drf_complex_filter-2.0.0/drf_complex_filter/settings.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+
+DEFAULTS = {
+    "COMPARISON_CLASSES": [
+        "drf_complex_filter.comparisons.CommonComparison",
+        "drf_complex_filter.comparisons.DynamicComparison",
+    ],
+    "VALUE_FUNCTIONS": ["drf_complex_filter.functions.DateFunctions"],
+    "QUERY_PARAMETER": "filters",
+    "DEFAULT_LOOKUP_FIELD": None,
+    "DEFAULT_COMPARISON_FUNCTION": None,
+}
+
+COMPLEX_FILTER_SETTINGS = getattr(settings, "COMPLEX_FILTER_SETTINGS", {})
+
+filter_settings = {**DEFAULTS, **COMPLEX_FILTER_SETTINGS}
