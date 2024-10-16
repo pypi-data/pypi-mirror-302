81cb6054ae76cf470f3a0b5d0b193133c2be1f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 08:04:07 +0000

    Add package together/1.3.2/together-1.3.2.tar.gz

---
+++ packages/together/together-1.3.2.tar.gz/together-1.3.2/src/together/cli/api/utils.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+import click
+
+from typing import Literal
+
+
+class AutoIntParamType(click.ParamType):
+    name = "integer"
+
+    def convert(
+        self, value: str, param: click.Parameter | None, ctx: click.Context | None
+    ) -> int | Literal["max"] | None:
+        if isinstance(value, int):
+            return value
+
+        if value == "max":
+            return "max"
+
+        self.fail("Invalid integer value: {value}")
+
+
+INT_WITH_MAX = AutoIntParamType()
