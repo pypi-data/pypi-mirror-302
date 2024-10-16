312e54ab30f47c16f6d5b86dcd2a92c25562264a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 07:06:12 +0000

    Add package ob-metaflow-stubs/6.0.3.109/ob_metaflow_stubs-6.0.3.109-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.109-py2.py3-none-any.whl/metaflow-stubs/plugins/azure/__init__.pyi
@@ -0,0 +1,12 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.25.2+obcheckpoint(0.0.11);ob(v1)                              #
+# Generated on 2024-10-15T21:29:55.357504                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+def create_azure_credential():
+    ...
+
---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.109-py2.py3-none-any.whl/metaflow-stubs/profilers/__init__.pyi
@@ -0,0 +1,16 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.25.2+obcheckpoint(0.0.11);ob(v1)                              #
+# Generated on 2024-10-15T21:29:55.307499                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+class gpu_profile(object, metaclass=type):
+    def __init__(self, include_artifacts = True, artifact_prefix = "gpu_profile_", interval = 1):
+        ...
+    def __call__(self, f):
+        ...
+    ...
+
