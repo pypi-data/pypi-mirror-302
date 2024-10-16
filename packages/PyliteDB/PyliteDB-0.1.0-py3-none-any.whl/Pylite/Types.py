7c90b3e4b289f70f605fbc05a0194f9564d813d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 15:03:48 +0000

    Add package PyliteDB/0.1.0/PyliteDB-0.1.0-py3-none-any.whl

---
+++ packages/PyliteDB/PyliteDB-0.1.0-py3-none-any.whl/Pylite/Types.py
@@ -0,0 +1,11 @@
+
+
+
+class PyliteTypes():
+    SupportedTypes = [
+            int,float, # Numeric
+            str, # String
+            bool, # Boolean
+            list, # List
+            dict, # Dictionary
+    ]
