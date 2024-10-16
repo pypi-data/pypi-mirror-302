e9941871a9741c39420325d438af885b581fdba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 07:04:30 +0000

    Add package pymultiplayer/0.2/pymultiplayer-0.2-py3-none-any.whl

---
+++ packages/pymultiplayer/pymultiplayer-0.2-py3-none-any.whl/pymultiplayer/__init__.py
@@ -0,0 +1,6 @@
+from .TCPserver import TCPMultiplayerServer
+from .client import MultiplayerClient
+print("This library is currently undergoing a major rewrite.")
+print("Not all features will work as intended.")
+print("pymultiplayer by iamdeedz")
+print("https://www.github.com/iamdeedz/pymultiplayer")
