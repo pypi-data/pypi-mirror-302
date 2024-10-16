d1afaaa79c0cc3d7ab59ccb4f97869e5835a382f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 03:12:02 +0000

    Add package pan-scm-sdk/0.1.5/pan_scm_sdk-0.1.5-py3-none-any.whl

---
+++ packages/pan-scm-sdk/pan_scm_sdk-0.1.5-py3-none-any.whl/scm/models/__init__.py
@@ -0,0 +1,10 @@
+# scm/models/__init__.py
+
+from .address import AddressRequestModel, AddressResponseModel
+from .address_group import AddressGroupRequestModel, AddressGroupResponseModel
+from .application import ApplicationRequestModel, ApplicationResponseModel
+from .application_group import (
+    ApplicationGroupRequestModel,
+    ApplicationGroupResponseModel,
+)
+from .service import ServiceRequestModel, ServiceResponseModel
