3ed666e0136b8a39eeb27060ac8f2e89135a6c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 14:04:59 +0000

    Add package sitepages/0.2.1/sitepages-0.2.1-py2.py3-none-any.whl

---
+++ packages/sitepages/sitepages-0.2.1-py2.py3-none-any.whl/sitepages/__init__.py
@@ -0,0 +1,5 @@
+# coding:utf-8
+
+from .response import FlaskResponse  # noqa:F401 pylint:disable=E0401
+from .sitepage import page  # noqa:F401 pylint:disable=E0401
+from .sitepage import site  # noqa:F401 pylint:disable=E0401
