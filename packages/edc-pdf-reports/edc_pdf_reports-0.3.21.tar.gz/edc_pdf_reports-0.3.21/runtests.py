1ba75210a0f70c1e5077bc8fb0545c360bb96209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 15:04:11 +0000

    Add package edc-pdf-reports/0.3.21/edc_pdf_reports-0.3.21.tar.gz

---
+++ packages/edc-pdf-reports/edc_pdf_reports-0.3.21.tar.gz/edc_pdf_reports-0.3.21/runtests.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+import logging
+import os
+import sys
+from os.path import abspath, dirname
+
+import django
+from django.conf import settings
+from django.test.runner import DiscoverRunner
+from edc_test_utils import DefaultTestSettings
+
+app_name = "edc_pdf_reports"
+base_dir = dirname(abspath(__file__))
+
+DEFAULT_SETTINGS = DefaultTestSettings(
+    calling_file=__file__,
+    BASE_DIR=base_dir,
+    APP_NAME=app_name,
+    SUBJECT_VISIT_MODEL="edc_visit_tracking.subjectvisit",
+    EDC_AUTH_SKIP_SITE_AUTHS=True,
+    EDC_AUTH_SKIP_AUTH_UPDATER=True,
+    SILENCED_SYSTEM_CHECKS=[
+        "sites.E101",
+        "edc_navbar.E002",
+        "edc_navbar.E003",
+        "edc_consent.E001",
+        "edc_sites.E001",
+        "edc_sites.E002",
+    ],
+    ETC_DIR=os.path.join(base_dir, app_name, "tests", "etc"),
+    INSTALLED_APPS=[
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
+        "django.contrib.sites",
+        "django_crypto_fields.apps.AppConfig",
+        "multisite",
+        "edc_action_item.apps.AppConfig",
+        "edc_appointment.apps.AppConfig",
+        "edc_auth.apps.AppConfig",
+        "edc_data_manager.apps.AppConfig",
+        "edc_device.apps.AppConfig",
+        "edc_facility.apps.AppConfig",
+        "edc_form_runners.apps.AppConfig",
+        "edc_identifier.apps.AppConfig",
+        "edc_lab.apps.AppConfig",
+        "edc_label.apps.AppConfig",
+        "edc_locator.apps.AppConfig",
+        "edc_metadata.apps.AppConfig",
+        "edc_notification.apps.AppConfig",
+        "edc_offstudy.apps.AppConfig",
+        "edc_registration.apps.AppConfig",
+        "edc_sites.apps.AppConfig",
+        "edc_timepoint.apps.AppConfig",
+        "edc_visit_schedule.apps.AppConfig",
+        "edc_visit_tracking.apps.AppConfig",
+        "edc_prn.apps.AppConfig",
+        "edc_pdf_reports.apps.AppConfig",
+        "edc_appconfig.apps.AppConfig",
+    ],
+    add_dashboard_middleware=True,
+    add_lab_dashboard_middleware=True,
+).settings
+
+
+def main():
+    if not settings.configured:
+        settings.configure(**DEFAULT_SETTINGS)
+    django.setup()
+    failures = DiscoverRunner(failfast=True).run_tests([f"{app_name}.tests"])
+    sys.exit(failures)
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    main()
