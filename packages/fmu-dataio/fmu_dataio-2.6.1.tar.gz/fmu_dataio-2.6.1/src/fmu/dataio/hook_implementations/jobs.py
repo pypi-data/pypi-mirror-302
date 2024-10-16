f5bdb2d2beda6bb8e007c54431f899f37660d2ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 03:11:20 +0000

    Add package fmu-dataio/2.6.1/fmu_dataio-2.6.1.tar.gz

---
+++ packages/fmu-dataio/fmu_dataio-2.6.1.tar.gz/fmu_dataio-2.6.1/src/fmu/dataio/hook_implementations/jobs.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+try:
+    from ert.shared.plugins.plugin_manager import hook_implementation
+    from ert.shared.plugins.plugin_response import plugin_response
+except ModuleNotFoundError:
+    from ert_shared.plugins.plugin_manager import hook_implementation
+    from ert_shared.plugins.plugin_response import plugin_response
+
+
+@hook_implementation
+@plugin_response(plugin_name="fmu_dataio")
+def installable_workflow_jobs() -> dict:
+    return {}
