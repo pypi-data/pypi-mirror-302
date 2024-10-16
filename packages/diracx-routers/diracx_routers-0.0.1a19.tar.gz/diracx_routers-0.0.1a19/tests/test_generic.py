504ce76a6cf340b599ef1c8ed681c9e5aaa4b58b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 16:04:53 +0000

    Add package diracx-routers/0.0.1a19/diracx_routers-0.0.1a19.tar.gz

---
+++ packages/diracx-routers/diracx_routers-0.0.1a19.tar.gz/diracx_routers-0.0.1a19/tests/test_generic.py
@@ -0,0 +1,48 @@
+import pytest
+
+pytestmark = pytest.mark.enabled_dependencies(
+    [
+        "ConfigSource",
+        "AuthSettings",
+        "OpenAccessPolicy",
+        "DevelopmentSettings",
+    ]
+)
+
+
+@pytest.fixture
+def test_client(client_factory):
+    with client_factory.unauthenticated() as client:
+        yield client
+
+
+def test_openapi(test_client):
+    r = test_client.get("/api/openapi.json")
+    assert r.status_code == 200
+    assert r.json()
+
+
+def test_oidc_configuration(test_client):
+    r = test_client.get("/.well-known/openid-configuration")
+    assert r.status_code == 200
+    assert r.json()
+
+
+def test_installation_metadata(test_client):
+    r = test_client.get("/.well-known/dirac-metadata")
+
+    assert r.status_code == 200
+    assert r.json()
+
+
+@pytest.mark.xfail(reason="TODO")
+def test_unavailable_db(monkeypatch, test_client):
+    # TODO
+    # That does not work because test_client is already initialized
+    monkeypatch.setenv(
+        "DIRACX_DB_URL_JOBDB", "mysql+aiomysql://tata:yoyo@dbod.cern.ch:3306/name"
+    )
+
+    r = test_client.get("/api/job/123")
+    assert r.status_code == 503
+    assert r.json()
