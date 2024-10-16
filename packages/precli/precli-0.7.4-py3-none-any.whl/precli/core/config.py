7d6540b35bf9246931f3f80c13929a8372dfa4e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 16:04:25 +0000

    Add package precli/0.7.4/precli-0.7.4-py3-none-any.whl

---
+++ packages/precli/precli-0.7.4-py3-none-any.whl/precli/core/config.py
@@ -0,0 +1,34 @@
+# Copyright 2024 Secure Sauce LLC
+from precli.core.level import Level
+
+
+class Config:
+    def __init__(
+        self,
+        enabled: bool = True,
+        level: Level = Level.WARNING,
+        rank: float = -1.0,
+    ):
+        self._enabled = enabled
+        self._level = level
+        self._rank = rank
+
+    @property
+    def enabled(self) -> bool:
+        """Whether the configuration indicates the rule is enabled."""
+        return self._enabled
+
+    @enabled.setter
+    def enabled(self, enabled: bool):
+        """Set whether the rule is enabled"""
+        self._enabled = enabled
+
+    @property
+    def level(self) -> Level:
+        """The default severity level."""
+        return self._level
+
+    @property
+    def rank(self) -> float:
+        """The default rank for the rule."""
+        return self._rank
