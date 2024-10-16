9aa13583604db25c546a8a5b6e70b45324e0668c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 15:04:32 +0000

    Add package cerberus-etl/0.0.1/cerberus_etl-0.0.1.tar.gz

---
+++ packages/cerberus-etl/cerberus_etl-0.0.1.tar.gz/cerberus_etl-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A utility ETL library for internal development at NUDA'
+LONG_DESCRIPTION = """cerberuslib is a robust ETL library designed for the NUDA team at Unimed SC. 
+This library provides essential utilities for data extraction, transformation, and loading processes, 
+streamlining workflows and enhancing data management capabilities.
+"""
+
+setup(
+    name="cerberus-etl",
+    version=VERSION,
+    author="Gabriel Deglmann Kasten",
+    author_email="gabriel.kasten@unimedsc.coop.br",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'oracledb',
+        'requests',
+    ],
+    keywords=['python', 'ETL', 'data pipeline', 'data stream', 'Unimed SC', 'NUDA'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
