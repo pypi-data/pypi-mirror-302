6e92ce3d1f02fc5eb88b19eeb8e444de295c60b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 17:04:09 +0000

    Add package cmind/3.2.3/cmind-3.2.3.tar.gz

---
+++ packages/cmind/cmind-3.2.3.tar.gz/cmind-3.2.3/cmind/__init__.py
@@ -0,0 +1,11 @@
+# Collective Mind init
+#
+# Written by Grigori Fursin
+
+__version__ = "3.2.3"
+
+from cmind.core import access
+from cmind.core import x
+from cmind.core import error
+from cmind.core import halt
+from cmind.core import CM
