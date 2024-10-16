97c1157faf827350648036131c6723b420c594da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 07:05:21 +0000

    Add package karaoke/0.0.1/karaoke-0.0.1.tar.gz

---
+++ packages/karaoke/karaoke-0.0.1.tar.gz/karaoke-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+NAME = "karaoke"
+VERSION = "0.0.1"
+DESCRIPTION = " "
+LONG_DESCRIPTION = (Path(__file__).parent / "README.md").read_text()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    author="0x15BA88FF",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    keywords=["karaoke", "music", "lrc"],
+    packages=find_packages(),
+)
