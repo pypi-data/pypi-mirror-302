9c3b67b60c827e983947989af697695af0205757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 06:04:13 +0000

    Add package botocraft/0.5.5/botocraft-0.5.5.tar.gz

---
+++ packages/botocraft/botocraft-0.5.5.tar.gz/botocraft-0.5.5/botocraft/services/__init__.py
@@ -0,0 +1,7 @@
+from .ssm import *  # noqa: F401,F403
+from .ecr import *  # noqa: F401,F403
+from .ecs import *  # noqa: F401,F403
+from .autoscaling import *  # noqa: F401,F403
+from .elbv2 import *  # noqa: F401,F403
+from .rds import *  # noqa: F401,F403
+from .ec2 import *  # noqa: F401,F403
