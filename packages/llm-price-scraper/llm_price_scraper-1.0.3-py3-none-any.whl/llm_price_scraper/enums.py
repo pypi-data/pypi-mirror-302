511d015723437d96a34626c9eccd85667d15b379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 04:04:09 +0000

    Add package llm-price-scraper/1.0.3/llm_price_scraper-1.0.3-py3-none-any.whl

---
+++ packages/llm-price-scraper/llm_price_scraper-1.0.3-py3-none-any.whl/llm_price_scraper/enums.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class DataSources(Enum):
+    DOCSBOT = "docsbot"  # Most likely to be used
+    BOTGENUITY = "botgenuity"
+    HUGGINGFACE = "huggingface"
+    HUHUHANG = "huhuhang"
+    OPENAI = "openai"
---
+++ packages/llm-price-scraper/llm_price_scraper-1.0.3-py3-none-any.whl/llm_price_scraper/scrapers/__init__.py
@@ -0,0 +1,28 @@
+from llm_price_scraper.enums import DataSources
+from llm_price_scraper.scrapers.botgenuity import BotgenuityScraper
+from llm_price_scraper.scrapers.docsbot import DocsBotScraper
+from llm_price_scraper.scrapers.huggingface import HuggingfaceScraper
+from llm_price_scraper.scrapers.huhuhang import HuhuhangScraper
+from llm_price_scraper.scrapers.openai import OpenaiScraper
+
+class LlmPricingScraper:
+    @staticmethod
+    def scrape(source: DataSources = DataSources.HUGGINGFACE):
+        """
+        Scrape the LLM pricing information from the specified source.
+
+        :returns: A list of LLMModelPricing objects.
+        """
+        if source == DataSources.DOCSBOT:
+            return DocsBotScraper.scrape()
+        elif source == DataSources.BOTGENUITY:
+            return BotgenuityScraper.scrape()
+        elif source == DataSources.HUGGINGFACE:
+            return HuggingfaceScraper.scrape()
+        elif source == DataSources.HUHUHANG:
+            return HuhuhangScraper.scrape()
+        elif source == DataSources.OPENAI:
+            raise Exception(f"Source '{source}' is not supported.")
+            # return OpenaiScraper.scrape()
+        else:
+            raise Exception(f"Source '{source}' is not supported.")
