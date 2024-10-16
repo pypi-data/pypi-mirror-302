4d3ac3530fb09014f6487d4ba80dc6284d19b00c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 17:04:38 +0000

    Add package new-logicsdk/0.0.1/new_logicsdk-0.0.1.tar.gz

---
+++ packages/new-logicsdk/new_logicsdk-0.0.1.tar.gz/new_logicsdk-0.0.1/new_logicsdk/__init__.py
@@ -0,0 +1,8 @@
+from .main import summarize, structredExtraction, DataMasking
+#from .main import structredExtraction
+
+# from loguru import logger
+
+# # Configure logger
+# logger.remove()
+# logger.add(lambda msg: print(msg, end=''), level="INFO")
