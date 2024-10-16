ca195928df5396b6820048a223d48f71b79e95e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 07:05:02 +0000

    Add package alvadescpy/0.1.4/alvadescpy-0.1.4.tar.gz

---
+++ packages/alvadescpy/alvadescpy-0.1.4.tar.gz/alvadescpy-0.1.4/alvadescpy/__init__.py
@@ -0,0 +1,5 @@
+from alvadescpy.wrapper import alvadesc
+from alvadescpy.functions import smiles_to_descriptors
+from alvadescpy.wrapper import CONFIG
+import pkg_resources
+__version__ = pkg_resources.get_distribution('alvadescpy')
