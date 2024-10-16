e043a9f794d62e96b4d28f4247f4a0365bbab0fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 05:06:03 +0000

    Add package RosettaPy/0.2.2rc170.post1/rosettapy-0.2.2rc170.post1.tar.gz

---
+++ packages/RosettaPy/rosettapy-0.2.2rc170.post1.tar.gz/rosettapy-0.2.2rc170.post1/src/RosettaPy/utils/escape.pyi
@@ -0,0 +1,80 @@
+# pylint: disable=unused-argument
+# pylint: disable=too-many-public-methods
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+
+class Colors:
+    BLACK: str
+    RED: str
+    GREEN: str
+    BROWN: str
+    BLUE: str
+    PURPLE: str
+    CYAN: str
+    LIGHT_GRAY: str
+    DARK_GRAY: str
+    LIGHT_RED: str
+    LIGHT_GREEN: str
+    YELLOW: str
+    LIGHT_BLUE: str
+    LIGHT_PURPLE: str
+    LIGHT_CYAN: str
+    LIGHT_WHITE: str
+    BOLD: str
+    FAINT: str
+    ITALIC: str
+    UNDERLINE: str
+    BLINK: str
+    NEGATIVE: str
+    CROSSED: str
+    RESET: str
+
+    @staticmethod
+    def black(text: str) -> str: ...
+    @staticmethod
+    def red(text: str) -> str: ...
+    @staticmethod
+    def green(text: str) -> str: ...
+    @staticmethod
+    def brown(text: str) -> str: ...
+    @staticmethod
+    def blue(text: str) -> str: ...
+    @staticmethod
+    def purple(text: str) -> str: ...
+    @staticmethod
+    def cyan(text: str) -> str: ...
+    @staticmethod
+    def light_gray(text: str) -> str: ...
+    @staticmethod
+    def dark_gray(text: str) -> str: ...
+    @staticmethod
+    def light_red(text: str) -> str: ...
+    @staticmethod
+    def light_green(text: str) -> str: ...
+    @staticmethod
+    def yellow(text: str) -> str: ...
+    @staticmethod
+    def light_blue(text: str) -> str: ...
+    @staticmethod
+    def light_purple(text: str) -> str: ...
+    @staticmethod
+    def light_cyan(text: str) -> str: ...
+    @staticmethod
+    def light_white(text: str) -> str: ...
+    @staticmethod
+    def bold(text: str) -> str: ...
+    @staticmethod
+    def faint(text: str) -> str: ...
+    @staticmethod
+    def italic(text: str) -> str: ...
+    @staticmethod
+    def underline(text: str) -> str: ...
+    @staticmethod
+    def blink(text: str) -> str: ...
+    @staticmethod
+    def negative(text: str) -> str: ...
+    @staticmethod
+    def crossed(text: str) -> str: ...
+    @staticmethod
+    def reset(text: str) -> str: ...
