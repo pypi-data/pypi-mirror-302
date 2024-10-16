f61b518edba9485496dd923078b23084651c9b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 14:04:15 +0000

    Add package osbot-local-stack/0.2.0/osbot_local_stack-0.2.0.tar.gz

---
+++ packages/osbot-local-stack/osbot_local_stack-0.2.0.tar.gz/osbot_local_stack-0.2.0/README.md
@@ -0,0 +1,5 @@
+# OSBot__Repo_Template
+
+## Repo details
+
+![Current Release](https://img.shields.io/badge/release-v0.2.0-blue)
---
+++ packages/osbot-local-stack/osbot_local_stack-0.2.0.tar.gz/osbot_local_stack-0.2.0/osbot_local_stack/local_stack/Local_Stack.py
@@ -0,0 +1,45 @@
+from osbot_aws.aws.sts.STS                               import STS
+from osbot_aws.AWS_Config                                import ENV_NAME__AWS_ENDPOINT_URL
+from osbot_local_stack.local_stack.Local_Stack__Internal import Local_Stack__Internal, DEFAULT__LOCAL_STACK__TARGET_SERVER
+from osbot_utils.utils.Env                               import get_env, set_env, del_env
+from osbot_utils.base_classes.Type_Safe                  import Type_Safe
+
+class Local_Stack(Type_Safe):
+    endpoint_url__saved  : str                   = None
+    local_stack__internal: Local_Stack__Internal
+
+    def __enter__(self):
+        self.activate()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.deactivate()
+        return self
+
+
+    def activate(self):
+        endpoint_url = self.local_stack__internal.endpoint_url
+        self.endpoint_url__saved = get_env(ENV_NAME__AWS_ENDPOINT_URL)
+        set_env(ENV_NAME__AWS_ENDPOINT_URL, endpoint_url)
+        return self
+
+    def deactivate(self):
+        if self.endpoint_url__saved is None:
+            del_env(ENV_NAME__AWS_ENDPOINT_URL)
+        else:
+            set_env(ENV_NAME__AWS_ENDPOINT_URL, self.endpoint_url__saved)
+        return self
+
+    def check__local_stack__health(self):
+        return self.local_stack__internal.get__internal_health() != {}
+
+    def check__local_stack__boto3_setup(self):
+        return STS().client().meta.endpoint_url == DEFAULT__LOCAL_STACK__TARGET_SERVER
+
+    def is_local_stack_configured_and_available(self):
+        return self.check__local_stack__health() and self.check__local_stack__boto3_setup()
+
+    def local_stack__health(self):
+        return self.local_stack__internal.get__internal_health()
+
+
