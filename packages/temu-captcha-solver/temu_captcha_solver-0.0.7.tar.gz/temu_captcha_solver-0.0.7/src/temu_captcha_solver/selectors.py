1b1adfbb5f5545d9fe3ca9d72fb7bd19ef8de1f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 15:03:56 +0000

    Add package temu-captcha-solver/0.0.7/temu_captcha_solver-0.0.7.tar.gz

---
+++ packages/temu-captcha-solver/temu_captcha_solver-0.0.7.tar.gz/temu_captcha_solver-0.0.7/src/temu_captcha_solver/selectors.py
@@ -0,0 +1,13 @@
+ARCED_SLIDE_PUZZLE_IMAGE_SELECTOR = "#slider > img"
+ARCED_SLIDE_PIECE_CONTAINER_SELECTOR = "#img-button"
+ARCED_SLIDE_PIECE_IMAGE_SELECTOR = "#img-button > img"
+ARCED_SLIDE_BUTTON_SELECTOR = "#slide-button"
+ARCED_SLIDE_UNIQUE_IDENTIFIERS = [".handleBar-vT4I5", ".vT4I57cQ"]
+
+PUZZLE_BUTTON_SELECTOR = "#slide-button"
+PUZZLE_PUZZLE_IMAGE_SELECTOR = ".slider-img-bg-1konc"
+PUZZLE_PIECE_IMAGE_SELECTOR = "#img-button > img"
+PUZZLE_BAR_SELECTOR = ".handlebar-xHMxu"
+PUZZLE_UNIQUE_IDENTIFIERS = ["#Slider"]
+
+CAPTCHA_PRESENCE_INDICATORS = ["#verification", "#slide-button", "#Slider", "#slider"]
