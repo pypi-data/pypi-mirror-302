c537fa1093862a6325d20c3cb0f70bed067fa1cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 08:04:02 +0000

    Add package tonutils/0.1.5/tonutils-0.1.5.tar.gz

---
+++ packages/tonutils/tonutils-0.1.5.tar.gz/tonutils-0.1.5/tonutils/wallet/__init__.py
@@ -0,0 +1,27 @@
+from .contract import (
+    Wallet,
+
+    HighloadWalletV2,
+    HighloadWalletV3,
+    PreprocessedWalletV2,
+    PreprocessedWalletV2R1,
+    WalletV3R1,
+    WalletV3R2,
+    WalletV4R1,
+    WalletV4R2,
+    WalletV5R1,
+)
+
+__all__ = [
+    "Wallet",
+
+    "HighloadWalletV2",
+    "HighloadWalletV3",
+    "PreprocessedWalletV2",
+    "PreprocessedWalletV2R1",
+    "WalletV3R1",
+    "WalletV3R2",
+    "WalletV4R1",
+    "WalletV4R2",
+    "WalletV5R1",
+]
