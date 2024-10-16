e95dd5e8e8275230a0babbda1e8110cb561e8d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 17:04:29 +0000

    Add package wahoomc/4.2.1a4/wahoomc-4.2.1a4.tar.gz

---
+++ packages/wahoomc/wahoomc-4.2.1a4.tar.gz/wahoomc-4.2.1a4/wahoomc/file_directory_functions.py
@@ -0,0 +1,217 @@
+"""
+constants, functions and object for file-system operations
+"""
+#!/usr/bin/python
+
+# import official python packages
+import json
+import os
+from os.path import isfile, join
+import sys
+import logging
+import shutil
+
+# import custom python packages
+
+log = logging.getLogger('main-logger')
+
+
+def move_content(src_folder_name, dst_path):
+    """
+    copy files from source directory of to destination directory
+    delete source directory afterwards
+
+    similar function to copy_or_move_files_and_folder but without user-request when overwriting
+    and only support for directories
+    """
+    # build path to old folder on the same level as wahooMapsCreator
+    par_dir = os.path.abspath(os.path.join(os.path.join(
+        os.path.dirname(__file__), os.pardir), os.pardir))
+    source_dir = os.path.join(par_dir, src_folder_name)
+
+    if os.path.exists(source_dir):
+        # copy & delete directory
+        for item in os.listdir(source_dir):
+            src = os.path.join(source_dir, item)
+            dst = os.path.join(dst_path, item)
+            # next, if destination directory exists
+            if os.path.isdir(dst):
+                continue
+
+            if os.path.isdir(src):
+                shutil.copytree(src, dst)
+            else:
+                shutil.copy2(src, dst)
+
+        shutil.rmtree(source_dir)
+
+
+def create_empty_directories(parent_dir, tiles_from_json, border_countries):
+    """
+    create empty directories for each tile and each country
+    """
+    for tile in tiles_from_json:
+        outdir = os.path.join(parent_dir,
+                              f'{tile["x"]}', f'{tile["y"]}')
+        os.makedirs(outdir, exist_ok=True)
+
+    for country in border_countries:
+        outdir = os.path.join(parent_dir, country)
+        os.makedirs(outdir, exist_ok=True)
+
+
+def read_json_file_country_config(json_file_path):
+    """
+    read the country config (of last run) from the given json file
+    """
+
+    log.debug('-' * 80)
+    log.debug('# Read country config json file')
+
+    country_config = read_json_file_generic(json_file_path)
+    if country_config == '':
+        log.error('! Json file could not be opened.')
+        sys.exit()
+
+    log.debug(
+        '+ Use country config file %s', json_file_path)
+    log.debug('+ Read country config json file: OK')
+
+    return country_config
+
+
+def read_json_file_generic(json_file_path):
+    """
+    reads content of given .json file
+    """
+    try:
+        with open(json_file_path, encoding="utf-8") as json_file:
+            json_content = json.load(json_file)
+            json_file.close()
+
+        return json_content
+
+    except FileNotFoundError:
+        return {}
+
+
+def write_json_file_generic(json_file_path, json_content):
+    """
+    writes content to .json file
+    """
+    # Serializing json
+    json_content = json.dumps(json_content, indent=4)
+
+    # Writing to file
+    with open(json_file_path, "w", encoding="utf-8") as json_file:
+        json_file.write(json_content)
+        json_file.close()
+
+
+def get_files_in_folder(folder):
+    """
+    return filenames of given folder without path as list
+    """
+    onlyfiles = [f for f in os.listdir(folder) if isfile(join(folder, f))]
+
+    return onlyfiles
+
+
+def delete_o5m_pbf_files_in_folder(folder):
+    """
+    delete .o5m and .pbf files of given folder
+    """
+    onlyfiles = [f for f in os.listdir(folder) if isfile(join(folder, f))]
+
+    for file in onlyfiles:
+        if file.endswith('.o5m') or file.endswith('.pbf'):
+            try:
+                os.remove(os.path.join(folder, file))
+            except OSError:
+                pass
+
+
+def delete_everything_in_folder(folder):
+    """
+    delete all files ald directories of given folder
+    """
+    files_and_folders = list(os.listdir(folder)) # [f for f in os.listdir(folder)]
+
+    for file in files_and_folders:
+        try:
+            file_or_dir = os.path.join(folder, file)
+            # file or dir?
+            if os.path.isfile(file_or_dir):
+                # delete file
+                os.remove(file_or_dir)
+            else:
+                # delete directory if exists. copytree fails if dir exists already
+                shutil.rmtree(file_or_dir)
+        except OSError:
+            pass
+
+
+def copy_or_move_files_and_folder(from_path, to_path, delete_from_dir=False):
+    """
+    copy content from source directory to destination directory
+    optionally delete source directory afterwards
+
+    similar function to move_content but with user-request when overwriting and support for files
+    """
+    # check if from path/file exists at all
+    if os.path.exists(from_path):
+
+        # given path is a directory
+        if os.path.isdir(from_path):
+            # first create the to-directory if not already there
+            os.makedirs(to_path, exist_ok=True)
+
+            for item in os.listdir(from_path):
+                from_item = os.path.join(from_path, item)
+                to_item = os.path.join(to_path, item)
+
+                # copy directory
+                if os.path.isdir(from_item):
+                    copy_directory_w_user_input(from_item, to_item)
+
+                # copy file
+                else:
+                    copy_file_w_user_input(from_item, to_item)
+
+        # given path is a file
+        else:
+            copy_file_w_user_input(from_path, to_path)
+
+        # directory
+        if delete_from_dir:
+            shutil.rmtree(from_path)
+
+
+def copy_directory_w_user_input(from_item, to_item):
+    """
+    copy content from source directory to destination directory
+    """
+    if not os.path.isdir(to_item):
+        shutil.copytree(from_item, to_item)
+    else:
+        val = input(f"{to_item} exists already. Overwrite? (y/n):")
+        if val == 'y':
+            shutil.copytree(from_item, to_item)
+            log.info('! %s overwritten', to_item)
+        else:
+            log.debug('! %s not copied, exists already.', to_item)
+
+
+def copy_file_w_user_input(from_item, to_item):
+    """
+    copy source file to destination file
+    """
+    if not os.path.isfile(to_item):
+        shutil.copy2(from_item, to_item)
+    else:
+        val = input(f"{to_item} exists already. Overwrite? (y/n):")
+        if val == 'y':
+            shutil.copy2(from_item, to_item)
+            log.info('! %s overwritten', to_item)
+        else:
+            log.debug('! %s not copied, exists already.', to_item)
---
+++ packages/wahoomc/wahoomc-4.2.1a4.tar.gz/wahoomc-4.2.1a4/wahoomc/setup_functions.py
@@ -0,0 +1,265 @@
+"""
+constants, functions and object for file-system operations
+"""
+#!/usr/bin/python
+
+# import official python packages
+import os
+import logging
+import platform
+import shutil
+from pathlib import Path
+import sys
+import pkg_resources
+
+# import custom python packages
+from wahoomc.file_directory_functions import delete_everything_in_folder, write_json_file_generic, \
+    read_json_file_generic, copy_or_move_files_and_folder
+from wahoomc.constants_functions import get_tooling_win_path, get_absolute_dir_user_or_repo
+from wahoomc.downloader import get_latest_pypi_version
+
+from wahoomc.constants import GEOFABRIK_PATH, USER_TOOLING_WIN_DIR, USER_WAHOO_MC
+from wahoomc.constants import USER_DL_DIR
+from wahoomc.constants import USER_MAPS_DIR
+from wahoomc.constants import USER_OUTPUT_DIR
+from wahoomc.constants import USER_CONFIG_DIR
+from wahoomc.constants import VERSION
+
+log = logging.getLogger('main-logger')
+
+config_file_path = os.path.join(USER_WAHOO_MC, ".config.json")
+
+
+def initialize_work_directories():
+    """
+    Initialize work directories
+    """
+    os.makedirs(USER_WAHOO_MC, exist_ok=True)
+    os.makedirs(USER_DL_DIR, exist_ok=True)
+    os.makedirs(USER_MAPS_DIR, exist_ok=True)
+    os.makedirs(USER_OUTPUT_DIR, exist_ok=True)
+    os.makedirs(USER_CONFIG_DIR, exist_ok=True)
+
+
+def adjustments_due_to_breaking_changes():
+    """
+    handle breaking changes
+    """
+    version_last_run = read_version_last_run() # pylint: disable=unused-variable
+
+    # Osmosis in v.0.49.2 seams not to be working on WINDOWS since now
+    # - due to the path into it was downloaded, 'tooling_win/Osmosis/osmosis-0.49.2'
+    #                                   and not 'tooling_win/Osmosis'
+    # - due to behaviour of reading/using plugins in v0.49.2 that seams not to be compatible
+    #   of using any plugins and in particular the mapwriter plugin
+    # due to that all Windows tooling files deleted here.
+    if (version_last_run is None or \
+            pkg_resources.parse_version(VERSION) <= pkg_resources.parse_version('4.2.1')) and \
+            platform.system() == "Windows":
+        log.info(
+            'Last run was with version %s, deleting Windows Tooling files of %s directory due to possible bad files.', version_last_run, USER_TOOLING_WIN_DIR)
+        delete_everything_in_folder(USER_TOOLING_WIN_DIR)
+
+
+def check_installation_of_required_programs():
+    """
+    check if required programs are installed
+    """
+    text_to_docu = "\nPlease refer to the Quickstart Guide of wahooMapsCreator for instructions:\n- https://github.com/treee111/wahooMapsCreator/blob/develop/docs/QUICKSTART_ANACONDA.md \
+                    \nor create an issue:\n- https://github.com/treee111/wahooMapsCreator/issues"
+
+    if not is_program_installed("java"):
+        sys.exit(
+            f"Java is not installed. {text_to_docu}")
+
+    if not os.path.isfile(GEOFABRIK_PATH):
+        sys.exit('Geofabrik file is not downloaded. Please create an issue:\n- https://github.com/treee111/wahooMapsCreator/issues"')
+
+    if platform.system() == "Windows":
+        if not os.path.exists(get_tooling_win_path(
+                os.path.join('Osmosis', 'bin', 'osmosis.bat'), in_user_dir=True)):
+            sys.exit(
+                f"Osmosis is not available. {text_to_docu}")
+
+        if not os.path.exists(get_tooling_win_path('osmconvert.exe')):
+            sys.exit(
+                f"osmconvert is not available. {text_to_docu}")
+
+        if not os.path.exists(get_tooling_win_path('osmfilter.exe', in_user_dir=True)):
+            sys.exit(
+                f"osmfilter is not available. {text_to_docu}")
+
+        if not os.path.exists(get_tooling_win_path('7za.exe')):
+            sys.exit(
+                f"7za is not available. {text_to_docu}")
+
+    else:
+        if not is_program_installed("osmium"):
+            sys.exit(
+                f"osmium-tool is not installed. {text_to_docu}")
+
+        if not is_program_installed("osmosis"):
+            sys.exit(
+                f"Osmosis is not installed. {text_to_docu}")
+
+        if not is_map_writer_plugin_installed():
+            sys.exit(
+                f"mapsforge-map-writer plugin is not installed. {text_to_docu}")
+
+
+def check_installation_of_programs_credentials_for_contour_lines():
+    """
+    check if additionals programs are installed
+    """
+    text_to_docu = "\nYou have choosen to process contour lines. That needs additional programs. \
+                    \nPlease refer to the Quickstart Guide of wahooMapsCreator for instructions:\n- https://github.com/treee111/wahooMapsCreator/blob/develop/docs/QUICKSTART_ANACONDA.md#additional-programs-for-generating-contour-lines \
+                    \nor create an issue:\n- https://github.com/treee111/wahooMapsCreator/issues"
+
+    if not is_program_installed("phyghtmap"):
+        sys.exit(
+            f"phyghtmap is not installed. {text_to_docu}")
+
+    username, password = read_earthexplorer_credentials()
+
+    if not username or not password:
+        username, password = ask_for_and_write_earthexplorer_credentials()
+
+
+def is_program_installed(program):
+    """
+    check if a given program is installed
+    """
+    if shutil.which(program) is not None:
+        return True
+
+    return False
+
+
+def is_map_writer_plugin_installed():
+    """
+    tests, if the mapwriter plugin is in the correct location
+
+    Example filename for the map-writer-plugin
+    mapsforge-map-writer-master-20210527.154736-408-jar-with-dependencies.jar
+    downloaded on 01.10.2022: mapsforge-map-writer-0.18.0-jar-with-dependencies.jar
+    """
+    map_writer_path = os.path.join(
+        str(Path.home()), '.openstreetmap', 'osmosis', 'plugins')
+
+    # test if the file is there
+    try:
+        for file in os.listdir(map_writer_path):
+            if "mapsforge-map-writer" in file:
+                return True
+    # if there is no file in the plugins directory
+    except FileNotFoundError:
+        pass
+
+    return False
+
+
+def write_config_file(config_to_write=''):
+    """
+    Write config file of wahoomc to root directory
+    incorporate given content to existing file
+    """
+    # Data to be written
+    default_config = {
+        "version_last_run": VERSION
+    }
+
+    # if no config to write is given, write default config - normally at the end of main()
+    if not config_to_write:
+        config_to_write = default_config
+
+    actual_content = read_json_file_generic(config_file_path)
+
+    for key, value in config_to_write.items():
+        # overwrite value or insert new item
+        actual_content[key] = value
+
+    # write changed content to disc
+    write_json_file_generic(config_file_path, actual_content)
+
+
+def read_version_last_run():
+    """
+    Read the version of wahoomc's last run
+    by reading json and access version attribute, if not set, give None
+    """
+    try:
+        version_last_run = read_json_file_generic(config_file_path)[
+            "version_last_run"]
+    except KeyError:
+        version_last_run = None
+
+    return version_last_run
+
+
+def ask_for_and_write_earthexplorer_credentials():
+    """
+    Ask user for credentials for https://ers.cr.usgs.gov and save in the config file
+    """
+    log.warning(
+        'No saved credentials found for https://ers.cr.usgs.gov. Please register and enter your credentials.')
+    username = input('https://ers.cr.usgs.gov username:')
+    password = input('https://ers.cr.usgs.gov password:')
+
+    credentials_to_write = {
+        'earthexplorer-user': username, 'earthexplorer-password': password}
+
+    write_config_file(credentials_to_write)
+
+    return username, password
+
+
+def read_earthexplorer_credentials():
+    """
+    Read the version of wahoomc's last run
+    by reading json and access version attribute, if not set, give None
+    """
+    try:
+        username = read_json_file_generic(config_file_path)[
+            "earthexplorer-user"]
+        password = read_json_file_generic(config_file_path)[
+            "earthexplorer-password"]
+    except KeyError:
+        username = None
+        password = None
+
+    return username, password
+
+
+def copy_jsons_from_repo_to_user(folder, file=''):
+    """
+    copies files from wahoomc repo/package to the user-directory
+    """
+    absolute_paths = get_absolute_dir_user_or_repo(folder, file)
+
+    log.debug('# Copy "%s" files from repo to directory if not existing: %s',
+              folder, absolute_paths[0])
+
+    # copy files of wahoomc package directory to user directory
+    copy_or_move_files_and_folder(
+        absolute_paths[1], absolute_paths[0], delete_from_dir=False)
+
+    log.info('# Copy "%s" files from wahoomc installation to directory if not existing: %s : OK',
+             folder, absolute_paths[0])
+
+
+def check_installed_version_against_latest_pypi():
+    """
+    get latest wahoomc version available on PyPI and compare with locally installed version
+    """
+    # get latest wahoomc version available on PyPI
+    latest_version = get_latest_pypi_version()
+
+    # compare installed version against latest and issue a info if a new version is available
+    if latest_version \
+            and pkg_resources.parse_version(VERSION) < pkg_resources.parse_version(latest_version):
+        log.info('\n\nUpdate available! \
+                \nA new version of wahoomc is available: "%s". You have installed version "%s". \
+                \nUpgrade wahoomc with "pip install wahoomc --upgrade". \
+                \nRelease notes are here: https://github.com/treee111/wahooMapsCreator/releases/latest. \
+                \n',
+                 latest_version, VERSION)
