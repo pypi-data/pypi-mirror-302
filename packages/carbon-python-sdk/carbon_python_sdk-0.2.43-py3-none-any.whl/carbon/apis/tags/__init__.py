3b3842bfdafda04c2050abd8e38f1516b1f3d668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 14:04:18 +0000

    Add package carbon-python-sdk/0.2.43/carbon_python_sdk-0.2.43-py3-none-any.whl

---
+++ packages/carbon-python-sdk/carbon_python_sdk-0.2.43-py3-none-any.whl/carbon/apis/tags/__init__.py
@@ -0,0 +1,23 @@
+# do not import all endpoints into this module because that uses a lot of memory and stack frames
+# if you need the ability to import all endpoints from this module, import them with
+# from carbon.apis.tag_to_api import tag_to_api
+
+import enum
+
+
+class TagValues(str, enum.Enum):
+    INTEGRATIONS = "Integrations"
+    FILES = "Files"
+    UTILITIES = "Utilities"
+    CRM = "CRM"
+    GITHUB = "Github"
+    USERS = "Users"
+    EMBEDDINGS = "Embeddings"
+    WEBHOOKS = "Webhooks"
+    ORGANIZATIONS = "Organizations"
+    AUTH = "Auth"
+    DATA_SOURCES = "Data Sources"
+    GETTING_STARTED = "Getting Started"
+    CARBON_CONNECT = "Carbon Connect"
+    HEALTH = "Health"
+    CONTACT_US = "Contact Us"
