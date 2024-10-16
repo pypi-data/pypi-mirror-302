57a67dffd5f5922b0e3170199882fee5443f3583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 04:04:05 +0000

    Add package govuk-frontend-wtf/3.2.0/govuk_frontend_wtf-3.2.0.tar.gz

---
+++ packages/govuk-frontend-wtf/govuk_frontend_wtf-3.2.0.tar.gz/govuk_frontend_wtf-3.2.0/setup.py
@@ -0,0 +1,49 @@
+import glob
+import os
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+templates = []
+directories = glob.glob("govuk_frontend_wtf/templates/*.html")
+for directory in directories:
+    templates.append(os.path.relpath(os.path.dirname(directory), "govuk_frontend_wtf") + "/*.html")
+
+setuptools.setup(
+    name="govuk-frontend-wtf",
+    version="3.2.0",
+    author="Matt Shaw",
+    author_email="matthew.shaw@landregistry.gov.uk",
+    description="GOV.UK Frontend WTForms Widgets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LandRegistry/govuk-frontend-wtf",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    package_data={"govuk_frontend_wtf": templates},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Software Development :: User Interfaces",
+        "Topic :: Text Processing :: Markup :: HTML",
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+        "deepmerge",
+        "flask",
+        "flask-wtf",
+        "govuk-frontend-jinja>=3.0.0",
+        "jinja2",
+        "wtforms>=3.1.0",
+    ],
+)
