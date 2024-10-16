b0a2f14163957ca564704aaee2f1ee59617efe19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 05:05:32 +0000

    Add package abtestools/1.1.3/abtestools-1.1.3-py3-none-any.whl

---
+++ packages/abtestools/abtestools-1.1.3-py3-none-any.whl/abtestools/audiences/__init__.py
@@ -0,0 +1,9 @@
+"""
+# Audiences Module
+
+Contains all the tools related to Campaign Audiences
+- Audience
+- User
+"""
+
+from abtestools.audiences._audiences import Audience, User
---
+++ packages/abtestools/abtestools-1.1.3-py3-none-any.whl/abtestools/test/__init__.py
@@ -0,0 +1,10 @@
+"""
+# Test Module
+
+Contains all the tools related to AB tests.
+- Metric
+- Test
+- TestResult
+"""
+
+from abtestools.test.abtest import Metric, Test, TestResult
