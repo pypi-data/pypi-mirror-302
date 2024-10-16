2143f25f366be833c3e2aef2b8653c5b5ee5562c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 05:04:39 +0000

    Add package edx-django-utils/6.1.0/edx_django_utils-6.1.0-py2.py3-none-any.whl

---
+++ packages/edx-django-utils/edx_django_utils-6.1.0-py2.py3-none-any.whl/edx_django_utils/monitoring/__init__.py
@@ -0,0 +1,33 @@
+"""
+Metrics utilities public api
+
+See README.rst for details.
+"""
+from .internal.backends import DatadogBackend, NewRelicBackend, OpenTelemetryBackend, TelemetryBackend
+from .internal.code_owner.middleware import CodeOwnerMonitoringMiddleware
+from .internal.code_owner.utils import (
+    get_code_owner_from_module,
+    set_code_owner_attribute,
+    set_code_owner_attribute_from_module
+)
+from .internal.middleware import (
+    CachedCustomMonitoringMiddleware,
+    CookieMonitoringMiddleware,
+    DeploymentMonitoringMiddleware,
+    FrontendMonitoringMiddleware,
+    MonitoringMemoryMiddleware,
+    MonitoringSupportMiddleware
+)
+from .internal.transactions import get_current_transaction, ignore_transaction
+from .internal.utils import (
+    accumulate,
+    background_task,
+    function_trace,
+    increment,
+    record_exception,
+    set_custom_attribute,
+    set_custom_attributes_for_course_key,
+    set_monitoring_transaction_name
+)
+# "set_custom_metric*" methods are deprecated
+from .utils import set_custom_metric, set_custom_metrics_for_course_key
