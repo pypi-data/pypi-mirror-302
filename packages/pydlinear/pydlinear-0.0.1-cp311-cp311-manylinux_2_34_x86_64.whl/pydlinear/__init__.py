9fe461f23eb75943cf5dd8a16164fc464048ae4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 08:04:09 +0000

    Add package pydlinear/0.0.1/pydlinear-0.0.1-cp311-cp311-manylinux_2_34_x86_64.whl

---
+++ packages/pydlinear/pydlinear-0.0.1-cp311-cp311-manylinux_2_34_x86_64.whl/pydlinear/__init__.py
@@ -0,0 +1,6 @@
+from ._pydlinear import *
+from ._pydlinear import __version__ as __pydlinear_version__, __doc__ as __pydlinear_doc__
+
+__version__ = __pydlinear_version__
+__doc__ = __pydlinear_doc__
+
