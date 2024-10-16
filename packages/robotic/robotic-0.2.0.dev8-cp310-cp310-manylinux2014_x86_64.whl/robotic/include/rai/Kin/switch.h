b6618407f1be9f1be434d36994087007e68b98c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 03:11:44 +0000

    Add package robotic/0.2.0.dev8/robotic-0.2.0.dev8-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/robotic/robotic-0.2.0.dev8-cp310-cp310-manylinux2014_x86_64.whl/robotic/include/rai/Kin/switch.h
@@ -0,0 +1,68 @@
+/*  ------------------------------------------------------------------
+    Copyright (c) 2011-2024 Marc Toussaint
+    email: toussaint@tu-berlin.de
+
+    This code is distributed under the MIT License.
+    Please see <root-path>/LICENSE for details.
+    --------------------------------------------------------------  */
+
+#pragma once
+
+#include "frame.h"
+
+namespace rai {
+
+enum SwitchType {
+  SW_none=-1,
+  SW_noJointLink=0,
+  SW_joint,
+  makeDynamic,
+  makeKinematic,
+  SW_delContact,
+  SW_addContact,
+  SW_addPOAonly,
+};
+
+enum SwitchInitializationType {
+  SWInit_zero=0,
+  SWInit_copy,
+  SWInit_random
+};
+
+struct KinematicSwitch {
+  Enum<SwitchType> symbol;
+  Enum<JointType> jointType;
+  Enum<SwitchInitializationType> init;
+  int timeOfApplication;
+  int fromId, toId;
+  bool isStable=false;
+  rai::Transformation rel;
+  KinematicSwitch();
+  KinematicSwitch(SwitchType op, JointType type,
+                  int aFrame, int bFrame,
+                  SwitchInitializationType _init=SWInit_zero,
+                  int _timeOfApplication=0,
+                  const rai::Transformation& _rel=NoTransformation); //, const rai::Transformation& jTo=NoTransformation);
+  KinematicSwitch(SwitchType op, JointType type,
+                  const char* ref1, const char* ref2,
+                  const rai::Configuration& K,
+                  SwitchInitializationType _init=SWInit_zero,
+                  int _timeOfApplication=0,
+                  const rai::Transformation& _rel=NoTransformation); //, const rai::Transformation& jTo=NoTransformation);
+  void setTimeOfApplication(double time, bool before, int stepsPerPhase, uint T);
+  Frame* apply(FrameL& frames) const;
+  rai::String shortTag(const Configuration* G) const;
+  void write(std::ostream& os, const FrameL& frames= {}) const;
+};
+
+} // namespace rai
+
+stdOutPipe(rai::KinematicSwitch)
+
+int conv_time2step(double time, uint stepsPerPhase);
+double conv_step2time(int step, uint stepsPerPhase);
+void conv_times2steps(int& fromStep, int& toStep, const arr& times, int stepsPerPhase, uint T,
+                      int deltaFromStep, int deltaToStep);
+intA conv_times2tuples(const arr& times, uint order, int stepsPerPhase, uint T,
+                       int deltaFromStep, int deltaToStep);
+
