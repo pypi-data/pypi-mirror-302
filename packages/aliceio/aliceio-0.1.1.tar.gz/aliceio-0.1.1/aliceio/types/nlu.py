a9692ba4677f0607250f97256f2b280283cf982e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 05:04:00 +0000

    Add package aliceio/0.1.1/aliceio-0.1.1.tar.gz

---
+++ packages/aliceio/aliceio-0.1.1.tar.gz/aliceio-0.1.1/aliceio/types/nlu.py
@@ -0,0 +1,35 @@
+from typing import TYPE_CHECKING, Any, Dict, List
+
+from .base import AliceObject
+from .entity import Entity
+
+Intents = Dict[str, Any]
+
+
+class NLU(AliceObject):
+    """
+    Слова и сущности, которые Диалоги извлекли из запроса пользователя.
+
+    [Source](https://yandex.ru/dev/dialogs/alice/doc/ru/request-simpleutterance#nlu-desc)
+    """
+
+    tokens: List[str]
+    entities: List[Entity]
+    intents: Intents
+
+    if TYPE_CHECKING:
+
+        def __init__(
+            __pydantic_self__,
+            *,
+            tokens: List[str],
+            entities: List[Entity],
+            intents: Intents,
+            **__pydantic_kwargs: Any,
+        ) -> None:
+            super().__init__(
+                tokens=tokens,
+                entities=entities,
+                intents=intents,
+                **__pydantic_kwargs,
+            )
