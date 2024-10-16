b3d02bc2cf8916b0a73e5a8336fe1a74b6867344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 17:04:39 +0000

    Add package performTestingforTime/0.2.0/performtestingfortime-0.2.0.tar.gz

---
+++ packages/performTestingforTime/performtestingfortime-0.2.0.tar.gz/performtestingfortime-0.2.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'performTestingforTime',
+    version = '0.2.0',
+    description = 'Tests the performance of functions according by time',
+    author = 'Tolga',
+    packages=find_packages(),
+)
