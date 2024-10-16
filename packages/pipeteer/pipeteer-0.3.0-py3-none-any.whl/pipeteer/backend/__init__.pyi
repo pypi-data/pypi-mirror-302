e15ffc07ef7b76ba934f11485324c602bc687c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 03:10:45 +0000

    Add package pipeteer/0.3.0/pipeteer-0.3.0-py3-none-any.whl

---
+++ packages/pipeteer/pipeteer-0.3.0-py3-none-any.whl/pipeteer/backend/__init__.pyi
@@ -0,0 +1,4 @@
+from .backend import Backend
+from .default import DefaultBackend
+
+__all__ = ['Backend', 'DefaultBackend']
---
+++ packages/pipeteer/pipeteer-0.3.0-py3-none-any.whl/pipeteer/util/__init__.pyi
@@ -0,0 +1,5 @@
+from .types import num_params, param_type, type_arg, Func1or2, Func2or3
+
+__all__ = [
+  'num_params', 'param_type', 'type_arg', 'Func1or2', 'Func2or3'
+]
