52c91b97738da9e99dbf0e9381e058fbc31107c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 17:04:16 +0000

    Add package pycompileo/1.3.5/pycompileo-1.3.5.tar.gz

---
+++ packages/pycompileo/pycompileo-1.3.5.tar.gz/pycompileo-1.3.5/pycompileo/obfuscation/__init__.py
@@ -0,0 +1,5 @@
+from .obfuscate_package import obfuscate_package
+from .multiversion_obfuscate import multiversion_obfuscate_package
+
+from .obfuscate_module import obfuscate_module
+from .obfuscate import obfuscate
