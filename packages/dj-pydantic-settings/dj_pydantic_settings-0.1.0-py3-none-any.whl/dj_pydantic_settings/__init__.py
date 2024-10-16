cf8183ef287c08667a4ad194a8c43fc029314ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 04:04:46 +0000

    Add package dj-pydantic-settings/0.1.0/dj_pydantic_settings-0.1.0-py3-none-any.whl

---
+++ packages/dj-pydantic-settings/dj_pydantic_settings-0.1.0-py3-none-any.whl/dj_pydantic_settings/__init__.py
@@ -0,0 +1,51 @@
+import dj_database_url
+from pydantic import AliasGenerator, BaseModel, ConfigDict, model_serializer
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+alias_generator = AliasGenerator(serialization_alias=str.upper)
+
+
+class DjangoModel(BaseModel):
+    """
+    Базовая модель для настроек. Так как в django конфигурация осуществляется путём определения констант
+    в модуле settings, удобно иметь модель, которая сама переводит ключи в верхний регистр. Также это удобно для
+    словарей TEMPLATES, REST_FRAMEWORK и т.п.
+    """
+
+    model_config = ConfigDict(alias_generator=alias_generator)
+
+
+class Database(BaseModel):
+    """
+    Параметры подключения к базе данных. Основано на параметрах функции dj_database_url.parse
+    """
+
+    url: str
+    engine: str | None = None
+    conn_max_age: int | None = 0
+    conn_health_checks: bool = False
+    disable_server_side_cursors: bool = False
+    ssl_require: bool = False
+    test_options: dict | None = None
+
+    @model_serializer
+    def serialize(self) -> dj_database_url.DBConfig:
+        return dj_database_url.parse(**{field: value for field, value in self})
+
+
+class DjangoSettings(BaseSettings):
+    """
+    Базовый класс для настроек django-проекта
+    """
+
+    model_config = SettingsConfigDict(alias_generator=alias_generator)
+
+    def __init__(self, **values):
+        super().__init__(**values)
+        self.set_globals()
+
+    def set_globals(self) -> None:
+        """
+        Заполняет содержимым модели глобальные переменные модуля, в котором вызван
+        """
+        globals().update(self.model_dump(by_alias=True))
