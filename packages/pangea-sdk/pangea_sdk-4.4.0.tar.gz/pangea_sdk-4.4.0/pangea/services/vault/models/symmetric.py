636f726bc514d3a6bfe760e3f910aa1797f3f8d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 06:03:53 +0000

    Add package pangea-sdk/4.4.0/pangea_sdk-4.4.0.tar.gz

---
+++ packages/pangea-sdk/pangea_sdk-4.4.0.tar.gz/pangea_sdk-4.4.0/pangea/services/vault/models/symmetric.py
@@ -0,0 +1,67 @@
+# Copyright 2022 Pangea Cyber Corporation
+# Author: Pangea Cyber Corporation
+from typing import Optional
+
+from pangea.response import APIRequestModel, PangeaResponseResult
+from pangea.services.vault.models.common import (
+    CommonGenerateRequest,
+    CommonGenerateResult,
+    CommonStoreRequest,
+    CommonStoreResult,
+    EncodedSymmetricKey,
+    KeyPurpose,
+    SymmetricAlgorithm,
+)
+
+
+class SymmetricStoreRequest(CommonStoreRequest):
+    key: EncodedSymmetricKey
+    algorithm: SymmetricAlgorithm
+    purpose: KeyPurpose
+    exportable: Optional[bool] = None
+    """Whether the key is exportable or not."""
+
+
+class SymmetricStoreResult(CommonStoreResult):
+    algorithm: str
+    purpose: str
+
+
+class SymmetricGenerateRequest(CommonGenerateRequest):
+    algorithm: SymmetricAlgorithm
+    purpose: KeyPurpose
+    exportable: Optional[bool] = None
+    """Whether the key is exportable or not."""
+
+
+class SymmetricGenerateResult(CommonGenerateResult):
+    algorithm: str
+    purpose: str
+
+
+class EncryptRequest(APIRequestModel):
+    id: str
+    plain_text: str
+    version: Optional[int] = None
+    additional_data: Optional[str] = None
+
+
+class EncryptResult(PangeaResponseResult):
+    id: str
+    version: int
+    algorithm: str
+    cipher_text: str
+
+
+class DecryptRequest(APIRequestModel):
+    id: str
+    cipher_text: str
+    version: Optional[int] = None
+    additional_data: Optional[str] = None
+
+
+class DecryptResult(PangeaResponseResult):
+    id: str
+    version: int
+    algorithm: str
+    plain_text: str
