9e4d6abcc640928987fcc35dbd93fa7a5378c7e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 07:04:09 +0000

    Add package pyhafas/0.6.0/pyhafas-0.6.0.tar.gz

---
+++ packages/pyhafas/pyhafas-0.6.0.tar.gz/pyhafas-0.6.0/tests/db/request/journey_test.py
@@ -0,0 +1,21 @@
+import datetime
+
+from pyhafas import HafasClient
+from pyhafas.profile import DBProfile
+from pyhafas.types.fptf import Journey
+
+
+def test_db_journey_request():
+    profile = DBProfile()
+    client = HafasClient(profile)
+    journeys = client.journeys(
+        destination="8011306",
+        origin="8011160",
+        date=datetime.datetime.now(),
+        min_change_time=0,
+        max_changes=-1
+    )
+    assert journeys
+
+    journey = client.journey(journey=journeys[0].id)
+    assert isinstance(journey, Journey)
