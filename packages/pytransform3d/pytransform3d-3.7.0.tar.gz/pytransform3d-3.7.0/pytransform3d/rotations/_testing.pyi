010afba71bae7eb0f03f5eed96d06722544d1307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 06:04:26 +0000

    Add package pytransform3d/3.7.0/pytransform3d-3.7.0.tar.gz

---
+++ packages/pytransform3d/pytransform3d-3.7.0.tar.gz/pytransform3d-3.7.0/pytransform3d/rotations/_testing.pyi
@@ -0,0 +1,14 @@
+import numpy as np
+import numpy.typing as npt
+
+
+def assert_axis_angle_equal(a1: npt.ArrayLike, a2: npt.ArrayLike, *args, **kwargs): ...
+
+
+def assert_compact_axis_angle_equal(a1: npt.ArrayLike, a2: npt.ArrayLike, *args, **kwargs): ...
+
+
+def assert_quaternion_equal(q1: npt.ArrayLike, q2: npt.ArrayLike, *args, **kwargs): ...
+
+
+def assert_rotation_matrix(R: npt.ArrayLike, *args, **kwargs): ...
