f7c0c4a8e86e22d49b851a8490fb08c9fd9ccfea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 15:03:54 +0000

    Add package p12/3.12.0.dev1/p12-3.12.0.dev1-py3-none-any.whl

---
+++ packages/p12/p12-3.12.0.dev1-py3-none-any.whl/p12/__init__.py
@@ -0,0 +1,6 @@
+"""."""
+from p12.twelve import v12
+from .app import (v3120_app, v3120_app_)
+
+
+__all__ = ["v12", "v3120_app", "v3120_app_"]
---
+++ packages/p12/p12-3.12.0.dev1-py3-none-any.whl/p12/twelve/__init__.py
@@ -0,0 +1,5 @@
+from version3900 import Version
+
+v12 = Version('3.12', 'nation-station-candy')
+
+__all__ = ["v12"]
