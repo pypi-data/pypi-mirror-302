10e10ed4a97425a6decf4608aae5daf88785d9d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-16 06:04:11 +0000

    Add package edsl/0.1.34/edsl-0.1.34.tar.gz

---
+++ packages/edsl/edsl-0.1.34.tar.gz/edsl-0.1.34/edsl/inference_services/registry.py
@@ -0,0 +1,31 @@
+from edsl.inference_services.InferenceServicesCollection import (
+    InferenceServicesCollection,
+)
+
+from edsl.inference_services.OpenAIService import OpenAIService
+from edsl.inference_services.AnthropicService import AnthropicService
+from edsl.inference_services.DeepInfraService import DeepInfraService
+from edsl.inference_services.GoogleService import GoogleService
+from edsl.inference_services.GroqService import GroqService
+from edsl.inference_services.AwsBedrock import AwsBedrockService
+from edsl.inference_services.AzureAI import AzureAIService
+from edsl.inference_services.OllamaService import OllamaService
+from edsl.inference_services.TestService import TestService
+from edsl.inference_services.MistralAIService import MistralAIService
+from edsl.inference_services.TogetherAIService import TogetherAIService
+
+default = InferenceServicesCollection(
+    [
+        OpenAIService,
+        AnthropicService,
+        DeepInfraService,
+        GoogleService,
+        GroqService,
+        AwsBedrockService,
+        AzureAIService,
+        OllamaService,
+        TestService,
+        MistralAIService,
+        TogetherAIService,
+    ]
+)
