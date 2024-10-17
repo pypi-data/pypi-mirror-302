56bcc4eaeb6eb41229b1e101959c5ac0e5549fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 06:04:22 +0000

    Add package al360-tai-test-utils/0.4.2/al360_tai_test_utils-0.4.2-py3-none-any.whl

---
+++ packages/al360-tai-test-utils/al360_tai_test_utils-0.4.2-py3-none-any.whl/al360_tai_test_utils/models/xgboost/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) AffectLog SAS
+# Licensed under the MIT License.
+
+"""Namespace for xgboost models."""
+
+from .xgboost_model_utils import create_xgboost_classifier
+
+__all__ = ["create_xgboost_classifier"]
---
+++ packages/al360-tai-test-utils/al360_tai_test_utils-0.4.2-py3-none-any.whl/al360_tai_test_utils/version.py
@@ -0,0 +1,8 @@
+# Copyright (c) AffectLog SAS
+# Licensed under the MIT License.
+
+name = 'al360_tai_test_utils'
+_major = '0'
+_minor = '4'
+_patch = '2'
+version = '{}.{}.{}'.format(_major, _minor, _patch)
