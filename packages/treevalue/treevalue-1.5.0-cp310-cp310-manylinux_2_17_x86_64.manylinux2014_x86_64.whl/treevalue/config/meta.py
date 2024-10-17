b1442c7a6a4dd72ff0a30533a4face23e79552b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 03:10:11 +0000

    Add package treevalue/1.5.0/treevalue-1.5.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/treevalue/treevalue-1.5.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/treevalue/config/meta.py
@@ -0,0 +1,19 @@
+"""
+Overview:
+    Meta information for treevalue package.
+"""
+
+#: Title of this project (should be `treevalue`).
+__TITLE__ = "treevalue"
+
+#: Version of this project.
+__VERSION__ = "1.5.0"
+
+#: Short description of the project, will be included in ``setup.py``.
+__DESCRIPTION__ = 'A flexible, generalized tree-based data structure.'
+
+#: Author of this project.
+__AUTHOR__ = "HansBug, DI-engine's Contributors"
+
+#: Email of the authors'.
+__AUTHOR_EMAIL__ = "hansbug@buaa.edu.cn"
