9ba24033aa9f1056d73fd95e68bb03404c0de8a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 06:04:44 +0000

    Add package al360-trustworthyai-text/0.2.7/al360_trustworthyai_text-0.2.7-py3-none-any.whl

---
+++ packages/al360-trustworthyai-text/al360_trustworthyai_text-0.2.7-py3-none-any.whl/al360_trustworthyai_text/rai_text_insights/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) AffectLog SAS
+# Licensed under the MIT License.
+
+"""Implementation of RAI Text Insights API."""
+
+from al360_trustworthyai_text.al360_tai_text_insights.al360_tai_text_insights import \
+    RAITextInsights
+
+__all__ = ['RAITextInsights']
