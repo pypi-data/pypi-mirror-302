4117a4350373fc6fdbbd654d72bae06aad333122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 05:04:28 +0000

    Add package pyamino/1.3.1/pyamino-1.3.1.tar.gz

---
+++ packages/pyamino/pyamino-1.3.1.tar.gz/pyamino-1.3.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyamino",
+    license="MIT",
+    author="pyminoTeam",
+    author_email="",
+    description="Easily create a bot for Amino Apps using a modern easy to use synchronous library.",
+    url="https://github.com/forevercynical/pymino",
+    version = "1.3.1",
+    packages=find_packages(),
+    install_requires=[
+        "requests==2.31.0",
+        "colorama==0.4.6",
+        "websocket-client==1.6.1",
+        "diskcache==5.6.1"
+    ],
+    keywords=[
+        "amino",
+        "pymino",
+        "narvii",
+        "amino-api",
+        "narvii-bots",
+        "aminoapps",
+        "amino-bot",
+        "amino-bots"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.8"
+)
