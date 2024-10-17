09c0e73020da2080c2c826829315d82dda356550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 06:04:20 +0000

    Add package mkdocs-audio/0.0.2/mkdocs_audio-0.0.2.tar.gz

---
+++ packages/mkdocs-audio/mkdocs_audio-0.0.2.tar.gz/mkdocs_audio-0.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='mkdocs-audio',
+    version='0.0.2',
+    author='Jean-Francois Cartier',
+    author_email='jfcartier@cmontmorency.qc.ca',
+    url='https://github.com/jfcmontmorency/mkdocs-audio',
+    description='MkDocs plugin to embed audio files',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'mkdocs>=1.4.2',
+        'lxml>=4.7.0'
+    ],
+    include_package_data=True,
+    python_requires='>=3.6',
+    entry_points={
+        'mkdocs.plugins': [
+            'mkdocs-audio = mkdocs_audio.plugin:Plugin',
+        ]
+    }
+)
