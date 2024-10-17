d2d3c5595036efa3d7e37895c1c40511ab5e05d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 03:09:02 +0000

    Add package protofy/0.1.1/protofy-0.1.1.tar.gz

---
+++ packages/protofy/protofy-0.1.1.tar.gz/protofy-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="protofy",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        "paho-mqtt>=2.1.0", 
+        "requests>=2.32.3"
+    ],
+)
