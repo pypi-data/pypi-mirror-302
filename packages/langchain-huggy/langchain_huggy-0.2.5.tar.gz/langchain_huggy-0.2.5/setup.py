41ec59e6196c6963fbf258c377fa53c4acbbde17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 16:08:40 +0000

    Add package langchain-huggy/0.2.5/langchain_huggy-0.2.5.tar.gz

---
+++ packages/langchain-huggy/langchain_huggy-0.2.5.tar.gz/langchain_huggy-0.2.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="langchain_huggy",
+    version="0.2.5",  # Incremented the version number
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[
+        "langchain",
+        "hugchat",
+        "python-dotenv",
+        "fastapi",
+        "uvicorn",
+        "click",  # Added for CLI functionality
+    ],
+    extras_require={
+        "dev": ["pytest", "black", "isort"],
+    },
+    entry_points={
+        'console_scripts': [
+            'langchain_huggy=langchain_huggy.cli:cli',
+        ],
+    },
+)
