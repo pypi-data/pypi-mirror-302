6339a339e04effe491362a54d43c0b046fa0ba29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 16:08:40 +0000

    Add package wearablehrv/0.2.1/wearablehrv-0.2.1-py3-none-any.whl

---
+++ packages/wearablehrv/wearablehrv-0.2.1-py3-none-any.whl/wearablehrv/__init__.py
@@ -0,0 +1,9 @@
+############################ Wearablehrv ############################
+#### A Python package designed for data preparation, pre-processing, feature exctraction, comparison, visualization, and statistical individual/group analysis of heart rate and heart rate variability outcome variables recorded from wearable devices that transmit raw interbeat intervals to a host device via Bluetooth.
+############################ Wearablehrv ############################
+
+__version__ = "0.2.1"
+
+from . import individual
+from . import group
+from . import data
