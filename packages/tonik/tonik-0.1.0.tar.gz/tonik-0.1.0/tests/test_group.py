c38dd163c93356a6b6f4f57ac7179f9641fb82e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 16:09:05 +0000

    Add package tonik/0.1.0/tonik-0.1.0.tar.gz

---
+++ packages/tonik/tonik-0.1.0.tar.gz/tonik-0.1.0/tests/test_group.py
@@ -0,0 +1,138 @@
+import os
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+import pytest
+
+from tonik import Storage
+from tonik.utils import generate_test_data
+
+
+def test_group(tmp_path_factory):
+    rootdir = tmp_path_factory.mktemp('data')
+    g = Storage('test_experiment', rootdir, backend='netcdf')
+    c = g.get_substore('site1', 'sensor1', 'channel1')
+    assert c.path == os.path.join(
+        rootdir, 'test_experiment/site1/sensor1/channel1')
+    assert len(g.children) == 1
+    cdir1 = os.path.join(rootdir, 'test_experiment', 'MDR1', '00', 'HHZ')
+    cdir2 = os.path.join(rootdir, 'test_experiment', 'MDR2', '10', 'BHZ')
+    for _d in [cdir1, cdir2]:
+        os.makedirs(_d, exist_ok=True)
+        with open(os.path.join(_d, 'feature.nc'), 'w') as f:
+            f.write('test')
+    g.from_directory()
+    assert len(g.children) == 3
+    c = g.get_substore('MDR1', '00', 'HHZ')
+
+
+def test_non_existant_feature(tmp_path_factory):
+    rootdir = tmp_path_factory.mktemp('data')
+    g = Storage('test_experiment', rootdir)
+    c = g.get_substore('site1', 'sensor1', 'channel1')
+    g.starttime = datetime(2016, 1, 1)
+    g.endtime = datetime(2016, 1, 2)
+    with pytest.raises(FileNotFoundError):
+        c('non_existant_feature')
+
+
+def test_from_directory(tmp_path_factory):
+    rootdir = tmp_path_factory.mktemp('data')
+    g = Storage('test_experiment', rootdir, backend='netcdf')
+    c = g.get_substore('site1', 'sensor1', 'channel1')
+    assert c.path == os.path.join(rootdir, 'test_experiment', 'site1',
+                                  'sensor1', 'channel1')
+    assert len(g.children) == 1
+    cdir1 = os.path.join(rootdir, 'test_experiment', 'MDR1', '00', 'HHZ')
+    cdir2 = os.path.join(rootdir, 'test_experiment', 'MDR2', '10', 'BHZ')
+    for _d in [cdir1, cdir2]:
+        os.makedirs(_d, exist_ok=True)
+        with open(os.path.join(_d, 'feature.nc'), 'w') as f:
+            f.write('test')
+    g.from_directory()
+    assert len(g.children) == 3
+    c = g.get_substore('MDR1', '00', 'HHZ')
+
+
+def test_to_dict(tmp_path_factory):
+    rootdir = tmp_path_factory.mktemp('data')
+    g = Storage('test_experiment', rootdir)
+    c = g.get_substore('site1', 'sensor1', 'channel1')
+    c1 = g.get_substore('MDR1', '00', 'HHZ')
+    c2 = g.get_substore('MDR2', '10', 'BHZ')
+    assert len(g.children) == 3
+    for _s in [c1, c2]:
+        with open(os.path.join(_s.path, 'feature.nc'), 'w') as f:
+            f.write('test')
+    _j = g.to_dict()
+    assert _j['test_experiment'][0]['MDR1'][0]['00'][0]['HHZ'][0] == 'feature'
+
+
+def test_call_multiple_days(tmp_path_factory):
+    startdate = datetime(2016, 1, 1)
+    enddate = datetime(2016, 1, 2, 12)
+
+    rootdir = tmp_path_factory.mktemp('data')
+    g = Storage('volcanoes', rootdir=rootdir).get_substore()
+    xdf = generate_test_data(dim=1, ndays=20, tstart=startdate)
+    g.save(xdf)
+    g.starttime = startdate
+    g.endtime = enddate
+    rsam = g('rsam')
+    xd_index = dict(datetime=slice(startdate, enddate))
+    np.testing.assert_array_almost_equal(
+        rsam.loc[startdate:enddate], xdf.rsam.loc[xd_index], 5)
+    # Check datetime range is correct
+    first_time = pd.to_datetime(rsam.datetime.values[0])
+    last_time = pd.to_datetime(rsam.datetime.values[-1])
+    assert pd.to_datetime(startdate) == first_time
+    assert pd.to_datetime(enddate) == last_time
+
+
+def test_call_single_day(tmp_path_factory):
+    rootdir = tmp_path_factory.mktemp('data')
+    g = Storage('volcanoes', rootdir=rootdir).get_substore()
+    startdate = datetime(2016, 1, 2, 1)
+    enddate = datetime(2016, 1, 2, 12)
+    xdf = generate_test_data(dim=1, tstart=startdate)
+    g.save(xdf)
+    g1 = Storage('volcanoes', rootdir=rootdir)
+    g1.starttime = startdate
+    g1.endtime = enddate
+    rsam = g1.get_substore()('rsam')
+    # Check datetime range is correct
+    first_time = pd.to_datetime(rsam.datetime.values[0])
+    last_time = pd.to_datetime(rsam.datetime.values[-1])
+    assert pd.to_datetime(startdate) == first_time
+    assert pd.to_datetime(enddate) == last_time
+
+
+def test_rolling_window(tmp_path_factory):
+    rootdir = tmp_path_factory.mktemp('data')
+    startdate = datetime(2016, 1, 1)
+    enddate = datetime(2016, 1, 2, 12)
+    xdf = generate_test_data(dim=1, ndays=20, tstart=startdate)
+    g = Storage('volcanoes', rootdir=rootdir)
+    g.save(xdf)
+
+    stack_len_seconds = 3600
+    stack_len_string = '1h'
+
+    num_windows = int(stack_len_seconds / pd.Timedelta(xdf.interval).seconds)
+    g.starttime = startdate
+    g.endtime = enddate
+    rsam = g('rsam')
+    rsam_rolling = g('rsam', stack_length=stack_len_string)
+
+    # Check correct datetime array
+    np.testing.assert_array_equal(rsam.datetime.values,
+                                  rsam_rolling.datetime.values)
+    # Check correct values
+    rolling_mean = [
+        np.nanmean(rsam.data[(ind-num_windows+1):ind+1])
+        for ind in np.arange(num_windows, len(rsam_rolling.data))
+    ]
+    np.testing.assert_array_almost_equal(
+        np.array(rolling_mean), rsam_rolling.values[num_windows:], 6
+    )
