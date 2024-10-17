a904cfb59194976912188f3ca5692dcf2e09c03e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 05:04:38 +0000

    Add package my_lib_gens/0.0.2/my_lib_gens-0.0.2.tar.gz

---
+++ packages/my_lib_gens/my_lib_gens-0.0.2.tar.gz/my_lib_gens-0.0.2/src/my_lib_gens/__init__.py
@@ -0,0 +1,7 @@
+from my_lib_gens._add import add
+from my_lib_gens._sub import sub
+from my_lib_gens._multiply import mult
+
+__version__ = "0.0.2"
+
+__all__ = ["add", "sub", "mult"]
---
+++ packages/my_lib_gens/my_lib_gens-0.0.2.tar.gz/my_lib_gens-0.0.2/src/my_lib_gens/_multiply.py
@@ -0,0 +1,8 @@
+"""Multiplication module."""
+
+__all__ = ["mult"]
+
+
+def mult(a: int, b: int) -> int:
+    """Perform multiplication between provided arguments."""
+    return a * b
