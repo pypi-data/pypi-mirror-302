349ba67d58838e37eaee41041222e26cac231d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 04:05:18 +0000

    Add package picometer/0.1.2/picometer-0.1.2-py3-none-any.whl

---
+++ packages/picometer/picometer-0.1.2-py3-none-any.whl/picometer/__main__.py
@@ -0,0 +1,29 @@
+from argparse import ArgumentParser, Namespace
+from picometer.routine import Routine
+from picometer.process import process
+import sys
+
+
+def parse_args() -> Namespace:
+    """Parse provided arguments if program was run directly from the CLI"""
+    desc = 'Precisely define and measure across multiple crystal structures'
+    author = 'Author: Daniel Tchoń, baharis @ GitHub'
+    ap = ArgumentParser(prog='picometer', description=desc, epilog=author)
+    ap.add_argument('filename', help='Path to yaml file with routine '
+                                     'settings and instructions')
+    if len(sys.argv) == 1:
+        ap.print_help(sys.stderr)
+        sys.exit(1)
+    return ap.parse_args()
+
+
+def main() -> int:
+    args = parse_args()
+    if filename := args.filename:
+        routine = Routine.from_yaml(filename)
+        process(routine)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
