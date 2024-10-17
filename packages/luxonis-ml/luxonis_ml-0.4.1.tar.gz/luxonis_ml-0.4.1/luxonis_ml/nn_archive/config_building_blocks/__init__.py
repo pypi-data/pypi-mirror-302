f83306e9f33fee03c91e02e263ae8382b142b2fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 05:04:27 +0000

    Add package luxonis-ml/0.4.1/luxonis_ml-0.4.1.tar.gz

---
+++ packages/luxonis-ml/luxonis_ml-0.4.1.tar.gz/luxonis_ml-0.4.1/luxonis_ml/nn_archive/config_building_blocks/__init__.py
@@ -0,0 +1,27 @@
+from .base_models import (
+    Head,
+    HeadType,
+    Input,
+    Metadata,
+    Output,
+    PreprocessingBlock,
+)
+from .enums import (
+    DataType,
+    InputType,
+    ObjectDetectionSubtypeSSD,
+    ObjectDetectionSubtypeYOLO,
+)
+
+__all__ = [
+    "Head",
+    "HeadType",
+    "Input",
+    "PreprocessingBlock",
+    "Output",
+    "Metadata",
+    "DataType",
+    "InputType",
+    "ObjectDetectionSubtypeSSD",
+    "ObjectDetectionSubtypeYOLO",
+]
