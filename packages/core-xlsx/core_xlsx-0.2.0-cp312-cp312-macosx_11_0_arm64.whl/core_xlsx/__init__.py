6314c195aff2104e404b6f07bcdd82aac68bcb4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 15:06:18 +0000

    Add package core-xlsx/0.2.0/core_xlsx-0.2.0-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/core-xlsx/core_xlsx-0.2.0-cp312-cp312-macosx_11_0_arm64.whl/core_xlsx/__init__.py
@@ -0,0 +1,20 @@
+from ._core_xlsx import (
+    Service,
+    HelperSheet,
+    HelperSheetCell,
+    XLSXSheet,
+    XLSXSheetCell,
+    column_number_to_letter,
+    version,
+)
+
+
+__all__ = (
+    "Service",
+    "XLSXSheet",
+    "XLSXSheetCell",
+    "HelperSheet",
+    "HelperSheetCell",
+    "column_number_to_letter",
+    "version",
+)
