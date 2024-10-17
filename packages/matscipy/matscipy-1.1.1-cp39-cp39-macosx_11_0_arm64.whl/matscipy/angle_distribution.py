63ae2b88f43fe15b9c694171ca09d9a93b7a03ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 03:10:18 +0000

    Add package matscipy/1.1.1/matscipy-1.1.1-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/matscipy/matscipy-1.1.1-cp39-cp39-macosx_11_0_arm64.whl/matscipy/angle_distribution.py
@@ -0,0 +1,50 @@
+#
+# Copyright 2014-2015 Lars Pastewka (U. Freiburg)
+#           2014 James Kermode (Warwick U.)
+#
+# matscipy - Materials science with Python at the atomic-scale
+# https://github.com/libAtoms/matscipy
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+Compute a bond angle distributions.
+"""
+
+import numpy as np
+
+from . import ffi
+
+###
+
+def angle_distribution(i, j, dr, nbins, *args):
+    """
+    Compute a bond angle distribution from a neighbour list.
+
+    Parameters
+    ----------
+    i, j, dr : array_like
+        Neighbour list, including list of distance vectors.
+    nbins : int
+        Number of bins for bond angle histogram.
+    cutoff : float, optional
+        Bond length cutoff, i.e. consider only bonds shorter than this length.
+
+    Returns
+    -------
+
+    """
+    return ffi.angle_distribution(np.asarray(i), np.asarray(j),
+                                  np.asarray(dr), nbins, *args)
