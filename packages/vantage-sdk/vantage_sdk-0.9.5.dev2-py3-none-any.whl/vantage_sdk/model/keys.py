75becd84bc2925bc282709a901bc17918bdb4d92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 16:08:52 +0000

    Add package vantage-sdk/0.9.5.dev2/vantage_sdk-0.9.5.dev2-py3-none-any.whl

---
+++ packages/vantage-sdk/vantage_sdk-0.9.5.dev2-py3-none-any.whl/vantage_sdk/model/keys.py
@@ -0,0 +1,105 @@
+"""
+Models for the Keys API.
+"""
+
+from enum import Enum
+from typing import Optional, List
+
+from pydantic import BaseModel, StrictStr
+
+
+class VantageAPIKeyRole(Enum):
+    """Supported Vantage API Key roles"""
+
+    Full = "Full"
+    ReadOnly = "ReadOnly"
+
+
+class VantageAPIKey(BaseModel):
+    """
+    Key for accessing Vantage API.
+
+    Attributes
+    ----------
+    vantage_api_key_id: Optional[StrictStr]
+        The unique identifier of the Vantage API key.
+    account_id : Optional[str], optional
+        The unique identifier of the account for which the Vantage API key is associated.
+    vantage_api_key_obfuscated: Optional[StrictStr]
+        Obfuscated value of the Vantage API key.
+    status: Optional[StrictStr]
+        Key status.
+    roles: Optional[List[StrictStr]]
+        List of Vantage API key roles that determines usage of the specific key.
+    """
+
+    vantage_api_key_id: Optional[StrictStr] = None
+    account_id: Optional[StrictStr] = None
+    vantage_api_key_created_date: Optional[StrictStr] = None
+    vantage_api_key_obfuscated: Optional[StrictStr] = None
+    status: Optional[StrictStr] = None
+    roles: Optional[List[StrictStr]] = None
+
+
+class LLMProvider(Enum):
+    """Supported LLM providers."""
+
+    HuggingFace = "Hugging"
+    OpenAI = "OpenAI"
+    Anthropic = "Anthropic"
+
+
+class ExternalKey(BaseModel):
+    """
+    Key for accessing one of the external APIs.
+
+    Attributes
+    ----------
+    external_key_id: Optional[StrictStr]
+        The unique identifier of the external API key.
+    account_id: Optional[StrictStr]
+        The unique identifier of the account for which the Vantage API key is associated.
+    external_key_created_date: Optional[StrictStr]
+        Date when the key was created in the user account.
+    llm_provider: Optional[LLMProvider]
+        For which LLM provider this key is intended.
+    llm_secret: Optional[StrictStr]
+        External key secret value.
+    state: Optional[StrictStr]
+        External key state.
+    """
+
+    external_key_id: Optional[StrictStr] = None
+    account_id: Optional[StrictStr] = None
+    external_key_created_date: Optional[StrictStr] = None
+    llm_provider: Optional[LLMProvider] = None
+    llm_secret: Optional[StrictStr] = None
+    state: Optional[StrictStr] = None
+
+
+class OpenAIKey(ExternalKey):
+    llm_provider: LLMProvider = LLMProvider.OpenAI
+
+
+class HuggingFaceKey(ExternalKey):
+    llm_provider: LLMProvider = LLMProvider.HuggingFace
+
+
+class AnthropicKey(ExternalKey):
+    llm_provider: LLMProvider = LLMProvider.Anthropic
+
+
+class SecondaryExternalAccount(BaseModel):
+    """
+    Secondary external account data.
+
+    Attributes
+    ----------
+    external_account_id: Optional[StrictStr]
+        Unique identifier of the external account.
+    external_type: Optional[StrictStr]
+        Type of the external account.
+    """
+
+    external_account_id: Optional[StrictStr] = None
+    external_type: Optional[StrictStr] = None
