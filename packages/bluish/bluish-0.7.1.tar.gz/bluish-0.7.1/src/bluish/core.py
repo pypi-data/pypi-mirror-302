cf1ebe56ddf59f9e99dedb2cf8cac68f14556490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 07:05:22 +0000

    Add package bluish/0.7.1/bluish-0.7.1.tar.gz

---
+++ packages/bluish/bluish-0.7.1.tar.gz/bluish-0.7.1/src/bluish/core.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+
+class ExecutionStatus(Enum):
+    PENDING = "PENDING"
+    RUNNING = "RUNNING"
+    FINISHED = "FINISHED"
+    SKIPPED = "SKIPPED"
+
+
+def init_commands() -> None:
+    import bluish.commands.core  # noqa
+    import bluish.commands.linux  # noqa
+    import bluish.commands.macos  # noqa
+    import bluish.commands.docker  # noqa
+    import bluish.commands.git  # noqa
+
+    pass
