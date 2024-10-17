fdaa89a3fdab910bad50608ad91b5d2986b96e68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 06:05:07 +0000

    Add package preset-cli/0.2.21/preset_cli-0.2.21.tar.gz

---
+++ packages/preset-cli/preset_cli-0.2.21.tar.gz/preset_cli-0.2.21/tests/cli/superset/import_test.py
@@ -0,0 +1,98 @@
+"""
+Tests for the import commands.
+"""
+
+# pylint: disable=invalid-name
+
+import yaml
+from click.testing import CliRunner
+from pyfakefs.fake_filesystem import FakeFilesystem
+from pytest_mock import MockerFixture
+
+from preset_cli.cli.superset.main import superset_cli
+
+
+def test_import_rls(mocker: MockerFixture, fs: FakeFilesystem) -> None:
+    """
+    Test the ``import_rls`` command.
+    """
+    mocker.patch("preset_cli.cli.superset.main.UsernamePasswordAuth")
+    SupersetClient = mocker.patch("preset_cli.cli.superset.import_.SupersetClient")
+    client = SupersetClient()
+    rls = [
+        {
+            "clause": "client_id = 9",
+            "description": "Rule description",
+            "filter_type": "Regular",
+            "group_key": "department",
+            "name": "Rule name",
+            "roles": ["Gamma"],
+            "tables": ["main.test_table"],
+        },
+    ]
+    fs.create_file("rls.yaml", contents=yaml.dump(rls))
+
+    runner = CliRunner()
+    result = runner.invoke(
+        superset_cli,
+        ["https://superset.example.org/", "import-rls"],
+        catch_exceptions=False,
+    )
+    assert result.exit_code == 0
+
+    client.import_rls.assert_called_with(rls[0])
+
+
+def test_import_roles(mocker: MockerFixture, fs: FakeFilesystem) -> None:
+    """
+    Test the ``import_roles`` command.
+    """
+    mocker.patch("preset_cli.cli.superset.main.UsernamePasswordAuth")
+    SupersetClient = mocker.patch("preset_cli.cli.superset.import_.SupersetClient")
+    client = SupersetClient()
+    roles = [
+        {
+            "name": "Role name",
+            "permissions": ["can do this", "can do that"],
+        },
+    ]
+    fs.create_file("roles.yaml", contents=yaml.dump(roles))
+
+    runner = CliRunner()
+    result = runner.invoke(
+        superset_cli,
+        ["https://superset.example.org/", "import-roles"],
+        catch_exceptions=False,
+    )
+    assert result.exit_code == 0
+
+    client.import_role.assert_called_with(roles[0])
+
+
+def test_import_ownership(mocker: MockerFixture, fs: FakeFilesystem) -> None:
+    """
+    Test the ``import_ownership`` command.
+    """
+    mocker.patch("preset_cli.cli.superset.main.UsernamePasswordAuth")
+    SupersetClient = mocker.patch("preset_cli.cli.superset.import_.SupersetClient")
+    client = SupersetClient()
+    ownership = {
+        "dataset": [
+            {
+                "name": "test_table",
+                "owners": ["admin@example.com"],
+                "uuid": "e4e6a14b-c3e8-4fdf-a850-183ba6ce15e0",
+            },
+        ],
+    }
+    fs.create_file("ownership.yaml", contents=yaml.dump(ownership))
+
+    runner = CliRunner()
+    result = runner.invoke(
+        superset_cli,
+        ["https://superset.example.org/", "import-ownership"],
+        catch_exceptions=False,
+    )
+    assert result.exit_code == 0
+
+    client.import_ownership.assert_called_with("dataset", ownership["dataset"])
