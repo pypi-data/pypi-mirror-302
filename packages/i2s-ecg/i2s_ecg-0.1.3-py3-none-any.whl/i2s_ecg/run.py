df506bde3fefb85dd86acc08ef8611b9c806b7fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 15:05:21 +0000

    Add package i2s-ecg/0.1.3/i2s_ecg-0.1.3-py3-none-any.whl

---
+++ packages/i2s-ecg/i2s_ecg-0.1.3-py3-none-any.whl/i2s_ecg/run.py
@@ -0,0 +1,19 @@
+import subprocess
+import os
+
+def run_app():
+    # 获取当前脚本的目录
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    # 构建 app.py 的完整路径
+    app_path = os.path.join(current_dir, 'app_myfile.py')
+    
+    # 确保 app.py 文件存在
+    if not os.path.exists(app_path):
+        print(f"Error: {app_path} does not exist.")
+        return
+    
+    # 使用 subprocess.run 来运行 streamlit run app.py 命令
+    subprocess.run(["streamlit", "run", app_path,"--server.enableXsrfProtection=false"])
+
+if __name__ == "__main__":
+    run_app()
