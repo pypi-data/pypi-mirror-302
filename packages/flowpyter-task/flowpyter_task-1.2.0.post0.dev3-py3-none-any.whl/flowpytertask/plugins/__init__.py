23506d06b8a9293d4c7eb1f03e5378b2f1634131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 03:09:46 +0000

    Add package flowpyter-task/1.2.0.post0.dev3/flowpyter_task-1.2.0.post0.dev3-py3-none-any.whl

---
+++ packages/flowpyter-task/flowpyter_task-1.2.0.post0.dev3-py3-none-any.whl/flowpytertask/plugins/__init__.py
@@ -0,0 +1,11 @@
+from airflow.plugins_manager import AirflowPlugin
+
+from . import base64_jinja
+
+
+class FptMacros(AirflowPlugin):
+    name = "flowpytertask"
+    macros = [base64_jinja.b64_encode, base64_jinja.dump_yaml]
+
+def hello_plugin():
+    print("PLUGIN")
