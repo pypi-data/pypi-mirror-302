8fb707ad50aa66c7834175746a1a7484775e58c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 16:09:01 +0000

    Add package py-llm-core/3.4.10/py_llm_core-3.4.10.tar.gz

---
+++ packages/py-llm-core/py_llm_core-3.4.10.tar.gz/py_llm_core-3.4.10/src/llm_core/assistants/__init__.py
@@ -0,0 +1,15 @@
+from .base import (
+    OpenAIAssistant,
+    OpenWeightsAssistant,
+    MistralAIAssistant,
+    AnthropicAssistant,
+    GoogleAIAssistant,
+)
+
+__all__ = [
+    "OpenAIAssistant",
+    "OpenWeightsAssistant",
+    "MistralAIAssistant",
+    "AnthropicAssistant",
+    "GoogleAIAssistant",
+]
