c3df4f9b71dc53ec47e7f113b4d812af88c956ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 16:09:19 +0000

    Add package item-matching/0.0.62/item_matching-0.0.62.tar.gz

---
+++ packages/item-matching/item_matching-0.0.62.tar.gz/item_matching-0.0.62/src/item_matching/__init__.py
@@ -0,0 +1,4 @@
+from .main_2 import ModelInput, PipelineMatch
+from .main_1 import ModelInputInner, PipelineMatchInner
+from .func.function_image import PipelineImage
+from .func.function_text import PipelineText
