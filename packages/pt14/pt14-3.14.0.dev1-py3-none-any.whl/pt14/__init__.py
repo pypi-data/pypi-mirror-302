b824f819dae58737e1ea961c5f7dd68150ae6bde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 14:06:02 +0000

    Add package pt14/3.14.0.dev1/pt14-3.14.0.dev1-py3-none-any.whl

---
+++ packages/pt14/pt14-3.14.0.dev1-py3-none-any.whl/pt14/__init__.py
@@ -0,0 +1,6 @@
+"""."""
+from pt14.fourteen import v14pre
+from .app import (v3140_app, v3140_app_)
+
+
+__all__ = ["v14pre", "v3140_app", "v3140_app_"]
---
+++ packages/pt14/pt14-3.14.0.dev1-py3-none-any.whl/pt14/fourteen/__init__.py
@@ -0,0 +1,6 @@
+from version3900 import Version
+
+# v14 = Version('3.14', 'optical-koala-victor')
+v14pre = Version('3.14.0a1', 'kingdom-rocket-balance')
+
+__all__ = ["v14pre"]
