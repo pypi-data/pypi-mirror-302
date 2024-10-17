41f4ecda1f25ad4665061e923764f8be85d901dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 03:09:40 +0000

    Add package PyMyMethods/1.0.5/pymymethods-1.0.5.tar.gz

---
+++ packages/PyMyMethods/pymymethods-1.0.5.tar.gz/pymymethods-1.0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# 运行installMode安装依赖库
+setup(
+    name='PyMyMethods',
+    version='1.0.5',
+    author='Mikuas',
+    packages=find_packages(),
+    install_requires=[
+        'PySide6',
+        'pyautogui',
+        'easycor',
+        'comtypes',
+        'pycaw'
+    ]
+)
