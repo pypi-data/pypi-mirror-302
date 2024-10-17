16d60c3d322849ab2c8f669808c7d251b16e8d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 16:09:15 +0000

    Add package prefect/3.0.11.dev2/prefect-3.0.11.dev2-py3-none-any.whl

---
+++ packages/prefect/prefect-3.0.11.dev2-py3-none-any.whl/prefect/server/ui/assets/404-BsTFLCEb.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,l as a,o as t,de as r}from"./index-CVLm0BGI.js";import{u as s}from"./usePageTitle-D5jxd7-3.js";const m=e({__name:"404",setup(n){return s("Page Not Found"),(c,p)=>(t(),o(a(r)))}});export{m as default};
+//# sourceMappingURL=404-BsTFLCEb.js.map
