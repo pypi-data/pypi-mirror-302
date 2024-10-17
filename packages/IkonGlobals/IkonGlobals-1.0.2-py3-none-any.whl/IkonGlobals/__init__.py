56ecdf56e93aa8700549c2a0c25c20bd9e0f8899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 16:09:02 +0000

    Add package IkonGlobals/1.0.2/IkonGlobals-1.0.2-py3-none-any.whl

---
+++ packages/IkonGlobals/IkonGlobals-1.0.2-py3-none-any.whl/IkonGlobals/__init__.py
@@ -0,0 +1,8 @@
+from .database.campaign import *
+from .database.song import *
+from .database.speaker import *
+from .database.speech import *
+from .database.user import *
+
+__version__ = "1.0.2"
+__author__ = 'Samuel Sallee'
---
+++ packages/IkonGlobals/IkonGlobals-1.0.2-py3-none-any.whl/IkonGlobals/database/song.py
@@ -0,0 +1,29 @@
+from pynamodb.models import Model
+from pynamodb.attributes import UnicodeAttribute, ListAttribute
+from ..utils.utils import random_id
+
+
+class Song(Model):
+    class Meta:
+        table_name = 'Song'
+        region = 'ap-southeast-1'
+        billing_mode = 'PAY_PER_REQUEST'
+
+    id = UnicodeAttribute(hash_key=True, default=lambda: random_id(is_uuid=True))
+    key = UnicodeAttribute()
+    key_preview = UnicodeAttribute(null=True)
+    title = UnicodeAttribute()
+    page_url = UnicodeAttribute()
+    file_url = UnicodeAttribute()
+    tags = ListAttribute(of=UnicodeAttribute)
+
+    def to_dict(self):
+        rval = {}
+        for key, _ in self.get_attributes().items():
+            rval[key] = getattr(self, key)
+
+        return rval
+
+
+if not Song.exists():
+    Song.create_table(wait=True)
