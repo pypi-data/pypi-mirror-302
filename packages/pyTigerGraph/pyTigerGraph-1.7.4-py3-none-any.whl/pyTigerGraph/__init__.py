79fde8b5ed319e5715915abef89219dcc6ce1099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 04:05:11 +0000

    Add package pyTigerGraph/1.7.4/pyTigerGraph-1.7.4-py3-none-any.whl

---
+++ packages/pyTigerGraph/pyTigerGraph-1.7.4-py3-none-any.whl/pyTigerGraph/__init__.py
@@ -0,0 +1,5 @@
+from pyTigerGraph.pyTigerGraph import TigerGraphConnection
+
+__version__ = "1.7.4"
+
+__license__ = "Apache 2"
---
+++ packages/pyTigerGraph/pyTigerGraph-1.7.4-py3-none-any.whl/pyTigerGraph/pyTigerGraphLoading.py
@@ -0,0 +1,90 @@
+"""Loading Job Functions
+
+The functions on this page run loading jobs on the TigerGraph server.
+All functions in this module are called as methods on a link:https://docs.tigergraph.com/pytigergraph/current/core-functions/base[`TigerGraphConnection` object].
+"""
+import logging
+import warnings
+from typing import Union
+from pyTigerGraph.pyTigerGraphBase import pyTigerGraphBase
+
+logger = logging.getLogger(__name__)
+
+
+class pyTigerGraphLoading(pyTigerGraphBase):
+
+    def runLoadingJobWithFile(self, filePath: str, fileTag: str, jobName: str, sep: str = None,
+            eol: str = None, timeout: int = 16000, sizeLimit: int = 128000000) -> Union[dict, None]:
+        """Execute a loading job with the referenced file.
+
+        The file will first be uploaded to the TigerGraph server and the value of the appropriate
+        FILENAME definition will be updated to point to the freshly uploaded file.
+
+        NOTE: The argument `USING HEADER="true"` in the GSQL loading job may not be enough to
+        load the file correctly. Remove the header from the data file before using this function.
+
+        Args:
+            filePath:
+                File variable name or file path for the file containing the data.
+            fileTag:
+                The name of file variable in the loading job (DEFINE FILENAME <fileTag>).
+            jobName:
+                The name of the loading job.
+            sep:
+                Data value separator. If your data is JSON, you do not need to specify this
+                parameter. The default separator is a comma `,`.
+            eol:
+                End-of-line character. Only one or two characters are allowed, except for the
+                special case `\\r\\n`. The default value is `\\n`
+            timeout:
+                Timeout in seconds. If set to `0`, use the system-wide endpoint timeout setting.
+            sizeLimit:
+                Maximum size for input file in bytes.
+
+        Endpoint:
+            - `POST /ddl/{graph_name}`
+                See xref:tigergraph-server:API:built-in-endpoints.adoc#_run_a_loading_job[Run a loading job]
+        """
+        logger.info("entry: runLoadingJobWithFile")
+        if logger.level == logging.DEBUG:
+            logger.debug("params: " + self._locals(locals()))
+
+        try:
+            data = open(filePath, 'rb').read()
+            params = {
+                "tag": jobName,
+                "filename": fileTag,
+            }
+            if sep is not None:
+                params["sep"] = sep
+            if eol is not None:
+                params["eol"] = eol
+        except OSError as ose:
+            logger.error(ose.strerror)
+            logger.info("exit: runLoadingJobWithFile")
+
+            return None
+            # TODO Should throw exception instead?
+
+        res = self._post(self.restppUrl + "/ddl/" + self.graphname, params=params, data=data,
+            headers={"RESPONSE-LIMIT": str(sizeLimit), "GSQL-TIMEOUT": str(timeout)})
+
+        if logger.level == logging.DEBUG:
+            logger.debug("return: " + str(res))
+        logger.info("exit: runLoadingJobWithFile")
+
+        return res
+
+    def uploadFile(self, filePath, fileTag, jobName="", sep=None, eol=None, timeout=16000,
+            sizeLimit=128000000) -> dict:
+        """DEPRECATED
+
+        Use `runLoadingJobWithFile()` instead.
+        """
+        warnings.warn(
+            "The `uploadFile()` function is deprecated; use `runLoadingJobWithFile()` instead.",
+            DeprecationWarning)
+
+        return self.runLoadingJobWithFile(filePath, fileTag, jobName, sep, eol, timeout, sizeLimit)
+
+    # TODO POST /restpploader/{graph_name}
