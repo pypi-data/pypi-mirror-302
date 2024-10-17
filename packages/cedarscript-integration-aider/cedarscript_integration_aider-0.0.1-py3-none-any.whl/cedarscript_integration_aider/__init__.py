d883b3e0f90597fd6a73a4d2abe91534a19fb773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 06:04:22 +0000

    Add package cedarscript-integration-aider/0.0.1/cedarscript_integration_aider-0.0.1-py3-none-any.whl

---
+++ packages/cedarscript-integration-aider/cedarscript_integration_aider-0.0.1-py3-none-any.whl/cedarscript_integration_aider/__init__.py
@@ -0,0 +1,12 @@
+from .cedarscript_prompts_g import CEDARScriptScriptPromptsGrammar
+from .cedarscript_prompts_rw import CEDARScriptPromptsRW
+from .cedarscript_prompts_w import CEDARScriptPromptsW
+from ._version import __version__
+
+__all__ = [
+    "CEDARScriptScriptPromptsGrammar",
+    "CEDARScriptPromptsRW",
+    "CEDARScriptPromptsW",
+    "__version__"
+]
+
