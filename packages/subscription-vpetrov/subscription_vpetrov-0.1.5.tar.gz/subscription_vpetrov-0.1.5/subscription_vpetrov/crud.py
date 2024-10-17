4dc4dbecff42414db8b6ce68a5eb5d3fb4a5fb64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 04:06:23 +0000

    Add package subscription-vpetrov/0.1.5/subscription_vpetrov-0.1.5.tar.gz

---
+++ packages/subscription-vpetrov/subscription_vpetrov-0.1.5.tar.gz/subscription_vpetrov-0.1.5/subscription_vpetrov/crud.py
@@ -0,0 +1,77 @@
+"""CRUD operations for payku subscriptions"""
+from sqlalchemy.orm import Session
+
+from . import models, exceptions
+
+
+def get_subscription_by_id(
+        db: Session, subscription_id: int) -> models.Subscription:
+    """
+    Returns a Subscription with selected id.
+    """
+    subscription_db = db.query(models.Subscription).filter(
+        models.Subscription.id == subscription_id).one_or_none()
+    if subscription_db is None:
+        raise exceptions.SubscriptionNotFound()
+    return subscription_db
+
+
+def get_subscription_by_user_id(
+        db: Session, user_id: int) -> models.Subscription:
+    """
+    Returns a Subscription with selected user_id.
+    """
+    subscription_db = db.query(models.Subscription).filter(
+        models.Subscription.user_id == user_id).one_or_none()
+    if subscription_db is None:
+        raise exceptions.SubscriptionNotFound()
+    return subscription_db
+
+
+def approve_subscription(
+        db: Session, subscription_id: int) -> models.Subscription:
+    """
+    Approves a Subscription.
+    """
+    db_subscription = get_subscription_by_id(db, subscription_id)
+    print(db_subscription)
+    db_subscription.approve()
+    db.refresh(db_subscription)
+    print(db_subscription)
+    db.commit()
+    return db_subscription
+
+
+def delete_subscription_if_exists(
+        db: Session, subscription_id: int) -> None:
+    """
+    Deletes a Subscription if exists.
+    """
+    subscription_db = db.query(models.Subscription).filter(
+        models.Subscription.id == subscription_id).first()
+    if subscription_db is None:
+        return
+    db.delete(subscription_db)
+    db.commit()
+
+
+def cancel_subscription(
+        db: Session, subscription_id: int) -> models.Subscription:
+    """
+    Cancels a Subscription.
+    """
+    db_subscription = get_subscription_by_id(db, subscription_id)
+    db_subscription.cancel()
+    db.commit()
+    return db_subscription
+
+
+def suspend_subscription(
+        db: Session, subscription_id: int) -> models.Subscription:
+    """
+    Suspends a Subscription
+    """
+    db_subscription = get_subscription_by_id(db, subscription_id)
+    db_subscription.suspend()
+    db.commit()
+    return db_subscription
