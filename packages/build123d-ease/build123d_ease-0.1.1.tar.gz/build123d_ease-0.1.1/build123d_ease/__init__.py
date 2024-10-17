e9803c84a97f14e909431beac8fe579f84d2fcfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 07:04:54 +0000

    Add package build123d-ease/0.1.1/build123d_ease-0.1.1.tar.gz

---
+++ packages/build123d-ease/build123d_ease-0.1.1.tar.gz/build123d_ease-0.1.1/build123d_ease/__init__.py
@@ -0,0 +1,12 @@
+"""Extensions, tools, and shortcuts to make modelling with Build123d easier."""
+
+from . import align, rotation
+from .ci_show import show
+
+__VERSION__ = '0.1.1'
+
+__all__ = [
+    "align",
+    "rotation",
+    "show",
+]
