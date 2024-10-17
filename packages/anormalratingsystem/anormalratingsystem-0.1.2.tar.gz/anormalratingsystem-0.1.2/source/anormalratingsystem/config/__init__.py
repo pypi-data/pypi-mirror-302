222868fe244198a01110ccb90ddd1b6f54cfe366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 03:09:32 +0000

    Add package anormalratingsystem/0.1.2/anormalratingsystem-0.1.2.tar.gz

---
+++ packages/anormalratingsystem/anormalratingsystem-0.1.2.tar.gz/anormalratingsystem-0.1.2/source/anormalratingsystem/config/__init__.py
@@ -0,0 +1,7 @@
+"""Config package for the project."""
+
+__version__ = "0.1.2"
+
+import logging
+
+logger = logging.getLogger(__name__)
---
+++ packages/anormalratingsystem/anormalratingsystem-0.1.2.tar.gz/anormalratingsystem-0.1.2/source/anormalratingsystem/interface/__init__.py
@@ -0,0 +1,7 @@
+"""Interface package for the project."""
+
+__version__ = "0.1.2"
+
+import logging
+
+logger = logging.getLogger(__name__)
