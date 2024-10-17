1b09b1e2ba41f974b4d93037ae35cccf8b023e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 03:09:02 +0000

    Add package MAMMOth-commons/0.0.28/MAMMOth_commons-0.0.28-py3-none-any.whl

---
+++ packages/MAMMOth-commons/MAMMOth_commons-0.0.28-py3-none-any.whl/tests/test_tc1_bias_mitigation_cv.py
@@ -0,0 +1,32 @@
+from mammoth import testing
+from catalogue.dataset_loaders.images import data_images
+from catalogue.model_loaders.pytorch import model_torch
+from catalogue.metrics.image_bias_analysis import image_bias_analysis
+
+
+def test_facex():
+    with testing.Env(data_images, model_torch, image_bias_analysis) as env:
+        target = "task"
+        task = (
+            "face verification"  # or "face verification" TODO: error on unknown tasks
+        )
+        protected = "protected"
+        data_dir = "./data/xai_images/race_per_7000"
+        csv_dir = "./data/xai_images/bupt_anno.csv"
+
+        dataset = env.data_images(
+            path=csv_dir,
+            image_root_dir=data_dir,
+            target=target,
+            data_transform_path="./data/xai_images/torch_transform.py",
+            batch_size=1,
+            shuffle=False,
+        )
+        model = env.model_torch(
+            model_path="./data/torch_model/model.py", state_path="./data/torch_model/model.pth"
+        )
+        html_result = env.image_bias_analysis(dataset, model, [protected], task)
+        html_result.show()
+
+
+test_facex()
