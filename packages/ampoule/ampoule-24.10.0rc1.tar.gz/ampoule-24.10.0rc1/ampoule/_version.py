27c8978d25db93cae9e4e910efd8e23f616877ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 04:06:55 +0000

    Add package ampoule/24.10.0rc1/ampoule-24.10.0rc1.tar.gz

---
+++ packages/ampoule/ampoule-24.10.0rc1.tar.gz/ampoule-24.10.0rc1/ampoule/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides ampoule version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update ampoule` to change this file.
+
+from incremental import Version
+
+__version__ = Version("ampoule", 24, 10, 0, release_candidate=1)
+__all__ = ["__version__"]
