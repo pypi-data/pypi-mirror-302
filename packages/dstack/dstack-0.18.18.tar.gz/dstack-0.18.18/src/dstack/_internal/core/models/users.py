9237b9569ae53ac24d1a2e1fe64160dd99fadc98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 03:09:24 +0000

    Add package dstack/0.18.18/dstack-0.18.18.tar.gz

---
+++ packages/dstack/dstack-0.18.18.tar.gz/dstack-0.18.18/src/dstack/_internal/core/models/users.py
@@ -0,0 +1,38 @@
+import enum
+from typing import Optional
+
+from pydantic import UUID4
+
+from dstack._internal.core.models.common import CoreModel
+
+
+class ProjectRole(str, enum.Enum):
+    ADMIN = "admin"
+    MANAGER = "manager"
+    USER = "user"
+
+
+class GlobalRole(str, enum.Enum):
+    ADMIN = "admin"
+    USER = "user"
+
+
+class UserPermissions(CoreModel):
+    can_create_projects: bool
+
+
+class User(CoreModel):
+    id: UUID4
+    username: str
+    global_role: GlobalRole
+    email: Optional[str]
+    active: bool
+    permissions: UserPermissions
+
+
+class UserTokenCreds(CoreModel):
+    token: str
+
+
+class UserWithCreds(User):
+    creds: UserTokenCreds
