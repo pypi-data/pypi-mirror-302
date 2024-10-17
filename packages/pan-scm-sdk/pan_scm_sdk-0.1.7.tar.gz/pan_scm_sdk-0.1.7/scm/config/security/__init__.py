57d640e854c3a9039771af65a5d071f8f1087aa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 07:04:54 +0000

    Add package pan-scm-sdk/0.1.7/pan_scm_sdk-0.1.7.tar.gz

---
+++ packages/pan-scm-sdk/pan_scm_sdk-0.1.7.tar.gz/pan_scm_sdk-0.1.7/scm/config/security/__init__.py
@@ -0,0 +1,3 @@
+# scm/config/security/__init__.py
+
+from .anti_spyware_profiles import AntiSpywareProfile
---
+++ packages/pan-scm-sdk/pan_scm_sdk-0.1.7.tar.gz/pan_scm_sdk-0.1.7/scm/models/security/__init__.py
@@ -0,0 +1,6 @@
+# scm/models/security/__init__.py
+
+from .anti_spyware_profiles import (
+    AntiSpywareProfileRequestModel,
+    AntiSpywareProfileResponseModel,
+)
