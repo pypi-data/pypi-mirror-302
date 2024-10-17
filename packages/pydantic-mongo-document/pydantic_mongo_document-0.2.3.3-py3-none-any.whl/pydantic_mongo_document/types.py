ffc19fe4276bf9ed6dbfa3ceef4fb126bafa6291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 04:06:20 +0000

    Add package pydantic-mongo-document/0.2.3.3/pydantic_mongo_document-0.2.3.3-py3-none-any.whl

---
+++ packages/pydantic-mongo-document/pydantic_mongo_document-0.2.3.3-py3-none-any.whl/pydantic_mongo_document/types.py
@@ -0,0 +1,41 @@
+from typing import Annotated, Literal
+
+import bson
+import bson.json_util
+from pydantic import BeforeValidator, StringConstraints, WrapSerializer
+from pydantic_core.core_schema import SerializationInfo
+
+
+_ObjectIdString = Annotated[
+    str, StringConstraints(min_length=24, max_length=24, pattern=r"^[a-f\d]{24}$")
+]
+
+_DictObjectId = dict[Literal["$oid"], _ObjectIdString]
+
+
+def check_object_id(value: _ObjectIdString | _DictObjectId) -> str:
+    if isinstance(value, dict):
+        value = value["$oid"]
+
+    if not bson.ObjectId.is_valid(value):
+        raise ValueError("Invalid ObjectId")
+
+    return str(value)
+
+
+def convert_object_id(
+    value: str, handler, info: SerializationInfo
+) -> _DictObjectId | str:
+    if info.context is not None and info.context.get("document"):
+        return {"$oid": handler(value, info)}
+
+    return handler(value, info)
+
+
+ObjectId = Annotated[
+    _ObjectIdString,
+    BeforeValidator(check_object_id),
+    WrapSerializer(convert_object_id),
+]
+
+__all__ = ["ObjectId"]
