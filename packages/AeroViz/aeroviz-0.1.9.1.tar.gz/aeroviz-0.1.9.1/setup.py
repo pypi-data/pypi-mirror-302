874f8f1778fa58597fdaada03fe321f179ecbc07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 04:05:44 +0000

    Add package AeroViz/0.1.9.1/aeroviz-0.1.9.1.tar.gz

---
+++ packages/AeroViz/aeroviz-0.1.9.1.tar.gz/aeroviz-0.1.9.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="AeroViz",
+    version="0.1.9.1",
+    author="alex",
+    author_email="alex870521@gmail.com",
+    description="Aerosol science",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/Alex870521/AeroViz",
+    python_requires=">=3.12",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    # Specify your project's dependencies
+    install_requires=requirements,
+    packages=find_packages(),
+    include_package_data=True
+)
