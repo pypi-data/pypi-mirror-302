5359ab57d31f13287817507d1f21e0a3f3f62cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 03:10:11 +0000

    Add package treevalue/1.5.0/treevalue-1.5.0-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/treevalue/treevalue-1.5.0-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/treevalue/tree/general/fast.py
@@ -0,0 +1,9 @@
+from .general import general_tree_value
+
+
+class FastTreeValue(general_tree_value()):
+    """
+    Overview:
+        Fast tree value, can do almost anything with this.
+    """
+    pass
