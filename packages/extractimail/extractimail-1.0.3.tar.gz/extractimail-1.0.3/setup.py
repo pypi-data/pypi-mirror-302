bddecfbdc147b512d4f97c9f864e7405dfb67e73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 05:05:13 +0000

    Add package extractimail/1.0.3/extractimail-1.0.3.tar.gz

---
+++ packages/extractimail/extractimail-1.0.3.tar.gz/extractimail-1.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='extractimail',
+    version='1.0.3',  # Incremented version number
+    author='Federico Toscano',
+    author_email='federico.toscano@axpo.com',
+    description='A tool to automatically recollect emails and save attachments.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'pywin32',
+        'PyPDF2',
+        'pandas',
+    ],
+    entry_points={
+        'console_scripts': [
+            'extractimail=extractimail.main:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: Microsoft :: Windows',
+    ],
+    python_requires='>=3.6',
+)
