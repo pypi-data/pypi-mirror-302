4dff4a68b8e11df85a8eab49de90ce9839ec9a94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 06:04:56 +0000

    Add package dytop/0.1.11/dytop-0.1.11.tar.gz

---
+++ packages/dytop/dytop-0.1.11.tar.gz/dytop-0.1.11/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup (
+    name = "dytop",
+    version = "0.1.11",
+    author = "Ewerton Rocha Vieira",
+    url = "https://github.com/Ewerton-Vieira/dytop.git",
+    description = "dytop: combinatorial DYnamics and TOPology",
+    long_description = open('README.md').read(),
+    long_description_content_type='text/markdown',
+    ext_package='dytop',
+    packages=find_packages(),
+    install_requires = ['numpy', 'scipy', 'matplotlib', 'CMGDB', 'pychomp2']
+)
