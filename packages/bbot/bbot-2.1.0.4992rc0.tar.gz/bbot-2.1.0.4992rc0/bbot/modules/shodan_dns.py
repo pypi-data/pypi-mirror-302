a291486da327725967b2ba03978eaa890cc4e9b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 07:05:01 +0000

    Add package bbot/2.1.0.4992rc0/bbot-2.1.0.4992rc0.tar.gz

---
+++ packages/bbot/bbot-2.1.0.4992rc0.tar.gz/bbot-2.1.0.4992rc0/bbot/modules/shodan_dns.py
@@ -0,0 +1,28 @@
+from bbot.modules.templates.shodan import shodan
+
+
+class shodan_dns(shodan):
+    watched_events = ["DNS_NAME"]
+    produced_events = ["DNS_NAME"]
+    flags = ["subdomain-enum", "passive", "safe"]
+    meta = {
+        "description": "Query Shodan for subdomains",
+        "created_date": "2022-07-03",
+        "author": "@TheTechromancer",
+        "auth_required": True,
+    }
+    options = {"api_key": ""}
+    options_desc = {"api_key": "Shodan API key"}
+
+    base_url = "https://api.shodan.io"
+
+    async def request_url(self, query):
+        url = f"{self.base_url}/dns/domain/{self.helpers.quote(query)}?key={{api_key}}"
+        response = await self.api_request(url)
+        return response
+
+    def parse_results(self, r, query):
+        json = r.json()
+        if json:
+            for hostname in json.get("subdomains", []):
+                yield f"{hostname}.{query}"
