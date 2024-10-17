6a492e06c3b944c400a333bdedce43e7c454d41a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 16:09:16 +0000

    Add package canonicalwebteam.discourse/5.7.3/canonicalwebteam.discourse-5.7.3-py3-none-any.whl

---
+++ packages/canonicalwebteam.discourse/canonicalwebteam.discourse-5.7.3-py3-none-any.whl/canonicalwebteam/discourse/__init__.py
@@ -0,0 +1,10 @@
+from canonicalwebteam.discourse.app import (  # noqa
+    Docs,  # noqa
+    EngagePages,  # noqa
+    Tutorials,  # noqa
+)
+from canonicalwebteam.discourse.models import DiscourseAPI  # noqa
+from canonicalwebteam.discourse.parsers import (  # noqa
+    DocParser,  # noqa
+    TutorialParser,  # noqa
+)
---
+++ packages/canonicalwebteam.discourse/canonicalwebteam.discourse-5.7.3-py3-none-any.whl/canonicalwebteam/discourse/parsers/__init__.py
@@ -0,0 +1,6 @@
+from canonicalwebteam.discourse.parsers.docs import (  # noqa
+    DocParser,  # noqa
+)
+from canonicalwebteam.discourse.parsers.tutorials import (  # noqa
+    TutorialParser,  # noqa
+)
