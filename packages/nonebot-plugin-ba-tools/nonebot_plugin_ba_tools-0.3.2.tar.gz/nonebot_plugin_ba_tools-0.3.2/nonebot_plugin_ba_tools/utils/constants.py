522ba899f735d0ba67beea53530f5725d3a2ca2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 15:05:52 +0000

    Add package nonebot-plugin-ba-tools/0.3.2/nonebot_plugin_ba_tools-0.3.2.tar.gz

---
+++ packages/nonebot-plugin-ba-tools/nonebot_plugin_ba_tools-0.3.2.tar.gz/nonebot_plugin_ba_tools-0.3.2/nonebot_plugin_ba_tools/utils/constants.py
@@ -0,0 +1,34 @@
+ASSERTS_URL = "https://api.hanasaki.tech/"
+
+BIRTHDAY_INFO_GROUP_LIST_FILE = "birthday_info_group_list.json"
+BATTLE_INFO_CONFIG_FILE = "battle_info_config.json"
+
+DATA_STUDENTS_JSON_FILE_PATH = "data/zh/students.json"
+DATA_STUDENT_JSON_FOLDER_PATH = "data/zh/student"
+
+WIKI_BASE_URL = "https://www.gamekee.com"
+BA_WIKI_URL = WIKI_BASE_URL + "/ba"
+
+ACTIVITYT_HTML_PATH = "activity.html"
+
+DATA_STUDENTS_ICON_PATH = "images/student/icon"
+DATA_STUDENTS_BIRTHDAY_IMG_PATH = "images/student/birthday"
+
+ASSERTS_CLAIRVOYANCE_IMG_PATH = "images/clairvoyance"
+
+MONTH_i18N = {
+    "1": "一",
+    "2": "二",
+    "3": "三",
+    "4": "四",
+    "5": "五",
+    "6": "六",
+    "7": "七",
+    "8": "八",
+    "9": "九",
+    "10": "十",
+    "11": "十一",
+    "12": "十二",
+}
+
+BA_PIC_BASE_URL = "https://cloudisk.hanasaki.tech"
