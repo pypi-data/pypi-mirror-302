ce963959e74b31de917984959c72ba3af688d834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 04:05:53 +0000

    Add package birder/0.0.5a52/birder-0.0.5a52-py3-none-any.whl

---
+++ packages/birder/birder-0.0.5a52-py3-none-any.whl/birder/net/detection/__init__.py
@@ -0,0 +1,9 @@
+from birder.net.detection.faster_rcnn import Faster_RCNN
+from birder.net.detection.retinanet import RetinaNet
+from birder.net.detection.ssd import SSD
+
+__all__ = [
+    "Faster_RCNN",
+    "RetinaNet",
+    "SSD",
+]
