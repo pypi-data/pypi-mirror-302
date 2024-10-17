1d2c6ddfb9d7debb4e7e7b820d5442258358c7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 05:04:09 +0000

    Add package MedoEncTT/1.1/medoenctt-1.1.tar.gz

---
+++ packages/MedoEncTT/medoenctt-1.1.tar.gz/medoenctt-1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+setup(
+    name='MedoEncTT',
+    version='1.1',
+    packages=find_packages(),
+    description='A brief description of my library MedoEncTT',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/MohammedNajih/signer-zziqz', 
+    author='Mohamed Almuswi',
+    author_email='mmaa58027@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6', 
+)
