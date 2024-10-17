74c63385ef9450060b4a6f2b2c7ac2427c192e44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 16:09:03 +0000

    Add package picsellia/6.18.3/picsellia-6.18.3-py3-none-any.whl

---
+++ packages/picsellia/picsellia-6.18.3-py3-none-any.whl/picsellia/services/error_manager.py
@@ -0,0 +1,32 @@
+from threading import Lock
+from typing import List
+
+
+class ErrorManager:
+    def __init__(self):
+        self._errors: List[Exception] = []
+        self._lock: Lock = Lock()
+
+    @property
+    def errors(self):
+        with self._lock:
+            return self._errors
+
+    def has_errors(self):
+        with self._lock:
+            return self._errors is not None and self._errors != []
+
+    def clear(self):
+        with self._lock:
+            self._errors = []
+
+    def append(self, error: Exception):
+        with self._lock:
+            self._errors.append(error)
+
+    def __str__(self):
+        with self._lock:
+            if self._errors:
+                return f"This ErrorManager has {len(self._errors)} errors."
+            else:
+                return "This ErrorManager has no error."
