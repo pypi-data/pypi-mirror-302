78b4651f607b146e76f3f99aa451f40ba9c77417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 16:09:17 +0000

    Add package shodafinder/1.0.2/shodafinder-1.0.2.tar.gz

---
+++ packages/shodafinder/shodafinder-1.0.2.tar.gz/shodafinder-1.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="shodafinder",
+    version="1.0.1",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "shodafinder=favicon_hash.cli:run",
+        ]
+    },
+    author="Thanh Sang",
+    author_email="logmilo12@gmail.com",
+    description="A tool to hash favicons for Shodan and ZoomEye searches.",
+    url="https://github.com/logm1lo/Shodafinder",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
