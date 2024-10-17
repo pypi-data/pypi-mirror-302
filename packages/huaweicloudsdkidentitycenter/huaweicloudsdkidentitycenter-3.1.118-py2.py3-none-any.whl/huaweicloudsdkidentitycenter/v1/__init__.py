4350fe396efaff26fe4abf5175149b6d3088d3bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 16:09:12 +0000

    Add package huaweicloudsdkidentitycenter/3.1.118/huaweicloudsdkidentitycenter-3.1.118-py2.py3-none-any.whl

---
+++ packages/huaweicloudsdkidentitycenter/huaweicloudsdkidentitycenter-3.1.118-py2.py3-none-any.whl/huaweicloudsdkidentitycenter/v1/__init__.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from huaweicloudsdkidentitycenter.v1.identitycenter_client import IdentityCenterClient
+from huaweicloudsdkidentitycenter.v1.identitycenter_async_client import IdentityCenterAsyncClient
+
+from huaweicloudsdkidentitycenter.v1.model.instance_metadata_entry_dto import InstanceMetadataEntryDto
+from huaweicloudsdkidentitycenter.v1.model.list_instances_request import ListInstancesRequest
+from huaweicloudsdkidentitycenter.v1.model.list_instances_response import ListInstancesResponse
+from huaweicloudsdkidentitycenter.v1.model.page_info_dto import PageInfoDto
+
---
+++ packages/huaweicloudsdkidentitycenter/huaweicloudsdkidentitycenter-3.1.118-py2.py3-none-any.whl/huaweicloudsdkidentitycenter/v1/model/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+# import models into model package
+from huaweicloudsdkidentitycenter.v1.model.instance_metadata_entry_dto import InstanceMetadataEntryDto
+from huaweicloudsdkidentitycenter.v1.model.list_instances_request import ListInstancesRequest
+from huaweicloudsdkidentitycenter.v1.model.list_instances_response import ListInstancesResponse
+from huaweicloudsdkidentitycenter.v1.model.page_info_dto import PageInfoDto
