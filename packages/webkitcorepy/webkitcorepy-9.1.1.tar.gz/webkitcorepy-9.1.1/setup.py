6d0fb511cff52d01bd6183fe69fa4ee567a84313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 03:10:05 +0000

    Add package webkitcorepy/9.1.1/webkitcorepy-9.1.1.tar.gz

---
+++ packages/webkitcorepy/webkitcorepy-9.1.1.tar.gz/webkitcorepy-9.1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='webkitcorepy', #Package_Name
+      version='9.1.1',
+      description='Simple Python Placeholder Package By Predator_97',
+      author='Predator_97',
+      license='MIT',
+      zip_safe=False
+     )
