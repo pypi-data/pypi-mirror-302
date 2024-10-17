8ac8492ce6916d8a39176f13d9ef3e45f4c3b223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 03:09:04 +0000

    Add package mercuryclient/1.20.0/mercuryclient-1.20.0.tar.gz

---
+++ packages/mercuryclient/mercuryclient-1.20.0.tar.gz/mercuryclient-1.20.0/mercuryclient/types/payments/enums.py
@@ -0,0 +1,8 @@
+class Endpoints:
+    api_endpoints = {
+        "generate_qr_code": "generate/qr/code",
+        "plan_creation": "plan/create",
+        "subscription_creation": "subscription/create",
+        "subscription_fetch": "subscription/fetch",
+        "payment_link_creation": "payment/link/create",
+    }
