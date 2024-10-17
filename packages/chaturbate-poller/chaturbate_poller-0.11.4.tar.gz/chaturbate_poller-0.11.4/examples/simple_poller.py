c300e06e3071e3e7402b2649a79c87c11557ac93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 14:06:35 +0000

    Add package chaturbate-poller/0.11.4/chaturbate_poller-0.11.4.tar.gz

---
+++ packages/chaturbate-poller/chaturbate_poller-0.11.4.tar.gz/chaturbate_poller-0.11.4/examples/simple_poller.py
@@ -0,0 +1,31 @@
+import asyncio
+import logging
+from contextlib import suppress
+
+from chaturbate_poller.chaturbate_client import ChaturbateClient
+from chaturbate_poller.config_manager import ConfigManager
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+config_manager = ConfigManager()
+username = config_manager.get("CB_USERNAME", "")
+token = config_manager.get("CB_TOKEN", "")
+
+
+async def main():
+    async with ChaturbateClient(username, token) as client:
+        url = None
+
+        while True:
+            response = await client.fetch_events(url)
+
+            for event in response.events:
+                print(event.dict())
+
+            url = response.next_url
+
+
+if __name__ == "__main__":
+    with suppress(KeyboardInterrupt):
+        asyncio.run(main())
