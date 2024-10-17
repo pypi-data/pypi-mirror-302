9cb7533b7815b139c2e4dc385c542c67083fdff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 07:05:12 +0000

    Add package odoo14-addon-quality-mitxelena/14.0.0.3.2/odoo14-addon-quality_mitxelena-14.0.0.3.2.tar.gz

---
+++ packages/odoo14-addon-quality-mitxelena/odoo14-addon-quality_mitxelena-14.0.0.3.2.tar.gz/odoo14-addon-quality_mitxelena-14.0.0.3.2/odoo/addons/quality_mitxelena/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Quality Mitxelena',
+    'version': '14.0.0.3.2',
+    'category': '',
+    'sequence': 20,
+    'summary': '',
+    'description': """
+        Customizations of quality for Mitxelena
+    """,
+    'website': 'https://www.talaios.coop',
+    'author': 'Talaios',
+    'depends': [
+        'base',
+        'quality_control_oca'
+    ],
+    'data': [
+        'views/quality_views.xml',
+
+    ],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
