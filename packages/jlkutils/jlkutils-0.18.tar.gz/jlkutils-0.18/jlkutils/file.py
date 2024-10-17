174b6815455c04c1d5d8c745c3b885767ee18c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 16:08:44 +0000

    Add package jlkutils/0.18/jlkutils-0.18.tar.gz

---
+++ packages/jlkutils/jlkutils-0.18.tar.gz/jlkutils-0.18/jlkutils/file.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+from tkinter import filedialog
+import ast
+
+def writenewline(filename, texttowrite):
+    """
+    Schreibt den angegebenen Text als neue Zeile in die Datei.
+
+    :param filename: Name der Datei, in die geschrieben werden soll.
+    :param texttowrite: Der Text, der als neue Zeile geschrieben werden soll.
+    """
+    with open(filename, 'a') as file:  # 'a' steht für 'append', um an die Datei anzuhängen
+        file.write(texttowrite + '\n')
+
+def choose_file():
+    root = tk.Tk()
+    root.withdraw()  # Versteckt das Hauptfenster
+    file_path = filedialog.askopenfilename()  # Öffnet das Dateiauswahlfenster
+    return file_path
+
+def getcoms(filename):
+    # Liste zum Speichern der gefundenen Funktionen
+    functions = []
+    
+    # Datei einlesen
+    with open(filename, "r") as file:
+        # Inhalt der Datei in den abstrakten Syntaxbaum (AST) umwandeln
+        tree = ast.parse(file.read(), filename=filename)
+    
+    # Alle Knoten im AST durchlaufen und nur die FunctionDef-Knoten extrahieren
+    for node in ast.walk(tree):
+        if isinstance(node, ast.FunctionDef):
+            functions.append(node.name)
+    
+    return functions
+    
+def writetofile(filename, texttowrite):
+    
+    with open(filename, 'a') as file:  # 'a' steht für 'append', um an die Datei anzuhängen
+        file.write(texttowrite)
