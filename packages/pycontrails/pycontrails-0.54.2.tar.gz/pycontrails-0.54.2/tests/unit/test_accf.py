da801aa115299a6c28be3be3feb1aba4857a9cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 03:09:51 +0000

    Add package pycontrails/0.54.2/pycontrails-0.54.2.tar.gz

---
+++ packages/pycontrails/pycontrails-0.54.2.tar.gz/pycontrails-0.54.2/tests/unit/test_accf.py
@@ -0,0 +1,89 @@
+"""Test ACCF model"""
+
+from __future__ import annotations
+
+import numpy as np
+import pandas as pd
+import pytest
+import xarray as xr
+
+from pycontrails import Flight, MetDataset
+from pycontrails.models.accf import ACCF
+from tests.unit import get_static_path
+
+pytest.importorskip("climaccf", reason="climaccf package not available")
+
+
+@pytest.fixture()
+def met_accf_pl() -> MetDataset:
+    """Met data (pressure levels) for ACCF algorithm testing.
+
+    Returns
+    -------
+    MetDataset
+    """
+    path = get_static_path("met-accf-pl.nc")
+    ds = xr.open_dataset(path)
+    return MetDataset(ds, provider="ECMWF", dataset="ERA5", product="reanalysis")
+
+
+@pytest.fixture()
+def met_accf_sl() -> MetDataset:
+    """Met data (single level) for ACCF algorithm testing.
+
+    Returns
+    -------
+    MetDataset
+    """
+    path = get_static_path("met-accf-sl.nc")
+    ds = xr.open_dataset(path)
+    ds = ds.expand_dims("level").assign_coords(level=("level", [-1]))
+    return MetDataset(ds, provider="ECMWF", dataset="ERA5", product="reanalysis")
+
+
+@pytest.fixture()
+def fl() -> Flight:
+    """Create a flight for testing."""
+
+    n = 10000
+    longitude = np.linspace(45, 75, n) + np.linspace(0, 1, n)
+    latitude = np.linspace(50, 60, n) - np.linspace(0, 1, n)
+    level = np.full_like(longitude, 225)
+
+    start = np.datetime64("2022-11-11")
+    time = pd.date_range(start, start + np.timedelta64(90, "m"), periods=n)
+    return Flight(
+        longitude=longitude,
+        latitude=latitude,
+        level=level,
+        time=time,
+        aircraft_type="B737",
+        flight_id=17,
+    )
+
+
+@pytest.mark.parametrize("use_watts", [True, False])
+def test_accf_default(
+    met_accf_pl: MetDataset,
+    met_accf_sl: MetDataset,
+    fl: Flight,
+    use_watts: bool,
+) -> None:
+    """Test Default accf algorithm."""
+
+    for name, da in met_accf_sl.data.items():
+        assert da.attrs["units"] == "J m**-2"
+        if use_watts:
+            da = da / 3600.0
+            da.attrs["units"] = "W m**-2"
+            met_accf_sl.data[name] = da
+
+    accf = ACCF(met=met_accf_pl, surface=met_accf_sl)
+    out = accf.eval(fl)
+
+    # Pin some values
+    rel = 0.01
+    assert np.mean(out["olr"]) == pytest.approx(-35.23, rel=rel)
+    assert np.mean(out["aCCF_CO2"]) == pytest.approx(6.94e-16, rel=rel)
+    assert np.mean(out["aCCF_NOx"]) == pytest.approx(1.19e-12, rel=rel)
+    assert np.mean(out["aCCF_Cont"]) == pytest.approx(9.91e-15, rel=rel)
