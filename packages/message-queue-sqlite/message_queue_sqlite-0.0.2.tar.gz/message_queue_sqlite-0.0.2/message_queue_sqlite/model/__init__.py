01b047199bd6b23761fbd3a6e01214fc88f15e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 05:05:12 +0000

    Add package message-queue-sqlite/0.0.2/message_queue_sqlite-0.0.2.tar.gz

---
+++ packages/message-queue-sqlite/message_queue_sqlite-0.0.2.tar.gz/message_queue_sqlite-0.0.2/message_queue_sqlite/model/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   __init__.py
+@Time    :   2024-10-16 21:29:13
+@Author  :   chakcy 
+@Email   :   947105045@qq.com
+@description   :   model 模块
+'''
+
+from .task_resutl import TaskResult
+
+__all__ = ['TaskResult']
