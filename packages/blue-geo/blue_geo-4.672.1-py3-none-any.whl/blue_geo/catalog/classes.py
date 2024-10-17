04d35221748066d7f5f44b976b1e7fd3453abfbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 14:06:20 +0000

    Add package blue-geo/4.672.1/blue_geo-4.672.1-py3-none-any.whl

---
+++ packages/blue-geo/blue_geo-4.672.1-py3-none-any.whl/blue_geo/catalog/classes.py
@@ -0,0 +1,42 @@
+from typing import List, Type
+from blue_geo.catalog.generic import (
+    GenericCatalog,
+    VoidCatalog,
+    GenericDatacube,
+    VoidDatacube,
+)
+from blue_geo.catalog.copernicus import CopernicusCatalog, CopernicusSentinel2Datacube
+from blue_geo.catalog.EarthSearch import (
+    EarthSearchCatalog,
+    EarthSearchSentinel2L1CDatacube,
+)
+from blue_geo.catalog.firms import FirmsCatalog
+from blue_geo.catalog.firms.area import FirmsAreaDatacube
+from blue_geo.catalog.SkyFox import SkyFoxCatalog
+from blue_geo.catalog.SkyFox.Venus import SkyFoxVenusDatacube
+from blue_geo.catalog.ukraine_timemap import (
+    UkraineTimemapCatalog,
+    UkraineTimemapDatacube,
+)
+
+list_of_catalog_classes: List[Type[GenericCatalog]] = [
+    GenericCatalog,
+    CopernicusCatalog,
+    EarthSearchCatalog,
+    FirmsCatalog,
+    SkyFoxCatalog,
+    UkraineTimemapCatalog,
+]
+
+list_of_catalogs: List[str] = sorted(
+    [catalog_class.name for catalog_class in list_of_catalog_classes]
+)
+
+list_of_datacube_classes: List[Type[GenericDatacube]] = [
+    GenericDatacube,
+    FirmsAreaDatacube,
+    UkraineTimemapDatacube,
+    CopernicusSentinel2Datacube,
+    EarthSearchSentinel2L1CDatacube,
+    SkyFoxVenusDatacube,
+]
