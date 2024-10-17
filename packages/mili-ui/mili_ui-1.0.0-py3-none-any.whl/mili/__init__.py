5bd53105cd1400537b900d6d093af528e81abe5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 04:05:52 +0000

    Add package mili-ui/1.0.0/mili_ui-1.0.0-py3-none-any.whl

---
+++ packages/mili-ui/mili_ui-1.0.0-py3-none-any.whl/mili/__init__.py
@@ -0,0 +1,71 @@
+from .mili import (
+    MILI,
+    register_custom_component,
+    pack_component,
+    get_font_cache,
+    clear_font_cache,
+)
+from .data import ElementData, Interaction, ImageCache
+from .style import RESIZE, X, CENTER, PADLESS, FLOATING, FILL
+from .utility import (
+    Selectable,
+    Dragger,
+    Scroll,
+    Scrollbar,
+    Slider,
+    GenericApp,
+    InteractionSound,
+    InteractionCursor,
+    CustomWindowBorders,
+    percentage,
+    indent,
+    fit_image,
+)
+
+from . import error
+from . import style
+from . import typing
+from . import data
+from . import animation
+
+from collections import namedtuple
+
+VERSION = namedtuple("VersionInfo", "major minor micro")(1, 0, 0)
+VERSION_STR = f"{VERSION.major}.{VERSION.minor}.{VERSION.micro}"
+del namedtuple
+
+__all__ = (
+    "MILI",
+    "ElementData",
+    "Interaction",
+    "ImageCache",
+    "Selectable",
+    "Dragger",
+    "Scroll",
+    "Scrollbar",
+    "Slider",
+    "GenericApp",
+    "InteractionSound",
+    "InteractionCursor",
+    "CustomWindowBorders",
+    "percentage",
+    "indent",
+    "fit_image",
+    "register_custom_component",
+    "pack_component",
+    "get_font_cache",
+    "clear_font_cache",
+    "RESIZE",
+    "X",
+    "CENTER",
+    "PADLESS",
+    "FLOATING",
+    "FILL",
+    "error",
+    "style",
+    "typing",
+    "data",
+    "animation",
+    "VERSION",
+    "VERSION_STR",
+)
