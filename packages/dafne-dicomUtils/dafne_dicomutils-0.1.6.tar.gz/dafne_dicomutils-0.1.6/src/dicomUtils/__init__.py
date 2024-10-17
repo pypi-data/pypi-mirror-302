db300eab11b51c5e634863d0887bbb1a8ce181b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 05:05:22 +0000

    Add package dafne-dicomUtils/0.1.6/dafne_dicomutils-0.1.6.tar.gz

---
+++ packages/dafne-dicomUtils/dafne_dicomutils-0.1.6.tar.gz/dafne_dicomutils-0.1.6/src/dicomUtils/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#  Copyright (c) 2021 Dafne-Imaging Team
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import flexidep
+from . import resources
+flexidep.standard_install_from_resource(resources, 'runtime_dependencies.cfg')
+
+from .dicom3D import load3dDicom, save3dDicom
+from .misc import loadDicomFile, dosma_volume_from_path, realign_medical_volume
+medical_volume_from_path = dosma_volume_from_path
+from .padorcut import padorcut
