e30b1146a3db5a360f3e9ab1b30d324bd68c2094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 03:09:56 +0000

    Add package zenmodel/0.1.10/zenmodel-0.1.10.tar.gz

---
+++ packages/zenmodel/zenmodel-0.1.10.tar.gz/zenmodel-0.1.10/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zenmodel",
+    version="0.1.10",
+    author="Clay",
+    author_email="clay.zhang@outlook.com",
+    description="A Python processor for ZenModel",
+    url="https://github.com/zenmodel/zenmodel",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+    ],
+)
---
+++ packages/zenmodel/zenmodel-0.1.10.tar.gz/zenmodel-0.1.10/zenmodel/__init__.py
@@ -0,0 +1,5 @@
+from .brain_context import BrainContext, BrainContextReader
+from .processor import Processor
+from .selector import Selector
+
+__all__ = ['BrainContext', 'BrainContextReader', 'Processor', 'Selector']
