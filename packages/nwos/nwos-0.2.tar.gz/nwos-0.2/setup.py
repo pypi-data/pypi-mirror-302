02ba0dc84241a7d62d542e183ec76ca59e3d08af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 06:04:13 +0000

    Add package nwos/0.2/nwos-0.2.tar.gz

---
+++ packages/nwos/nwos-0.2.tar.gz/nwos-0.2/setup.py
@@ -0,0 +1,21 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="nwos",
+    version="0.2",
+    description="A library for working with prime and composite numbers",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Nlolik1111",
+    author_email="nikolay9047670014@gmail.com",
+    url="https://github.com/yourusername/nwos",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
