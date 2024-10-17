73d620e62312270dee690abe50cd0956a8f180d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 03:10:02 +0000

    Add package pytest-gitlab/0.3.1/pytest_gitlab-0.3.1-py3-none-any.whl

---
+++ packages/pytest-gitlab/pytest_gitlab-0.3.1-py3-none-any.whl/pytest_gitlab/docker/docker.py
@@ -0,0 +1,39 @@
+"""
+pytest-docker fixture overrides.
+See https://github.com/avast/pytest-docker#available-fixtures.
+"""
+
+import pytest
+import pytest_docker
+
+
+@pytest.fixture(scope='session')
+def docker_compose_project_name():
+    """Set a consistent project name to enable optional reuse of containers."""
+    return 'pytest-python-gitlab'
+
+
+pytest_docker.docker_compose_project_name = docker_compose_project_name
+
+
+@pytest.fixture(scope='session')
+def docker_compose_file(fixture_dir):
+    return fixture_dir / 'docker-compose.yml'
+
+
+pytest_docker.docker_compose_file = docker_compose_file
+
+
+@pytest.fixture(scope='session')
+def docker_cleanup(request):
+    """Conditionally keep containers around by overriding the cleanup command."""
+    if request.config.getoption('--keep-containers-running'):
+        # Print version and exit.
+        return '-v'
+    if request.config.getoption('--keep-containers'):
+        # Print version and exit.
+        return 'stop'
+    return 'down'
+
+
+pytest_docker.docker_cleanup = docker_cleanup
