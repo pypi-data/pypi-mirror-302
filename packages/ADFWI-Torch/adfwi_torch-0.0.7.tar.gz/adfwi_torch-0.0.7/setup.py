92a1a63073c42685fd8051245469b155bc181c75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 04:06:25 +0000

    Add package ADFWI-Torch/0.0.7/adfwi_torch-0.0.7.tar.gz

---
+++ packages/ADFWI-Torch/adfwi_torch-0.0.7.tar.gz/adfwi_torch-0.0.7/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ADFWI-Torch',
+    version='0.0.7',
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.24.0',
+        'scipy>=1.10.0',
+        'matplotlib>=3.7.0',
+        'torch>=2.0.0',
+        'disba>=0.6.0',
+        'torchinfo',
+        'ncg_optimizer',
+        'pysdtw',
+        'geomloss',
+        'h5py',
+        'obspy',
+        'tqdm',
+    ],
+    description='adfwi: A framework for high-resolution subsurface parameter estimation using Full Waveform Inversion',
+    long_description=open('README_PIP.md').read(),
+    long_description_content_type='text/markdown',
+    author='Liu Feng',
+    author_email='liufeng1@pjlab.org.cn',
+    url='https://github.com/liufeng2317/ADFWI',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering',
+    ],
+)
