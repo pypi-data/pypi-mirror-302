b8c5c3f0a006169c3ccc649a83c194e2db61aa8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 05:04:24 +0000

    Add package argopy/1.0.0/argopy-1.0.0.tar.gz

---
+++ packages/argopy/argopy-1.0.0.tar.gz/argopy-1.0.0/argopy/stores/__init__.py
@@ -0,0 +1,24 @@
+# from .argo_index_deprec import indexstore, indexfilter_wmo, indexfilter_box
+from .filesystems import filestore, httpstore, memorystore, ftpstore, s3store
+from .filesystems import httpstore_erddap, httpstore_erddap_auth
+from .filesystems import has_distributed, distributed  # noqa: F401
+
+from .argo_index_pa import indexstore_pyarrow as indexstore_pa
+from .argo_index_pd import indexstore_pandas as indexstore_pd
+
+from .argo_index import ArgoIndex
+
+#
+__all__ = (
+    # Classes:
+    "ArgoIndex",
+    "indexstore_pa",
+    "indexstore_pd",
+    "filestore",
+    "httpstore",
+    "httpstore_erddap",
+    "httpstore_erddap_auth",
+    "ftpstore",
+    "memorystore",
+    "s3store",
+)
