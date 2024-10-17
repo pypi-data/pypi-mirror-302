c30540c60864cd34af14bb3a002f637218923ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 03:09:43 +0000

    Add package digitalhub-runtime-python/0.8.0b3/digitalhub_runtime_python-0.8.0b3-py3-none-any.whl

---
+++ packages/digitalhub-runtime-python/digitalhub_runtime_python-0.8.0b3-py3-none-any.whl/digitalhub_runtime_python/__init__.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+from digitalhub_runtime_python.utils.utils import handler
+
+from digitalhub.entities.utils.entity_types import EntityTypes
+from digitalhub.registry.registry import registry
+from digitalhub.registry.utils import create_info
+
+root = "digitalhub_runtime_python"
+runtime_info = {
+    "module": f"{root}.runtimes.runtime",
+    "class_name": "RuntimePython",
+    "kind_registry_module": f"{root}.runtimes.kind_registry",
+    "kind_registry_class_name": "kind_registry",
+}
+
+root_ent = f"{root}.entities"
+
+
+# Function
+entity_type = EntityTypes.FUNCTION.value
+exec_kind = "python"
+prefix = entity_type.capitalize()
+suffix = exec_kind.capitalize()
+exec_info = create_info(root_ent, entity_type, exec_kind, prefix, suffix, runtime_info)
+registry.register(exec_kind, exec_info)
+
+
+# Tasks
+entity_type = EntityTypes.TASK.value
+for i in ["job", "build", "serve"]:
+    task_kind = f"{exec_kind}+{i}"
+    prefix = entity_type.capitalize()
+    suffix = exec_kind.capitalize() + i.capitalize()
+    kind_underscore = task_kind.replace("+", "_")
+    task_info = create_info(root_ent, entity_type, kind_underscore, prefix, suffix, runtime_info)
+    registry.register(task_kind, task_info)
+
+
+# Runs
+entity_type = EntityTypes.RUN.value
+run_kind = f"{exec_kind}+run"
+prefix = entity_type.capitalize()
+suffix = exec_kind.capitalize() + entity_type.capitalize()
+kind_underscore = run_kind.replace("+", "_")
+run_info = create_info(root_ent, entity_type, kind_underscore, prefix, suffix, runtime_info)
+registry.register(run_kind, run_info)
