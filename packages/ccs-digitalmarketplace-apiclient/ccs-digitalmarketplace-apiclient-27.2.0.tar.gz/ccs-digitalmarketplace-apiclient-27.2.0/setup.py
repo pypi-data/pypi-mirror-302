1b4dd0f7c8604b52c26373590c247b97fbeb9291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 16:08:49 +0000

    Add package ccs-digitalmarketplace-apiclient/27.2.0/ccs-digitalmarketplace-apiclient-27.2.0.tar.gz

---
+++ packages/ccs-digitalmarketplace-apiclient/ccs-digitalmarketplace-apiclient-27.2.0.tar.gz/ccs-digitalmarketplace-apiclient-27.2.0/setup.py
@@ -0,0 +1,31 @@
+"""
+API clients for Digital Marketplace Data API and Search API.
+"""
+
+import re
+import ast
+from setuptools import setup, find_packages
+
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('dmapiclient/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+setup(
+    name='ccs-digitalmarketplace-apiclient',
+    version=version,
+    url='https://github.com/Crown-Commercial-Service/ccs-digitalmarketplace-apiclient',
+    license='MIT',
+    author='GDS Developers',
+    description='Digital Marketplace Data and Search API clients',
+    long_description=__doc__,
+    packages=find_packages(),
+    package_data={'dmapiclient': ['py.typed']},
+    include_package_data=True,
+    install_requires=[
+        'requests<3,>=2.18.4',
+    ],
+    python_requires=">=3.9,<3.13",
+)
