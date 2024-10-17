1551ea99b66aefc70ec04acf5b208752cbf50fab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 14:06:31 +0000

    Add package tsafe/1.0.3/tsafe-1.0.3.tar.gz

---
+++ packages/tsafe/tsafe-1.0.3.tar.gz/tsafe-1.0.3/tsafe/__init__.py
@@ -0,0 +1,28 @@
+
+
+def type_safe(func):
+    """
+    Wrapper function to force a function to be type safe
+    """
+
+    def force_safety(*args, **kwargs):
+        flag = True
+        for argType in [args, kwargs.values()]:
+            for index in range(0, len(argType)):
+                arg = args[index]
+                reqType = list(func.__annotations__.values())[index]
+                
+                if type(arg) == reqType or reqType == object:
+                    continue
+                else:
+                    flag = False
+                    raise Exception(f"argument {arg} is not type of {reqType}")
+                    break
+        
+        if flag:
+            return func(*args, **kwargs)
+
+    return force_safety
+
+safe = type_safe
+
