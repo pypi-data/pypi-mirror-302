e8f8a796a8703baa9cd3244c349d317528d51330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 06:05:20 +0000

    Add package drf-excel/2.5.0/drf_excel-2.5.0.tar.gz

---
+++ packages/drf-excel/drf_excel-2.5.0.tar.gz/drf_excel-2.5.0/tests/testapp/views.py
@@ -0,0 +1,21 @@
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
+from drf_excel.mixins import XLSXFileMixin
+from drf_excel.renderers import XLSXRenderer
+
+from .models import AllFieldsModel, ExampleModel
+from .serializers import AllFieldsSerializer, ExampleSerializer
+
+
+class ExampleViewSet(XLSXFileMixin, ReadOnlyModelViewSet):
+    queryset = ExampleModel.objects.all()
+    serializer_class = ExampleSerializer
+    renderer_classes = (XLSXRenderer,)
+    filename = "my_export.xlsx"
+
+
+class AllFieldsViewSet(XLSXFileMixin, ReadOnlyModelViewSet):
+    queryset = AllFieldsModel.objects.all()
+    serializer_class = AllFieldsSerializer
+    renderer_classes = (XLSXRenderer,)
+    filename = "al_fileds.xlsx"
