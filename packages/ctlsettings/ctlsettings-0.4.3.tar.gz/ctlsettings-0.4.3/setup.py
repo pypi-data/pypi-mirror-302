1c6f88b79b592726b48c33cbe8f53ce0b71ab664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 07:05:19 +0000

    Add package ctlsettings/0.4.3/ctlsettings-0.4.3.tar.gz

---
+++ packages/ctlsettings/ctlsettings-0.4.3.tar.gz/ctlsettings-0.4.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+    name='ctlsettings',
+    version='0.4.3',
+    author='Columbia University Center for Teaching and Learning',
+    author_email='ctl-dev@columbia.edu',
+    url='https://github.com/ccnmtl/ctlsettings',
+    description='Columbia CTL common Django base settings',
+    long_description='common settings we use across all our projects',
+    install_requires=[
+        'django-cas-ng',
+        'django-debug-toolbar',
+        'coverage',
+        'django-smoketest',
+        'django-extensions',
+        'django-statsd-mozilla',
+        'sentry-sdk',
+        'django-storages',
+        'boto3',
+        'requests',
+        'statsd',
+        'gunicorn',
+        'django-impersonate',
+    ],
+    scripts=[],
+    license='GPL-3.0-or-later',
+    platforms=['any'],
+    packages=['ctlsettings', 'ctlsettings.templates'],
+    package_data={
+        'ctlsettings': ['*.py'],
+        'ctlsettings.templates': ['*/*.html'],
+    },
+)
