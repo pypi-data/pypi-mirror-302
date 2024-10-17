c9163ee1e35890734513c77b44e4d686a89db61a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 04:06:57 +0000

    Add package FuXLogger/0.2.7.post3/FuXLogger-0.2.7.post3-py3-none-any.whl

---
+++ packages/FuXLogger/FuXLogger-0.2.7.post3-py3-none-any.whl/FuXLogger/__metadata__.py
@@ -0,0 +1,10 @@
+import sys
+
+__version__ = "0.2.7.post3"
+__author__ = "<符玄(KOKOMI12345)>"
+__email__ = "3072252442@qq.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2024 KOKOMI12345"
+__python_version__ = fr"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
+__project_name__ = "FuXLogger"
+__github_url__ = "https://github.com/KOKOMI12345/FuXLogger"
