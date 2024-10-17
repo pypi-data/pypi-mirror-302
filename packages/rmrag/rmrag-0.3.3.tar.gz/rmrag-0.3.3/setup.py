6e897f41df2203c8718378485686bd11c0faa716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 03:10:19 +0000

    Add package rmrag/0.3.3/rmrag-0.3.3.tar.gz

---
+++ packages/rmrag/rmrag-0.3.3.tar.gz/rmrag-0.3.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rmrag',
+    version='0.3.3',
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=[],
+    author='Lasse Tranekjær Leed',
+    author_email='lasseleed@gmail.com',
+    description='RAG functions',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 3 - Alpha', 
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.9']
+)
+
+#0.2 = added tests for all function + get_response into class 
+#0.3 = Fixed tests and added Chatbot_OpenSource
+#0.3.1 = Updated Chatbot_OpenAI and made default model to GPT-4o mini
+#0.3.2 = Fixed show_progress_bar in load_faiss_vectorstore
+#0.3.3 = No longer forcing specific models
