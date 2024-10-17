d56728a1aa1ce8adbc64256adf086d2d366a41fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 16:08:45 +0000

    Add package CyberSecurityTools/0.1/cybersecuritytools-0.1.tar.gz

---
+++ packages/CyberSecurityTools/cybersecuritytools-0.1.tar.gz/cybersecuritytools-0.1/setup.py
@@ -0,0 +1,25 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="CyberSecurityTools",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+        "colorama",
+        "pyfiglet",
+        "sockets"
+    ],
+    description="CyberSecurityTool is a comprehensive Python package designed for performing essential cybersecurity scans. It includes features like subdomain enumeration, broken link checking, SQL injection testing, XSS detection, CSRF testing, and network vulnerability scanning. Built with ease of use and efficiency in mind, CyberSecurityTool empowers developers and security professionals to enhance their web application's security and identify vulnerabilities with minimal effort.",
+    author="Ahmad_Bulbul",
+    author_email="Ahmadbulbulprof1@gmail.com",
+    url="https://github.com/ZoroHacker/CyberSecurityTools",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
