b390a36cf2deb4e611311a88ab905b12dd53c953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 15:05:32 +0000

    Add package cisco-config-parser/2.2.6/cisco_config_parser-2.2.6-py3-none-any.whl

---
+++ packages/cisco-config-parser/cisco_config_parser-2.2.6-py3-none-any.whl/cisco_config_parser/__init__.py
@@ -0,0 +1,17 @@
+from .cisco_config_parser import ConfigParser, ConfigParserOld
+
+
+_ALL_ = [
+    "ConfigParser"
+    "ConfigParserOld",
+]
+
+__version__ = "2.2.5"
+__author__ = "Ahmad Rezazadeh"
+__email__ = "ahmad@klevernet.io"
+__url__ = "klevernet.ai"
+__license__ = "MIT"
+__description__ = "A simple library to parse Cisco configuration files"
+
+
+__all__ = _ALL_
