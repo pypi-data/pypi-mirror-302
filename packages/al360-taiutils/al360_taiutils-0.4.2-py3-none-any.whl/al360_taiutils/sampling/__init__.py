a727370b9621c2a82cef79d9db44c0be6bbffd0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 06:04:31 +0000

    Add package al360-taiutils/0.4.2/al360_taiutils-0.4.2-py3-none-any.whl

---
+++ packages/al360-taiutils/al360_taiutils-0.4.2-py3-none-any.whl/al360_taiutils/sampling/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) AffectLog SAS
+# Licensed under the MIT License.
+
+"""Module for defining various sampling techniques."""
+from .random_sampling import generate_random_sample
+
+__all__ = ['generate_random_sample']
