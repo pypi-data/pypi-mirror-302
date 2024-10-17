daf7847e0901a147526ad7038cff5424f6faa924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 07:04:55 +0000

    Add package siepic-ebeam-pdk/0.4.20/siepic_ebeam_pdk-0.4.20.tar.gz

---
+++ packages/siepic-ebeam-pdk/siepic_ebeam_pdk-0.4.20.tar.gz/siepic_ebeam_pdk-0.4.20/siepic_ebeam_pdk/pymacros/__init__.py
@@ -0,0 +1,9 @@
+print('SiEPIC-EBeam-PDK v0.4.20 Python module: pymacros')
+
+from . import (
+    SiEPIC_EBeam_Library,
+    SiEPIC_EBeam_Library_SiN,
+    SiEPIC_EBeam_Library_Dream,
+    SiEPIC_EBeam_Library_Beta,
+    SiEPIC_EBeam_Library_ANT,
+)
