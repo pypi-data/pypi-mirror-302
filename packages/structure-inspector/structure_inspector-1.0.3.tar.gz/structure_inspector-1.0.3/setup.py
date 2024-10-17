fef8c76074ef4afb7539d38b22c085d13ad05ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 05:04:09 +0000

    Add package structure-inspector/1.0.3/structure_inspector-1.0.3.tar.gz

---
+++ packages/structure-inspector/structure_inspector-1.0.3.tar.gz/structure_inspector-1.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="structure_inspector",
+    version="1.0.3",
+    description="A Python package to print the structure of complex, nested Python objects.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="Every AI LLC",
+    author_email="opensource@everyai.llc",
+    url="https://github.com/everyai/structure_inspector",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
