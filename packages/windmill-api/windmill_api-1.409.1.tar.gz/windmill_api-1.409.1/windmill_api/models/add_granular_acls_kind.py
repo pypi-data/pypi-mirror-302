1e03763656282bfe75db9bc9232f1a56b0f25c51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-17 07:05:13 +0000

    Add package windmill-api/1.409.1/windmill_api-1.409.1.tar.gz

---
+++ packages/windmill-api/windmill_api-1.409.1.tar.gz/windmill_api-1.409.1/windmill_api/models/add_granular_acls_kind.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class AddGranularAclsKind(str, Enum):
+    APP = "app"
+    FLOW = "flow"
+    FOLDER = "folder"
+    GROUP = "group_"
+    HTTP_TRIGGER = "http_trigger"
+    RAW_APP = "raw_app"
+    RESOURCE = "resource"
+    SCHEDULE = "schedule"
+    SCRIPT = "script"
+    VARIABLE = "variable"
+
+    def __str__(self) -> str:
+        return str(self.value)
