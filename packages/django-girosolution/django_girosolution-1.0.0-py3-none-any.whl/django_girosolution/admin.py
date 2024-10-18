0f7c69ac5b2b5282dda7c75bde3b398849864f6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 16:05:36 +0000

    Add package django-girosolution/1.0.0/django_girosolution-1.0.0-py3-none-any.whl

---
+++ packages/django-girosolution/django_girosolution-1.0.0-py3-none-any.whl/django_girosolution/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import GirosolutionTransaction
+
+
+class GirosolutionTransactionAdmin(admin.ModelAdmin):
+    list_display = ('merchant_tx_id', 'reference', 'latest_response_code')
+    list_filter = ('latest_response_code',)
+    ordering = ('-created_at',)
+    fields = ('merchant_tx_id', 'reference', 'latest_response_code')
+    search_fields = ('merchant_tx_id', 'reference')
+
+
+
+admin.site.register(GirosolutionTransaction, GirosolutionTransactionAdmin)
