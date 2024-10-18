45520d80ffe3fb71557412b12c7426ccf95ca7ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 04:06:44 +0000

    Add package digitalhub-runtime-kfp/0.8.0b4/digitalhub_runtime_kfp-0.8.0b4.tar.gz

---
+++ packages/digitalhub-runtime-kfp/digitalhub_runtime_kfp-0.8.0b4.tar.gz/digitalhub_runtime_kfp-0.8.0b4/digitalhub_runtime_kfp/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from digitalhub_runtime_kfp.entities.run.kfp_run.builder import RunKfpRunBuilder
+from digitalhub_runtime_kfp.entities.task.kfp_pipeline.builder import TaskKfpPipelineBuilder
+from digitalhub_runtime_kfp.entities.workflow.kfp.builder import WorkflowKfpBuilder
+
+entity_builders = (
+    (RunKfpRunBuilder.ENTITY_KIND, RunKfpRunBuilder),
+    (TaskKfpPipelineBuilder.ENTITY_KIND, TaskKfpPipelineBuilder),
+    (WorkflowKfpBuilder.ENTITY_KIND, WorkflowKfpBuilder),
+)
+
+try:
+    from digitalhub_runtime_kfp.runtimes.builder import RuntimeKfpBuilder
+    from digitalhub_runtime_kfp.runtimes.kind_registry import kind_registry
+
+    runtime_builders = ((kind, RuntimeKfpBuilder) for kind in kind_registry.get_all_kinds())
+except ImportError:
+    runtime_builders = tuple()
