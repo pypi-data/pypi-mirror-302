c3d5c56f675ce1bca61a7d87c44cec74092b06ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 15:06:17 +0000

    Add package peeringdb/2.3.0/peeringdb-2.3.0-py3-none-any.whl

---
+++ packages/peeringdb/peeringdb-2.3.0-py3-none-any.whl/peeringdb/client.py
@@ -0,0 +1,90 @@
+from collections import OrderedDict
+
+import munge.util
+
+from peeringdb import (
+    _config_logs,
+    backend_initialized,
+    config,
+    get_backend,
+    initialize_backend,
+    resource,
+)
+from peeringdb._update import Updater
+from peeringdb.fetch import Fetcher
+
+
+class _Query:
+    """Wrapper to access a specific resource"""
+
+    def __init__(self, client, res):
+        self.client = client
+        self.res = res
+
+    def get(self, pk):
+        return self.client.get(self.res, pk)
+
+    def all(self, **kwargs):
+        return self.client.all(self.res, **kwargs)
+
+
+class Client:
+    """Main PeeringDB client."""
+
+    def __init__(self, cfg=None, **kwargs):
+        """
+        Arguments:
+            - cfg <dict>: dict of complete config options (see config.ClientSchema),
+                by default loads from DEFAULT_CONFIG_DIR
+            - url<str>: URL to connect to
+            - user<str>: username to connect to api with
+            - password<str>: password
+            - timeout<float>: timeout to fail after
+        """
+        if cfg is None:
+            cfg = config.load_config()
+        self.config = cfg
+        log_config = cfg["log"]
+        _config_logs(**log_config)
+        orm_config = cfg["orm"]
+        orm_name = orm_config["backend"]
+        if not backend_initialized():
+            initialize_backend(orm_name, **orm_config)
+
+        sync_config = cfg["sync"]
+        # override config with kwargs
+        munge.util.recursive_update(sync_config, kwargs)
+
+        self.fetcher = Fetcher(**sync_config)
+        self.updater = Updater(self.fetcher)
+
+        tag_res = OrderedDict(
+            [(res.tag, _Query(self, res)) for res in resource.all_resources()]
+        )
+        tag_attrs = {
+            **tag_res,
+            **{
+                "keys": lambda self: list(tag_res.keys()),
+                "all": lambda self: list(tag_res.values()),
+            },
+        }
+        self._Tags = type("_Tags", (), tag_attrs)
+        self.tags = self._Tags()
+
+    def get(self, res, pk):
+        """Get a resource instance by primary key (id)"""
+        backend = get_backend()
+        return backend.get_object(backend.get_concrete(res), pk)
+
+    def all(self, res):
+        """Get resources using a filter condition"""
+        backend = get_backend()
+        return backend.get_objects(backend.get_concrete(res))
+
+    def update_all(self):
+        """Update all resources from the API."""
+        return self.updater.update_all(resource.all_resources())
+
+    @property
+    def backend(self):
+        return get_backend()
---
+++ packages/peeringdb/peeringdb-2.3.0-py3-none-any.whl/peeringdb/output/_dict.py
@@ -0,0 +1,72 @@
+from datetime import datetime
+from decimal import Decimal
+from ipaddress import IPv4Address, IPv6Address
+
+import django_countries.fields
+
+from peeringdb import get_backend
+from peeringdb.util import group_fields
+
+
+class DictWrap:
+    def __init__(self, o, depth):
+        self.object = o
+        self.depth = depth
+        backend = get_backend()
+        self.fields = group_fields(backend, o.__class__)
+
+    @staticmethod
+    def _resolve_one(name, value, depth):
+        if depth > 0:
+            return DictWrap(value, depth - 1).to_dict()
+        elif value is None:
+            return None
+        else:
+            return value.id
+
+    @staticmethod
+    def _resolve_many(name, value, depth):
+        if depth > 1:
+            return [DictWrap(o, depth - 1).to_dict() for o in value.all()]
+        elif depth == 1:
+            return list(value.values_list("id", flat=True))
+
+    def resolve(self, group, name, value):
+        if group == "scalars":
+            if isinstance(value, datetime):
+                return value.isoformat()
+            elif isinstance(value, django_countries.fields.Country):
+                return str(value)
+            elif isinstance(value, Decimal):
+                return float(value)
+            elif isinstance(value, (IPv4Address, IPv6Address)):
+                return str(value)
+            return value
+        elif group == "single_refs":
+            return DictWrap._resolve_one(name, value, self.depth)
+        elif group == "many_refs":
+            return DictWrap._resolve_many(name, value, self.depth)
+        else:
+            raise ValueError(group)
+
+    def field_values(self):
+        for group in self.fields:
+            if self.depth == 0 and group == "many_refs":
+                continue
+            for name in self.fields[group]:
+                value = self.resolve(group, name, getattr(self.object, name))
+                if value is None:
+                    value = "None"
+                yield name, value
+
+    def to_dict(self):
+        data = {}
+        for name, value in self.field_values():
+            data[name] = value
+        return data
+
+
+def dump_python_dict(obj, depth):
+    if get_backend().is_concrete(type(obj)):
+        obj = DictWrap(obj, depth).to_dict()
+    return obj
