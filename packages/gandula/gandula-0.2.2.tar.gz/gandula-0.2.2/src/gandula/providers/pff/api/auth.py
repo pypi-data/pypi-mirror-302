08c9c7743578e707adc321fcbddb288d4d676649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 07:06:00 +0000

    Add package gandula/0.2.2/gandula-0.2.2.tar.gz

---
+++ packages/gandula/gandula-0.2.2.tar.gz/gandula-0.2.2/src/gandula/providers/pff/api/auth.py
@@ -0,0 +1,17 @@
+from gandula.providers.pff.api.api_client import execute_query, get_client
+
+from .queries import queries
+
+
+def account_active(
+    pff_api_url: str | None = None, pff_api_key: str | None = None
+) -> dict:
+    query = queries['account_active']
+    client = get_client(pff_api_url, pff_api_key)
+    return execute_query(query, client=client)
+
+
+def version(pff_api_url: str | None = None, pff_api_key: str | None = None) -> dict:
+    query = queries['version']
+    client = get_client(pff_api_url, pff_api_key)
+    return execute_query(query, client=client)
