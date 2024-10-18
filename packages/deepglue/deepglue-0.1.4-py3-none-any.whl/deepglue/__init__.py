ec1b832dfaf35c35b7da99bdaa5dc1a1ed131e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 04:05:55 +0000

    Add package deepglue/0.1.4/deepglue-0.1.4-py3-none-any.whl

---
+++ packages/deepglue/deepglue-0.1.4-py3-none-any.whl/deepglue/__init__.py
@@ -0,0 +1,36 @@
+"""
+deepGlue: A utility package for deep learning projects.
+"""
+
+__version__ = "0.1.4"  
+
+from .training_utils import train_one_epoch
+from .training_utils import validate_one_epoch
+from .training_utils import train_and_validate
+from .training_utils import accuracy
+
+from .plot_utils import plot_category_samples
+from .plot_utils import plot_batch
+from .plot_utils import plot_transformed
+from .plot_utils import convert_for_plotting
+
+from .file_utils import create_subdirs
+from .file_utils import get_category_counts_by_split
+from .file_utils import get_samples_per_category
+from .file_utils import get_samples_per_split
+
+
+__all__ = ["train_one_epoch",
+           "validate_one_epoch",
+           "train_and_validate",
+           "accuracy",
+           
+           "plot_category_samples",
+           "plot_batch",
+           "plot_transformed",
+           "convert_for_plotting",
+           
+           "create_subdirs", 
+           "get_category_counts_by_split",
+           "get_samples_per_category",
+           "get_samples_per_split"]
