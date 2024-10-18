aaa32d7ef13d13990908d0f94cb7a79764d4ac00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 16:05:52 +0000

    Add package django-ilmoitin/0.7.1/django_ilmoitin-0.7.1.tar.gz

---
+++ packages/django-ilmoitin/django_ilmoitin-0.7.1.tar.gz/django_ilmoitin-0.7.1/django_ilmoitin/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class DjangoIlmoitinConfig(AppConfig):
+    name = "django_ilmoitin"
+    verbose_name = _("Ilmoitin")
+    default_auto_field = "django.db.models.BigAutoField"
