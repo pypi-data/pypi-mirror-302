20cf99fbaccf5139afe9b07bb810b561c5080a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 16:05:42 +0000

    Add package FoxLabel/1.0/FoxLabel-1.0-py2.py3-none-any.whl

---
+++ packages/FoxLabel/FoxLabel-1.0-py2.py3-none-any.whl/FoxLabel/__init__.py
@@ -0,0 +1,17 @@
+"""
+FoxLabel
+
+An python parametters library.
+"""
+
+__version__ = "1.0"
+__author__ = "Nguyễn Tuấn Anh - nt.anh.fai@gmail.com"
+__name__ = "Foxlabel - Phần mềm chuyên dụng đánh nhãn ảnh Object Detection"
+__credits__ = "MIT License"
+__console__ = "ntanh_foxlabel"
+__help__ = ""
+
+from FoxLabel.FoxLabel import Fmain
+
+def start_program():
+    Fmain()
