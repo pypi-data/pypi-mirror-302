0d14c4fe3f4e20d9fd20e10307056507c337b160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 03:10:16 +0000

    Add package ts-tokenizer/0.1.4/ts_tokenizer-0.1.4.tar.gz

---
+++ packages/ts-tokenizer/ts_tokenizer-0.1.4.tar.gz/ts_tokenizer-0.1.4/ts_tokenizer/cli.py
@@ -0,0 +1,19 @@
+"""
+ts-tokenizer
+tanersezerr@gmail.com
+"""
+import sys
+from ts_tokenizer.tokenizer import TSTokenizer
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <args>")
+        sys.exit(1)
+
+    # Call the TSTokenizer main method which handles argument parsing and processing
+    TSTokenizer.ts_tokenize()
+
+
+if __name__ == '__main__':
+    main()
