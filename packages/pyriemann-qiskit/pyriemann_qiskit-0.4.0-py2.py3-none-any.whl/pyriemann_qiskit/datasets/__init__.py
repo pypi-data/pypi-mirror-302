056b6fb6a43fa279c2f5e49e3bb6fc3bf9ee8e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 07:05:21 +0000

    Add package pyriemann-qiskit/0.4.0/pyriemann_qiskit-0.4.0-py2.py3-none-any.whl

---
+++ packages/pyriemann-qiskit/pyriemann_qiskit-0.4.0-py2.py3-none-any.whl/pyriemann_qiskit/datasets/__init__.py
@@ -0,0 +1,16 @@
+from .utils import (
+    get_mne_sample,
+    generate_linearly_separable_dataset,
+    generate_qiskit_dataset,
+    get_feature_dimension,
+    MockDataset,
+)
+
+
+__all__ = [
+    "get_mne_sample",
+    "generate_linearly_separable_dataset",
+    "generate_qiskit_dataset",
+    "get_feature_dimension",
+    "MockDataset",
+]
