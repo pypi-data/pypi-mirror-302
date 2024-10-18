59fa1f358e9177965a75f737c5ff085a5af428e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 16:06:13 +0000

    Add package PyQtPath/0.1.0/PyQtPath-0.1.0-py3-none-any.whl

---
+++ packages/PyQtPath/PyQtPath-0.1.0-py3-none-any.whl/PyQtPath/path_str_pyqt6.py
@@ -0,0 +1,41 @@
+from types import ModuleType
+from typing import Optional
+import importlib
+
+from PyQt6.QtCore import QObject
+from PyQt6.QtWidgets import QWidget
+
+
+def child(widget: QWidget, path: str) -> Optional:
+    if widget is None or path is None or len(path) == 0:
+        return None
+    normalized_path: list[(type[QWidget], int)] = __normalize_path(path)
+    return __nested_child(widget, normalized_path)
+
+
+def __nested_child(widget: QObject, path: list[(type[QObject], int)]) -> Optional:
+    current_widget: QObject = widget
+    for part in path:
+        clazz: type[QObject] = part[0]
+        index: int = part[1]
+        children: list[QObject] = current_widget.findChildren(clazz)
+        if len(children) == 0:
+            return None
+        current_widget = children[index]
+    return current_widget
+
+
+def __normalize_path(path: str) -> list[(type[QObject], int)]:
+    if path is None or len(path) == 0:
+        return []
+    parts: list[str] = path.split("/")
+    for i, part in enumerate(parts):
+        if part.isdigit():
+            continue
+        next_part: str = parts[i + 1] if i < len(parts) - 1 else None
+        module: ModuleType = importlib.import_module("PyQt6.QtWidgets")
+        clazz: type[QObject] = getattr(module, part)
+        if next_part is not None and next_part.isdigit():
+            yield clazz, int(next_part)
+        else:
+            yield clazz, 0
