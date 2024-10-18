b3a1f6365c78254d0d3cb4c240d9cd6a46e22fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 16:05:25 +0000

    Add package MeUtils/2024.10.18.16.59.58/MeUtils-2024.10.18.16.59.58-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2024.10.18.16.59.58-py3-none-any.whl/examples/gr/d.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : d
+# @Time         : 2024/8/16 14:07
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+from meutils.pipe import *
+
+# 使用示例
+url = "https://fluxpro.art/api/view/clzwako9n01l6e2zoy3q5mpfe/3.webp"
+
+print(download(url))
