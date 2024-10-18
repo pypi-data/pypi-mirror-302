99e4faae6c964a3436f4725d1141479a97ab91de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 03:10:20 +0000

    Add package dstack-sdk/0.1.1/dstack_sdk-0.1.1.tar.gz

---
+++ packages/dstack-sdk/dstack_sdk-0.1.1.tar.gz/dstack_sdk-0.1.1/tests/test_client.py
@@ -0,0 +1,27 @@
+import pytest
+from dstack_sdk import TappdClient, AsyncTappdClient, DeriveKeyResponse, TdxQuoteResponse
+
+# SOCKET_PATH = '../../tappd.sock'
+SOCKET_PATH = 'http://127.0.0.1:8090'
+
+def test_sync_client_derive_key():
+    client = TappdClient(SOCKET_PATH)
+    result = client.derive_key('/', 'test')
+    assert isinstance(result, DeriveKeyResponse)
+
+def test_sync_client_tdx_quote():
+    client = TappdClient(SOCKET_PATH)
+    result = client.tdx_quote('test')
+    assert isinstance(result, TdxQuoteResponse)
+
+@pytest.mark.asyncio
+async def test_async_client_derive_key():
+    client = AsyncTappdClient(SOCKET_PATH)
+    result = await client.derive_key('/', 'test')
+    assert isinstance(result, DeriveKeyResponse)
+
+@pytest.mark.asyncio
+async def test_async_client_derive_key():
+    client = AsyncTappdClient(SOCKET_PATH)
+    result = await client.tdx_quote('test')
+    assert isinstance(result, TdxQuoteResponse)
