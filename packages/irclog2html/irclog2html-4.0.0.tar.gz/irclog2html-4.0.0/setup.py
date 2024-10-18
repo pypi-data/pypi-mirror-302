a0e3706288fd600935636971eeb7fe442851c28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 03:10:06 +0000

    Add package irclog2html/4.0.0/irclog2html-4.0.0.tar.gz

---
+++ packages/irclog2html/irclog2html-4.0.0.tar.gz/irclog2html-4.0.0/setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import os
+import re
+from io import open
+
+from setuptools import setup
+
+
+here = os.path.dirname(__file__)
+
+
+def read(filename):
+    with open(os.path.join(here, filename), encoding='utf-8') as f:
+        return f.read()
+
+
+long_description = read('README.rst') + '\n\n' + read('CHANGES.rst')
+
+version_file = os.path.join(here, 'src/irclog2html/_version.py')
+d = dict(re.findall('''(__[a-z]+__) *= *'([^']*)''', read(version_file)))
+version = d['__version__']
+homepage = d['__homepage__']
+
+setup(
+    name='irclog2html',
+    version=version,
+    author='Marius Gedminas',
+    author_email='marius@gedmin.as',
+    license='GPL v2 or v3',
+    platforms=['any'],
+    url=homepage,
+    description='Convert IRC logs to HTML',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    python_requires='>=3.7',
+    keywords='irc log colorizer html wsgi',
+    extras_require=dict(test=[
+        "zope.testing",
+    ]),
+    packages=['irclog2html'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    entry_points="""
+        [console_scripts]
+        irclog2html = irclog2html.irclog2html:main
+        logs2html = irclog2html.logs2html:main
+        irclogsearch = irclog2html.irclogsearch:main
+        irclogserver = irclog2html.irclogserver:main
+    """,
+    zip_safe=False,
+)
