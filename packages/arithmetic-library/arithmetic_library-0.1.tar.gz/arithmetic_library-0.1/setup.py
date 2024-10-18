ccf78ec5ab788cc556153aa5c590fb766e383e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 14:05:24 +0000

    Add package arithmetic-library/0.1/arithmetic_library-0.1.tar.gz

---
+++ packages/arithmetic-library/arithmetic_library-0.1.tar.gz/arithmetic_library-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='arithmetic_library',
+    version='0.1',
+    py_modules=['arithmetic_library'],
+    description='A simple arithmetic library',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/arithmetic_library',  # URL to your project (if applicable)
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Change as necessary
+    ],
+)
+
