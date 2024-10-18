b2edb8596caf562782caaae4d04c311ccd2bc4ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 04:07:30 +0000

    Add package ods-utils-py/0.1.8/ods_utils_py-0.1.8.tar.gz

---
+++ packages/ods-utils-py/ods_utils_py-0.1.8.tar.gz/ods_utils_py-0.1.8/src/ods_utils_py/__init__.py
@@ -0,0 +1,12 @@
+from ._config import _check_all_environment_variables_are_set
+
+_check_all_environment_variables_are_set()
+
+
+### Public Methods ###
+from .get_dataset_custom_view import get_dataset_custom_view
+from .get_dataset_license import get_dataset_license
+from .get_number_of_datasets import get_number_of_datasets
+from .get_uid_by_id import get_uid_by_id
+from .set_dataset_license import set_dataset_license
+from .set_dataset_public import set_dataset_public
