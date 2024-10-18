0ff84d8636d67a79f05f8a84bb296572e7dff567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 07:05:41 +0000

    Add package sdwire/0.1.2/sdwire-0.1.2.tar.gz

---
+++ packages/sdwire/sdwire-0.1.2.tar.gz/sdwire-0.1.2/sdwire/backend/device/sdwire.py
@@ -0,0 +1,25 @@
+from serial import Serial
+from backend.device.usb_device import USBDevice, PortInfo
+
+
+class SDWire(USBDevice):
+
+    def __init__(self, port_info: PortInfo):
+        super().__init__(port_info)
+
+    @property
+    def serial_string(self) -> str:
+        return "sdwire_gen2_101"
+
+    def invoke(self, command: str) -> None:
+        with Serial(self.dev_string) as channel:
+            channel.timeout = 0.05
+            data = command.encode() + b"\n"
+            print(f"data={data}")
+            channel.write(data)
+
+    def __str__(self):
+        return f"{self.serial_string}\t\t[{self.product_string}::{self.manufacturer_string}]"
+
+    def __repr__(self):
+        return self.__str__()
