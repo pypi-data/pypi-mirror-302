f9b6a5b409676b0c91960bc06dfe3b3217e2adc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 14:05:15 +0000

    Add package spitch/1.11.0/spitch-1.11.0-py3-none-any.whl

---
+++ packages/spitch/spitch-1.11.0-py3-none-any.whl/spitch/types/__init__.py
@@ -0,0 +1,8 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .text_tone_mark_params import TextToneMarkParams as TextToneMarkParams
+from .text_translate_params import TextTranslateParams as TextTranslateParams
+from .speech_generate_params import SpeechGenerateParams as SpeechGenerateParams
+from .speech_transcribe_params import SpeechTranscribeParams as SpeechTranscribeParams
