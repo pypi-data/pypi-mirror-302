68125d024cd20cc0eb24f94c5df56a3de4c0dd53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 03:10:10 +0000

    Add package weas-widget/0.1.8/weas_widget-0.1.8.tar.gz

---
+++ packages/weas-widget/weas_widget-0.1.8.tar.gz/weas_widget-0.1.8/src/weas_widget/plugins/species.py
@@ -0,0 +1,44 @@
+from ..base_class import WidgetWrapper, ChangeTrackingDict
+from ase.data import atomic_numbers
+from weas_widget.data import color_data, radii_data
+
+
+class SpeciesManager(WidgetWrapper):
+
+    catalog = "species"
+
+    _attribute_map = {}
+
+    _extra_allowed_attrs = ["_settings", "settings"]
+
+    def __init__(self, _widget):
+        super().__init__(_widget)
+        self._settings = ChangeTrackingDict(widget=self._widget, key="speciesSettings")
+
+    @property
+    def settings(self):
+        return self._settings
+
+    @settings.setter
+    def settings(self, value):
+        self._settings = ChangeTrackingDict(
+            value, widget=self._widget, key="speciesSettings"
+        )
+
+    def update_atoms(self):
+        self.settings = self.get_default_settings()
+
+    def get_default_settings(self):
+        settings = {}
+        species_dict = self._widget.atoms.get("species", {})
+        for species in species_dict:
+            element = species_dict[species]
+            number = atomic_numbers[element]
+            color = color_data[self._widget.colorType][number]
+            settings[species] = {
+                "element": element,
+                "symbol": species,
+                "color": color,
+                "radius": radii_data[self._widget.radiusType.upper()][number],
+            }
+        return settings
