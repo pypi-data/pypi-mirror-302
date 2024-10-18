debeb88fe1fccdafe4c1a01d3afa2df89818ec05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 04:08:08 +0000

    Add package my_lib_gens/0.0.3/my_lib_gens-0.0.3.tar.gz

---
+++ packages/my_lib_gens/my_lib_gens-0.0.3.tar.gz/my_lib_gens-0.0.3/src/my_lib_gens/__init__.py
@@ -0,0 +1,8 @@
+from my_lib_gens._add import add
+from my_lib_gens._sub import sub
+from my_lib_gens._multiply import mult
+from my_lib_gens._divide import div
+
+__version__ = "0.0.3"
+
+__all__ = ["add", "sub", "mult", "div"]
