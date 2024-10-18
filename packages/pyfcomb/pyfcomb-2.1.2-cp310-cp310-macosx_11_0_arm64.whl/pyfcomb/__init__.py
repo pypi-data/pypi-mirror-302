3dde45288446b734a682edad6c35d2986d4228d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 03:10:20 +0000

    Add package pyfcomb/2.1.2/pyfcomb-2.1.2-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/pyfcomb/pyfcomb-2.1.2-cp310-cp310-macosx_11_0_arm64.whl/pyfcomb/__init__.py
@@ -0,0 +1,4 @@
+from pyfcomb.reader import get_combinations,_solution_strings,_calculate_residual,_parse_frequencies
+from .rfcomb import RFrequency, r_get_combinations
+
+__all__ = ['RFrequency', 'r_get_combinations']
