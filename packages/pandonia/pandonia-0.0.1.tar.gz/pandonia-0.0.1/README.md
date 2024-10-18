208e4972f7519e71807fab853bbe78f50a2936c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 15:06:14 +0000

    Add package pandonia/0.0.1/pandonia-0.0.1.tar.gz

---
+++ packages/pandonia/pandonia-0.0.1.tar.gz/pandonia-0.0.1/README.md
@@ -0,0 +1,2 @@
+# pandora-data-handler
+This repository is a placeholder for the development of a utility interface for interacting with the Pandora network.
---
+++ packages/pandonia/pandonia-0.0.1.tar.gz/pandonia-0.0.1/src/pandonia/example.py
@@ -0,0 +1,8 @@
+"""
+This is just a placeholder.
+It was created to have a structure to allow the registration of the project wit Pip.
+"""
+
+
+def hello():
+    return "hello"
