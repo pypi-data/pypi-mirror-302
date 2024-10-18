ba826774945df5f68561966413865a45a4c29f1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 17:05:28 +0000

    Add package digitalhub-runtime-container/0.8.0b6/digitalhub_runtime_container-0.8.0b6.tar.gz

---
+++ packages/digitalhub-runtime-container/digitalhub_runtime_container-0.8.0b6.tar.gz/digitalhub_runtime_container-0.8.0b6/digitalhub_runtime_container/entities/run/container_run/entity.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+import typing
+
+from digitalhub.entities.run._base.entity import Run
+
+if typing.TYPE_CHECKING:
+    from digitalhub_runtime_container.entities.run.container_run.spec import RunSpecContainerRun
+    from digitalhub_runtime_container.entities.run.container_run.status import RunStatusContainerRun
+
+    from digitalhub.entities._base.entity.metadata import Metadata
+
+
+class RunContainerRun(Run):
+    """
+    RunContainerRun class.
+    """
+
+    def __init__(
+        self,
+        project: str,
+        uuid: str,
+        kind: str,
+        metadata: Metadata,
+        spec: RunSpecContainerRun,
+        status: RunStatusContainerRun,
+        user: str | None = None,
+    ) -> None:
+        super().__init__(project, uuid, kind, metadata, spec, status, user)
+
+        self.spec: RunSpecContainerRun
+        self.status: RunStatusContainerRun
---
+++ packages/digitalhub-runtime-container/digitalhub_runtime_container-0.8.0b6.tar.gz/digitalhub_runtime_container-0.8.0b6/digitalhub_runtime_container/entities/run/container_run/status.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from digitalhub.entities.run._base.status import RunStatus
+
+
+class RunStatusContainerRun(RunStatus):
+    """
+    RunStatusContainerRun status.
+    """
