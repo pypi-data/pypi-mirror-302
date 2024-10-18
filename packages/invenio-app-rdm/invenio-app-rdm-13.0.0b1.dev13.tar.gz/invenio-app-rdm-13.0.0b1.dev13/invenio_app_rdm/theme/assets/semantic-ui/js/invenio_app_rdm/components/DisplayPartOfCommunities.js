6687b95bf87b3b75c3a3d29206a0d6023b1bb56d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 05:06:39 +0000

    Add package invenio-app-rdm/13.0.0b1.dev13/invenio-app-rdm-13.0.0b1.dev13.tar.gz

---
+++ packages/invenio-app-rdm/invenio-app-rdm-13.0.0b1.dev13.tar.gz/invenio-app-rdm-13.0.0b1.dev13/invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/components/DisplayPartOfCommunities.js
@@ -0,0 +1,55 @@
+// This file is part of InvenioRDM
+// Copyright (C) 2024 CERN.
+//
+// Invenio App RDM is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+import { i18next } from "@translations/invenio_app_rdm/i18next";
+import { Popup, Icon } from "semantic-ui-react";
+
+export const DisplayPartOfCommunities = ({ communities }) => {
+  const PartOfCommunities = () => {
+    // FIXME: Uncomment to enable themed banner
+    // const communitiesEntries = communities.entries?.filter((community) => !(community.id === communities?.default && community?.theme));
+    const communitiesEntries = communities.entries;
+
+    if (communitiesEntries?.length > 0) {
+      return (
+        <>
+          {i18next.t("Part of ")}
+          {communitiesEntries.map((community, index) => {
+            return (
+              <Fragment key={community.slug}>
+                <a href={`/communities/${community.slug}`}>
+                  {community.metadata?.title}
+                </a>
+                <span>&nbsp;</span>
+                {community.is_verified && (
+                  <Popup
+                    trigger={<Icon name="check outline circle" color="green mr-0" />}
+                    content="Verified community"
+                    position="top center"
+                  />
+                )}
+
+                {index !== communitiesEntries.length - 1 && ", "}
+              </Fragment>
+            );
+          })}
+        </>
+      );
+    }
+  };
+
+  return (
+    <p>
+      <b>{PartOfCommunities()}</b>
+    </p>
+  );
+};
+
+DisplayPartOfCommunities.propTypes = {
+  communities: PropTypes.object.isRequired,
+};
