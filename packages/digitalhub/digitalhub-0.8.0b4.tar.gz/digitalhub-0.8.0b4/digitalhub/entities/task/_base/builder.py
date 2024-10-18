89404420f40c9074f3898dc55d3f4825736cf336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 04:06:43 +0000

    Add package digitalhub/0.8.0b4/digitalhub-0.8.0b4.tar.gz

---
+++ packages/digitalhub/digitalhub-0.8.0b4.tar.gz/digitalhub-0.8.0b4/digitalhub/entities/task/_base/builder.py
@@ -0,0 +1,76 @@
+from __future__ import annotations
+
+import typing
+
+from digitalhub.entities._base.unversioned.builder import UnversionedBuilder
+from digitalhub.entities.utils.entity_types import EntityTypes
+from digitalhub.utils.exceptions import BuilderError
+
+if typing.TYPE_CHECKING:
+    from digitalhub.entities.task._base.entity import Task
+
+
+class TaskBuilder(UnversionedBuilder):
+    """
+    Task builder.
+    """
+
+    ACTION: str = None
+    ENTITY_TYPE = EntityTypes.TASK.value
+
+    def __init__(self) -> None:
+        super().__init__()
+        if self.ACTION is None:
+            raise BuilderError("ACTION must be set")
+
+    def build(
+        self,
+        project: str,
+        kind: str,
+        uuid: str | None = None,
+        labels: list[str] | None = None,
+        function: str | None = None,
+        **kwargs,
+    ) -> Task:
+        """
+        Create a new object.
+
+        Parameters
+        ----------
+        project : str
+            Project name.
+        kind : str
+            Kind the object.
+        uuid : str
+            ID of the object.
+        labels : list[str]
+            List of labels.
+        function : str
+            Name of the executable associated with the task.
+        **kwargs : dict
+            Spec keyword arguments.
+
+        Returns
+        -------
+        Task
+            Object instance.
+        """
+        uuid = self.build_uuid(uuid)
+        metadata = self.build_metadata(
+            project=project,
+            name=uuid,
+            labels=labels,
+        )
+        spec = self.build_spec(
+            function=function,
+            **kwargs,
+        )
+        status = self.build_status()
+        return self.build_entity(
+            project=project,
+            uuid=uuid,
+            kind=kind,
+            metadata=metadata,
+            spec=spec,
+            status=status,
+        )
