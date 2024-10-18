466309e854aed79f043f09171992a908d8a459e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 14:05:24 +0000

    Add package servicestack/0.1.6/servicestack-0.1.6.tar.gz

---
+++ packages/servicestack/servicestack-0.1.6.tar.gz/servicestack-0.1.6/tests/config.py
@@ -0,0 +1,18 @@
+from servicestack import JsonServiceClient, Authenticate
+
+# TEST_URL = "https://localhost:5001"
+# TEST_URL = "http://localhost:5000"
+TEST_URL = "http://test.servicestack.net"
+TECHSTACKS_URL = "https://techstacks.io"
+
+
+def create_test_client():
+    return JsonServiceClient(TEST_URL)
+
+
+def create_techstacks_client():
+    return JsonServiceClient(TECHSTACKS_URL)
+
+
+def clear_session(client: JsonServiceClient):
+    client.post(Authenticate(provider="logout"))
