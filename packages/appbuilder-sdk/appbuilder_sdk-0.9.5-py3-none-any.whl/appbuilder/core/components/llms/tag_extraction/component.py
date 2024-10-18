a0f10c0603415cecc3b2a4b130db959bc489a64c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 15:06:05 +0000

    Add package appbuilder-sdk/0.9.5/appbuilder_sdk-0.9.5-py3-none-any.whl

---
+++ packages/appbuilder-sdk/appbuilder_sdk-0.9.5-py3-none-any.whl/appbuilder/core/components/llms/tag_extraction/component.py
@@ -0,0 +1,99 @@
+# Copyright (c) 2023 Baidu, Inc. All Rights Reserved.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from appbuilder.core.components.llms.base import CompletionBaseComponent
+from appbuilder.core.message import Message
+from appbuilder.core.component import ComponentArguments
+from pydantic import BaseModel, Field
+from typing import Optional
+from appbuilder.utils.trace.tracer_wrapper import components_run_trace, components_run_stream_trace
+
+
+class TagExtractionArgs(ComponentArguments):
+    """
+    标签抽取配置
+
+    Attributes:
+        message (Message): 输入消息，用于模型的主要输入内容
+    """
+    message: Message = Field(...,
+                             variable_name="query",
+                             description="""输入消息，用于模型的主要输入内容，例如'本实用新型公开了一种可利用热能的太阳能光伏光热一体化组件，
+                             包括太阳能电池，还包括有吸热板，太阳能电池粘附在吸热板顶面，吸热板内嵌入有热电材料制成的内芯，吸热板底面设置有蛇形管。
+                             本实用新型结构紧凑，安装方便，能充分利用太阳能电池散发的热能，具有较高的热能利用率。'""")
+
+
+class TagExtraction(CompletionBaseComponent):
+    """
+    标签抽取组件，基于生成式大模型进行关键标签的抽取。
+
+    Examples:
+
+    .. code-block:: python
+
+        import appbuilder
+        # 请前往千帆AppBuilder官网创建密钥，流程详见：https://cloud.baidu.com/doc/AppBuilder/s/Olq6grrt6#1%E3%80%81%E5%88%9B%E5%BB%BA%E5%AF%86%E9%92%A5
+        os.environ["APPBUILDER_TOKEN"] = '...'
+
+        tag_extraction = appbuilder.TagExtraction(model="ERNIE Speed-AppBuilder")
+        answer = tag_extraction(appbuilder.Message("从这段文本中抽取关键标签"))
+
+    """
+
+    name = "tag_extraction"
+    version = "v1"
+    meta = TagExtractionArgs
+
+    def __init__(
+        self, 
+        model=None,
+        secret_key: Optional[str] = None, 
+        gateway: str = "",
+        lazy_certification: bool = False,
+    ):
+        """初始化TagExtraction模型。
+        
+        Args:
+            model (str|None): 模型名称，用于指定要使用的千帆模型。
+            secret_key (str, 可选): 用户鉴权token, 默认从环境变量中获取: os.getenv("APPBUILDER_TOKEN", "").
+            gateway (str, 可选): 后端网关服务地址，默认从环境变量中获取: os.getenv("GATEWAY_URL", "")
+            lazy_certification (bool, 可选): 延迟认证，为True时在第一次运行时认证. Defaults to False.
+        
+        Returns:
+            None
+        
+        """
+        super().__init__(
+                TagExtractionArgs, model=model, secret_key=secret_key, gateway=gateway, lazy_certification=lazy_certification)
+
+    @components_run_trace
+    def run(self, message, stream=False, temperature=1e-10, top_p=0.0):
+        """
+        使用给定的输入运行模型并返回结果。
+        
+        Args:
+            message (obj:`Message`, 必选): 输入消息，用于模型的主要输入内容。
+            stream (bool, 可选): 指定是否以流式形式返回响应。默认为 False。
+            temperature (float, 可选): 模型配置的温度参数，用于调整模型的生成概率。
+                取值范围为 0.0 到 1.0，其中较低的值使生成更确定性，较高的值使生成更多样性。
+                默认值为 1e-10。
+            top_p (float, 可选): 影响输出文本的多样性，取值越大，生成文本的多样性越强。
+                取值范围为 0.0 到 1.0，其中较低的值使生成更确定性，较高的值使生成更多样性。
+                默认值为 0。
+        
+        Returns:
+            obj:`Message`: 模型运行后的输出消息。
+        """
+        return super().run(message=message, stream=stream, temperature=temperature, top_p=top_p)
