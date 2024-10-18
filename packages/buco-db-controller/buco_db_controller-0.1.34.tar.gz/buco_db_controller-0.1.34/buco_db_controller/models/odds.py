3ed573857941009bd1dd582a49d8642af124c53e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 03:10:06 +0000

    Add package buco-db-controller/0.1.34/buco_db_controller-0.1.34.tar.gz

---
+++ packages/buco-db-controller/buco_db_controller-0.1.34.tar.gz/buco_db_controller-0.1.34/buco_db_controller/models/odds.py
@@ -0,0 +1,50 @@
+from buco_db_controller.models.team import Team
+
+
+class Odds:
+    def __init__(
+            self,
+            fixture_id: int,
+            date,
+
+            home_team: Team,
+            away_team: Team,
+
+            result,
+            over_under,
+            btts,
+            dnb,
+    ):
+        self.fixture_id = fixture_id
+        self.date = date
+
+        self.home_team = home_team
+        self.away_team = away_team
+
+        self.result = result
+        self.over_under = over_under
+        self.btts = btts
+        self.dnb = dnb
+
+    @classmethod
+    def from_dict(cls, response):
+        data = response['data']
+
+        return cls(
+            fixture_id=response['parameters']['fixture'],
+            date=data['date'],
+
+            home_team=Team(
+                team_id=data['teams']['home']['id'],
+                name=data['teams']['home']['name'],
+            ),
+            away_team=Team(
+                team_id=data['teams']['away']['id'],
+                name=data['teams']['away']['name'],
+            ),
+
+            result=data['odds']['1X2'],
+            over_under=data['odds']['over_under'],
+            btts=data['odds']['btts'],
+            dnb=data['odds']['dnb'],
+        )
