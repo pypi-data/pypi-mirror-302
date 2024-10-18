31512a9ce95f3bb745d4215ff2e3b4b95fc37998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 05:06:16 +0000

    Add package InfoGerm/0.0.1/InfoGerm-0.0.1.tar.gz

---
+++ packages/InfoGerm/InfoGerm-0.0.1.tar.gz/InfoGerm-0.0.1/InfoGerm/main.py
@@ -0,0 +1,6 @@
+def hello():
+    print("Hello from InfoGerm!")
+
+def intro():
+    name = input("Please Enter your Name: ")
+    print("Hi ", name,"\n\nWelcome to InfoGerm\nWe are a platform dedicated to help students to achieve their dream. \nWe are into Education, Freelancing and Tech Consultancy.\n")
