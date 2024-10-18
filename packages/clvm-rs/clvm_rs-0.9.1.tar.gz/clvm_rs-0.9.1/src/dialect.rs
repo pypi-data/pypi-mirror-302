cfd965f06154deee61fdbfc40afed6bf57ea73a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 14:05:11 +0000

    Add package clvm-rs/0.9.1/clvm_rs-0.9.1.tar.gz

---
+++ packages/clvm-rs/clvm_rs-0.9.1.tar.gz/clvm_rs-0.9.1/src/dialect.rs
@@ -0,0 +1,26 @@
+use crate::allocator::{Allocator, NodePtr};
+use crate::cost::Cost;
+use crate::reduction::Response;
+
+#[repr(u32)]
+#[derive(Clone, Copy, Eq, PartialEq)]
+pub enum OperatorSet {
+    Default,
+    BLS,
+}
+
+pub trait Dialect {
+    fn quote_kw(&self) -> u32;
+    fn apply_kw(&self) -> u32;
+    fn softfork_kw(&self) -> u32;
+    fn softfork_extension(&self, ext: u32) -> OperatorSet;
+    fn op(
+        &self,
+        allocator: &mut Allocator,
+        op: NodePtr,
+        args: NodePtr,
+        max_cost: Cost,
+        extensions: OperatorSet,
+    ) -> Response;
+    fn allow_unknown_ops(&self) -> bool;
+}
