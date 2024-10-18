fa72dc95eac03952ca0bec367371f1c1200d0afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 04:07:05 +0000

    Add package my_lib_gens/0.0.4/my_lib_gens-0.0.4.tar.gz

---
+++ packages/my_lib_gens/my_lib_gens-0.0.4.tar.gz/my_lib_gens-0.0.4/src/my_lib_gens/__init__.py
@@ -0,0 +1,9 @@
+from my_lib_gens._add import add
+from my_lib_gens._sub import sub
+from my_lib_gens._multiply import mult
+from my_lib_gens._divide import div
+from my_lib_gens._modulo import mod
+
+__version__ = "0.0.4"
+
+__all__ = ["add", "sub", "mult", "div", "mod"]
