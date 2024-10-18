a20bd403ce91949a8997293f00354b31e3cc038f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 07:05:09 +0000

    Add package img-proof/7.33.0/img_proof-7.33.0.tar.gz

---
+++ packages/img-proof/img_proof-7.33.0.tar.gz/img_proof-7.33.0/usr/share/lib/img_proof/tests/SLES/test_sles_motd.py
@@ -0,0 +1,32 @@
+import pytest
+
+
+@pytest.mark.skipinbeta
+def test_sles_motd(host, get_release_value):
+    motd = host.file('/etc/motd')
+
+    if not motd.exists:
+        motd = host.file('/usr/lib/motd.d/10_header')
+
+    assert motd.exists
+    assert motd.is_file
+
+    pretty_name = get_release_value('PRETTY_NAME')
+    assert pretty_name
+
+    version = get_release_value('VERSION')
+    assert version
+
+    variant = get_release_value('VARIANT_ID') or ''
+
+    if 'hardened' in variant:
+        pytest.skip('Unable to validate motd in hardened images.')
+
+    assert (
+        motd.contains(pretty_name) or
+        motd.contains(
+            'SUSE Linux Enterprise Server {0}'.format(
+                version.replace('-', ' ')
+            )
+        )
+    )
