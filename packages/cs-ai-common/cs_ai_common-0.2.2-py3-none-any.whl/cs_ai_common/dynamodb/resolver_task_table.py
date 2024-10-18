c82426611ea3f81280fbedce720fee18a983531d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 16:05:25 +0000

    Add package cs-ai-common/0.2.2/cs_ai_common-0.2.2-py3-none-any.whl

---
+++ packages/cs-ai-common/cs_ai_common-0.2.2-py3-none-any.whl/cs_ai_common/dynamodb/resolver_task_table.py
@@ -0,0 +1,28 @@
+from cs_ai_common.dynamodb.utils import build_expression_attribute_names, build_expression_attribute_values, build_update_expression
+from botocore.exceptions import ClientError
+import boto3
+import os
+from cs_ai_common.logging.internal_logger import InternalLogger
+
+def update_resolver_task(task_id: str, resolver_name: str, **kwargs) -> None:
+    dynamodb = boto3.client('dynamodb')
+    table_name = os.getenv("RESULTS_TABLE_NAME")
+    try:
+        dynamodb.update_item(
+            TableName=table_name,
+            Key={
+                'task_id': {
+                    'S': task_id
+                },
+                'resolver_name': {
+                    'S': resolver_name
+                }
+            },
+            UpdateExpression=build_update_expression(kwargs),
+            ExpressionAttributeNames=build_expression_attribute_names(kwargs),
+            ExpressionAttributeValues=build_expression_attribute_values(kwargs)
+        )
+        InternalLogger.LogDebug("UpdateItem succeeded")
+    except ClientError as e:
+        InternalLogger.LogError(f"Unable to update item: {e.response['Error']['Message']}")
+        raise e
