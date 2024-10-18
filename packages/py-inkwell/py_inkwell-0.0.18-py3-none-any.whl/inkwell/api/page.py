30dd8c4aa7541898e42487267e0a0b6399e4e0ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 06:05:15 +0000

    Add package py-inkwell/0.0.18/py_inkwell-0.0.18-py3-none-any.whl

---
+++ packages/py-inkwell/py_inkwell-0.0.18-py3-none-any.whl/inkwell/api/page.py
@@ -0,0 +1,72 @@
+from base64 import b64encode
+from enum import Enum
+from typing import List, Optional, Union
+
+from pydantic import BaseModel
+
+from inkwell.api.figure import Figure
+from inkwell.api.table import Table
+from inkwell.api.text import TextBox
+
+
+class PageFragmentType(str, Enum):
+    """
+    Type of a page fragment.
+    """
+
+    TEXT = "text"
+    TABLE = "table"
+    FIGURE = "figure"
+
+
+class PageFragment(BaseModel):
+    fragment_type: PageFragmentType
+    content: Union[TextBox, Table, Figure]
+
+
+class Page(BaseModel):
+    """
+    Page in a document.
+    """
+
+    page_number: int
+    page_fragments: Optional[List[PageFragment]] = []
+    layout: Optional[dict] = {}
+    page_image: Optional[str] = None
+
+    def text_fragments(self) -> List[TextBox]:
+        return [
+            fragment
+            for fragment in self.page_fragments
+            if fragment.fragment_type == PageFragmentType.TEXT
+        ]
+
+    def table_fragments(self) -> List[Table]:
+        return [
+            fragment
+            for fragment in self.page_fragments
+            if fragment.fragment_type == PageFragmentType.TABLE
+        ]
+
+    def figure_fragments(self) -> List[Figure]:
+        return [
+            fragment
+            for fragment in self.page_fragments
+            if fragment.fragment_type == PageFragmentType.FIGURE
+        ]
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    @staticmethod
+    def encode_image(image_bytes: bytes) -> str:
+        """Convert image bytes to base64 encoded string."""
+        return b64encode(image_bytes).decode("utf-8")
+
+
+class Document(BaseModel):
+    """
+    Document in a document.
+    """
+
+    pages: List[Page]
