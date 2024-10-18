d1beb4a6d3173b564a046ae003356b55bbd4a5b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 04:07:45 +0000

    Add package digitalhub-runtime-modelserve/0.8.0b5/digitalhub_runtime_modelserve-0.8.0b5-py3-none-any.whl

---
+++ packages/digitalhub-runtime-modelserve/digitalhub_runtime_modelserve-0.8.0b5-py3-none-any.whl/digitalhub_runtime_modelserve/__init__.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from digitalhub_runtime_modelserve.entities.function.huggingfaceserve.builder import FunctionHuggingfaceserveBuilder
+from digitalhub_runtime_modelserve.entities.function.mlflowserve.builder import FunctionMlflowserveBuilder
+from digitalhub_runtime_modelserve.entities.function.sklearnserve.builder import FunctionSklearnserveBuilder
+from digitalhub_runtime_modelserve.entities.run.huggingfaceserve_run.builder import RunHuggingfaceserveRunBuilder
+from digitalhub_runtime_modelserve.entities.run.mlflowserve_run.builder import RunMlflowserveRunBuilder
+from digitalhub_runtime_modelserve.entities.run.sklearnserve_run.builder import RunSklearnserveRunBuilder
+from digitalhub_runtime_modelserve.entities.task.huggingfaceserve_serve.builder import TaskHuggingfaceserveServeBuilder
+from digitalhub_runtime_modelserve.entities.task.mlflowserve_serve.builder import TaskMlflowserveServeBuilder
+from digitalhub_runtime_modelserve.entities.task.sklearnserve_serve.builder import TaskSklearnserveServeBuilder
+
+entity_builders = (
+    (FunctionHuggingfaceserveBuilder.ENTITY_KIND, FunctionHuggingfaceserveBuilder),
+    (FunctionMlflowserveBuilder.ENTITY_KIND, FunctionMlflowserveBuilder),
+    (FunctionSklearnserveBuilder.ENTITY_KIND, FunctionSklearnserveBuilder),
+    (TaskHuggingfaceserveServeBuilder.ENTITY_KIND, TaskHuggingfaceserveServeBuilder),
+    (TaskMlflowserveServeBuilder.ENTITY_KIND, TaskMlflowserveServeBuilder),
+    (TaskSklearnserveServeBuilder.ENTITY_KIND, TaskSklearnserveServeBuilder),
+    (RunHuggingfaceserveRunBuilder.ENTITY_KIND, RunHuggingfaceserveRunBuilder),
+    (RunMlflowserveRunBuilder.ENTITY_KIND, RunMlflowserveRunBuilder),
+    (RunSklearnserveRunBuilder.ENTITY_KIND, RunSklearnserveRunBuilder),
+)
+
+try:
+    from digitalhub_runtime_modelserve.runtimes.builder import RuntimeModelserveBuilder
+    from digitalhub_runtime_modelserve.runtimes.kind_registry import (
+        huggingfaceserve_kind_registry,
+        mlflowserve_kind_registry,
+        sklearnserve_kind_registry,
+    )
+
+    runtime_builders = (
+        *[(kind, RuntimeModelserveBuilder) for kind in sklearnserve_kind_registry.get_all_kinds()],
+        *[(kind, RuntimeModelserveBuilder) for kind in mlflowserve_kind_registry.get_all_kinds()],
+        *[(kind, RuntimeModelserveBuilder) for kind in huggingfaceserve_kind_registry.get_all_kinds()],
+    )
+except ImportError:
+    runtime_builders = tuple()
