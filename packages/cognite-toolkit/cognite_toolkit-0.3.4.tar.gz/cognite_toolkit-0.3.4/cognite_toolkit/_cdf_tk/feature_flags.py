3415d110ee83c3b8b500d898aadcbc158dc4e505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 16:05:21 +0000

    Add package cognite-toolkit/0.3.4/cognite_toolkit-0.3.4.tar.gz

---
+++ packages/cognite-toolkit/cognite_toolkit-0.3.4.tar.gz/cognite_toolkit-0.3.4/cognite_toolkit/_cdf_tk/feature_flags.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from enum import Enum
+from functools import lru_cache
+from typing import Any, ClassVar
+
+from cognite_toolkit._cdf_tk.cdf_toml import CDFToml
+from cognite_toolkit._cdf_tk.constants import clean_name
+
+
+class Flags(Enum):
+    INTERNAL: ClassVar[dict[str, Any]] = {"visible": False, "description": "Does nothing"}
+    IMPORT_CMD: ClassVar[dict[str, Any]] = {"visible": True, "description": "Enables the import sub application"}
+    GRAPHQL: ClassVar[dict[str, Any]] = {
+        "visible": True,
+        "description": "Enables the support for deploying data models as GraphQL schemas",
+    }
+    ADO_PIPELINES: ClassVar[dict[str, Any]] = {
+        "visible": True,
+        "description": "Enables creation of Azure DevOps pipelines in repo init command",
+    }
+
+
+class FeatureFlag:
+    @staticmethod
+    @lru_cache(typed=True)
+    def is_enabled(flag: Flags) -> bool:
+        return CDFToml.load().feature_flags.get(clean_name(flag.name), False)
