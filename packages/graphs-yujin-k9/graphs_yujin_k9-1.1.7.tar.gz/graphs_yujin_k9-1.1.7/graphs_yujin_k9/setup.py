5062d234e6733d7a8f26fe378be21296e4faf906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 14:05:30 +0000

    Add package graphs-yujin-k9/1.1.7/graphs_yujin_k9-1.1.7.tar.gz

---
+++ packages/graphs-yujin-k9/graphs_yujin_k9-1.1.7.tar.gz/graphs_yujin_k9-1.1.7/graphs_yujin_k9/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="graphs_yujin_k9",
+    version="0.0.1",
+    description="A Python package for Dijkstra's algorithm and other graph-related algorithms.",
+    author="Kaylee Kim",
+    author_email="ykim28627@gmail.com",
+    packages=["graphs_yujin_k9"],
+    install_requires=[],
+)
---
+++ packages/graphs-yujin-k9/graphs_yujin_k9-1.1.7.tar.gz/graphs_yujin_k9-1.1.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="graphs_yujin_k9",
+    version="1.1.7",
+    packages=find_packages(include=['graphs_yujin_k9', 'graphs_yujin_k9.*']), 
+    include_package_data=True,
+    package_data={
+        '': ['*.py'],  
+    },
+    install_requires=[],
+)
