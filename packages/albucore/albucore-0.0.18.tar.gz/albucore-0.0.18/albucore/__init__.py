5e9851c1f9a3cc8696d0c5bf5ad43390bfaf9887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 15:05:28 +0000

    Add package albucore/0.0.18/albucore-0.0.18.tar.gz

---
+++ packages/albucore/albucore-0.0.18.tar.gz/albucore-0.0.18/albucore/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.0.18"
+
+from .decorators import *
+from .functions import *
+from .utils import *
---
+++ packages/albucore/albucore-0.0.18.tar.gz/albucore-0.0.18/setup.py
@@ -0,0 +1,55 @@
+import re
+from typing import List, Tuple
+import os
+
+from pkg_resources import DistributionNotFound, get_distribution
+from setuptools import setup, find_packages
+
+INSTALL_REQUIRES = [
+    "numpy>=1.24.4",
+    "typing-extensions>=4.9.0; python_version<'3.10'",
+    "stringzilla==3.10.4",
+
+]
+
+MIN_OPENCV_VERSION = "4.9.0.80"
+
+CHOOSE_INSTALL_REQUIRES = [
+    (
+        (f"opencv-python>={MIN_OPENCV_VERSION}", f"opencv-contrib-python>={MIN_OPENCV_VERSION}", f"opencv-contrib-python-headless>={MIN_OPENCV_VERSION}"),
+        f"opencv-python-headless>={MIN_OPENCV_VERSION}",
+    ),
+]
+
+def get_version():
+    version_file = os.path.join(os.path.dirname(__file__), 'albucore', '__init__.py')
+    with open(version_file, 'r') as f:
+        version_line = f.read().strip()
+    version_regex = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    match = re.match(version_regex, version_line, re.M)
+    if match:
+        return match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+def choose_requirement(mains: Tuple[str, ...], secondary: str) -> str:
+    chosen = secondary
+    for main in mains:
+        try:
+            name = re.split(r"[!<>=]", main)[0]
+            get_distribution(name)
+            chosen = main
+            break
+        except DistributionNotFound:
+            pass
+    return chosen
+
+def get_install_requirements(install_requires: List[str], choose_install_requires: List[Tuple[Tuple[str, ...], str]]) -> List[str]:
+    for mains, secondary in choose_install_requires:
+        install_requires.append(choose_requirement(mains, secondary))
+    return install_requires
+
+setup(
+    version=get_version(),
+    packages=find_packages(exclude=["tests", "benchmark"], include=['albucore*']),
+    install_requires=get_install_requirements(INSTALL_REQUIRES, CHOOSE_INSTALL_REQUIRES),
+)
