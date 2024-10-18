fee6faf58395753432d20421c0262846aa737b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 06:05:21 +0000

    Add package gptbioinsightor/0.5.1/gptbioinsightor-0.5.1-py3-none-any.whl

---
+++ packages/gptbioinsightor/gptbioinsightor-0.5.1-py3-none-any.whl/gptbioinsightor/core.py
@@ -0,0 +1,53 @@
+from openai import OpenAI
+from .prompt import SYSTEM_PROMPT
+from .constant import API_SOURCE
+from .utils import get_api_key
+
+
+def openai_client(msgs, apikey, model, provider, base_url=None, sys_prompt=None, tools=None):
+    if base_url is None:
+        base_url = API_SOURCE[provider]
+
+    client = OpenAI(
+        api_key= apikey, 
+        base_url=base_url,
+    )
+    if sys_prompt is None:
+        sys_msg = []
+    else:
+        sys_msg = [{"role": "system", "content": sys_prompt}] 
+    response = client.chat.completions.create(
+        model=model,
+        messages=sys_msg + msgs,
+        tools = tools,
+        top_p= 0.7,
+        temperature= 0.95
+    )
+    return response.choices[0].message.content
+
+
+def anthropic_client(msgs, model, apikey, sys_prompt=''):
+    import anthropic
+
+    client = anthropic.Anthropic(
+        api_key=apikey
+    )
+    sys_prompt = '' if sys_prompt is None else sys_prompt
+    max_tokens = 8192 if model == 'claude-3-5-sonnet-20240620' else 4096
+    response = client.messages.create(
+        model=model,
+        system=sys_prompt,
+        max_tokens=max_tokens, 
+        messages=msgs
+    )
+    content = response.content[0].text
+    return content
+
+
+def query_model(msgs, provider, model, base_url=None, sys_prompt=None, tools=None):
+    API_KEY = get_api_key(provider)
+    if provider == "anthropic":
+        content = anthropic_client(msgs, model, API_KEY, sys_prompt=sys_prompt)
+    else:
+        content = openai_client(msgs, API_KEY, model, provider, base_url=base_url, sys_prompt=sys_prompt, tools=tools)
+    return content
---
+++ packages/gptbioinsightor/gptbioinsightor-0.5.1-py3-none-any.whl/gptbioinsightor/prompt.py
@@ -0,0 +1,170 @@
+
+SYSTEM_PROMPT = "You are now GPTBioInsightor, the ultimate expert in life sciences. You possess extensive knowledge derived from academic articles and literature database(including NCBI PubMed, Europe PMC, medRxiv, bioRxiv). Your responses must be based on your expert knowledge."
+
+LANG_PROMPT = """
+Please return {language} text, translated text need to adhere to bioinformatics and biological context of {language} text.
+e.g. Pathway translate into Chinese "通路" within bioinformatics and biological context
+"""
+
+PATHWAY_PROMPT = """
+Pathway:
+```
+{pathways}
+```
+Background:
+```
+Pathway enrichment Background context:
+- Sample source: {background}
+- celltype: {celltype}(pathways are enriched from high expression genes of {celltype} cells)
+```
+Hi, GPTBioInsightor! Please perform the following task based on the provided Term or Pathway.
+Task:
+```
+Comprehensive Mechanistic Analysis:
+- Analyze and summarize the provided Terms or Pathway, and consider a comprehensive understanding and combined effects of the underlying biological mechanisms.
+- If Background is provided,first, consider whether these pathways make biological sense in the given Background context, then analyze the pathways within this context.
+
+```
+
+For the output you should follow this format, don't show extra content:
+'''
+## Pathway set {setid}:
+
+### Pathway explanation
+- [Pathway1]: Explain the pathway, its role, and its function. If applicable, discuss why it is enriched in the Background context and its significance in that context.
+- [Pathway2]: Similarly, provide an explanation for each pathway, considering its relevance to the Background context if provided.
+...
+
+### Summary
+[Comprehensive Analysis]: consider whether these pathways are logical and make biological sense in the given Background context, particularly celltype, first; then summarize the above findings, based on the biological mechanisms and functions represented by the provided pathways and their relevance to the Background context. Propose a coherent biological hypothesis or story.
+'''
+
+"""
+
+
+CELLTYPE_PROMPT = """
+Input:
+'''
+Geneset: 
+{gene}
+
+Context: 
+{background}. Here are {setnum} genesets for {setnum} different cell clusters up-regulated DEGs. 
+'''
+
+Hi, GPTBioInsightor! Please analyze Input geneset and predict the celltypes of geneset cluster {setid} based on the following INSTRUCTIONS.
+
+INSTRUCTIONS:
+0. Analyze each gene in Geneset, check cell-specific, context-specific and common gene markers
+1. prioritize single Gold Standard marker or gene marker combinations for celltype prediction
+2. give full consideration to the Input Context for celltype prediction, e.g. common celltypes in tissue, disease, etc.
+3. Integrate Context of Input to speculate on some novel insights 
+4. Focus on positive evidence, avoid using marker absence as primary reasoning.
+5. Exclude celltypes with clear negative markers in the geneset.
+6. Consider each cluster has different celltype prediction in most time, exclude celltypes represented by ohter cluster gene markers. 
+7. Consider one Optimal celltypes and two alternative celltypes.
+
+Output Format:, without any additional prompt or string:
+'''
+## cluster geneset {setid}
+
+### Gene List
+```
+[cluster {setid} gene list]
+```
+
+### Celltype Prediction
+#### Optimal Celltype: [OPTIMAL CELLTYPE NAME]
+**Key Markers**:
+- Cell-specific: [CELL-SPECIFIC MARKERS]
+- Context-specific: [CONTEXT-SPECIFIC MARKERS]
+
+**Evidence and Reasoning**
+- [PRIMARY EVIDENCE]
+- [SECONDARY EVIDENCE]
+- [ADDITIONAL EVIDENCE AS NEEDED]
+
+**Validation**: [OTHER Gold Standard MARKERS(NOT IN Geneset {setid}) TO VALIDATE THE OPTIMAL CELLTYPE]
+
+#### Alternative Considerations
+- Alternative celltype1
+    - [WHY Alternative? Key MARKERS, Evidence and Reasoning]
+    - [OTHER Gold Standard MARKERS(NOT IN Geneset {setid}) TO VALIDATE THE Alternative celltype1]
+
+- Alternative celltype2
+    - [WHY Alternative? Key MARKERS, Evidence and Reasoning]
+    - [OTHER Gold Standard MARKERS(NOT IN Geneset {setid}) TO VALIDATE THE Alternative celltype2]
+
+### Novel Insights
+- [NOTEWORTHY PATTERNS]
+- [CELL STATE]
+- [POTENTIAL NEW FINDINGS]
+'''
+"""
+
+
+SUBTYPE_PROMPT = """
+Hi, GPTBioInsightor! Please determine cell subtypes of {celltype} for each geneset.Your reasoning process must be based on INSTRUCTION.
+
+GENESET:
+'''
+{genesets}
+'''
+
+INSTRUCTION:
+1. determine cell subtype according to specific gene markers, please provide evidence and reason
+2. give full consideration to context of cell: BACKGROUND, determine the most logical subtype
+3. consider the context: BACKGROUND; speculate the cell state, such as stress, invasive, proliferative, developmental stages, or other transient or dynamically responsive properties
+
+
+BACKGROUND:
+{background}
+
+For the output you should follow this format:
+'''
+### [geneset id] : [ SUBTYPE NAME ] 
+** gene marker**: [ALL GENE MARKER SUPPORTED THE CELLTYPE]
+** subtype gene marker**: [SPECIFIC GENE MARKER FOR CELL SUBTYPE]
+**reason**: [REASON]
+**cell state**: [POTENTIAL CELL STATE]
+
+### [geneset id] : [ SUBTYPE NAME ] 
+** gene marker**: [ALL GENE MARKER SUPPORTED THE CELLTYPE]
+** subtype gene marker**: [SPECIFIC GENE MARKER FOR CELL SUBTYPE]
+**reason**: [REASON]
+**cell state**: [POTENTIAL CELL STATE]
+...
+
+'''
+"""
+
+
+
+CHECK_TYPE_PROMPT = """
+Hi, GPTBioInsightor! Please check celltype of corresponding to each geneset.Your reasoning process must be based on INSTRUCTION.
+
+GENESET:
+'''
+{genesets}
+'''
+
+INSTRUCTION:
+1. review and check if the celltype is reasonable based on the provided genes.
+2. give full consideration to context of cell: BACKGROUND
+3. provide new celltype in reason if the celltype is not reasonable 
+
+
+BACKGROUND:
+{background}
+
+For the output you should follow this format:
+'''
+### [geneset celltype]: [ YES or NO ]  // Is this type reasonable?
+**reason**: [REASON] // Why do you think it is or isn't reasonable?
+
+### [geneset celltype]: [ YES or NO ]  // Is this type reasonable?
+**reason**: [REASON] // Why do you think it is or isn't reasonable?
+...
+
+'''
+"""
