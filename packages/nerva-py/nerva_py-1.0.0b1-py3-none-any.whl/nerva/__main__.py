4549883286525cb88569816f00ab1607a7d7a9e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 05:06:05 +0000

    Add package nerva-py/1.0.0b1/nerva_py-1.0.0b1-py3-none-any.whl

---
+++ packages/nerva-py/nerva_py-1.0.0b1-py3-none-any.whl/nerva/__main__.py
@@ -0,0 +1,58 @@
+import sys
+import argparse
+import platform
+
+import aiohttp
+
+import nerva
+
+
+def show_version() -> None:
+    entries = list()
+
+    v = sys.version_info
+    entries.append(
+        f"- Python v{v.major}.{v.minor}.{v.micro}"
+        + (f"-{v.releaselevel}" if v.releaselevel != "final" else "")
+    )
+
+    v = nerva.version_info
+    entries.append(
+        f"- pyxnv v{v.major}.{v.minor}.{v.micro}"
+        + (f"{v.releaselevel[0]}{v.serial}" if v.releaselevel != "final" else "")
+    )
+
+    entries.append(f"- aiohttp v{aiohttp.__version__}")
+
+    uname = platform.uname()
+    entries.append(f"- System Info: {uname.system} {uname.release} {uname.version}")
+
+    print("\n".join(entries))
+
+
+def core(parser: argparse.ArgumentParser, args: argparse.Namespace) -> None:
+    if args.version:
+        show_version()
+    else:
+        parser.print_help()
+
+
+def parse_args() -> tuple[argparse.ArgumentParser, argparse.Namespace]:
+    parser = argparse.ArgumentParser(
+        prog="pyxnv", description="Tools for helping with the library"
+    )
+    parser.add_argument(
+        "-v", "--version", action="store_true", help="Shows the library version"
+    )
+    parser.set_defaults(func=core)
+
+    return parser, parser.parse_args()
+
+
+def main() -> None:
+    parser, args = parse_args()
+    args.func(parser, args)
+
+
+if __name__ == "__main__":
+    main()
