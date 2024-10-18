f781b10dc33a51de9fbe3e0996ed9fd926f32d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 05:05:23 +0000

    Add package fin-art/5.0/fin_art-5.0.tar.gz

---
+++ packages/fin-art/fin_art-5.0.tar.gz/fin_art-5.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fin_art",  
+    version="5.0", 
+    description="A module for generating stylistic images",
+    author="madvasik",  
+    author_email="ma2sic@yandex.ru",  
+    packages=find_packages(), 
+    python_requires=">=3.6",  
+    install_requires=[  
+        "torch",
+        "transformers",
+        "diffusers",
+        "matplotlib",
+        "peft",
+        "deep_translator"
+    ],
+)
