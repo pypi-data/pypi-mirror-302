0478eb45151db17d22817352a559e9becf5063f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 14:05:21 +0000

    Add package tsafe/1.2.0/tsafe-1.2.0.tar.gz

---
+++ packages/tsafe/tsafe-1.2.0.tar.gz/tsafe-1.2.0/tsafe/__init__.py
@@ -0,0 +1,75 @@
+from inspect import signature
+
+class StringList():
+    REQTYPE = str
+    def __init__(self):
+        return
+
+class IntList():
+    REQTYPE = int
+    def __init__(self):
+        return
+
+class FloatList():
+    REQTYPE = float
+    def __init__(self):
+        return
+
+class ComplexList():
+    REQTYPE = complex
+    def __init__(self):
+        return
+
+class BoolList():
+    REQTYPE = float
+    def __init__(self):
+        return
+
+def type_safe(func):
+    """
+    Wrapper function to force a function to be type safe
+    """
+    def force_safety(*args, **kwargs):
+        flag = True
+        classCheck = False
+        for argType in [args, kwargs.values()]:
+            for index in range(0, len(argType)):
+                arg = argType[index]
+
+                try:
+                    reqType = list(func.__annotations__.values())[index]
+                except IndexError:
+                    reqType = list(func.__annotations__.values())[index - 1]
+
+                # skip self if in class
+                if func.__qualname__ and 'self' in signature(func).parameters and not classCheck:
+                    classCheck = True
+                    continue
+
+                if type(arg) == list:
+                    listTypes = [StringList, IntList, FloatList, ComplexList, BoolList]
+                    for l in listTypes:
+                        
+                        if (reqType == l and all(type(x) == l.REQTYPE for x in arg)) or reqType == list:
+                            flag = True
+                            break
+                        else:
+                            flag = False
+
+                    if not flag:
+                        raise Exception(f"argument {arg} is not type of {reqType}")
+                        break
+
+                elif type(arg) == reqType or reqType == object:
+                    continue
+                else:
+                    flag = False
+                    raise Exception(f"argument {arg} is not type of {reqType}")
+                    break
+
+        if flag:
+            return func(*args, **kwargs)
+
+    return force_safety
+
+safe = type_safe
