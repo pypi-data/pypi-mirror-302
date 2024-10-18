093ad687b420d4546e8e6fde9d1986a55a854afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 06:05:41 +0000

    Add package royalflush/0.1.0/royalflush-0.1.0-py3-none-any.whl

---
+++ packages/royalflush/royalflush-0.1.0-py3-none-any.whl/royalflush/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.1.0"
+
+
+def main() -> None:
+    """
+    Entry point for the application script. It runs the system and requires an XMPP server at localhost with in-band
+    register enabled.
+    """
+    print("Royal Flush")
