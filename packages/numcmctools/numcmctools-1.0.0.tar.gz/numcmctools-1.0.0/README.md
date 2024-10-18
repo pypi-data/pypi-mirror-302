076d978bfbe308db0bd5de404127bed84555533a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 05:06:39 +0000

    Add package numcmctools/1.0.0/numcmctools-1.0.0.tar.gz

---
+++ packages/numcmctools/numcmctools-1.0.0.tar.gz/numcmctools-1.0.0/README.md
@@ -0,0 +1,218 @@
+# NuMCMCTools
+
+This package is intended as a lightweight tool to assist in analysis using 
+public releases of MCMC chains from neutrino oscillation analyses. The
+following experiments have released data compatible with this software:
+
+* Experiment 1
+* Experiment 2
+
+## Installation
+
+To install `numcmctools` with PyPI, run the following command:
+
+```bash
+pip install numcmctools
+```
+
+To install directly from source, first clone numcmctools with git:
+
+```bash
+git clone https://github.com/NuMCMCTools/NuMCMCTools.git
+```
+
+enter the new NuMCMCTool directory and simply run:
+
+```bash
+pip install .
+```
+
+The pacakge does not need to be installed to use it, you can simply `git clone`
+the package and add your own macros that include the `numcmcmtools` folder.
+
+## File Format
+
+The expected input format for this code is a ROOT file containing at
+least one TTree. The tree can have any name. Inside the tree, there
+will be at least six branches called:
+
+* DeltaCP, in radians, across any $2\pi$ interval
+* Theta13, in radians, [0, $\pi$/2]
+* Theta23,  in radians, [0, $\pi$/2]
+* Theta12,  in radians, [0, $\pi$/2]
+* Deltam2_32, in $\text{eV}^2$
+* Deltam2_21, in $\text{eV}^2$
+
+which correspond to the standard parameters of neutrino mixing; the
+first four are the PMNS angles, and the last two are the mass
+splittings. For more information on the parameterization see the [PDG
+review on neutrino mixing](https://pdg.lbl.gov/2024/web/viewer.html?file=../reviews/rpp2024-rev-neutrino-mixing.pdf).
+
+Each parameter has some prior set by the original analyzers. The
+format of this information is a TList containing a TNamed for each branch, 
+which specifies the name of the branch and its prior. 
+
+The priors are specified as either `Uniform` or `Gaussian(mean, sigma)`.
+There is then a further specification as to which variable the functional form
+applies to. For example, a specification of
+
+``` Theta23  Uniform:sin^2(Theta23)```
+
+indicates that the prior for Theta23 is uniform in $\sin^2\theta_{23}$.
+
+The file additionally contains a citation to the original analysis that
+produced the chain.
+
+There may be additional information contained in the file; please see
+the data release from the particular analysis for more detail.
+
+In this code, the class `mcmcsamples` is responsible for loading
+in the required information from the file. The constructor takes the
+filepath for the input file and the name of the oscillation parameters
+tree, and an optional argument for additional variables to be loaded.
+
+An example tree (`examples/testchaindata.root`) with the required
+features is provided for testing the functionality of this software.
+
+A very basic script for loading the example tree is
+`examples/load_mcmc.py` and can be run as follows:
+
+```bash
+ python -m examples.load_mcmc
+```
+
+## Posterior Density Functions and Contours
+
+After the tree is loaded, an instance of `plotstack` can be created,
+which is initialized with an `mcmcsamples` instance. Individual
+`plot`s can be added to the plot stack. One and two dimensional plots
+are currently supported.
+
+A plot is added to the plot stack with the `add_plot` function, which
+takes as arguments an array of the name(s) of the variables to be
+plotted as strings, any change in priors to be applied (see section below for details),
+the bins and axis ranges for the plots, as defined in
+[numpy.histogram](https://numpy.org/doc/stable/reference/generated/numpy.histogram.html)
+or
+[numpy.histogram2d](https://numpy.org/doc/stable/reference/generated/numpy.histogram2d.html),
+and finally whether to treat the two mass orderings separately
+(`True`) or together (`False`, default).
+
+Any number of plots can be added to the stack. When all plots are
+created, they can be filled by calling `fill_plots`, which has two
+optional arguments. The first causes only the first
+`n_steps` to be filled from the tree; this is useful in testing the
+plot stack for very large input trees; the second restricts the number
+of steps read from the tree to be `batchsize` at any given time, which
+is helpful for memory management. Above a batch size of approximately
+100000, there is no performance impact on the time it takes to read
+large trees and fill plots; this value is therefore set at the
+default. The number of plots to be filled does have an impact on the run
+time of the code.
+
+After the histograms are filled, they are normalized to create
+probability _density_ functions, such that individual bins are
+normalized to the width/area of the bin and the integral is normalized
+to 1.
+
+Credible intervals can be created by calling `make_intervals`
+on either the plot stack or individual plots within the stack. The
+function takes an array of credible interval levels.
+
+PDFs and intervals can be drawn with `draw_plot` and `draw_interval`
+respectively, and can take an `Figure` argument from matplotlib. When
+dividing a figure into subfigures, use the `subfigures` command, rather
+than `subplots`, as `subplots` is used internally.
+
+The plot stack can also be used to automatically draw all of the plots
+in the stack, choosing the optimal dimensions for the subfigure array.
+
+An example of the plotting features is in `examples/simpleplots.py` and
+can be run as follows:
+
+```bash
+ python -m examples.simpleplots
+```
+
+The output of this example should look as follows
+
+![simpleplots](https://github.com/user-attachments/assets/e79060c5-42f4-43b5-90b9-6e89aa0b7f0a)
+
+## Derived Variables
+
+Variables derived from the six required parameters—for example,
+$\sin^2\theta_{23}$-can be defined and used for plotting. Each new
+variable must be given a unique name and defined as a function which
+uses the required parameters as input variables, spelled exactly as
+they are defined above.
+
+The new variable is then attached to an instance of the `mcmcsamples`
+class using the `add_variable` function, which takes as arguments the
+name of the new variable and the function defining it.
+
+An example of this feature is in `examples/custom_variables.py` and
+can be run as follows:
+
+```bash
+ python -m examples.custom_variables
+```
+
+The output of this example should have two canvases which look as follows
+
+![custom_variable_1](https://github.com/user-attachments/assets/a6243f95-7cc4-4e21-bd27-27777a404b6e)
+![custom_variable_2](https://github.com/user-attachments/assets/f377d5fa-6154-41d7-b3aa-1a3822b2a95b)
+
+## Changing Priors
+
+Priors for parameters can be changed automatically when making plots.
+A list of new priors is passed when adding a new `plot` to the `plotstack`.
+This feature currently only works in 1D and with Uniform or Gaussian priors
+in a limited set of transformations of variables. The list of currently
+available transformations is:
+
+```
+x
+sin(x)
+sin^2(x)
+cos(x)
+cos^2(x)
+2x
+sin(2x)
+sin^2(2x)
+cos(2x)
+cos^2(2x)
+exp(-ix)
+```
+
+For example, an input chain that has a prior set to be uniform in DeltaCP can
+have the following passed to the input chain to make plots with a prior uniform
+in $\sin\delta_{CP}$:
+
+```
+["Uniform:sin(DeltaCP)"]
+```
+
+Examples of this in practice are in the `custom_variables` example.
+
+## Plotting
+
+Plots can be drawn directly from the `plotstack` (shown in the `custom_variables` 
+example), where the arrangement of pdfs or intervals is done algorithmically. 
+For more control, the figure and its subdivisions can be arranged by the user
+and each plot drawn individually. An example of the latter is shown in both the
+`simpleplots` example, and in the `plot_triangle` example, which creates a standard
+triangle (corner) plot with the mass ordering separated. This example can be run as 
+follows:
+
+```bash
+  python -m examples.plot_triangle -f examples/testchaindata.root -c mcmc    
+```
+
+or 
+
+```bash
+  plot_triangle -f examples/testchaindata.root -c mcmc    
+```
+if installed throgh pip, which produces the output below.
+
+![triangle_noio](https://github.com/user-attachments/assets/3f5940dc-cf99-4244-b82e-608c4112cd84)
---
+++ packages/numcmctools/numcmctools-1.0.0.tar.gz/numcmctools-1.0.0/numcmctools/plot.py
@@ -0,0 +1,282 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import logging
+from .jacobiangraph import JacobianGraph
+from typing import Union, List
+
+logger = logging.getLogger(__name__)
+
+class Plot:
+    def __init__(self, variables, jacobians, bins, axrange=None, mo_option=False):
+        """
+        Initialise a Plot instance.
+
+        :variables: Array of strings indicating the variables to be plotted.
+                    Only 1D and 2D are currently supported. Custom variables can be declared
+                    when through the mcmcsamples class
+        :priors: A dictionary of jacobian transform functions (can be None for
+                 each parameter if no transformations are needed)
+        :bins: number of bins or bin edges, formatted for either 1 or 2D as in the numpy
+               documentation for histogram (https://numpy.org/doc/stable/reference/generated/numpy.histogram.html)
+               or histogram2D (https://numpy.org/doc/stable/reference/generated/numpy.histogram2d.html)
+        :axrange: See as for bins
+        :mo_option: When creating intervals, calculate intervals jointly over the mass hierarchies (True) or 
+                    marginalized over hierarchies (False). Default is False; to be implemented.
+        """
+        self.variables = variables
+        self.jacobian_funcs = jacobians
+        self.bins = bins
+        self.axrange = axrange
+        self.mo_option = mo_option
+        self.nvar = len(self.variables)
+        self.edges = []
+        self.finalized = False
+
+        if(self.nvar==1):
+            self.hist, edges = np.histogram([], self.bins, self.axrange, weights=[])
+            self.edges.append(edges)
+            if self.mo_option:
+                self.hist_no = np.zeros(np.shape(self.hist))
+                self.hist_io = np.zeros(np.shape(self.hist))
+        elif(self.nvar==2):
+            self.hist, edgesx, edgesy = np.histogram2d([], [],self.bins, self.axrange, weights=[])
+            self.edges.append(edgesx)
+            self.edges.append(edgesy)
+            if self.mo_option:
+                self.hist_no = np.zeros(np.shape(self.hist))
+                self.hist_io = np.zeros(np.shape(self.hist))
+        else:
+            logger.error("too many or too few variables!")
+            return
+                
+    def fill_plot(self, data, weights=None):
+        """
+        Fill the plot. If the plot has been finalized, no more filling is allowed.
+
+        :data: An array of the data
+        :weights: Data weights
+        """
+
+        if weights==None:
+            weights = np.ones(np.shape(data[self.variables[0]]))
+
+        # Apply the jacobian transformation functions
+        for var in self.jacobian_funcs:
+            if not self.jacobian_funcs[var]:
+                continue
+            # Apply the weight
+            weights *= self.jacobian_funcs[var](data[var])
+        
+        if not self.finalized:
+            if(self.nvar==1):
+                if self.mo_option:
+                    hist, edges = np.histogram(data[self.variables[0]], self.bins, self.axrange, weights = weights*np.greater_equal(data["Deltam2_32"],0))
+                    self.hist_no += hist
+                    hist, edges = np.histogram(data[self.variables[0]], self.bins, self.axrange, weights = weights*np.less_equal(data["Deltam2_32"],0))
+                    self.hist_io += hist
+                else:
+                    hist, edges = np.histogram(data[self.variables[0]], self.bins, self.axrange, weights = weights)
+                    self.hist += hist
+            elif(self.nvar==2):
+                if self.mo_option:
+                    hist, edgesx, edgesy = np.histogram2d(data[self.variables[0]], data[self.variables[1]], self.bins, self.axrange,
+                                                              weights = weights*np.greater_equal(data["Deltam2_32"],0))
+                    self.hist_no += hist
+                    hist, edgesx, edgesy = np.histogram2d(data[self.variables[0]], data[self.variables[1]], self.bins, self.axrange,
+                                                              weights = weights*np.less_equal(data["Deltam2_32"],0))
+                    self.hist_io += hist
+                else:
+                    hist, edgesx, edgesy = np.histogram2d(data[self.variables[0]], data[self.variables[1]], self.bins, self.axrange, weights = weights)
+                    self.hist += hist
+        else:
+            logger.warn("histogram was finalized already! No filling allowed!")
+
+    def finalize_histogram(self):
+        """
+        Finalize the plot. The plot is finalized to make a probability density function.
+        """
+        if(not self.finalized):
+            if(self.nvar==1):
+                self.areas = np.diff(self.edges[0])
+            if(self.nvar==2):
+                self.areas = np.outer(np.diff(self.edges[0]),np.diff(self.edges[1]))
+
+            if self.mo_option:
+                self.hist = np.concatenate((self.hist_no,self.hist_io))
+                self.areas = np.concatenate((self.areas,self.areas))
+            total = np.sum(self.hist)
+            self.hist = self.hist/self.areas/total
+            if self.mo_option:
+                if(self.nvar==1):
+                    sh = np.shape(self.hist_no)
+                    self.hist_no = self.hist[:sh[0]]
+                    self.hist_io = self.hist[sh[0]:]
+                if(self.nvar==2):
+                    sh = np.shape(self.hist_no)
+                    self.hist_no = self.hist[:sh[0],:]
+                    self.hist_io = self.hist[sh[0]:,:]
+            self.finalized = True
+            
+    def draw_plot(self, saxis: Union[plt.Axes, List[plt.Axes]]):
+        """
+        Draw the plot. 
+        :saxis: matplotlib axis, or a list of axes to draw the plot on
+
+        returns 1 or 2 matplotlib subplots for further manipulation
+        """
+
+        # Make sure the input either plt.Axes or array of plt.Axes
+        is_axis_array = False
+        if isinstance(saxis, plt.Axes):
+            if self.nvar > 1 and self.mo_option:
+                raise ValueError(f"Cannot plot 2 2D heatmaps on top of each other, need to provide 2 plt.Axes in a list")
+        elif isinstance(saxis, (list, np.ndarray)) and all(isinstance(ax, plt.Axes) for ax in saxis):
+            if len(saxis) > 2:
+                raise ValueError(f"Maximum length of the axis list is 2, one per mass ordering of Deltam2_32")
+            is_axis_array = True
+        else:
+            raise ValueError("Wrong instance of axes, should be either plt.Axes or List[plt.axes]!")
+
+        if(self.nvar==1):
+            if self.mo_option:
+                if is_axis_array:
+                    saxis[0].stairs(self.hist_no,self.edges[0])
+                    saxis[0].set_xlabel(self.variables[0]+" NO")
+                    saxis[1].stairs(self.hist_io,self.edges[0])
+                    saxis[1].set_xlabel(self.variables[0]+" IO")
+                else:
+                    saxis.stairs(self.hist_no,self.edges[0])
+                    saxis.stairs(self.hist_io,self.edges[0])
+                    saxis.set_xlabel(self.variables[0])
+
+            else:
+                saxis.stairs(self.hist,self.edges[0])
+                saxis.set_xlabel(self.variables[0])
+
+        if(self.nvar==2):
+            if self.mo_option:
+                if is_axis_array:
+                    cm = saxis[0].pcolormesh(self.edges[0], self.edges[1], self.hist_no.T)
+                    saxis[0].set_xlabel(self.variables[0]+" NO")
+                    saxis[0].set_ylabel(self.variables[1])
+                    cm = saxis[1].pcolormesh(self.edges[0], self.edges[1], self.hist_io.T)
+                    saxis[1].set_xlabel(self.variables[0]+" IO")
+                else:
+                    logger.error("2D color orverlap looks terrible, not drawn!")
+                    #cm = saxis.pcolormesh(self.edges[0], self.edges[1], self.hist_no.T)
+                    #saxis[0].set_xlabel(self.variables[0]+" NO")
+                    #saxis[0].set_ylabel(self.variables[1])
+                    #cm = saxis[1].pcolormesh(self.edges[0], self.edges[1], self.hist_io.T)
+                    #saxis[1].set_xlabel(self.variables[0]+" IO")
+            else:
+                cm = saxis.pcolormesh(self.edges[0], self.edges[1], self.hist.T)
+                saxis.set_xlabel(self.variables[0])
+                saxis.set_ylabel(self.variables[1])
+
+    def draw_interval(self, saxis: Union[plt.Axes, List[plt.Axes]]):
+        """
+        Draw the intervals.
+        :saxes: matplotlib axes, or list of axes, to draw the plot on
+
+        returns 1 or 2 matplotlib subplots for further manipulation
+        """
+
+        is_axis_array = False
+        if isinstance(saxis, plt.Axes):
+            is_axis_array = False
+        elif isinstance(saxis, (list, np.ndarray)) and all(isinstance(ax, plt.Axes) for ax in saxis):
+            if len(saxis) > 2:
+                raise ValueError(f"Maximum length of the axis list is 2, one per mass ordering of Deltam2_32")
+            is_axis_array = True
+        else:
+            raise ValueError("Wrong instance of axes, should be either plt.Axes or List[plt.axes]!")
+        
+        #need to put in a check if the intervals have been calculated
+        if(self.nvar==1):
+            if self.mo_option:
+
+                if is_axis_array:
+                    saxis[0].stairs(self.hist_no,self.edges[0], color='black')
+                    for lev in self.prob_levels:
+                        saxis[0].stairs(self.hist_no*np.greater_equal(self.hist_no,lev),self.edges[0], fill=True, color='grey', alpha=0.3)
+                    saxis[0].set_xlabel(self.variables[0]+" NO")
+
+                    saxis[1].stairs(self.hist_io,self.edges[0], color='black')
+                    for lev in self.prob_levels:
+                        saxis[1].stairs(self.hist_io*np.greater_equal(self.hist_io,lev),self.edges[0], fill=True, color='grey', alpha=0.3)
+                    saxis[1].set_xlabel(self.variables[0]+" IO")
+                else:
+                    saxis.stairs(self.hist_no,self.edges[0], color='cornflowerblue')
+                    for lev in self.prob_levels:
+                        saxis.stairs(self.hist_no*np.greater_equal(self.hist_no,lev),self.edges[0], fill=True, color='cornflowerblue', alpha=0.3)
+                    saxis.set_xlabel(self.variables[0])
+
+                    saxis.stairs(self.hist_io,self.edges[0], color='lightcoral')
+                    for lev in self.prob_levels:
+                        saxis.stairs(self.hist_io*np.greater_equal(self.hist_io,lev),self.edges[0], fill=True, color='lightcoral', alpha=0.3)
+            else:
+                saxis.stairs(self.hist,self.edges[0], color='black')
+                for lev in self.prob_levels:
+                    saxis.stairs(self.hist*np.greater_equal(self.hist,lev),self.edges[0], fill=True, color='grey', alpha=0.3)
+                saxis.set_xlabel(self.variables[0])
+        
+        if(self.nvar==2):
+
+            linestyles_base = ['solid','dashed','dashdot','dotted']
+            linestyles=['']*len(self.prob_levels)
+            for i in range(len(self.prob_levels)):
+                linestyles[i] = linestyles_base[i%len(linestyles_base)]
+            linestyles = list(reversed(linestyles))
+
+            if self.mo_option:
+                if is_axis_array:
+                    saxis[0].contour(0.5*(self.edges[0][:-1]+self.edges[0][1:]), 0.5*(self.edges[1][:-1]+self.edges[1][1:]),self.hist_no.T, np.sort(self.prob_levels), linestyles=linestyles, colors='lightgrey')
+                    saxis[0].set_xlabel(self.variables[0]+" NO")
+                    saxis[0].set_ylabel(self.variables[1])
+                    saxis[1].contour(0.5*(self.edges[0][:-1]+self.edges[0][1:]), 0.5*(self.edges[1][:-1]+self.edges[1][1:]),self.hist_io.T, np.sort(self.prob_levels), linestyles=linestyles, colors='lightgrey')
+                    saxis[1].set_xlabel(self.variables[0]+" IO")
+                else:
+                    saxis.contour(0.5*(self.edges[0][:-1]+self.edges[0][1:]), 0.5*(self.edges[1][:-1]+self.edges[1][1:]),self.hist_no.T, np.sort(self.prob_levels), linestyles=linestyles, colors='cornflowerblue')
+                    saxis.set_xlabel(self.variables[0]+" NO")
+                    saxis.set_ylabel(self.variables[1])
+                    saxis.contour(0.5*(self.edges[0][:-1]+self.edges[0][1:]), 0.5*(self.edges[1][:-1]+self.edges[1][1:]),self.hist_io.T, np.sort(self.prob_levels), linestyles=linestyles, colors='lightcoral')
+                    saxis.set_xlabel(self.variables[0]+" IO")
+            else:
+                saxis.contour(0.5*(self.edges[0][:-1]+self.edges[0][1:]), 0.5*(self.edges[1][:-1]+self.edges[1][1:]),self.hist.T, np.sort(self.prob_levels), linestyles=linestyles, colors='lightgrey')
+                saxis.set_xlabel(self.variables[0])
+                saxis.set_ylabel(self.variables[1])
+        
+    def make_intervals(self,levels):
+        """
+        Create intervals. 
+        :levels: an array of numbers between 0 and 1 representing the credible interval
+                 levels required. 
+        """
+        if(not self.finalized):
+            self.finalize_histogram()
+        
+        self.levels = -1*np.sort(-1*np.array(levels))
+        nlev = len(self.levels)-1
+
+        index_sort = np.argsort(-1*self.hist*self.areas, axis=None) #what a stupid hack--order high to low
+        index_sort_unrav = np.unravel_index(index_sort,self.hist.shape)
+
+        self.prob_levels = np.zeros(self.levels.shape)
+
+        total = np.sum(self.hist*self.areas)
+        index = 0
+        process_sum = 0
+        if(self.nvar==1):
+            while nlev >= 0:
+                process_sum+=self.hist[index_sort_unrav[0][index]]*self.areas[index_sort_unrav[0][index]]
+                index+=1
+                if(process_sum/total > self.levels[nlev]):
+                    self.prob_levels[nlev]=self.hist[index_sort_unrav[0][index-1]]
+                    nlev-=1
+        if(self.nvar==2):       
+            while nlev >= 0:
+                process_sum+=self.hist[index_sort_unrav[0][index],index_sort_unrav[1][index]]*self.areas[index_sort_unrav[0][index],index_sort_unrav[1][index]]
+                index+=1
+                if(process_sum/total > self.levels[nlev]):
+                    self.prob_levels[nlev]=self.hist[index_sort_unrav[0][index-1],index_sort_unrav[1][index-1]]
+                    nlev-=1            
