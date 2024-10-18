dcd06c26aa026f53407bef063cb9138ae19340ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 05:05:30 +0000

    Add package nestipy-inertia/0.1.0/nestipy_inertia-0.1.0-py3-none-any.whl

---
+++ packages/nestipy-inertia/nestipy_inertia-0.1.0-py3-none-any.whl/nestipy_inertia/config.py
@@ -0,0 +1,31 @@
+from typing import Literal, Type, AsyncGenerator, Union, Callable
+from json import JSONEncoder
+from .utils import InertiaJsonEncoder
+from dataclasses import dataclass
+
+try:
+    import httpx
+except ImportError:
+    httpx = None
+
+
+@dataclass
+class InertiaConfig:
+    """
+    Configuration class for Inertia
+    """
+    environment: Literal["development", "production"] = "development"
+    version: str = "1.0"
+    json_encoder: Type[JSONEncoder] = InertiaJsonEncoder
+    dev_url: str = "http://localhost:5173"
+    ssr_url: str = "http://localhost:13714"
+    ssr_enabled: bool = False
+    manifest_json_path: str = ""
+    root_directory: str = "src"
+    root_template_filename: str = "index.html"
+    entrypoint_filename: str = "main.js"
+    use_flash_messages: bool = False
+    use_flash_errors: bool = False
+    flash_message_key: str = "messages"
+    flash_error_key: str = "errors"
+    assets_prefix: str = ""
