f52d6f8a66af3055358068686b1720096a59f4f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 05:06:54 +0000

    Add package django-mapengine/2.2.0/django_mapengine-2.2.0.tar.gz

---
+++ packages/django-mapengine/django_mapengine-2.2.0.tar.gz/django_mapengine-2.2.0/django_mapengine/static/django_mapengine/js/init.js
@@ -0,0 +1,40 @@
+
+const map_store = initMapStore();
+const map = initMap();
+
+
+function initMap() {
+    const setup = JSON.parse(document.getElementById("mapengine_setup").textContent);
+    const map = new maplibregl.Map(setup);
+
+    if (map_store.cold.debugMode) {
+        map.showTileBoundaries = true;
+    }
+
+    // Disable zoom on double click
+    map.doubleClickZoom.disable();
+
+    // Zoom to feature on click
+    if (map_store.cold.fly_to_clicked_feature) {
+        map.on("click", function (element) {
+            flyToElement(element);
+        });
+    }
+
+    const nav = new maplibregl.NavigationControl();
+    map.addControl(nav, "bottom-left");
+    map.addControl(new maplibregl.ScaleControl({position: 'bottom-right'}));
+    return map;
+}
+
+
+function initMapStore() {
+    const storeColdInit = JSON.parse(document.getElementById("mapengine_store_cold_init").textContent);
+    storeColdInit.state = {};
+    storeColdInit.storedChoroplethPaintProperties = {"default": {}};
+    for (const choroplethName in storeColdInit.choropleths) {
+        storeColdInit.storedChoroplethPaintProperties[choroplethName] = {};
+    }
+    storeColdInit.currentChoropleth = null;
+    return new Store(storeColdInit);
+}
