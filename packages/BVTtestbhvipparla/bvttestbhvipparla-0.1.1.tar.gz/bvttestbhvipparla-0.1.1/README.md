f2e50efb950e672844c1981f96da98236245511e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 03:10:06 +0000

    Add package BVTtestbhvipparla/0.1.1/bvttestbhvipparla-0.1.1.tar.gz

---
+++ packages/BVTtestbhvipparla/bvttestbhvipparla-0.1.1.tar.gz/bvttestbhvipparla-0.1.1/README.md
@@ -0,0 +1,1 @@
+# BVT Framework test
---
+++ packages/BVTtestbhvipparla/bvttestbhvipparla-0.1.1.tar.gz/bvttestbhvipparla-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BVTtestbhvipparla',
+    version='0.1.1',
+    description='A package to validate unique keys in Spark DataFrames',
+    long_description='A basic package to run BVT checks',
+    long_description_content_type='text/markdown',
+    author='Bhanu Krishna Prasad',
+    author_email='bhanuk@maqsoftware.com',
+    packages=find_packages(),
+    install_requires=[
+        'pyspark',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.1',
+)
