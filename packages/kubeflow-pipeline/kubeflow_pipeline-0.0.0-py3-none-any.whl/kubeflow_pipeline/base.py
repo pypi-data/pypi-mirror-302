497ac8bf9ee8476b5f3e2a5b3ee34378ce723a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 15:05:32 +0000

    Add package kubeflow-pipeline/0.0.0/kubeflow_pipeline-0.0.0-py3-none-any.whl

---
+++ packages/kubeflow-pipeline/kubeflow_pipeline-0.0.0-py3-none-any.whl/kubeflow_pipeline/base.py
@@ -0,0 +1,17 @@
+"""
+kubeflow_pipeline base module.
+
+This is the principal module of the kubeflow_pipeline project.
+here you put your main classes and objects.
+
+Be creative! do whatever you want!
+
+If you want to replace this with a Flask application run:
+
+    $ make init
+
+and then choose `flask` as template.
+"""
+
+# example constant variable
+NAME = "kubeflow_pipeline"
