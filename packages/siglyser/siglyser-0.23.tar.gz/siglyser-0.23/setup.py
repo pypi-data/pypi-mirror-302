1828b034b4462e1891ec091fdc7c1dbec22a2f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 03:10:31 +0000

    Add package siglyser/0.23/siglyser-0.23.tar.gz

---
+++ packages/siglyser/siglyser-0.23.tar.gz/siglyser-0.23/setup.py
@@ -0,0 +1,24 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+
+setup(
+    name='siglyser',
+    version='0.23',
+    description='Signal processing library',
+    author='Pushkar Sheth',
+    author_email='siglyserdev@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here.
+
+        
+    ],
+
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
