ac591c6ef09ee8ef5e10e6809086fa0224057408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 05:06:22 +0000

    Add package owlapy/1.3.1/owlapy-1.3.1-py3-none-any.whl

---
+++ packages/owlapy/owlapy-1.3.1-py3-none-any.whl/owlapy/abstracts/__init__.py
@@ -0,0 +1,5 @@
+from .abstract_owl_ontology_manager import AbstractOWLOntologyManager, AbstractOWLOntologyChange, AbstractOWLOntology
+from .abstract_owl_reasoner import AbstractOWLReasoner, AbstractOWLReasonerEx
+
+__all__ = ['AbstractOWLOntologyManager', 'AbstractOWLOntologyChange', 'AbstractOWLOntology', 'AbstractOWLReasoner',
+           'AbstractOWLReasonerEx']
