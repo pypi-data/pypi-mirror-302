c7fbab7e7081a1561feb0cc629fcbde82f636590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 04:06:44 +0000

    Add package digitalhub-runtime-dbt/0.8.0b4/digitalhub_runtime_dbt-0.8.0b4-py3-none-any.whl

---
+++ packages/digitalhub-runtime-dbt/digitalhub_runtime_dbt-0.8.0b4-py3-none-any.whl/digitalhub_runtime_dbt/entities/run/dbt_run/status.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from digitalhub.entities.run._base.status import RunStatus
+
+
+class RunStatusDbtRun(RunStatus):
+    """
+    RunStatusDbtRun status.
+    """
