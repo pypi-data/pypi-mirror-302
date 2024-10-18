69889c0683ac12613943f71781cd2c731d0c29cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 17:05:06 +0000

    Add package aioeufyclean/0.3.1/aioeufyclean-0.3.1.tar.gz

---
+++ packages/aioeufyclean/aioeufyclean-0.3.1.tar.gz/aioeufyclean-0.3.1/aioeufyclean/__init__.py
@@ -0,0 +1,27 @@
+from .device import VacuumDevice, VacuumState
+from .eufy_cloud import VacuumCloudDiscovery, get_cloud_vacuums
+from .exceptions import (
+    AuthenticationFailed,
+    ConnectionException,
+    ConnectionFailed,
+    ConnectionTimeoutException,
+    EufyCleanException,
+    InvalidKey,
+    InvalidMessage,
+    MessageDecodeFailed,
+)
+
+__all__ = [
+    "EufyCleanException",
+    "ConnectionFailed",
+    "AuthenticationFailed",
+    "ConnectionException",
+    "ConnectionTimeoutException",
+    "InvalidKey",
+    "InvalidMessage",
+    "MessageDecodeFailed",
+    "get_cloud_vacuums",
+    "VacuumCloudDiscovery",
+    "VacuumDevice",
+    "VacuumState",
+]
