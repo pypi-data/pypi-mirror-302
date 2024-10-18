398f7a21c577725211bd3eebf0ea93252f18f14f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 03:10:41 +0000

    Add package indoxJudge/0.0.7/indoxjudge-0.0.7.tar.gz

---
+++ packages/indoxJudge/indoxjudge-0.0.7.tar.gz/indoxjudge-0.0.7/indoxJudge/models/__init__.py
@@ -0,0 +1,6 @@
+from .openai import OpenAi
+from .googleai import GoogleAi
+from .indox_api import IndoxApi
+from .huggingface import HuggingFaceModel
+from .mistral import Mistral
+# from .ollama import Ollama
