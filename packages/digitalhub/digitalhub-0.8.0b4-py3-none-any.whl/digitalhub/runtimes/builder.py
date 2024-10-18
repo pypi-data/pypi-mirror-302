7528958083518125eb0a199a2068bdc19e03621a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 04:06:43 +0000

    Add package digitalhub/0.8.0b4/digitalhub-0.8.0b4-py3-none-any.whl

---
+++ packages/digitalhub/digitalhub-0.8.0b4-py3-none-any.whl/digitalhub/runtimes/builder.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+import typing
+
+from digitalhub.utils.exceptions import BuilderError
+
+if typing.TYPE_CHECKING:
+    from digitalhub.runtimes._base import Runtime
+    from digitalhub.runtimes.kind_registry import KindRegistry
+
+
+class RuntimeBuilder:
+    """
+    Builder class for building runtimes.
+    """
+
+    # Class variables
+    RUNTIME_CLASS: Runtime = None
+    KIND_REGISTRY: KindRegistry = None
+
+    def __init__(self) -> None:
+        if self.RUNTIME_CLASS is None:
+            raise BuilderError("RUNTIME_CLASS must be set")
+        if self.KIND_REGISTRY is None:
+            raise BuilderError("KIND_REGISTRY must be set")
+
+    def build(self, *args, **kwargs) -> Runtime:
+        """
+        Build runtime object.
+
+        Returns
+        -------
+        Runtime
+            Runtime object.
+        """
+        return self.RUNTIME_CLASS(self.KIND_REGISTRY, *args, **kwargs)
