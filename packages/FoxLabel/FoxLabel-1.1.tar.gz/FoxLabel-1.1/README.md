fed6d73db950abfe6711ce912521d4bc75afb8f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 16:05:10 +0000

    Add package FoxLabel/1.1/FoxLabel-1.1.tar.gz

---
+++ packages/FoxLabel/FoxLabel-1.1.tar.gz/FoxLabel-1.1/README.md
@@ -0,0 +1,33 @@
+# Giới thiệu
+FoxLabel là một công cụ đánh nhãn hình ảnh đồ họa chuyên cho training Yolo.
+
+# Cài đặt bằng các cách sau:
+
+```bash
+pip install FoxLabel
+
+``` 
+
+# Cách dùng:
+
+Chạy bằng 1 trong 2 cách sau:
+
+```bash
+ntanh_foxlabel
+FoxLabel
+```
+
+### 1. Chạy chương trình
+Command: `ntanh_foxlabel`, `FoxLabel`
+
+### 1. Help
+Command: `ntanh`, 
+
+Lệnh này sẽ hiển thị help có các trường hợp sử dụng cơ bản
+
+
+# Version changed
+- Version 1.0:
+    - Release date: 2024-10-18
+    - Khởi tạo dự án, đẩy lên môi trường cài đặt
+
---
+++ packages/FoxLabel/FoxLabel-1.1.tar.gz/FoxLabel-1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="FoxLabel",  # tên của gói thư viện
+    version="1.1",
+    description="Thư viện hữu ích của Tuấn Anh.",
+    url="https://pypi.org/project/FoxLabel/",
+    author="Tuấn Anh - Foxconn",
+    author_email="nt.anh.fai@gmail.com",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["lxml", "opencv-python", "ruamel.yaml", "pyyaml", "ntanh"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": [
+            "FoxLabel=FoxLabel:start_program",
+            "ntanh_foxlabel=FoxLabel:start_program",
+        ],
+    },
+)
