818cc1d86a8bff374fdb628b10c69dc924e9640d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 05:06:05 +0000

    Add package simon-library/0.2.0/simon_library-0.2.0-py3-none-any.whl

---
+++ packages/simon-library/simon_library-0.2.0-py3-none-any.whl/simon_library/__init__.py
@@ -0,0 +1,7 @@
+from .clean_data import clean_data
+from .core import core
+from .create_features import create_features
+from .get_data import get_data
+from .get_roc_auc import get_roc_auc
+from .split_data import split_data
+from .train_model import train_model
