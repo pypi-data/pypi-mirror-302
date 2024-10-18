6a8cb9cd02bb544a39bd303c0d51d52a11e8a54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 15:05:29 +0000

    Add package gkfutils/1.0.8/gkfutils-1.0.8-py3-none-any.whl

---
+++ packages/gkfutils/gkfutils-1.0.8-py3-none-any.whl/gkfutils/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+from . import cv
+from . import nlp
+from . import radar
+from . import utils
+
+__appname__ = "gkfutils"
+__version__ = "1.0.8"
---
+++ packages/gkfutils/gkfutils-1.0.8-py3-none-any.whl/gkfutils/main_test_gkfutils.py
@@ -0,0 +1,8 @@
+import gkfutils
+
+
+if __name__ == '__main__':
+    gkfutils.utils.gen_data_txt_list(data_path="", one_dir_flag=True)
+
+
+
