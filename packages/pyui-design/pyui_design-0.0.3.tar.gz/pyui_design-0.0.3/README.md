968e2c3d982c9a46d98f3dc0a038eb6daa8a1f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 03:10:31 +0000

    Add package pyui-design/0.0.3/pyui_design-0.0.3.tar.gz

---
+++ packages/pyui-design/pyui_design-0.0.3.tar.gz/pyui_design-0.0.3/README.md
@@ -0,0 +1,21 @@
+# pyui-design - Python User Interface Design Solution
+
+This is pyui-design package.
+
+This is not UI framework.
+
+This is UI solution for python application.
+
+- CLI
+- WEB
+- DESKTOP
+- MOBILE
+- AI
+- 
+
+#### 0.0.2 2024/10/17
+* Add some defination from other project.
+
+#### 0.0.1 2024/10/17
+* Init project.
+* Create __pyui-design__ PyPI package name and domain __pyui.design__ .
---
+++ packages/pyui-design/pyui_design-0.0.3.tar.gz/pyui_design-0.0.3/src/pyui_design/lib/constants/service.py
@@ -0,0 +1,18 @@
+# key
+SERVICECALL_STATUS = "service_status"
+SERVICECALL_RESULT = "service_result"
+SERVICECALL_MESSAGE = "service_message"
+SERVICECALL_ORGMESSAGE = "service_orgmessage"
+
+
+
+# service_callstatus
+SERVICESTATUS_OK = "ok"
+SERVICESTATUS_ERROR = "error"
+SERVICESTATUS_UNKNOW = "unknow"
+
+# service_callmessage
+SERVICEMESSAGE_SUCCESS = "success"
+SERVICEMESSAGE_FAIL = "fail"
+SERVICEMESSAGE_UNKNOW = "unknow"
+
