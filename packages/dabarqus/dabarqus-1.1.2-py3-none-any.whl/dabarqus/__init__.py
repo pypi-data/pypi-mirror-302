b3db3416a99aa720607afa3076f4dad36ad2737e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 04:07:38 +0000

    Add package dabarqus/1.1.2/dabarqus-1.1.2-py3-none-any.whl

---
+++ packages/dabarqus/dabarqus-1.1.2-py3-none-any.whl/dabarqus/__init__.py
@@ -0,0 +1,7 @@
+# dabarqus/__init__.py
+
+from .client import barq
+
+__all__ = ['barq']
+
+__version__ = '0.1.0'
---
+++ packages/dabarqus/dabarqus-1.1.2-py3-none-any.whl/dabarqus_sdk/__init__.py
@@ -0,0 +1,7 @@
+# dabarqus/__init__.py
+
+from .client import DabarqusSDK
+
+__all__ = ['DabarqusSDK']
+
+__version__ = '0.1.0'
