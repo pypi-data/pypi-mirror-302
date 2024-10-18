74278d9353a2737aea2fd1864318c3511e5c1a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 03:10:05 +0000

    Add package flowjax/16.0.0/flowjax-16.0.0-py3-none-any.whl

---
+++ packages/flowjax/flowjax-16.0.0-py3-none-any.whl/flowjax/bijections/exp.py
@@ -0,0 +1,31 @@
+"""Exponential bijection."""
+
+from typing import ClassVar
+
+import jax.numpy as jnp
+
+from flowjax.bijections.bijection import AbstractBijection
+
+
+class Exp(AbstractBijection):
+    """Elementwise exponential transform (forward) and log transform (inverse).
+
+    Args:
+        shape: Shape of the bijection. Defaults to ().
+    """
+
+    shape: tuple[int, ...] = ()
+    cond_shape: ClassVar[None] = None
+
+    def transform(self, x, condition=None):
+        return jnp.exp(x)
+
+    def transform_and_log_det(self, x, condition=None):
+        return jnp.exp(x), x.sum()
+
+    def inverse(self, y, condition=None):
+        return jnp.log(y)
+
+    def inverse_and_log_det(self, y, condition=None):
+        x = jnp.log(y)
+        return x, -x.sum()
