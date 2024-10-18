51f92beab900c56b691716bd836b195f7a8fc16b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 04:05:31 +0000

    Add package grpc-oauth-vet1ments/0.1.19/grpc_oauth_vet1ments-0.1.19.tar.gz

---
+++ packages/grpc-oauth-vet1ments/grpc_oauth_vet1ments-0.1.19.tar.gz/grpc_oauth_vet1ments-0.1.19/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+setup(
+name="grpc-oauth-vet1ments",
+version="0.1.19",
+description="assistant proto",
+long_description="s",
+long_description_content_type="text/markdown",
+author="vet1ments",
+author_email="vet1ments@naver.com",
+url="https://github.com/vet1ments/grpc-oauth-python.git",
+packages=find_packages(exclude=[]),
+python_requires=">=3.11",
+)
