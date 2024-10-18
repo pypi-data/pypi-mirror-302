53207f11ff3efd84b3711e3c30980e709cc254c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 03:10:21 +0000

    Add package mixture-of-attention/0.0.25/mixture_of_attention-0.0.25.tar.gz

---
+++ packages/mixture-of-attention/mixture_of_attention-0.0.25.tar.gz/mixture_of_attention-0.0.25/mixture_of_attention/__init__.py
@@ -0,0 +1,5 @@
+from mixture_of_attention.mixture_of_attention import (
+    MixtureOfAttention,
+    MixtureOfAutoregressiveAttention,
+    Attention
+)
---
+++ packages/mixture-of-attention/mixture_of_attention-0.0.25.tar.gz/mixture_of_attention-0.0.25/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'mixture-of-attention',
+  packages = find_packages(exclude=[]),
+  version = '0.0.25',
+  license='MIT',
+  description = 'Mixture of Attention',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/mixture-of-attention',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'transformers',
+    'attention mechanism',
+    'mixture-of-experts',
+    'routed attention'
+  ],
+  install_requires=[
+    'colt5-attention>=0.10.14',
+    'einops>=0.6.1',
+    'local-attention>=1.8.6',
+    'torch>=1.6',
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
