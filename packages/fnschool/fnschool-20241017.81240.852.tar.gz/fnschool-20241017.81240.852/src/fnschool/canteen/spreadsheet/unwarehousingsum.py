e99479f3b82892e5dc75b79007009ae8dae20818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 03:10:47 +0000

    Add package fnschool/20241017.81240.852/fnschool-20241017.81240.852.tar.gz

---
+++ packages/fnschool/fnschool-20241017.81240.852.tar.gz/fnschool-20241017.81240.852/src/fnschool/canteen/spreadsheet/unwarehousingsum.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+from fnschool import *
+from fnschool.canteen.spreadsheet.base import *
+
+
+class UnwarehousingSum(SpreadsheetBase):
+    def __init__(self, bill):
+        super().__init__(bill)
+        pass
