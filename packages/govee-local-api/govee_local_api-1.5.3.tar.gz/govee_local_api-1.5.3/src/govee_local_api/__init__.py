70e393e8d3bf57018addc531f29f9430e0ee21e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 16:06:10 +0000

    Add package govee-local-api/1.5.3/govee_local_api-1.5.3.tar.gz

---
+++ packages/govee-local-api/govee_local_api-1.5.3.tar.gz/govee_local_api-1.5.3/src/govee_local_api/__init__.py
@@ -0,0 +1,8 @@
+from .controller import GoveeController
+from .device import GoveeDevice
+from .light_capabilities import GoveeLightCapability
+
+
+__all__ = ["GoveeController", "GoveeDevice", "GoveeLightCapability"]
+
+__version__ = "1.5.3"
