9eb2d2ff14ceb35df11d51858395b60f3e8c5b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-18 04:07:39 +0000

    Add package digitalhub-runtime-dbt/0.8.0b5/digitalhub_runtime_dbt-0.8.0b5-py3-none-any.whl

---
+++ packages/digitalhub-runtime-dbt/digitalhub_runtime_dbt-0.8.0b5-py3-none-any.whl/digitalhub_runtime_dbt/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from digitalhub_runtime_dbt.entities.function.dbt.builder import FunctionDbtBuilder
+from digitalhub_runtime_dbt.entities.run.dbt_run.builder import RunDbtRunBuilder
+from digitalhub_runtime_dbt.entities.task.dbt_transform.builder import TaskDbtTransformBuilder
+
+entity_builders = (
+    (FunctionDbtBuilder.ENTITY_KIND, FunctionDbtBuilder),
+    (RunDbtRunBuilder.ENTITY_KIND, RunDbtRunBuilder),
+    (TaskDbtTransformBuilder.ENTITY_KIND, TaskDbtTransformBuilder),
+)
+
+try:
+    from digitalhub_runtime_dbt.runtimes.builder import RuntimeDbtBuilder
+    from digitalhub_runtime_dbt.runtimes.kind_registry import kind_registry
+
+    runtime_builders = ((kind, RuntimeDbtBuilder) for kind in kind_registry.get_all_kinds())
+except ImportError:
+    runtime_builders = tuple()
