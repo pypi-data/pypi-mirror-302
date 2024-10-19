911d273b7fef16e7bae33ad02ecd7da4a600c919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 07:05:44 +0000

    Add package pydantable/0.2.1/pydantable-0.2.1.tar.gz

---
+++ packages/pydantable/pydantable-0.2.1.tar.gz/pydantable-0.2.1/pydantable/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.2.1'
+
+from pydantable.base import BaseTableModel
+from pydantable.generators.dicts.readers.csv import CSVDictReader
+from pydantable.generators.dicts.validators.dicts import DictValidator
+from pydantable.generators.dicts.transformers.dicts import DictTransformer
+from pydantable.generators.dicts.writers.csv import CSVDictWriter
