efc1062e26e31b21ec558c267a9b0a5de2af2181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 07:05:50 +0000

    Add package psat-server-web/0.4.1/psat_server_web-0.4.1.tar.gz

---
+++ packages/psat-server-web/psat_server_web-0.4.1.tar.gz/psat_server_web-0.4.1/psat_server_web/atlas/atlasapi/urls.py
@@ -0,0 +1,22 @@
+from django.urls import include, path
+from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+from . import views
+
+urlpatterns = [
+    path('api/cone/',                  views.ConeView.as_view()),
+    path('api/objects/',               views.ObjectsView.as_view()),
+    path('api/objectlist/',            views.ObjectListView.as_view()),
+    path('api/vrascores/',             views.VRAScoresView.as_view()),
+    path('api/vrascoreslist/',         views.VRAScoresListView.as_view()),
+    path('api/vratodo/',               views.VRATodoView.as_view()),
+    path('api/vratodolist/',           views.VRATodoListView.as_view()),
+    path('api/objectgroups/',          views.TcsObjectGroupsView.as_view()),
+    path('api/objectgroupslist/',      views.TcsObjectGroupsListView.as_view()),
+    path('api/objectgroupsdelete/',    views.TcsObjectGroupsDeleteView.as_view()),
+    path('api/vrarank/',               views.VRARankView.as_view()),
+    path('api/vraranklist/',           views.VRARankListView.as_view()),
+    path('api/externalxmlist/',        views.ExternalCrossmatchesListView.as_view()),
+    path('api/objectdetectionlist/',   views.ObjectDetectionListView.as_view()),
+    path('api/auth-token/',            obtain_auth_token, name='auth_token'),
+]
