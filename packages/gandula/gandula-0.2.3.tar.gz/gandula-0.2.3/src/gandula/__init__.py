7d9973077bac261b21c3804a21723a2555e0a35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 06:07:28 +0000

    Add package gandula/0.2.3/gandula-0.2.3.tar.gz

---
+++ packages/gandula/gandula-0.2.3.tar.gz/gandula-0.2.3/src/gandula/__init__.py
@@ -0,0 +1,34 @@
+from matplotlib.axes import Axes
+
+from . import logging as logging
+from .api.auth import api_version as api_version
+from .api.auth import validate_api_key as validate_api_key
+from .api.competition import get_available_competitions as get_available_competitions
+from .api.competition import get_competition as get_competition
+from .api.competition import get_competitions as get_competitions
+from .api.match import get_enhanced as get_enhanced
+from .api.match import get_frames as get_frames
+from .api.match import get_match as get_match
+from .api.match import get_match_event as get_match_event
+from .api.match import get_match_events as get_match_events
+from .providers.pff.local import loader as loader
+from .providers.pff.schema.event import PFF_Game as PFF_Game
+from .providers.pff.schema.event import PFF_PossessionEvent
+from .providers.pff.schema.tracking import Frame_Event as Frame_Event
+from .providers.pff.schema.tracking import PFF_Frame
+
+# from .schemas.base import Team as Team
+# from .schema.adapters.pff_stuff import FramePFF as FramePFF
+from .utils.formatter import Format, ResponseType, format_response
+from .export.export import export
+from .visualization.video import video as video
+from .visualization.view import view as view
+
+
+def _repr_html_() -> str:
+    # TODO: Switch to jinja template
+    return ''
+
+
+# If there is a connection, try to fetch api_version.
+# Match it with provder.response_type.PFF_EVENT_VERSION
