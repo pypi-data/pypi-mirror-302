cb47e7e5a0d389cb54a57a4d27d30de55fe76205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 06:06:11 +0000

    Add package peruscp/0.0.2/peruscp-0.0.2.tar.gz

---
+++ packages/peruscp/peruscp-0.0.2.tar.gz/peruscp-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='peruscp',
+    version='0.0.2',
+    license='MIT',
+    description='Es un paquete que realiza una solicitud a eldni.com extrayendo todos los datos a consultar, util para bots, APIS, Paginas WEB, son datos basicos de Reniec.',
+    author='bapinzon',
+    packages=find_packages(),
+    install_requires=['requests','BeautifulSoup'],
+    author_email="bryanpinzon469@gmail.com",
+    url='https://github.com/bapinzon/peruscp'
+)
