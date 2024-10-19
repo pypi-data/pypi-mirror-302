784a43ca78513ceced9ba3c6b33322e59288ebdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 03:12:11 +0000

    Add package luxos/0.2.4b52/luxos-0.2.4b52-py3-none-any.whl

---
+++ packages/luxos/luxos-0.2.4b52-py3-none-any.whl/luxos/version.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+import sys
+import types
+from pathlib import Path
+
+__version__ = "0.2.4b52"
+__hash__ = "d99821753cba1e46f3d9550d9e75e7393b5cf98d"
+
+
+def get_version_info(
+    modules: list[types.ModuleType] | None = None, short: bool = False
+) -> dict[str, str]:
+    result = {
+        "py": sys.version.partition(" ")[0],
+        "luxos": ", ".join(
+            str(c) if str(c) else "N/A"
+            for c in [__version__, __hash__[:7] if short else __hash__]
+        ),
+    }
+
+    module = modules[-1] if modules else None
+    if module:
+        if path := getattr(module, "__file__", None):
+            name = Path(path).name
+        else:
+            name = getattr(module, "__name__", str(module))
+        if name not in {
+            "luxos.py",
+            "luxos_run.py",
+        }:
+            result[name] = getattr(module, "__version__", "N/A")
+
+    return result
+
+
+def get_version(modules: list[types.ModuleType] | None = None) -> str:
+    return ", ".join(
+        f"{k}[{v}]" for k, v in get_version_info(modules, short=True).items()
+    )
