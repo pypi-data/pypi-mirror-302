c8ec1fcd80a667438da22ae581b65abacb823fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 07:05:34 +0000

    Add package bylexa/1.0.1/bylexa-1.0.1.tar.gz

---
+++ packages/bylexa/bylexa-1.0.1.tar.gz/bylexa-1.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bylexa",
+    version="1.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "click",        # For CLI functionality
+        "websockets",   # For WebSocket communication
+        "requests",     # For making HTTP API calls
+        "typing-extensions",  # Optional for typing hints in some Python versions
+    ],
+    entry_points={
+        'console_scripts': [
+            'bylexa=bylexa.cli:main',
+        ],
+    },
+    author="exploring-solver",
+    description="A Python package for controlling your PC using Bylexa voice commands.",
+    url="https://github.com/exploring-solver/bylexa",  # Update this with your actual GitHub repo URL
+    license="MIT",
+)
