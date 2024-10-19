276a157766fdc7eb5984412e0622fee9064de999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 03:10:25 +0000

    Add package drf-kit/1.45.0/drf_kit-1.45.0-py3-none-any.whl

---
+++ packages/drf-kit/drf_kit-1.45.0-py3-none-any.whl/drf_kit/views/__init__.py
@@ -0,0 +1,58 @@
+from drf_kit.views.nested_viewsets import (
+    CachedNestedModelViewSet,
+    CachedReadOnlyNestedModelViewSet,
+    CachedSearchableNestedModelViewSet,
+    CachedSearchableReadOnlyNestedModelViewSet,
+    NestedModelViewSet,
+    ReadOnlyNestedModelViewSet,
+    WriteOnlyNestedModelViewSet,
+)
+from drf_kit.views.single_nested_viewsets import (
+    CachedReadOnlySingleNestedModelViewSet,
+    CachedSingleNestedModelViewSet,
+    ReadOnlySingleNestedModelViewSet,
+    SingleNestedModelViewSet,
+)
+from drf_kit.views.stats_views import StatsViewMixin
+from drf_kit.views.viewsets import (
+    BulkMixin,
+    CachedModelViewSet,
+    CachedNonDestructiveModelViewSet,
+    CachedReadOnlyModelViewSet,
+    CachedSearchableMixin,
+    CachedSearchableModelViewSet,
+    CachedSearchableNonDestructiveModelViewSet,
+    CachedSearchableReadOnlyModelViewSet,
+    ModelViewSet,
+    NonDestructiveModelViewSet,
+    ReadOnlyModelViewSet,
+    UpsertMixin,
+    WriteOnlyModelViewSet,
+)
+
+__all__ = (
+    "NestedModelViewSet",
+    "CachedNestedModelViewSet",
+    "ReadOnlyNestedModelViewSet",
+    "CachedReadOnlyNestedModelViewSet",
+    "NonDestructiveModelViewSet",
+    "CachedNonDestructiveModelViewSet",
+    "WriteOnlyNestedModelViewSet",
+    "SingleNestedModelViewSet",
+    "CachedSingleNestedModelViewSet",
+    "ReadOnlySingleNestedModelViewSet",
+    "CachedReadOnlySingleNestedModelViewSet",
+    "CachedSearchableMixin",
+    "CachedSearchableModelViewSet",
+    "CachedSearchableNonDestructiveModelViewSet",
+    "CachedSearchableReadOnlyModelViewSet",
+    "ModelViewSet",
+    "CachedModelViewSet",
+    "ReadOnlyModelViewSet",
+    "CachedReadOnlyModelViewSet",
+    "WriteOnlyModelViewSet",
+    "UpsertMixin",
+    "StatsViewMixin",
+    "CachedSearchableNestedModelViewSet",
+    "CachedSearchableReadOnlyNestedModelViewSet",
+)
