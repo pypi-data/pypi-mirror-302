47c95bb747ddc7017fae354bcc37170045239a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 03:11:16 +0000

    Add package ojax/3.0.0/ojax-3.0.0.tar.gz

---
+++ packages/ojax/ojax-3.0.0.tar.gz/ojax-3.0.0/ojax/__init__.py
@@ -0,0 +1,20 @@
+from . import otree, pureclass
+from .otree import (
+    OTreeField,
+    Aux,
+    Child,
+    Ignore,
+    fields,
+    aux,
+    child,
+    ignore,
+    OTree,
+)
+from .pureclass import (
+    new,
+    PureClass,
+    NoAnnotationWarning,
+)
+
+
+__version__ = "3.0.0"
