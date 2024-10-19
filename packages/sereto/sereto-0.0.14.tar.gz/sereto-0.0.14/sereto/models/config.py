45cc2242ebe10a4f0d11c93cb97b485614ba8d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 03:11:27 +0000

    Add package sereto/0.0.14/sereto-0.0.14.tar.gz

---
+++ packages/sereto/sereto-0.0.14.tar.gz/sereto-0.0.14/sereto/models/config.py
@@ -0,0 +1,144 @@
+from copy import deepcopy
+from pathlib import Path
+
+from pydantic import Field, model_validator, validate_call
+
+from sereto.exceptions import SeretoPathError, SeretoValueError
+from sereto.models.base import SeretoBaseModel
+from sereto.models.date import Date
+from sereto.models.person import Person
+from sereto.models.target import Target
+from sereto.models.version import ReportVersion, SeretoVersion
+
+
+class BaseConfig(SeretoBaseModel):
+    """Model with core attributes for a specific version of the report configuration.
+
+    Attributes:
+        id: The ID of the report.
+        name: The name of the report.
+        report_version: The version of the report.
+        targets: List of targets.
+        dates: List of dates.
+        people: List of people.
+    """
+
+    id: str
+    name: str
+    report_version: ReportVersion
+    targets: list[Target] = []
+    dates: list[Date] = []
+    people: list[Person] = []
+
+    @model_validator(mode="after")
+    def unique_names(self) -> "BaseConfig":
+        unames = [target.uname for target in self.targets]
+        if len(unames) != len(set(unames)):
+            raise ValueError("duplicate target uname")
+        return self
+
+
+class Config(BaseConfig):
+    """Model representing the full report configuration.
+
+    Attributes:
+        sereto_version: Version of SeReTo which produced the config.
+        updates: List of updates.
+    """
+
+    sereto_version: SeretoVersion
+    updates: list[BaseConfig] = Field(default=[])
+
+    @model_validator(mode="after")
+    def config_validator(self) -> "Config":
+        # if self.id is None or self.name is None:
+        #     raise ValueError("'id' and 'name' variables cannot be None")
+
+        previous: BaseConfig = self
+
+        for update in self.updates:
+            # report_version is incremented in subsequent update sections
+            if previous.report_version >= update.report_version:
+                raise ValueError(f"report_version {update.report_version!r} after {previous.report_version!r}")
+
+            # copy values from previous versions, which are not explicitly stated
+            for field in ["id", "name", "targets", "people"]:
+                if not getattr(update, field):
+                    setattr(update, field, deepcopy(getattr(previous, field)))
+
+            previous = update
+
+        return self
+
+    @classmethod
+    def from_file(cls, filepath: Path) -> "Config":
+        """Load the configuration from a file.
+
+        Args:
+            filepath: The path to the configuration file.
+
+        Returns:
+            The configuration object.
+
+        Raises:
+            SeretoPathError: If the file is not found or permission is denied.
+            SeretoValueError: If the configuration is invalid.
+        """
+        try:
+            return cls.model_validate_json(filepath.read_bytes())
+        except FileNotFoundError:
+            raise SeretoPathError(f"file not found at '{filepath}'") from None
+        except PermissionError:
+            raise SeretoPathError(f"permission denied for '{filepath}'") from None
+        except ValueError as e:
+            raise SeretoValueError("invalid config") from e
+
+    @validate_call
+    def versions(self) -> list[ReportVersion]:
+        """Get a sorted list of report versions in ascending order.
+
+        Returns:
+            A list of report versions.
+        """
+        return [self.report_version] + [update.report_version for update in self.updates]
+
+    @validate_call
+    def last_version(self) -> ReportVersion:
+        """Get the last report version present in the configuration.
+
+        Returns:
+            The last report version.
+        """
+        return self.versions()[-1]
+
+    def at_version(self, version: str | ReportVersion | None) -> BaseConfig:
+        """Return the configuration at a specific version.
+
+        Args:
+            version: A version of the report configuration to return. If None is provided, return the whole config with
+                all the updates sections.
+
+        Returns:
+            Configuration for the report at the specified version.
+
+        Raises:
+            SeretoValueError: If the specified version is unknown.
+        """
+        if version is None:
+            return self
+        if isinstance(version, str):
+            version = ReportVersion.from_str(version)
+
+        # For v1.0, we need to convert Config to BaseConfig (excluding extra fields)
+        if self.report_version == version:  # v1.0
+            cfg = BaseConfig.model_validate(self.model_dump(exclude={"sereto_version", "updates"}))
+            # copy values of the excluded fields
+            for t1, t2 in zip(self.targets, cfg.targets, strict=True):
+                t2.path = t1.path
+            return cfg
+
+        # Otherwise, we need to find the matching update section
+        if len(res := [cfg for cfg in self.updates if cfg.report_version == version]) != 1:
+            raise SeretoValueError(f"version '{version}' not found")
+
+        return res[0]
---
+++ packages/sereto/sereto-0.0.14.tar.gz/sereto-0.0.14/sereto/retest.py
@@ -0,0 +1,64 @@
+import shutil
+from copy import deepcopy
+
+from pydantic import validate_call
+
+from sereto.config import write_config
+from sereto.models.finding import FindingsConfig
+from sereto.models.report import Report
+from sereto.models.settings import Settings
+from sereto.utils import YAML
+
+
+@validate_call
+def add_retest(report: Report, settings: Settings) -> None:
+    last_cfg = report.config.at_version(version=report.config.last_version())
+    report_path = Report.get_path_from_cwd(dir_subtree=settings.reports_path)
+
+    # Copy last version's config to the updates section
+    retest_cfg = deepcopy(last_cfg)
+    retest_cfg.report_version = last_cfg.report_version.next_major_version()
+    report.config.updates.append(retest_cfg)
+    write_config(config=report.config, settings=settings)
+
+    old_suffix = last_cfg.report_version.path_suffix
+    new_suffix = retest_cfg.report_version.path_suffix
+
+    # Copy files from previous version
+    copy_report_files = ["report", "sow"]
+    for file in copy_report_files:
+        shutil.copy(src=report_path / f"{file}{old_suffix}.tex.j2", dst=report_path / f"{file}{new_suffix}.tex.j2")
+
+    for target in last_cfg.targets:
+        assert target.path is not None
+
+        copy_target_files = ["approach", "scope", "target"]
+
+        for file in copy_target_files:
+            shutil.copy(src=target.path / f"{file}{old_suffix}.tex.j2", dst=target.path / f"{file}{new_suffix}.tex.j2")
+
+        findings_path = target.path / "findings.yaml"
+        findings = YAML.load(findings_path)
+        fc = FindingsConfig.from_yaml_file(filepath=target.path / "findings.yaml")
+
+        # Create new version of each included finding
+        for finding in fc.included_findings():
+            finding_dir = target.path / "findings" / finding.path_name
+            shutil.copy(
+                src=finding_dir / f"{finding.path_name}{old_suffix}.{finding.format.value}.j2",
+                dst=finding_dir / f"{finding.path_name}{new_suffix}.{finding.format.value}.j2",
+            )
+
+            # Update findings.yaml
+            # select the specific finding
+            yaml_finding = [f for f in findings["findings"] if f["path_name"] == finding.path_name]
+            assert len(yaml_finding) == 1
+            yaml_finding = yaml_finding[0]
+
+            # set risk for the new version same as the last one
+            if str(retest_cfg.report_version) not in yaml_finding["risks"]:  # type: ignore[call-overload]
+                yaml_finding["risks"][str(retest_cfg.report_version)] = finding.risks[last_cfg.report_version].value  # type: ignore[call-overload]
+
+        # Write updated findings.yaml
+        with findings_path.open(mode="w", encoding="utf-8") as f:
+            YAML.dump(findings, f)
