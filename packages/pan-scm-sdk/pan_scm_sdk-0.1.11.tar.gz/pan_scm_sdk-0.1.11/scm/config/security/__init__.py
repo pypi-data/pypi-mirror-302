94fd916022842c98794782474b1d05e373f92442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 04:05:46 +0000

    Add package pan-scm-sdk/0.1.11/pan_scm_sdk-0.1.11.tar.gz

---
+++ packages/pan-scm-sdk/pan_scm_sdk-0.1.11.tar.gz/pan_scm_sdk-0.1.11/scm/config/security/__init__.py
@@ -0,0 +1,5 @@
+# scm/config/security/__init__.py
+
+from .anti_spyware_profile import AntiSpywareProfile
+from .vulnerability_protection_profile import VulnerabilityProtectionProfile
+from .wildfire_antivirus_profile import WildfireAntivirusProfile
---
+++ packages/pan-scm-sdk/pan_scm_sdk-0.1.11.tar.gz/pan_scm_sdk-0.1.11/scm/models/security/__init__.py
@@ -0,0 +1,14 @@
+# scm/models/security/__init__.py
+
+from .anti_spyware_profiles import (
+    AntiSpywareProfileRequestModel,
+    AntiSpywareProfileResponseModel,
+)
+from .vulnerability_protection_profiles import (
+    VulnerabilityProtectionProfileRequestModel,
+    VulnerabilityProtectionProfileResponseModel,
+)
+from .wildfire_antivirus_profiles import (
+    WildfireAntivirusProfileResponseModel,
+    WildfireAntivirusProfileRequestModel,
+)
