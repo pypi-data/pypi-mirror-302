ad0f4aee9eed18d30c47d5df8442518c8111e972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 04:05:42 +0000

    Add package jupyterlab-training/0.8.0/jupyterlab_training-0.8.0-py3-none-any.whl

---
+++ packages/jupyterlab-training/jupyterlab_training-0.8.0-py3-none-any.whl/jupyterlab_training-0.8.0.data/data/share/jupyter/labextensions/jupyterlab-training/static/_78a8.ace2f02fab4c0769a94c.js
@@ -0,0 +1,14 @@
+(self["webpackChunkjupyterlab_training"] = self["webpackChunkjupyterlab_training"] || []).push([["_78a8"],{
+
+/***/ "?78a8":
+/*!************************!*\
+  !*** buffer (ignored) ***!
+  \************************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ })
+
+}]);
+//# sourceMappingURL=_78a8.ace2f02fab4c0769a94c.js.map
