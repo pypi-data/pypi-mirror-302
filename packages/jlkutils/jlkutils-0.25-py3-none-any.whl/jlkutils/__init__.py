b9bf78f090a42c91131b1a186e2b94292a0852a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 03:11:14 +0000

    Add package jlkutils/0.25/jlkutils-0.25-py3-none-any.whl

---
+++ packages/jlkutils/jlkutils-0.25-py3-none-any.whl/jlkutils/__init__.py
@@ -0,0 +1,5 @@
+from .file import writenewline, choose_file, getcoms, writetofile, get_imports
+from .crypt import decrypt, encrypt
+from .cam import photo
+from .chatgpt import chatgpt, dalle
+from .web import upload
