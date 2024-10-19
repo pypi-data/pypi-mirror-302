5a6bb3b164329b0b58975a153a32e99c8d61e4c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 07:05:28 +0000

    Add package cli-text-tool/0.2/cli-text-tool-0.2.tar.gz

---
+++ packages/cli-text-tool/cli-text-tool-0.2.tar.gz/cli-text-tool-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cli-text-tool',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[],
+    description='Инструмент командной строки для поиска и замены текста в файлах с поддержкой регулярных выражений.',
+    author='Klorri',
+    author_email='trosticplk@gmail.com',
+    url='https://pypi.org/project/cli-text-tool/',
+    )
