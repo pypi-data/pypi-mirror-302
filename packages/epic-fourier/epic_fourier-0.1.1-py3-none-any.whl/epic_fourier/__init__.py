d92c6f7424a2cb031129d322d8222961895dc45d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 04:05:34 +0000

    Add package epic-fourier/0.1.1/epic_fourier-0.1.1-py3-none-any.whl

---
+++ packages/epic-fourier/epic_fourier-0.1.1-py3-none-any.whl/epic_fourier/__init__.py
@@ -0,0 +1,21 @@
+"""
+epic_fourier - A library for computing Fourier coefficients.
+
+This package provides tools to compute Fourier coefficients for periodic functions.
+"""
+__version__ = '0.1.1'
+
+from .epic_fourier import series
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,  # Adjust to DEBUG for more detailed logs
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("fourier_series.log"),  # Log to a file
+        logging.StreamHandler()
+    ]
+)   
+
+logger = logging.getLogger(__name__)
+logger.info('epic_fourier package initialized, have fun :)')
