93f6b1ca940ab18387fd6ee319d95884d859f6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 04:05:50 +0000

    Add package PawSum/0.1/pawsum-0.1.tar.gz

---
+++ packages/PawSum/pawsum-0.1.tar.gz/pawsum-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="PawSum",  # The name of your package
+    version="0.1",  # The initial release version
+    description="A module to perform basic mathematical operation e.g factorial and squareroot.",
+    author="Tipjack",
+    author_email="tipjack985@gmail.com",
+    packages=find_packages(),  # Automatically finds all packages
+    # List dependencies here
+)
