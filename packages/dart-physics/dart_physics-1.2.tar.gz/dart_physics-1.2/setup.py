f3f8f1ca4cc87f48090fea7d590a93d7e0a3cdbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 04:05:16 +0000

    Add package dart-physics/1.2/dart_physics-1.2.tar.gz

---
+++ packages/dart-physics/dart_physics-1.2.tar.gz/dart_physics-1.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dart_physics',
+    version='1.2',
+    packages=find_packages(),
+    install_requires=[
+        'mujoco',
+        'gdown',
+        'adam-robotics[jax]==0.3.0',
+        'dm_control',
+        'loop_rate_limiters',
+        'avp_stream',
+        'robot_descriptions',
+        'obj2mjcf',
+        'flask>=3.0.3',
+        'psutil', 
+        'mediapy', 
+        'pyzmq', 
+        'grpcio',
+        'grpcio-tools',
+        'numpy',
+        'imageio>=2.36.0',
+        'opencv-python',
+        'qpsolvers[quadprog] >= 4.3.1',
+        'typing_extensions',
+        'dexhub-api>=0.3',
+        'importlib-metadata',  # Added to satisfy Dash requirement
+        'nest-asyncio',        # Added to satisfy Dash and orbax-checkpoint requirements
+        'plotly>=5.0.0',       # Added to satisfy Dash requirement
+        'pandas>=1.0',         # Added to satisfy objaverse and open3d requirements
+        'scikit-learn>=0.21',  # Added to satisfy open3d requirement
+        'msgpack',             # Added to satisfy orbax-checkpoint requirement
+        'sympy',               # Added to satisfy PyTorch requirement
+        'PyOpenGL==3.1.0',     # Locked to avoid conflict with pyrender
+    ],
+    author='Younghyo Park',
+    author_email='younghyo@mit.edu',
+    python_requires='>=3.6',
+)
