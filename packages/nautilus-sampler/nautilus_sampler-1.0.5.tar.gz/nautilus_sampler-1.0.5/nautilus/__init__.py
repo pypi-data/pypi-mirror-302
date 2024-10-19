2d1e0065873855089df7830406d34c3aaaedf48e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 04:05:18 +0000

    Add package nautilus-sampler/1.0.5/nautilus_sampler-1.0.5.tar.gz

---
+++ packages/nautilus-sampler/nautilus_sampler-1.0.5.tar.gz/nautilus_sampler-1.0.5/nautilus/__init__.py
@@ -0,0 +1,8 @@
+"""Neural Network-Boosted Importance Sampling for Bayesian Statistics"""
+
+from .sampler import Sampler
+from .prior import Prior
+
+__author__ = 'Johannes U. Lange'
+__email__ = 'julange.astro@pm.me'
+__version__ = '1.0.5'
