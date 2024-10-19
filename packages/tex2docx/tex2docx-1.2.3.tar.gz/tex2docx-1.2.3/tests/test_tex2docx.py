32b21d748d35a2617a11c7e0ad6738525637bc8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 03:11:34 +0000

    Add package tex2docx/1.2.3/tex2docx-1.2.3.tar.gz

---
+++ packages/tex2docx/tex2docx-1.2.3.tar.gz/tex2docx-1.2.3/tests/test_tex2docx.py
@@ -0,0 +1,38 @@
+import unittest
+
+from tex2docx import LatexToWordConverter
+
+
+class TestLatexToWordConverter(unittest.TestCase):
+    def setUp(self):
+        # config for tests/en
+        self.en_config = {
+            "input_texfile": "./en/main.tex",
+            "output_docxfile": "./en/main.docx",
+            "reference_docfile": "../my_temp.docx",
+            "cslfile": "../ieee.csl",
+            "bibfile": "./ref.bib",
+            "debug": True,
+        }
+        # config for tests/zh
+        self.zh_config = {
+            "input_texfile": "./zh/main.tex",
+            "output_docxfile": "./zh/main.docx",
+            "bibfile": "./ref.bib",
+            "fix_table": True,
+            "debug": False,
+        }
+
+    def test_convert_en(self):
+        # test convert en
+        converter = LatexToWordConverter(**self.en_config)
+        converter.convert()
+
+    def test_convert_zh(self):
+        # test convert zh
+        converter = LatexToWordConverter(**self.zh_config)
+        converter.convert()
+
+
+if __name__ == "__main__":
+    unittest.main()
