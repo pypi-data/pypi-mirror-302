68dd3ec80e19f5084b6da1c66480cb5e5b7e0a46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 04:05:34 +0000

    Add package idp-engine/0.11.2/idp_engine-0.11.2.tar.gz

---
+++ packages/idp-engine/idp_engine-0.11.2.tar.gz/idp_engine-0.11.2/idp_engine/__init__.py
@@ -0,0 +1,5 @@
+from .Parse import IDP
+
+from .Run import model_check, model_expand, model_propagate, execute, decision_table
+from .Theory import Propagation, Theory
+from .Assignments import Status, Assignment, Assignments
