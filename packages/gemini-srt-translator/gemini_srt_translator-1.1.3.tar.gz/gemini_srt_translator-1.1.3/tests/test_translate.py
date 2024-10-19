a7f6d9242df4c11920863a179cade12ccdbf6cbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 06:07:30 +0000

    Add package gemini-srt-translator/1.1.3/gemini_srt_translator-1.1.3.tar.gz

---
+++ packages/gemini-srt-translator/gemini_srt_translator-1.1.3.tar.gz/gemini_srt_translator-1.1.3/tests/test_translate.py
@@ -0,0 +1,7 @@
+import gemini_srt_translator as gst
+
+gst.gemini_api_key = "AIzaSyBYizgJLf2Li5x3NrRoBCeT9YfosYxJ_o0"
+gst.target_language = "Brazilian Portuguese"
+gst.input_file = "Colin.from.Accounts.S01E02.Benedict.Cumbercrapp.1080p.FXTL.WEB-DL.AAC2.0.H.264-BTN.srt"
+
+gst.translate()
