46785aab16c541a5e8ce275c49810c1e03df4eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 03:11:37 +0000

    Add package funcnodes-microscopy/0.0.1/funcnodes_microscopy-0.0.1-py3-none-any.whl

---
+++ packages/funcnodes-microscopy/funcnodes_microscopy-0.0.1-py3-none-any.whl/funcnodes_microscopy/__init__.py
@@ -0,0 +1,12 @@
+import funcnodes as fn
+from .SEM import SEM_NODE_SHELF
+from .segmentation import SEGMENTATION_NODE_SHELF
+
+__version__ = "0.0.1"
+
+NODE_SHELF = fn.Shelf(
+    nodes=[],
+    name="Microscopy",
+    description="The nodes of Funcnodes Microscopy package",
+    subshelves=[SEM_NODE_SHELF, SEGMENTATION_NODE_SHELF],
+)
