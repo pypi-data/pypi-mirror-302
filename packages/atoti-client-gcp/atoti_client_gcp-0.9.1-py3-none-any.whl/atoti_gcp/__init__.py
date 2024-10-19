8fd2b9f85b0f1e87d01cb39a00264f7dfbc1a92f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 05:06:10 +0000

    Add package atoti-client-gcp/0.9.1/atoti_client_gcp-0.9.1-py3-none-any.whl

---
+++ packages/atoti-client-gcp/atoti_client_gcp-0.9.1-py3-none-any.whl/atoti_gcp/__init__.py
@@ -0,0 +1,14 @@
+"""Code to load CSV and parquet files from Google Cloud Storage into Atoti tables.
+
+Authentication is handled by the underlying GCS SDK for Java library.
+Automatic credentials retrieval is explained in `their documentation <https://cloud.google.com/docs/authentication/production#automatically>`__.
+
+Example:
+    >>> table = session.read_csv("gs://atoti-anonymous/city.csv", keys=["city"])
+    >>> table.head().sort_index()
+            value
+    city
+    London  200.0
+    Paris   100.0
+
+"""
