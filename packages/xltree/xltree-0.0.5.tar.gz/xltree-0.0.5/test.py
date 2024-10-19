fca1f40a674d16df170779fad15d9565a01da55b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 05:06:21 +0000

    Add package xltree/0.0.5/xltree-0.0.5.tar.gz

---
+++ packages/xltree/xltree-0.0.5.tar.gz/xltree-0.0.5/test.py
@@ -0,0 +1,49 @@
+#
+# python test.py 1
+#
+# エクセルで樹形図を描こう
+#
+
+import traceback
+import datetime
+import sys
+
+from tests.no_1 import execute as execute_no_1
+from tests.no_2 import execute as execute_no_2
+from tests.no_3 import execute as execute_no_3
+
+
+########################################
+# コマンドから実行時
+########################################
+if __name__ == '__main__':
+    """コマンドから実行時"""
+
+    try:
+        args = sys.argv
+
+        if 1 < len(args):
+            if args[1] == '1':
+                execute_no_1()
+
+            if args[1] == '2':
+                execute_no_2()
+
+            if args[1] == '3':
+                execute_no_3()
+            
+            else:
+                raise ValueError(f'unsupported {args[1]=}')
+        
+        else:
+            raise ValueError(f'unsupported {len(args)=}')
+
+
+    except Exception as err:
+        print(f"""\
+[{datetime.datetime.now()}] おお、残念！　例外が投げられてしまった！
+{type(err)=}  {err=}
+
+以下はスタックトレース表示じゃ。
+{traceback.format_exc()}
+""")
