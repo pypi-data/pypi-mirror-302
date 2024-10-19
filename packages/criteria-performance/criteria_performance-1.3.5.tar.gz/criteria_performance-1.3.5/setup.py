21a27fbdcf591b6d5d0b20ab74e84309cb9cb397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 07:05:48 +0000

    Add package criteria-performance/1.3.5/criteria_performance-1.3.5.tar.gz

---
+++ packages/criteria-performance/criteria_performance-1.3.5.tar.gz/criteria_performance-1.3.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='criteria_performance',  # Nom du package
+    version='1.3.5',  # Version initiale
+    description='A Python package for performance criteria visualization',  # Courte description
+    long_description=open("README.md").read(),  # Description longue à partir du README.md
+    long_description_content_type='text/markdown',  # Le format de description longue (ici, Markdown)
+    url='https://github.com/teach-genius/aryad.git',  # Lien vers ton dépôt GitHub
+    author="Olanda-Eyiba Chantry",
+    author_email='chantryolanda85@gmail.com',
+    license='MIT',  # Choix de la licence
+    packages=find_packages(where="."),  # Trouve tous les packages à partir du répertoire courant
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'mplcursors'
+    ],  # Liste des dépendances
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Version minimale de Python
+)
