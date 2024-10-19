135f4647168d88d01fc3916c416b9d4ce2d692ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 05:05:56 +0000

    Add package heterogeneity-index/0.0.2/heterogeneity_index-0.0.2.tar.gz

---
+++ packages/heterogeneity-index/heterogeneity_index-0.0.2.tar.gz/heterogeneity_index-0.0.2/src/heterogeneity_index/__init__.py
@@ -0,0 +1,28 @@
+"""Heterogeneity Index computations.
+
+Refer to the documentation here <add link>.
+Refer to the modules documentations.
+"""
+
+from .components import (
+    compute_components_dask,
+    compute_components_numpy,
+    compute_components_xarray,
+)
+from .normalization import (
+    apply_coefficients,
+    compute_coefficient_hi,
+    compute_coefficients_components,
+)
+
+__version__ = "0.1.0"
+
+
+__all__ = [
+    "compute_components_dask",
+    "compute_components_numpy",
+    "compute_components_xarray",
+    "apply_coefficients",
+    "compute_coefficient_hi",
+    "compute_coefficients_components",
+]
