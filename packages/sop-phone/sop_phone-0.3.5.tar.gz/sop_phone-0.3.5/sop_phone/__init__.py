fd338f276bc8e57311392c879784224ef0b2343e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 04:05:47 +0000

    Add package sop-phone/0.3.5/sop_phone-0.3.5.tar.gz

---
+++ packages/sop-phone/sop_phone-0.3.5.tar.gz/sop_phone-0.3.5/sop_phone/__init__.py
@@ -0,0 +1,13 @@
+from netbox.plugins import PluginConfig
+
+
+class SopPhoneConfig(PluginConfig):
+    name = "sop_phone"
+    verbose_name = "SOP Phone"
+    description = "Manage phone informations of each site."
+    version='0.3.5'
+    author = "Leorevoir"
+    author_email = "leoquinzler@epitech.eu"
+    base_url = "sop-phone"
+
+config = SopPhoneConfig
