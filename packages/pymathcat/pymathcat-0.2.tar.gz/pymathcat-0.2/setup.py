ea90e56020422812708f2440f237bd9b7746898f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 04:05:16 +0000

    Add package pymathcat/0.2/pymathcat-0.2.tar.gz

---
+++ packages/pymathcat/pymathcat-0.2.tar.gz/pymathcat-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup,find_packages
+modulename = "pymathcat"
+moduleversion = 0.2
+authorname = "Tipjack"
+authoremail = "tipjack985@gmail.com"
+moduledescription = " A module to perfrom basic mathematical opretaions including operations like factorial fibbonaci and squareroot."
+setup(name=modulename,version = moduleversion,description= moduledescription,author = authorname, author_email= authoremail,packages=find_packages(),)
