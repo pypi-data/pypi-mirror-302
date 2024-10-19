5a5e5a94497d9a9dc395eb7644ff6d063a84fbd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 03:11:02 +0000

    Add package amsdal_data/0.2.3/amsdal_data-0.2.3-cp310-cp310-win_amd64.whl

---
+++ packages/amsdal_data/amsdal_data-0.2.3-cp310-cp310-win_amd64.whl/amsdal_data/services/table_schema_manager.pyi
@@ -0,0 +1,22 @@
+import amsdal_glue as glue
+from _typeshed import Incomplete
+from amsdal_data.aliases.using import LAKEHOUSE_DB_ALIAS as LAKEHOUSE_DB_ALIAS
+from amsdal_data.errors import RegisterTableError as RegisterTableError
+from amsdal_data.transactions.manager import AmsdalTransactionManager as AmsdalTransactionManager
+from amsdal_utils.models.data_models.address import Address as Address
+from amsdal_utils.utils.singleton import Singleton
+
+class TableSchemasManager(metaclass=Singleton):
+    _tables: Incomplete
+    _schemas_cache: Incomplete
+    _operation_manager: Incomplete
+    def __init__(self) -> None: ...
+    def set_schemas_cache(self, class_name: str, schema: glue.Schema) -> None: ...
+    def register_table(self, schema: glue.Schema, *, using: str | None = None) -> None:
+        """
+        Creates a new table in the database through connection.
+        """
+    def unregister_table(self, address: Address, *, using: str | None = None) -> None: ...
+    def _get_existing_schema(self, schema: glue.Schema, *, using: str | None = None) -> glue.Schema | None: ...
+    def _create_table(self, schema: glue.Schema, *, using: str | None = None) -> None: ...
+    def _update_table(self, schema: glue.Schema, existing_schema: glue.Schema, *, using: str | None = None) -> None: ...
