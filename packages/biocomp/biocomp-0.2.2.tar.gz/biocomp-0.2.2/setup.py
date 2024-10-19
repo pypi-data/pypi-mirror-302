107432d4097d59b29c18f05ee1f90187e3e84f99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 06:06:31 +0000

    Add package biocomp/0.2.2/biocomp-0.2.2.tar.gz

---
+++ packages/biocomp/biocomp-0.2.2.tar.gz/biocomp-0.2.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="biocomp",
+    version="0.2.2",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+
+    ],
+    entry_points={
+        "console_scripts": [
+            "biocomp=biocomp.biocomp:main",
+        ],
+    },
+    author="Tom Sapletta",
+    author_email="info@softreck.dev",
+    description="Biocomputing library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://www.biokomputer.pl",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
