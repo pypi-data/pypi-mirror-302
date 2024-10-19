3d37f79a9f64031293f459fbc58447e368e6625f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 04:05:18 +0000

    Add package easetools/0.2/easetools-0.2-py3-none-any.whl

---
+++ packages/easetools/easetools-0.2-py3-none-any.whl/easetools/__init__.py
@@ -0,0 +1,7 @@
+from .module1 import (
+    add, sub, mul, div, 
+    maximum, minimum, mean, median, mode, variance, 
+    gcd, lcm, isprime, primes, factors, factorial, 
+    permutation, combination, subarray, 
+    transpose, determinant, inverse, dotproduct
+)
