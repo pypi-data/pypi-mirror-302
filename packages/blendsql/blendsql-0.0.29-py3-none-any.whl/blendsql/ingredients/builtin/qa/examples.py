8233f52ce60d748af4e2c57e749981cfda3ff16e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 04:05:18 +0000

    Add package blendsql/0.0.29/blendsql-0.0.29-py3-none-any.whl

---
+++ packages/blendsql/blendsql-0.0.29-py3-none-any.whl/blendsql/ingredients/builtin/qa/examples.py
@@ -0,0 +1,40 @@
+from attr import attrs, attrib
+import pandas as pd
+from typing import Optional, List, Callable
+
+from blendsql.ingredients.few_shot import Example
+
+
+@attrs(kw_only=True)
+class QAExample(Example):
+    question: str = attrib()
+    context: Optional[pd.DataFrame] = attrib(
+        converter=lambda d: pd.DataFrame.from_dict(d) if isinstance(d, dict) else d,
+        default=None,
+    )
+    options: Optional[List[str]] = attrib(default=None)
+
+    def to_string(self, context_formatter: Callable[[pd.DataFrame], str]) -> str:
+        s = ""
+        s += f"\n\nQuestion: {self.question}\n"
+        if self.options is not None:
+            s += f"Options: {', '.join(self.options)}\n"
+        if self.context is not None:
+            s += f"Context:\n{context_formatter(self.context)}"
+        s += "\nAnswer: "
+        return s
+
+
+@attrs(kw_only=True)
+class AnnotatedQAExample(QAExample):
+    answer: str = attrib()
+
+    def to_string(
+        self,
+        context_formatter: Callable[[pd.DataFrame], str],
+        include_answer: bool = False,
+    ):
+        s = super().to_string(context_formatter=context_formatter)
+        if include_answer:
+            s += self.answer
+        return s
