5fcd8e26dc142dfe6ed4cde7832f6ef6cae833c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 03:11:36 +0000

    Add package arikaim/0.5.20/arikaim-0.5.20.tar.gz

---
+++ packages/arikaim/arikaim-0.5.20.tar.gz/arikaim-0.5.20/arikaim/core/db/db.py
@@ -0,0 +1,50 @@
+from arikaim.core.path import Path
+import imp,os,importlib;
+from sqlmodel import create_engine, Session
+from sqlalchemy.orm import sessionmaker
+
+class Db: 
+    _instance = None
+
+    def __init__(self):
+        self._config = None
+        self._engine = None
+
+    def __new__(cls, *args, **kwargs):
+        if not isinstance(cls._instance, cls):
+            cls._instance = object.__new__(cls, *args, **kwargs)
+        return cls._instance
+    
+    def connect(self, config):     
+        self._config = config
+        connect = 'mysql+pymysql://' + self._config['username'] + ':' + self._config['password'] + '@' + self._config['host'] + '/' +  self._config['database']
+        args = {"check_same_thread": False}
+        self._engine = create_engine(connect, echo = True)
+        print(connect)
+      
+
+    def close(self):
+        if not self._engine:
+            return False
+        self._engine.dispose()
+        
+        return True
+
+    @property
+    def engine(self):
+        return self._engine
+    
+    def load_model(self,model_class, module_name, service_name = None):
+        return load_model_class(model_class,module_name,service_name)
+
+
+db = Db()
+
+def load_model_class(model_class, module_name, service_name = None):
+    if not service_name:        
+        module = importlib.import_module('arikaim.core.db.models.' + module_name,model_class)
+    else:
+        path = os.path.join(Path.model(service_name),module_name)      
+        module = imp.load_source(module_name,path + '.py')
+
+    return getattr(module,model_class)
