344364e77857bc1049c43a0b008ad7fce85dcb0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 05:05:47 +0000

    Add package gptchangelog/0.6.3/gptchangelog-0.6.3-py3-none-any.whl

---
+++ packages/gptchangelog/gptchangelog-0.6.3-py3-none-any.whl/gptchangelog/__main__.py
@@ -0,0 +1,8 @@
+"""The main entry point. Invoke as `gptchangelog' or `python -m gptchangelog'.
+
+"""
+
+if __name__ == '__main__':  # pragma: nocover
+    from gptchangelog.cli import app
+
+    app()
