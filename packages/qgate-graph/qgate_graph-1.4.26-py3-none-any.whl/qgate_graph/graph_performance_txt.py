265221721f4af52296edf154683c615b9013ebd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 04:05:18 +0000

    Add package qgate-graph/1.4.26/qgate_graph-1.4.26-py3-none-any.whl

---
+++ packages/qgate-graph/qgate_graph-1.4.26-py3-none-any.whl/qgate_graph/graph_performance_txt.py
@@ -0,0 +1,17 @@
+from qgate_graph.graph_performance import GraphPerformance
+from qgate_graph.percentile_item import PercentileItem
+import os.path, os
+import logging
+
+
+class GraphPerformanceTxt(GraphPerformance):
+
+    def __init__(self, min_precision=-1, max_precision=-1, raw_format=False):
+        super().__init__(0, min_precision, max_precision, raw_format)
+
+    def _create_output(self, percentiles: {PercentileItem}, title, file_name, output_dir) -> str:
+        output_file = os.path.join(output_dir, f"TXT{file_name}.txt")
+        with open(output_file, 'w') as file:
+            file.write(str(super()._create_table(percentiles)))
+            logging.info(f"  ... {output_file}")
+        return output_file
