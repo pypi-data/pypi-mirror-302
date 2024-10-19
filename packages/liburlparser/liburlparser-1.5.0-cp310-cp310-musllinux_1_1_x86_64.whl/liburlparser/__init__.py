c8d7db9e746101151a681ffa42738bddd497c79c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 06:07:31 +0000

    Add package liburlparser/1.5.0/liburlparser-1.5.0-cp310-cp310-musllinux_1_1_x86_64.whl

---
+++ packages/liburlparser/liburlparser-1.5.0-cp310-cp310-musllinux_1_1_x86_64.whl/liburlparser/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from ._about import __version__
+from .core import Host, Url, __doc__, psl
+
+__all__ = [
+    "__doc__", "__version__", "Url", "Host", "psl"
+]
