90552922a03ce8c7411003d87f744608439fe7a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 03:11:18 +0000

    Add package pan-scm-sdk/0.1.10/pan_scm_sdk-0.1.10-py3-none-any.whl

---
+++ packages/pan-scm-sdk/pan_scm_sdk-0.1.10-py3-none-any.whl/scm/models/security/__init__.py
@@ -0,0 +1,10 @@
+# scm/models/security/__init__.py
+
+from .anti_spyware_profiles import (
+    AntiSpywareProfileRequestModel,
+    AntiSpywareProfileResponseModel,
+)
+from .wildfire_antivirus_profiles import (
+    WildfireAntivirusProfileResponseModel,
+    WildfireAntivirusProfileRequestModel,
+)
