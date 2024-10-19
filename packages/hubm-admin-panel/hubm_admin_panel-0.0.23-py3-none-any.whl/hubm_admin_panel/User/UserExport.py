b9da2f712cc74d2d895c0d88c94f196a7c1df73d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 04:05:19 +0000

    Add package hubm-admin-panel/0.0.23/hubm_admin_panel-0.0.23-py3-none-any.whl

---
+++ packages/hubm-admin-panel/hubm_admin_panel-0.0.23-py3-none-any.whl/hubm_admin_panel/User/UserExport.py
@@ -0,0 +1,28 @@
+from PyQt6.QtWidgets import QDialog
+from PyQt6 import QtGui
+from ui.ui_user_export import Ui_win_user_export
+import os
+
+def resource_path(relative):
+    return os.path.join(
+        os.environ.get(
+            "_MEIPASS2",
+            os.path.abspath(".")
+        ),
+        relative
+    )
+
+class UserExport(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        # Создаем экземпляр класса Ui_win_new_policies
+        self.ui = Ui_win_user_export()
+        icon = QtGui.QIcon(resource_path("res/icon.png"))
+        self.setWindowIcon(icon)
+        # Инициализируем интерфейс дополнительного окна
+        self.ui.setupUi(self)
+
+        #self.ui.btns.button(QtWidgets.QDialogButtonBox.StandardButton.Save).setEnabled(False)
+        # Здесь можно добавить дополнительную логику и функциональность вашего дополнительного окна
+
