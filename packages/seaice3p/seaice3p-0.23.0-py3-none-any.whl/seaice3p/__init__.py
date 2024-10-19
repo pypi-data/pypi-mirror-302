5d8c9c65a37d8d896a68c4525f8dd195d184f7db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 04:05:35 +0000

    Add package seaice3p/0.23.0/seaice3p-0.23.0-py3-none-any.whl

---
+++ packages/seaice3p/seaice3p-0.23.0-py3-none-any.whl/seaice3p/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.23.0"
+
+# Exported functions and classes
+from .params import *
+from .run_simulation import solve, run_batch
+from .grids import Grids
+from .load import load_simulation
