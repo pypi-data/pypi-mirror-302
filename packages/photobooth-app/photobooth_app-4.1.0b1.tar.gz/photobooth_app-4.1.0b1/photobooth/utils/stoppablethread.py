4147831090146b2e2a73e589dc560b6563954e81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 06:06:02 +0000

    Add package photobooth-app/4.1.0b1/photobooth_app-4.1.0b1.tar.gz

---
+++ packages/photobooth-app/photobooth_app-4.1.0b1.tar.gz/photobooth_app-4.1.0b1/photobooth/utils/stoppablethread.py
@@ -0,0 +1,25 @@
+"""_summary_"""
+
+import threading
+
+
+class StoppableThread(threading.Thread):
+    """Thread class with a stop() method. The thread itself has to check
+    regularly for the stopped() condition."""
+
+    def __init__(self, *args, **kwargs):
+        """_summary_"""
+        super().__init__(*args, **kwargs)
+        self._stop_event = threading.Event()
+
+    def stop(self):
+        """_summary_"""
+        self._stop_event.set()
+
+    def stopped(self):
+        """check in run loop. break loop if stopped() returns True
+
+        Returns:
+            _type_: _description_
+        """
+        return self._stop_event.is_set()
