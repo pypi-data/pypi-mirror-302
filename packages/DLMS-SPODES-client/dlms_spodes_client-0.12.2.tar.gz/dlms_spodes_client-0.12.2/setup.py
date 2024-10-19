38df831279d78345dd766ce5a0671444f709b92c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 03:10:45 +0000

    Add package DLMS-SPODES-client/0.12.2/dlms_spodes_client-0.12.2.tar.gz

---
+++ packages/DLMS-SPODES-client/dlms_spodes_client-0.12.2.tar.gz/dlms_spodes_client-0.12.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+setuptools.setup(
+    long_description_content_type="text/markdown",
+    url="https://github.com/youserj/SPODESclient_prj",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License"
+    ],
+    entry_points={
+        'console_scripts': [
+            'parser=src.DlmsSPODES.setting:version'
+        ]
+    },
+)
