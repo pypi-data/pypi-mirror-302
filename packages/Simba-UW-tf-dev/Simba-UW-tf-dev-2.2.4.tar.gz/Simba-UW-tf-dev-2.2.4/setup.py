a36e4e43003d7a0f6001b66cf1db942ebdec4424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 05:06:23 +0000

    Add package Simba-UW-tf-dev/2.2.4/Simba-UW-tf-dev-2.2.4.tar.gz

---
+++ packages/Simba-UW-tf-dev/Simba-UW-tf-dev-2.2.4.tar.gz/Simba-UW-tf-dev-2.2.4/setup.py
@@ -0,0 +1,53 @@
+"""
+SimBA (Simple Behavioral Analysis)
+https://github.com/sgoldenlab/simba
+Contributors.
+https://github.com/sgoldenlab/simba#contributors-
+Licensed under GNU Lesser General Public License v3.0
+"""
+
+import setuptools
+import platform
+
+REQUIREMENTS_PATH = 'requirements.txt'
+ARM_REQUIREMENTS_PATH = 'requirements_arm.txt'
+
+with open("docs/project_description.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open(REQUIREMENTS_PATH, "r") as f:
+    requirements = f.read().splitlines()
+
+with open(ARM_REQUIREMENTS_PATH, "r") as f:
+    arm_requirements = f.read().splitlines()
+
+# Setup configuration
+setuptools.setup(
+    name="Simba-UW-tf-dev",
+    version="2.2.4",
+    author="Simon Nilsson, Jia Jie Choong, Sophia Hwang",
+    author_email="sronilsson@gmail.com",
+    description="Toolkit for computer classification and analysis of behaviors in experimental animals",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sgoldenlab/simba",
+    install_requires=requirements,
+    extras_require={'arm': [arm_requirements]},
+    license='GNU General Public License v3 (GPLv3)',
+    packages=setuptools.find_packages(exclude=["*.tests",
+                                               "*.tests.*",
+                                               "tests.*",
+                                               "tests",
+                                               "__pycache__",
+                                               "pose_configurations_archive",
+                                               "sandbox",
+                                               "sandbox.*",
+                                               ]),
+    include_package_data=True,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ),
+    entry_points={'console_scripts':['simba=simba.SimBA:main'],}
+)
