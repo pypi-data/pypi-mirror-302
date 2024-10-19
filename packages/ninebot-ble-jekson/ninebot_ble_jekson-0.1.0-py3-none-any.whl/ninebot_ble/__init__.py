e20ad0b2bf49caa0848224c97e84928007cb5115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 03:10:06 +0000

    Add package ninebot-ble-jekson/0.1.0/ninebot_ble_jekson-0.1.0-py3-none-any.whl

---
+++ packages/ninebot-ble-jekson/ninebot_ble_jekson-0.1.0-py3-none-any.whl/ninebot_ble/__init__.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from sensor_state_data import (
+    BinarySensorDeviceClass,
+    BinarySensorValue,
+    DeviceKey,
+    SensorDescription,
+    SensorDeviceClass,
+    SensorDeviceInfo,
+    SensorUpdate,
+    SensorValue,
+    Units,
+)
+
+from .register import BmsIdx, CtrlIdx, get_register_desc, iter_register
+from .sensor import NinebotBleSensor
+from .transport import NinebotClient, Packet, Command, DeviceId
+from .util import async_scooter_scan
+
+__version__ = "0.0.6"
+
+__all__ = [
+    "NinebotBleSensor",
+    "NinebotClient",
+    "Packet",
+    "Command",
+    "DeviceId",
+    "BmsIdx",
+    "CtrlIdx",
+    "iter_register",
+    "get_register_desc",
+    "async_scooter_scan",
+    "BinarySensorDeviceClass",
+    "BinarySensorValue",
+    "SensorDescription",
+    "SensorDeviceInfo",
+    "DeviceKey",
+    "SensorUpdate",
+    "SensorDeviceClass",
+    "SensorDeviceInfo",
+    "SensorValue",
+    "Units",
+]
---
+++ packages/ninebot-ble-jekson/ninebot_ble_jekson-0.1.0-py3-none-any.whl/ninebot_ble/__main__.py
@@ -0,0 +1,83 @@
+"""Command-line client for reading Ninebot scooter registers
+
+Running with no arguments will start the ninebot sensor and dump all data.
+
+It is also possible to only read specific registers using flags, see --help.
+"""
+
+import argparse
+import asyncio
+import logging
+import time
+import os
+from typing import Any
+
+from home_assistant_bluetooth import BluetoothServiceInfo
+
+from ninebot_ble import (
+    BmsIdx,
+    CtrlIdx,
+    NinebotClient,
+    async_scooter_scan,
+    get_register_desc,
+    iter_register,
+)
+
+logger = logging.getLogger(__name__)
+
+
+def clear():
+    os.system('cls')
+
+
+def dump_reg(name: str, val: Any, unit: str) -> None:
+    print("\033[1A", end="")
+    print("\033[1A", end="")
+    print(f"{name:<10}: {val:.0f} {unit}")
+    print('|' * abs(int(val/10)))
+
+
+
+async def main() -> None:
+    logging.basicConfig(format="%(asctime)s %(levelname)s %(message)s", level=logging.INFO)
+    logging.getLogger("bleak.backends.bluezdbus.manager").level = logging.WARNING
+    logging.getLogger("bleak.backends.bluezdbus.client").level = logging.WARNING
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, description=__doc__)
+
+    arg_mapping: dict[str, BmsIdx | CtrlIdx] = {}
+    for idx in iter_register(CtrlIdx, BmsIdx):
+        arg = "_".join(str(idx).lower().split())
+        arg_mapping[arg] = idx
+        parser.add_argument("--" + arg, action="store_true", help=f"read {str(idx).lower()}")
+
+    args = parser.parse_args()
+
+    device, advertisement = await async_scooter_scan()
+
+    indices: list[BmsIdx | CtrlIdx] = []
+    for idx_arg, idx in arg_mapping.items():
+        if args.__dict__.get(idx_arg):
+            indices.append(idx)
+
+    client = NinebotClient()
+    try:
+        await client.connect(device)
+        print("")
+        print("asd")
+        print("asd")
+        for i in range(100):
+            current = await client.read_reg('Battery current')
+            voltage = await client.read_reg('Battery voltage')
+            dump_reg("Power", current*voltage, "W")
+            time.sleep(0.1)
+    finally:
+        await client.disconnect()
+
+
+def entrypoint() -> None:
+    asyncio.run(main())
+
+
+if __name__ == "__main__":
+    entrypoint()
