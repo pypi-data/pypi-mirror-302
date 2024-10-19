f4cd7efdcab900d9a625afbb12cc25aa29e6f9ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 04:05:33 +0000

    Add package transformers-modules/0.1.0/transformers_modules-0.1.0.tar.gz

---
+++ packages/transformers-modules/transformers_modules-0.1.0.tar.gz/transformers_modules-0.1.0/setup.py
@@ -0,0 +1,29 @@
+import re
+
+from setuptools import setup
+
+with open("src/__init__.py") as fd:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE
+    )[1]
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+extras_require = {}
+extras_require["complete"] = sorted(set(sum(extras_require.values(), [])))
+
+setup(
+    name="transformers_modules",
+    package_dir={"": "src"},
+    version=version,
+    author="",
+    author_email="",
+    description=long_description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    install_requires=open("requirements.txt").readlines(),
+    python_requires=">= 3.8, != 3.11.*",
+)
+
